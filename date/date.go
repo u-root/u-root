814a8d8fe9dcdaed5640d5aa7076b473c4501192
Ronald G. Minnich <rminnich@gmail.com>
2012-10-29 10:21:33 -0700

    Initial import, moving from hg to git Started from the repo of 2011, with everything changed :-)
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ date/date.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"time"
+);
+
+func main(){
+	fmt.Printf("%v\n", time.Now())
+}
---
+++ installcommand/installcommand.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"path"
+	"os"
+	"os/exec"
+	"log"
+)
+
+func main(){
+	goBin := os.Getenv("GOBIN")
+	buildPath := os.Getenv("BUILDPATH")
+
+	cleanPath := path.Clean(os.Args[0])
+	arg0 := path.Base(cleanPath)
+
+	cmd := exec.Command("go", "install")
+	cmd.Dir = path.Join(buildPath, arg0)
+	
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatal(err, string(out))
+	}
+
+	cmd = exec.Command(path.Join(goBin, arg0))
+	cmd.Args = os.Args[1:]
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
