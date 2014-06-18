5bdfd80bca5c02fc7715c78f3725228eb467c920
Ronald G. Minnich <rminnich@google.com>
2014-06-17 18:39:55 -0700

    It lives and breathes.
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

--- src/installcommand/installcommand.go
+++ src/installcommand/installcommand.go
@@ -9,32 +9,17 @@ import (
 
 func main(){
 	/* e.g. (GOBIN=`pwd`/bin go install date) */
- 	/*
-	myRoot, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
- */
 	cleanPath := path.Clean(os.Args[0])
 	binDir, commandName := path.Split(cleanPath)
 	myRoot := path.Base(binDir)
-	buildingBin := path.Join(myRoot, "/buildbin")
 	destDir := path.Join(myRoot, "/bin")
 	destFile := path.Join(destDir, commandName)
-	//sourceDir := path.Join(myRoot, commandName)
-	log.Println("%v, %v, %v, %v, %v %v\n", 
-	cleanPath,
-	commandName,
-	buildingBin,
-	myRoot,
-	destDir, destFile)
 
 	e := os.Environ()
 	e = append(e, "GOBIN="+path.Join(myRoot,"bin"))
 
 	cmd := exec.Command("go", "install", commandName)
 	cmd.Dir = myRoot
-	log.Println("RUn %v\n", cmd)
 	
 	out, err := cmd.CombinedOutput()
 
