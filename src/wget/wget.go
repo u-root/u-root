8c292ef063fca2ca36ec1af6437a9e07195fa8b7
Ronald G. Minnich <rminnich@google.com>
2014-06-24 19:45:35 -0700

    wget
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

---
+++ src/wget/wget.go
@@ -0,0 +1,30 @@
+// Copyright 2012 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Wget reads one file from the argument and writes it on the standard output.
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 1 {
+		os.Exit(1)
+	}
+
+	resp, err := http.Get(os.Args[1])
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(os.Stdout, resp.Body)
+}
