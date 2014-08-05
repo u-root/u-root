16a1cd83ba0086300a8a26233911ade89bf49032
Ronald G. Minnich <rminnich@gmail.com>
2014-08-05 15:28:11 +0000

    Add an ldd command that's actually useful in scripts.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/ldd/ldd.go
@@ -0,0 +1,51 @@
+// Copyright 2009 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// ldd prints the full path of dependencies.
+// unlike the standard one, you can use it in a script, e.g.
+// i=`ldd whatever`
+// leaves you with a list of files you can usefully copy.
+// You can also feed it a long list of files (/bin/*) and get
+// a short list of libraries; further, it will read stdin.
+package main
+
+import (
+	"debug/elf"
+	"fmt"
+	"os"
+)
+
+var list map[string]bool
+
+func process(file *os.File, name string) error {
+	if f, err := elf.NewFile(file); err == nil {
+		if fl, err := f.ImportedLibraries(); err == nil {
+			for _, i := range fl {
+				list[i] = true
+			}
+			return nil
+		}
+		return err
+	} else {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	list = make(map[string]bool)
+	if len(os.Args) < 2 {
+		process(os.Stdin, "stdin")
+	} else {
+		for _, i := range os.Args[1:] {
+			if f, err := os.Open(i); err == nil {
+				process(f, i)
+			} else {
+				fmt.Fprintf(os.Stderr, "%v: %v\n", i, err)
+			}
+		}
+	}
+	for n := range list {
+		fmt.Printf("%v\n", n)
+	}
+}
