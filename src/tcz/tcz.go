0f0c4c97d38a394f30e02a488af5b4b1ad2ac35d
Ronald G. Minnich <rminnich@gmail.com>
2014-08-12 16:54:54 +0000

    Tcz is further along; we needed a -bind mount for devs.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/tcz/tcz.go
+++ src/tcz/tcz.go
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Wget reads one file from the argument and writes it on the standard output.
-*/
-
 package main
 
 import (
@@ -19,40 +15,59 @@ import (
 	"syscall"
 )
 
+const (
+	/*
+	 * IOCTL commands --- we will commandeer 0x4C ('L')
+	 */
+	LOOP_SET_CAPACITY = 0x4C07
+	LOOP_CHANGE_FD    = 0x4C06
+	LOOP_GET_STATUS64 = 0x4C05
+	LOOP_SET_STATUS64 = 0x4C04
+	LOOP_GET_STATUS   = 0x4C03
+	LOOP_SET_STATUS   = 0x4C02
+	LOOP_CLR_FD       = 0x4C01
+	LOOP_SET_FD       = 0x4C00
+	LO_NAME_SIZE      = 64
+	LO_KEY_SIZE       = 32
+	/* /dev/loop-control interface */
+	LOOP_CTL_ADD      = 0x4C80
+	LOOP_CTL_REMOVE   = 0x4C81
+	LOOP_CTL_GET_FREE = 0x4C82
+	SYS_ioctl         = 16
+)
+
 const tcz = "/tinycorelinux.net/5.x/x86/tcz"
+
+var l = log.New(os.Stdout, "tcz: ", 0)
 
 // consider making this a goroutine which pushes the string down the channel.
 func findloop() (name string, err error) {
-     for i := 0; i < 1024; i++ {
-     	 l := fmt.Sprintf("/dev/loop%d", i)
-	 f, err := os.Open(l)
-	 if err != nil {
-	    continue
-	    }
-	    _, err = f.Stat()
-	    if err != nil {
-	       continue
-	       }
-	       // if we can get the status, it's in use, too bad.
-		// so do this:
-//ioctl(3, LOOP_GET_STATUS, {number=0, offset=0, flags=0, name="encstateful", ...}) = 0
-//	   and if is 0, you failed; if it fails, you succeed.
-	   }
-	   return name, nil
+	cfd, err := syscall.Open("/dev/loop-control", syscall.O_RDWR, 0)
+	if err != nil {
+		log.Fatalf("/dev/loop-control: %v", err)
+	}
+
+	a, b, errno := syscall.Syscall(SYS_ioctl, uintptr(cfd), LOOP_CTL_GET_FREE, 0)
+	if errno != 0 {
+		log.Fatalf("ioctl: %v\n", err)
+	}
+	log.Printf("a %v b %v err %v\n", a, b, err)
+	name = fmt.Sprintf("/dev/loop%d", a)
+	return name, nil
 }
 func linkone(p string, i os.FileInfo, err error) error {
-	log.Printf("symtree: p %v\n", p)
+	l.Printf("symtree: p %v\n", p)
 	if err != nil {
 		return err
 	}
 
 	// the tree of symlinks starts at /tcz
-	l := filepath.SplitList(p)
+	packagel := filepath.SplitList(p)
 	// surely there's a better way.
-	n := append([]string{"/"}, l[2:]...)
+	n := append([]string{"/"}, packagel[2:]...)
 	to := path.Join(n...)
 
-	log.Printf("symtree: symlink %v to %v\n", p, to)
+	l.Printf("symtree: symlink %v to %v\n", p, to)
 	return os.Symlink(p, to)
 }
 func main() {
@@ -60,7 +75,6 @@ func main() {
 		os.Exit(1)
 	}
 	cmdName := os.Args[1]
-	l := log.New(os.Stdout, "tcz: ", 0)
 
 	if err := os.MkdirAll(tcz, 0600); err != nil {
 		l.Fatal(err)
@@ -101,10 +115,14 @@ func main() {
 		/* OK, these are compressed tars ... */
 		l.Printf("c %v err %v\n", c, err)
 	}
+	/* now walk it, and fetch anything we need. */
+	name, err := findloop()
+	l.Printf("findloop gets %v err %v\n", name, err)
+
+	//ffd := syscall.Open("backing-file", syscall.O_RDONLY)
+	//lfd := syscall.Open(loopname, syscall.O_RDONLY)
 	/* now mount it. The convention is the mount is in /tcz/packagename */
 	if err := syscall.Mount(filepath, packagePath, "squashfs", 0, ""); err != nil {
 		l.Fatalf("Mount %s on %s: %v\n", filepath, packagePath, err)
 	}
-	/* now walk it, and fetch anything we need. */
-
 }
