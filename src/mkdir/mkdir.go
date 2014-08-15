08db4ff21917ee7194708512180646824c6b7dc7
Ronald G. Minnich <rminnich@gmail.com>
2014-08-15 02:27:57 +0000

    mkdir
    Has the -p, -v, and -m switches.
    Invoke with no args to get help.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/mkdir/mkdir.go
@@ -0,0 +1,42 @@
+// Copyright 2012 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Cat reads each file from its arguments in sequence and writes it on the standard output.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	mkall   = flag.Bool("p", false, "Make all needed directories in the path")
+	mode    = flag.Int("m", 0666, "Directory mode")
+	verbose = flag.Bool("v", false, "Print each directory as it is made")
+	f = os.Mkdir
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		fmt.Printf("Usage: mkdir [-p] <directory> [more directories]\n")
+		os.Exit(1)
+	}
+	if *mkall {
+		f = os.MkdirAll
+	}
+	for _, name := range flag.Args() {
+		if err := f(name, os.FileMode(*mode)); err != nil {
+			fmt.Printf("%v: %v\n", name, err)
+		} else {
+			if *verbose {
+				fmt.Printf("%v\n", name)
+			}
+		}
+	}
+}
