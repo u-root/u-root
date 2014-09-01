9985e2534550df9632f8aa2f0b7c03828e3ef02f
Ronald G. Minnich <rminnich@gmail.com>
2014-09-01 03:58:47 +0000

    We can (I think?) finally not have to prebuild sh
    Move all the logic of path setting, etc. to installcommand.
    
    In theory, this means we can remove it from sh.go
    
    This also is going to require that you run this with
    /
    as your "root", but that's ok I think.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/installcommand/installcommand.go
+++ src/installcommand/installcommand.go
@@ -1,21 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 )
+
+var urpath = "/go/bin:/buildbin:/bin:/usr/local/bin:"
 
 func main() {
 	/* e.g. (GOBIN=`pwd`/bin go install date) */
 	p := os.Getenv("PATH")
 	log.Printf("PATH in installcommand: %s\n", p)
 	cleanPath := path.Clean(os.Args[0])
+	log.Printf("cleanPath %v\n", cleanPath)
 	binDir, commandName := path.Split(cleanPath)
+	log.Printf("bindri, commandname %v %v\n", binDir, commandName)
 	myRoot := path.Base(binDir)
-	destDir := path.Join(myRoot, "/bin")
+	log.Printf("Myroot i %v\n", myRoot)
+	destDir := path.Join(/*myRoot*/"/", "/bin")
 	destFile := path.Join(destDir, commandName)
+	// Still not totally sure if we want to do all this here
+	// or in some hypothetical 'init' command.
+	// Pros for doing it here are if the user screws up
+	// the environment, it will break installcommand.
+	e := os.Environ()
+	// sudo sensibly doesn't inherit the path if you are root.
+	// and probably doesn't in general so we need to do this much.
+	// interestingly, on arch, I did not need to do this. Sounds bad.
+	np := strings.NewReplacer("PATH=", "PATH=/go/bin:/buildbin:/bin:")
+	for i := range e {
+		e[i] = np.Replace(e[i])
+	}
+	e = append(e, "GOROOT=/go")
+	e = append(e, "GOPATH=/")
+	e = append(e, "GOBIN=/bin")
+	os.Setenv("GOROOT", "/go")
+	os.Setenv("GOPATH", "/")
+	os.Setenv("GOBIN", "/bin")
+	// oh, and, Go looks in the environment, NOT the env in the cmd.
+	// Add the prefix if we don't have it.
+	if !strings.HasPrefix(p, urpath) {
+		if err := os.Setenv("PATH", urpath+p); err != nil {
+			fmt.Printf("Couldn't set path; %v\n", err)
+			os.Exit(1)
+		}
+	}
+	p = os.Getenv("PATH")
+	log.Printf("PATH in installcommand after change: %s\n", p)
+	p = os.Getenv("LD_LIBRARY_PATH")
+	// tinycore requires /usr/local/lib; make it always last.
+	if !strings.HasSuffix(p, ":/usr/local/lib") {
+		if err := os.Setenv("LD_LIBRARY_PATH", p+":/usr/local/lib"); err != nil {
+			fmt.Printf("Couldn't set LD_LIBRARY_PATH; %v\n", err)
+			os.Exit(1)
+		}
+	}
 
 	cmd := exec.Command("go", "install" /*"-x", */, commandName)
 	cmd.Env = append(os.Environ(),
@@ -35,6 +78,7 @@ func main() {
 		log.Printf(string(out))
 	}
 
+	log.Printf("WE BUILT IT? %v\n", destFile)
 	cmd = exec.Command(destFile)
 
 	cmd.Args = append([]string{commandName}, os.Args[1:]...)
