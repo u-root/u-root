faf77c84d216bc4c840ffb7c2c42f20fd9c1ba5e
Ronald G. Minnich <rminnich@gmail.com>
2014-09-01 04:01:49 +0000

    Woo Hoo!
    All the path and environment crap is out of sh.go.
    
    It never should have been there.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/sh/sh.go
+++ src/sh/sh.go
@@ -30,36 +30,7 @@ func main() {
 	for scanner.Scan() {
 		cmd := scanner.Text()
 		argv := strings.Split(cmd, " ")
-		e := os.Environ()
-		// sudo sensibly doesn't inherit the path if you are root.
-		// and probably doesn't in general so we need to do this much.
-		// interestingly, on arch, I did not need to do this. Sounds bad.
-		np := strings.NewReplacer("PATH=", "PATH=/go/bin:/buildbin:/bin:")
-		for i := range(e) {
-			e[i] = np.Replace(e[i])
-		}
-		e = append(e, "GOROOT=/go")
-		e = append(e, "GOPATH=/")
-		e = append(e, "GOBIN=/bin")
-		// oh, and, Go looks in the environment, NOT the env in the cmd.
-		// Add the prefix if we don't have it.
-		p := os.Getenv("PATH")
-		if ! strings.HasPrefix(p, urpath) {
-			if err := os.Setenv("PATH",  urpath + p); err != nil {
-				fmt.Printf("Couldn't set path; %v\n", err)
-				continue
-			}
-		}
-		p = os.Getenv("LD_LIBRARY_PATH")
-		// tinycore requires /usr/local/lib; make it always last.
-		if ! strings.HasSuffix(p, ":/usr/local/lib") {
-			if err := os.Setenv("LD_LIBRARY_PATH", p + ":/usr/local/lib"); err != nil {
-				fmt.Printf("Couldn't set LD_LIBRARY_PATH; %v\n", err)
-				continue
-			}
-		}
 		run := exec.Command(argv[0], argv[1:]...)
-		run.Env = e
 		run.Stdin = os.Stdin
 		run.Stdout = os.Stdout
 		run.Stderr = os.Stderr
