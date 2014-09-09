28e719b31c7c5b5e7ae2b662c2d624204ca3a43a
Ronald G. Minnich <rminnich@gmail.com>
2014-09-09 21:31:33 +0000

    Looks like we only need init binaries!
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/init/init.go
+++ src/init/init.go
@@ -20,22 +20,38 @@ var env = []string{
 	"PATH=/go/bin:/bin:/buildbin:/usr/local/bin:",
 	"GOROOT=/go",
 	"GOBIN=/bin",
+	"GOPATH=/",
+	"CGO_ENABLED=0",
 }
 
 func main() {
-	log.Printf("Welcome to u-root")
-	cmd := exec.Command("/buildbin/sh")
-	cmd.Env = env
+	log.Printf("Welcome to u-root 4")
 	os.Setenv("GOROOT", "/go")
 	os.Setenv("GOPATH", "/")
 	os.Setenv("GOBIN", "/bin")
 	os.Setenv("PATH", "/go/bin:/bin:/buildbin:/usr/local/bin")
+
+	cmd := exec.Command("go", "install", "-x", "installcommand")
+	cmd.Env = env
+	cmd.Dir = "/"
 
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	log.Printf("Run %v", cmd)
 	err := cmd.Run()
+	if err != nil {
+		log.Printf("%v\n", err)
+	}
+
+
+	cmd = exec.Command("/buildbin/sh")
+	cmd.Env = env
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	log.Printf("Run %v", cmd)
+	err = cmd.Run()
 	if err != nil {
 		log.Printf("%v\n", err)
 	}
