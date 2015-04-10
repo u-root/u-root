9a88d6e9fb46046dc6bb868046ade3c9cb56052b
Ronald G. Minnich <rminnich@gmail.com>
2015-04-09 20:34:39 -0700

    Fix uname to use the uroot uname package
    The one in syscall is just too painful.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/cmds/uname/uname.go
+++ src/cmds/uname/uname.go
@@ -3,20 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
-	"syscall"
+	"uroot"
 )
 
 func main() {
-	var u syscall.Utsname
-	if err := syscall.Uname(&u); err != nil {
+	if u, err := uroot.Uname(); err != nil {
 		log.Fatalf("%v", err)
+	} else {
+        	fmt.Printf("%v", u)
 	}
-        fmt.Printf("'%s' '%s' '%s' '%s' '%s' '%s'",
-                strings.Trim(string([]uint8(u.Sysname[:])), "\000"),
-                strings.Trim(string(u.Nodename[:]), "\000"),
-                strings.Trim(string(u.Release[:]), "\000"),
-                strings.Trim(string(u.Version[:]), "\000"),
-                strings.Trim(string(u.Machine[:]), "\000"),
-                strings.Trim(string(u.Domainname[:]), "\000"))
 }
