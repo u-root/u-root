63b86c73c15a7a26f8d5de35058265a92f429ed6
Ron Minnich <rminnich@gmail.com>
2015-05-21 21:33:27 +0000

    Properly fix up imports since we change the code a bit
    We're almost done building the full fork-free shell
    
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- src/bb/bb.go
+++ src/bb/bb.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"go/ast"
-	"go/format"
+"go/format"
 	"go/parser"
 	"go/token"
 	"io/ioutil"
@@ -15,6 +15,8 @@ import (
 	"path"
 	"reflect"
 	"text/template"
+
+	"golang.org/x/tools/imports"
 )
 
 const cmdFunc = `func {{.CmdName}}(c *Command) (err error) {
@@ -37,31 +39,13 @@ func init() {
 `
 
 var config struct {
+	Args []string
 	CmdName string
+	FullPath string
 	Src string
 }
 
-func main() {
-	flag.Parse()
-	a := flag.Args()
-	os.Args = []string{"hi", "there"}
-	if len(a) > 0 {
-		b, err := ioutil.ReadFile(a[0])
-		if err != nil {
-			log.Fatalf("%v\n", err)
-		}
-		config.Src = string(b)
-		// assume it ends in .go. Not much point otherwise.
-		n := path.Base(a[0])
-		config.CmdName = n[:len(n)-3]
-	}
-
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "src.go", config.Src, 0)
-	if err != nil {
-		panic(err)
-	}
-
+func one(s string, fset *token.FileSet, f *ast.File) error {
 	// Inspect the AST and change all instances of main()
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch x := n.(type) {
@@ -89,12 +73,12 @@ func main() {
 		return true
 	})
 	
-	if true {
+	if false {
 		ast.Fprint(os.Stderr, fset, f, nil)
 	}
 	var buf bytes.Buffer
 	if err := format.Node(&buf, fset, f); err != nil {
-		panic(err)
+	panic(err)
 	}
 	fmt.Printf("%s", buf.Bytes())
 
@@ -103,6 +87,46 @@ func main() {
 	if err := t.Execute(&b, config); err != nil {
 		log.Fatalf("spec %v: %v\n", cmdFunc, err)
 	}
-	fmt.Printf("%v\n", b.String())
+	out := string(buf.Bytes()) + b.String()
 
+	// fix up any imports. We may have forced the issue
+	// with os.Args
+	opts := imports.Options{
+		Fragment:  true,
+		AllErrors: true,
+		Comments:  true,
+		TabIndent: true,
+		TabWidth:  8,
+	}
+	fullCode, err := imports.Process("commandline", []byte(out), &opts)
+	if err != nil {
+		log.Fatalf("bad parse: '%v': %v", out, err)
+	}
+
+	of := path.Join("bbsh", path.Base(s))
+	if err := ioutil.WriteFile(of, []byte(fullCode), 0666); err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	config.Args = flag.Args()
+	config.CmdName = config.Args[0]
+	if len(config.Args) != 1 {
+		log.Fatalf("usage: bb <one directory>\n")
+	}
+	fset := token.NewFileSet()
+	config.FullPath = path.Join(os.Getenv("UROOT"), "src/cmds", config.CmdName)
+	p, err := parser.ParseDir(fset, config.FullPath, nil, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, f := range p {
+		for n, v := range f.Files {
+			one(n, fset, v)
+		}
+	}
 }
