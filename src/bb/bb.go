b15293187d0bcdbe708d19cfb0d0c76ed9c39b2e
Ron Minnich <rminnich@gmail.com>
2015-05-21 18:02:38 +0000

    Intermediate verison, but we're not quite there.
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- src/bb/bb.go
+++ src/bb/bb.go
@@ -37,7 +37,7 @@ func main() {
 	src := `package main
 
 func main() {
-fmt.Printf("%v\n", os.Args)
+os.exit(1)
 }
 `
 	config.CmdName = "c"
@@ -60,65 +60,45 @@ fmt.Printf("%v\n", os.Args)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Inspect the AST and change all instances of main()
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch x := n.(type) {
-		case *ast.File:
-			// placeholder in case we want to add something.
-			//fmt.Printf("-->file! %v d %v   ..>\n", x, reflect.TypeOf(x.Decls))
-			return true
 		case *ast.FuncDecl:
-			if false {fmt.Printf("%v", reflect.TypeOf(x.Type.Params.List[0].Type))}
+			if false {
+				fmt.Printf("%v", reflect.TypeOf(x.Type.Params.List[0].Type))
+			}
 			if x.Name.Name == "main" {
 				x.Name.Name = fmt.Sprintf("%v_main", config.CmdName)
-				// don't rewrite the param list now, but leave this here so we remember
-				// how it's done.
-				if false {
-					x.Type.Params.List = []*ast.Field{ 
-						&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"cmd"}}, Type: &ast.Ident{Name: "string",}},
-						&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"args"}}, Type: &ast.Ident{Name: "...string",}},				}
-				}
 			}
-			
-			return true
-			// someday, we'll know how to change all instances of os.Args to something else. Someday.
-		// case *ast.SelectorExpr:
-		// 	if true {
-		// 		fmt.Printf("%v\n", x)//reflect.TypeOf(x.Type.Params.List[0].Type))
-		// 		fmt.Printf("%v %v %v\n", x.X, reflect.TypeOf(x.X), x.X.(*ast.Ident).Name)//reflect.TypeOf(x.Type.Params.List[0].Type))
-		// 		fmt.Printf("%v\n", reflect.TypeOf(x))//reflect.TypeOf(x.Type.Params.List[0].Type))
-		// 	}
-		// 	// This is hoky.Need to check for whether this is a package. One thing at a time.
-		// 	if x.X.(*ast.Ident).Name != "os" {
-		// 		return true
-		// 	}
-		// 	if x.Sel.Name != "Arg" {
-		// 	}
-		// 	fmt.Printf("Got a hit on os.arg\n")
-		// 	x.X.(*ast.Ident).Name = "main"
-		// 	fmt.Printf("can set %v\n", reflect.ValueOf(x).CanSet())
-		// 	return true
-   // 123  .  .  .  .  .  .  .  .  Args: []ast.Expr (len = 1) {
-   // 124  .  .  .  .  .  .  .  .  .  0: *ast.SelectorExpr {
-   // 125  .  .  .  .  .  .  .  .  .  .  X: *ast.Ident {
-   // 126  .  .  .  .  .  .  .  .  .  .  .  NamePos: src.go:18:9
-   // 127  .  .  .  .  .  .  .  .  .  .  .  Name: "os"
-   // 128  .  .  .  .  .  .  .  .  .  .  .  Obj: nil
-   // 129  .  .  .  .  .  .  .  .  .  .  }
-   // 130  .  .  .  .  .  .  .  .  .  .  Sel: *ast.Ident {
-   // 131  .  .  .  .  .  .  .  .  .  .  .  NamePos: src.go:18:12
-   // 132  .  .  .  .  .  .  .  .  .  .  .  Name: "Args"
-   // 133  .  .  .  .  .  .  .  .  .  .  .  Obj: nil
-   // 134  .  .  .  .  .  .  .  .  .  .  }
-   // 135  .  .  .  .  .  .  .  .  .  }
-   // 136  .  .  .  .  .  .  .  .  }
+			// Append a return.
+			x.Body.List = append(x.Body.List, &ast.ReturnStmt{})
 
+		case *ast.BlockStmt:
+			for i, v := range x.List {
+				ast.Inspect(v, func(n ast.Node) bool {
+					switch y := n.(type) {
+					case *ast.CallExpr:
+						fmt.Fprintf(os.Stderr, "%d %v %v\n", i, reflect.TypeOf(y), *y)
+						switch z := y.Fun.(type) {
+						case *ast.SelectorExpr:
+							
+						fmt.Fprintf(os.Stderr, "%d %v %v\n", i, reflect.TypeOf(z), *z)
+						//	if z.X.Name == "os" && z.Sel.Name == "Exit" {
+								//fmt.Printf("found os.Exit at %v / %v\n", i,n)
+							//}
+						}
+					}
+					return true
+				})
+			}
 		}
 		return true
 	})
 
-	if true {ast.Fprint(os.Stderr, fset, f, nil)}
+	if true {
+		ast.Fprint(os.Stderr, fset, f, nil)
+	}
 	var buf bytes.Buffer
 	if err := format.Node(&buf, fset, f); err != nil {
 		panic(err)
