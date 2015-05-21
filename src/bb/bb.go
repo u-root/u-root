802e4a9dcf547abf9d7df040e3f1b761ff9083b9
Ronald G. Minnich <rminnich@gmail.com>
2015-05-20 18:43:39 -0700

    Now rewriting the file, this actually should work for cat.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/bb/bb.go
+++ src/bb/bb.go
@@ -13,21 +13,33 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"text/template"
 )
+
+const cmdFunc = `func {{.CmdName}}(cmd string, args ...string) {
+save := os.Args
+os.Args = append([]string{cmd}, args...)
+{{.CmdName}}_main()
+os.Args = save
+}
+
+func init() {
+	addBuiltIn("{{.CmdName}}", {{.CmdName}})
+}
+`
+
+var config struct {
+	CmdName string
+}
 
 func main() {
 	src := `package main
-func init() {
-add("c", c)
-}
-func c(args ...string) {
-_ = args
-}
 
 func main() {
-_ = os.Args
+fmt.Printf("%v\n", os.Args)
 }
 `
+	config.CmdName = "c"
 	flag.Parse()
 	a := flag.Args()
 	os.Args = []string{"hi", "there"}
@@ -37,8 +49,10 @@ _ = os.Args
 			log.Fatalf("%v\n", err)
 		}
 		src = string(b)
+		// assume it ends in .go. Not much point otherwise.
+		config.CmdName = a[0][:len(a)-3]
 	}
-	
+
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "src.go", src, 0)
 	if err != nil {
@@ -49,18 +63,24 @@ _ = os.Args
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch x := n.(type) {
 		case *ast.File:
-			fmt.Printf("-->file! %v d %v   ..>\n", x, reflect.TypeOf(x.Decls))
+			// placeholder in case we want to add something.
+			//fmt.Printf("-->file! %v d %v   ..>\n", x, reflect.TypeOf(x.Decls))
 			return true
 		case *ast.FuncDecl:
 			if false {fmt.Printf("%v", reflect.TypeOf(x.Type.Params.List[0].Type))}
 			if x.Name.Name == "main" {
-				x.Name.Name = "cat"
-				x.Type.Params.List = []*ast.Field{ 
-					&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"cmd"}}, Type: &ast.Ident{Name: "string",}},
-					&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"args"}}, Type: &ast.Ident{Name: "...string",}},				}
+				x.Name.Name = fmt.Sprintf("%v_main", config.CmdName)
+				// don't rewrite the param list now, but leave this here so we remember
+				// how it's done.
+				if false {
+					x.Type.Params.List = []*ast.Field{ 
+						&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"cmd"}}, Type: &ast.Ident{Name: "string",}},
+						&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"args"}}, Type: &ast.Ident{Name: "...string",}},				}
+				}
 			}
 			
 			return true
+			// someday, we'll know how to change all instances of os.Args to something else. Someday.
 		// case *ast.SelectorExpr:
 		// 	if true {
 		// 		fmt.Printf("%v\n", x)//reflect.TypeOf(x.Type.Params.List[0].Type))
@@ -96,11 +116,18 @@ _ = os.Args
 		return true
 	})
 
-	if true {ast.Fprint(os.Stderr, fset, f, nil)}
+	if false {ast.Fprint(os.Stderr, fset, f, nil)}
 	var buf bytes.Buffer
 	if err := format.Node(&buf, fset, f); err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s", buf.Bytes())
-	fmt.Printf("func init(){\nadd(\"c\", c)\n}\n")
+
+	t := template.Must(template.New("cmdFunc").Parse(cmdFunc))
+	var b bytes.Buffer
+	if err := t.Execute(&b, config); err != nil {
+		log.Fatalf("spec %v: %v\n", cmdFunc, err)
+	}
+	fmt.Printf("%v\n", b.String())
+
 }
