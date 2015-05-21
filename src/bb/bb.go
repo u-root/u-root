372f8ade5b7357c91ea009a2d485c7f2497749f0
Ronald G. Minnich <rminnich@gmail.com>
2015-05-20 18:01:41 -0700

    Adding init. Renaming things.
    This is going to get weird in a bit, as we rewrite os.Args. But hey, it's there to be written.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/bb/bb.go
+++ src/bb/bb.go
@@ -11,47 +11,96 @@ import (
 	"go/token"
 	"io/ioutil"
 	"log"
+	"os"
 	"reflect"
 )
 
-func x(a string, b string,) {
+func main() {
+	src := `package main
+func init() {
+add("c", c)
+}
+func c(args ...string) {
+_ = args
 }
 
 func main() {
+_ = os.Args
+}
+`
 	flag.Parse()
 	a := flag.Args()
-	b, err := ioutil.ReadFile(a[0])
-	if err != nil {
-		log.Fatalf("%v\n", err)
+	os.Args = []string{"hi", "there"}
+	if len(a) > 0 {
+		b, err := ioutil.ReadFile(a[0])
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		src = string(b)
 	}
-	// src is the input for which we want to inspect the AST.
-	src := string(b)
-
-	// Create the AST by parsing src.
-	fset := token.NewFileSet() // positions are relative to fset
+	
+	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "src.go", src, 0)
 	if err != nil {
 		panic(err)
 	}
-
+	
 	// Inspect the AST and change all instances of main()
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch x := n.(type) {
+		case *ast.File:
+			fmt.Printf("-->file! %v d %v   ..>\n", x, reflect.TypeOf(x.Decls))
+			return true
 		case *ast.FuncDecl:
 			if false {fmt.Printf("%v", reflect.TypeOf(x.Type.Params.List[0].Type))}
 			if x.Name.Name == "main" {
 				x.Name.Name = "cat"
-				x.Type.Params.List = []*ast.Field{ &ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"a"}}, Type: &ast.Ident{Name: "string",}}}
+				x.Type.Params.List = []*ast.Field{ 
+					&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"cmd"}}, Type: &ast.Ident{Name: "string",}},
+					&ast.Field{Names: []*ast.Ident{&ast.Ident{Name:"args"}}, Type: &ast.Ident{Name: "...string",}},				}
 			}
-			// we're done.
-			return false
+			
+			return true
+		// case *ast.SelectorExpr:
+		// 	if true {
+		// 		fmt.Printf("%v\n", x)//reflect.TypeOf(x.Type.Params.List[0].Type))
+		// 		fmt.Printf("%v %v %v\n", x.X, reflect.TypeOf(x.X), x.X.(*ast.Ident).Name)//reflect.TypeOf(x.Type.Params.List[0].Type))
+		// 		fmt.Printf("%v\n", reflect.TypeOf(x))//reflect.TypeOf(x.Type.Params.List[0].Type))
+		// 	}
+		// 	// This is hoky.Need to check for whether this is a package. One thing at a time.
+		// 	if x.X.(*ast.Ident).Name != "os" {
+		// 		return true
+		// 	}
+		// 	if x.Sel.Name != "Arg" {
+		// 	}
+		// 	fmt.Printf("Got a hit on os.arg\n")
+		// 	x.X.(*ast.Ident).Name = "main"
+		// 	fmt.Printf("can set %v\n", reflect.ValueOf(x).CanSet())
+		// 	return true
+   // 123  .  .  .  .  .  .  .  .  Args: []ast.Expr (len = 1) {
+   // 124  .  .  .  .  .  .  .  .  .  0: *ast.SelectorExpr {
+   // 125  .  .  .  .  .  .  .  .  .  .  X: *ast.Ident {
+   // 126  .  .  .  .  .  .  .  .  .  .  .  NamePos: src.go:18:9
+   // 127  .  .  .  .  .  .  .  .  .  .  .  Name: "os"
+   // 128  .  .  .  .  .  .  .  .  .  .  .  Obj: nil
+   // 129  .  .  .  .  .  .  .  .  .  .  }
+   // 130  .  .  .  .  .  .  .  .  .  .  Sel: *ast.Ident {
+   // 131  .  .  .  .  .  .  .  .  .  .  .  NamePos: src.go:18:12
+   // 132  .  .  .  .  .  .  .  .  .  .  .  Name: "Args"
+   // 133  .  .  .  .  .  .  .  .  .  .  .  Obj: nil
+   // 134  .  .  .  .  .  .  .  .  .  .  }
+   // 135  .  .  .  .  .  .  .  .  .  }
+   // 136  .  .  .  .  .  .  .  .  }
+
 		}
 		return true
 	})
+
+	if true {ast.Fprint(os.Stderr, fset, f, nil)}
 	var buf bytes.Buffer
 	if err := format.Node(&buf, fset, f); err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s", buf.Bytes())
-
+	fmt.Printf("func init(){\nadd(\"c\", c)\n}\n")
 }
