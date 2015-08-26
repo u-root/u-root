08da13a83dc824193c158ba289e51a8f92ddda6a
Beletti <rhiguita@gmail.com>
2015-08-25 22:37:01 -0700

    new file: mv.go

---
+++ src/cmds/mv/mv.go
@@ -0,0 +1,28 @@
+// Copyright 2012 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+ move (rename) files
+ created by Beletti (rhiguita@gmail.com)
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		fmt.Printf("mv - missing file operand\n")
+	} else {
+		error := os.Rename(os.Args[1], os.Args[2])
+		if error != nil {
+			fmt.Printf("%v\n", error)
+		}
+	}
+}
