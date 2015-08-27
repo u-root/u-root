b417b098c60ac7c76d6f7d49df1e472ec8c991d0
Beletti <rhiguita@gmail.com>
2015-08-27 16:13:56 -0700

    fix mv.go

--- src/cmds/mv/mv.go
+++ src/cmds/mv/mv.go
@@ -12,25 +12,48 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
+func usage() {
+	fmt.Printf("mv - missing file operand\n")
+	os.Exit(1)
+}
+
 func main() {
+	todir := false
 	flag.Parse()
-	a := flag.Args()
-	if len(a) < 2 {
-		fmt.Printf("mv - missing file operand\n")
-	} else {
-		err := os.Rename(a[0], a[1])
+
+	switch flag.NArg() {
+	case 0, 1:
+		usage()
+	}
+
+	files := flag.Args()
+	lf := files[len(files)-1]
+	lfdir, err := os.Stat(lf)
+	if err == nil {
+		todir = lfdir.IsDir()
+	}
+	if flag.NArg() > 2 && todir == false {
+		fmt.Printf("not a directory: %s\n", lf)
+		os.Exit(1)
+	}
+
+	if len(files) == 2 && todir == false {
+		// rename file
+		err := os.Rename(files[0], files[1])
 		if err != nil {
-			if os.IsNotExist(err) {
-				fmt.Printf("file does not exist\n")
-			} else {
-				if os.IsPermission(err) {
-					fmt.Printf("you do not have permission\n")
-				} else {
-					fmt.Printf("unknow error\n")
-				}
+			log.Fatalf("%v", err)
+		}
+	} else {
+		// "copying" N files to 1 directory
+		for i := 0; i < flag.NArg()-1; i++ {
+			ndir := lf + "/" + files[i]
+			err := os.Rename(files[i], ndir)
+			if err != nil {
+				log.Fatalf("%v", err)
 			}
 		}
 	}
