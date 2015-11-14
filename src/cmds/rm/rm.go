d008c50c4b409155c43198ff1b080011b789171d
Manoel Vilela <manoel_vilela@engineer.com>
2015-11-14 04:12:03 -0300

    Add POSIX flags for rm.go and create rm_test.go
    The flags implemented are:
    -R	Recursive remove
    -r	Recursive flag alias
    -i	Interactive mode
    -v 	Verbose mode
    
    Change-Id: Id5d8c2c9c30294fd708b52e781421919c9deeb29
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

--- src/cmds/rm/rm.go
+++ src/cmds/rm/rm.go
@@ -2,24 +2,87 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Rm removes the named files.
-
-The options are:
-*/
 
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"path"
 )
 
-func main() {
-	for _,v := range(os.Args[1:]) {
-		err := os.Remove(v)
-		if err != nil {
-			fmt.Printf("%v: %v\n", v, err)
+var (
+	flags struct {
+		r bool
+		v bool
+        	i bool
+        }
+	cmd = "rm [-Rrvi] file..."
+)
+
+func init() {
+	flag.Parse()
+	flag.BoolVar(&flags.i, "i", false, "Interactive mode.")
+	flag.BoolVar(&flags.v, "v", false, "Verbose mode.")
+	flag.BoolVar(&flags.r, "R", false, "Remove file hierarchies")
+	flag.BoolVar(&flags.r, "r", false, "Equivalent to -R.")
+}
+
+func rm(files []string, recursive bool, verbose bool, interactive bool) error {
+	f := os.Remove
+	if recursive {
+		f = os.RemoveAll
+	}
+
+	workingPath, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return err
+	}
+
+	// loop for remove files and folders
+	for _, file := range files {
+		// -i ask for remove file or no
+		if interactive {
+			fmt.Printf("%v: remove '%v'?: ", cmd.name, file)
+			input := bufio.NewScanner(os.Stdin)
+			input.Scan()
+			if input.Text() != "y" {
+				continue
+			}
 		}
+
+		// -v print the deleting named path/files
+		if verbose {
+			toRemove := path.Join(workingPath, file)
+			fmt.Printf("Deleting: %v\n", toRemove)
+		}
+
+		// try remove the file and return err if exists
+		if err := f(file); err != nil {
+			fmt.Fprintf(os.Stderr, "%v: %v\n", file, err)
+			return err
+		}
+	}
+	return nil
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage:", cmd)
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func main() {
+	flag.Usage = usage
+
+	if flag.NArg() < 1 {
+		usage()
+	}
+
+	if err := rm(flag.Args(), flags.r, flags.v, flags.i); err != nil {
+		os.Exit(1)
 	}
 }
