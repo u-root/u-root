ffce7287b11fe511d9d11ce7daddd20125c18c89
Rafael Campos Nunes <rafaelnunes@engineer.com>
2015-11-24 16:55:39 -0300

    Created a cat_test.go
    Change-Id: I600272b710417f57d610953a0a49d2f183a04e40
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/cmds/cat/cat.go
+++ src/cmds/cat/cat.go
@@ -1,37 +1,64 @@
-// Copyright 2012 the u-root Authors. All rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-/*
-Cat reads each file from its arguments in sequence and writes it on the standard output.
-*/
+/* Copyright 2012 the u-root Authors. All rights reserved
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ *
+ *
+ * Cat reads each file from its arguments in sequence and writes it on the standard output.
+ */
 
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
+var (
+	flags struct {
+		_ bool
+	}
+	cmd = "cat [-u] filename ..."
+)
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage:", cmd)
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func init() {
+	flag.BoolVar(&flags._, "u", false, "ignored")
+	flag.Parse()
+	flag.Usage = usage
+}
+
+func cat(writer io.Writer, files []string) error {
+	for _, name := range files {
+		f, err := os.Open(name)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(writer, f)
+		if err != nil {
+			return err
+		}
+		f.Close()
+	}
+
+	return nil
+}
+
 func main() {
 	if len(os.Args) == 1 {
 		io.Copy(os.Stdout, os.Stdin)
-	} else {
-		for _, name := range os.Args[1:] {
-			f, err := os.Open(name)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "can't open %s: %v\n", name, err)
-				os.Exit(1)
-			}
+	}
 
-			_, err = io.Copy(os.Stdout, f)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error %s: %v", name, err)
-				os.Exit(1)
-			}
-
-			f.Close()
-		}
+	err := cat(os.Stdout, flag.Args())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
 	}
 }
