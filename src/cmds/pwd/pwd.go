f9cdd64e0898179bb29ac935383627232e6162f1
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-01 12:18:04 -0300

    Add parameters -P and -L to pwd and the order implication
    Change-Id: I1ed7d15e8e171522c14baa1450cb5325385815bf
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

--- src/cmds/pwd/pwd.go
+++ src/cmds/pwd/pwd.go
@@ -10,15 +10,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-func main() {
-	if path, error := os.Getwd(); error != nil {
-		log.Fatalf("%v", error)
-	} else {
-		fmt.Println(path)
+var (
+	logical  = flag.Bool("L", true, "Follow symlinks") //this is the default behavior
+	physical = flag.Bool("P", false, "Don't follow symlinks")
+	cmd      = "pwd [-LP]"
+)
+
+func usage() {
+	fmt.Printf("Usage: %v", cmd)
+	flag.PrintDefaults()
+}
+
+func init() {
+	args := os.Args[1:] // saving the args
+	flag.Usage = usage
+	flag.Parse()
+	var L, P int
+	for index, flag := range args {
+		if flag == "-L" {
+			L = index
+		} else if flag == "-P" {
+			P = index
+		}
 	}
+	if P > L {
+		*physical = true // if P appears after
+	} else if L > P {
+		*physical = false // if L appears after
+	}
+}
+
+func pwd() {
+	if *physical {
+		if path, err := os.Getwd(); err != nil {
+			log.Fatalf("%v", err)
+		} else {
+			path, _ = filepath.EvalSymlinks(path)
+			fmt.Println(path)
+		}
+	} else if *logical {
+		if path, err := os.Getwd(); err != nil {
+			log.Fatalf("%v", err)
+		} else {
+			fmt.Println(path)
+		}
+	}
+
+}
+
+func main() {
+	pwd()
 }
