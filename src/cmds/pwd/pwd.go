d34852f7cad7913c4b24d2dee2389485809f9efe
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-01 12:18:04 -0300

    Add parameters -P and -L to pwd
    Change-Id: I1ed7d15e8e171522c14baa1450cb5325385815bf
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

--- src/cmds/pwd/pwd.go
+++ src/cmds/pwd/pwd.go
@@ -10,15 +10,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-func main() {
-	if path, error := os.Getwd(); error != nil {
-		log.Fatalf("%v", error)
-	} else {
-		fmt.Println(path)
+var (
+	logical  = flag.Bool("L", true, "Follow symlinks") //this is the default behavior
+	physical = flag.Bool("P", false, "Don't follow symlinks")
+)
+
+func pwd() {
+
+	if *physical {
+		if path, err := os.Getwd(); err != nil {
+			log.Fatalf("%v", err)
+		} else {
+			path, _ = filepath.EvalSymlinks(path)
+			fmt.Println(path)
+		}
+	} else if *logical {
+		if path, err := os.Getwd(); err != nil {
+			log.Fatalf("%v", err)
+		} else {
+			fmt.Println(path)
+		}
 	}
+
+}
+
+func main() {
+
+	flag.Parse()
+	pwd()
+
 }
