c74b06b191d532ddacbfa95dd9c8931eaf588639
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-01 12:18:04 -0300

    Add parameters -P and -L to pwd and the order implication
    Change-Id: I1ed7d15e8e171522c14baa1450cb5325385815bf
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

--- src/cmds/pwd/pwd.go
+++ src/cmds/pwd/pwd.go
@@ -10,15 +10,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-func main() {
-	if path, error := os.Getwd(); error != nil {
-		log.Fatalf("%v", error)
-	} else {
+var (
+	logical  = flag.Bool("L", true, "Follow symlinks") // this is the default behavior
+	physical = flag.Bool("P", false, "Don't follow symlinks")
+	cmd      = "pwd [-LP]"
+)
+
+func usage() {
+	fmt.Printf("Usage: %v\n", cmd)
+	flag.PrintDefaults()
+}
+
+func init() {
+	args := os.Args[1:]
+	flag.Usage = usage
+	flag.Parse()
+	for _, flag := range args {
+		switch flag {
+		case "-L":
+			*physical = false
+		case "-P":
+			*physical = true
+		}
+	}
+}
+
+func pwd() error {
+	path, err := os.Getwd()
+	if err == nil && *physical {
+		path, err = filepath.EvalSymlinks(path)
+	}
+
+	if err == nil {
 		fmt.Println(path)
+	}
+
+	return err
+}
+
+func main() {
+	if err := pwd(); err != nil {
+		log.Fatalf("%v", err)
 	}
 }
