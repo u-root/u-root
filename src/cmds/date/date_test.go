2ba430ed26cacaab3df9f22e50431b81a1fd04ff
Manoel Vilela <manoel_vilela@engineer.com>
2015-11-30 22:02:21 -0300

    Add -u POSIX flag for date.go and created date_test.go
    The +format of POSIX compliant is more difficult
    and I didn't found a portable way using
    the lib "time".
    
    Change-Id: I64312a6e9e73622a2f5a0685ef5ed5d93476601e
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

---
+++ src/cmds/date/date_test.go
@@ -0,0 +1,50 @@
+// Copyright 2012 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+//  created by Manoel Vilela (manoel_vilela@engineer.com)
+
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+// without any flag
+func TestDateNoFlags(t *testing.T) {
+	t.Log("::  Printing date with default location (no flags)...")
+	d, err := date()
+	t.Logf("Date: %v\n", d)
+	if err != nil {
+		t.Error(err)
+	}
+	dParsed, err := time.Parse(time.UnixDate, d)
+	if err != nil {
+		t.Error(err)
+	}
+	dTest := dParsed.Format(time.UnixDate)
+	if d != dTest {
+		t.Errorf("Mismatched dates; expected %v, returns %v\n", d, dTest)
+	}
+
+}
+
+// using u flag
+func TestDateUniversal(t *testing.T) {
+	flags.universal = true
+	t.Log("::  Printing date with UTC (using -u flag)...")
+	d, err := date()
+	t.Logf("Date: %v\n", d)
+	if err != nil {
+		t.Error(err)
+	}
+	dParsed, err := time.Parse(time.UnixDate, d)
+	if err != nil {
+		t.Error(err)
+	}
+	dTest := dParsed.Format(time.UnixDate)
+	if d != dTest {
+		t.Errorf("Mismatched dates; expected %v, returns %v\n", d, dTest)
+	}
+}
