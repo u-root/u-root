67901c4fa88f41db1d3b59b5ad0dd4c3a9df275f
Rafael Campos Nunes <rafaelnunes@engineer.com>
2015-12-04 06:57:31 -0300

    Refactored code on which.go, it's functional now
    Change-Id: I7ae8fabdde54b0f1a8b39219d7943d1ae3191fc2
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/cmds/which/which.go
+++ src/cmds/which/which.go
@@ -1,35 +1,55 @@
-// Copyright 2012 the u-root Authors. All rights reserved
+// Copyright 2016 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Which locates a command.
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"path"
+	"strings"
 )
 
-var a = flag.Bool("a", false, "What?")
+var (
+	flags struct {
+		a bool
+	}
+)
+
+const L = os.ModeSymlink
+
+func init() {
+	flag.BoolVar(&flags.a, "a", false, "print all matching pathnames of each argument")
+}
+
+func which(p string, writer io.Writer, cmds []string) {
+	pathArray := strings.Split(p, ":")
+
+	for _, name := range cmds {
+		for i := range pathArray {
+			f := path.Join(pathArray[i], name)
+			if info, err := os.Stat(f); err == nil {
+				if m := info.Mode(); !m.IsDir() && m&0111 != 0 && !(m&L == L) {
+					writer.Write([]byte(f + "\n"))
+				}
+			}
+		}
+	}
+}
 
 func main() {
 	flag.Parse()
-	p, err := os.Getenv("PATH")
-	if err != nil {
-		fmt.Printf("No path! %v\n", err)
-		os.Exit(1)
+
+	p := os.Getenv("PATH")
+	if len(p) == 0 {
+		// The default bin path of uroot is ubin, fallbacking to it.
+		fmt.Println("No path variable found! Fallbacking to /ubin")
+		p = "/ubin"
 	}
 
-	p := strings.Split(p, ":")
-
-		for _, name := range flag.Args() {
-			
-			for i := range(p) {
-				f := path.Join(p[i], name)
-				if s, err := os.Stat(f); err == nil {
-					if 
-				
-				}
-		}
-	}
+	which(p, os.Stdout, flag.Args())
 }
