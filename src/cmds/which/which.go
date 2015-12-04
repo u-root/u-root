d900b4468abf7aaa419e969a112248932cf07038
Rafael Campos Nunes <rafaelnunes@engineer.com>
2015-12-04 06:57:31 -0300

    Refactored code on which.go, it's functional now
    Change-Id: I7ae8fabdde54b0f1a8b39219d7943d1ae3191fc2
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/cmds/which/which.go
+++ src/cmds/which/which.go
@@ -5,31 +5,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io"
 	"os"
+	"path"
+	"strings"
 )
 
-var a = flag.Bool("a", false, "What?")
+var (
+	flags struct {
+		a bool
+	}
+	cmd = "which [-a] filename ..."
+)
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage:", cmd)
+	flag.PrintDefaults()
+	flag.Usage = usage
+}
+
+func init() {
+	flag.BoolVar(&flags.a, "a", false, "print all matching pathnames of each argument")
+}
+
+func which(p string, cmds []string) {
+	pathArray := strings.Split(p, ":")
+
+	for _, name := range cmds {
+		for i := range pathArray {
+			f := path.Join(pathArray[i], name)
+			if info, err := os.Stat(f); err == nil {
+				if (info.Mode() & 0111) == 0111 {
+					fmt.Println(f)
+				}
+			}
+		}
+	}
+}
 
 func main() {
 	flag.Parse()
-	p, err := os.Getenv("PATH")
-	if err != nil {
-		fmt.Printf("No path! %v\n", err)
+
+	p := os.Getenv("PATH")
+	if len(p) == 0 {
+		fmt.Println("No path variable found!")
 		os.Exit(1)
 	}
 
-	p := strings.Split(p, ":")
-
-		for _, name := range flag.Args() {
-			
-			for i := range(p) {
-				f := path.Join(p[i], name)
-				if s, err := os.Stat(f); err == nil {
-					if 
-				
-				}
-		}
-	}
+	which(p, flag.Args())
 }
---
+++ src/cmds/which/which_test.go
@@ -0,0 +1,85 @@
+/* Copyright 2012 the u-root Authors. All rights reserved
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ *
+ * created by Rafael Campos Nunes <rafaelnunes@engineer.com>
+ */
+
+package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
+
+type commands struct {
+	name      string
+	pathOnSys []byte
+}
+
+// in setup we fill the pathOnSys variables with their corresponding path on the system.
+var (
+	tests = []commands{
+		{
+			"cat",
+			[]byte{},
+		},
+		{
+			"which",
+			[]byte{},
+		},
+		{
+			"sed",
+			[]byte{},
+		},
+		{
+			"ldd",
+			[]byte{},
+		},
+	}
+
+	p = os.Getenv("PATH")
+)
+
+func setup() error {
+	var err error
+
+	for i := range tests {
+		tests[i].pathOnSys, err = exec.Command("which", tests[i].name).Output()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+/* Test_which_1 tests `which` command against one POSIX command that are included in Linux.
+ * The output of which.go has to be exactly equal to the output of which itself.
+ */
+func Test_which_1(t *testing.T) {
+	err := setup()
+
+	if err != nil {
+		t.Fatal("setup has failed, %v", err)
+	}
+
+	commands := [1]string{"cat"}
+	which(p, commands[:])
+}
+
+/* Test_which_1 tests `which` command against the three POSIX commands that are included in Linux.
+ * The output of which.go has to be exactly equal to the output of which itself. If it works with
+ * three, it should work with more commands as well.
+ */
+func Test_which_2(t *testing.T) {
+	err := setup()
+
+	if err != nil {
+		t.Fatal("setup has failed, %v", err)
+	}
+
+	commands := [3]string{"which", "ldd", "sed"}
+	which(p, commands[:])
+}
