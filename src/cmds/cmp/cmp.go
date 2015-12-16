eda07f0530ce16a3a4c8707595851de4249920e0
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-16 11:49:22 -0300

    cmp reads from stdin if argument is -
    Change-Id: I0d2fe588b3d37cc62148424528c40839e6168355
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

--- src/cmds/cmp/cmp.go
+++ src/cmds/cmp/cmp.go
@@ -2,35 +2,25 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Cmp compares the two files and prints a message if the contents differ.
+//Cmp compares the two files and prints a message if the contents differ.
 
-cmp [ –lLs ] file1 file2 [ offset1 [ offset2 ] ]
-
-The options are:
-	–l    Print the byte number (decimal) and the differing bytes (octal) for each difference.
-	–L    Print the line number of the first differing byte.
-	–s    Print nothing for differing files, but set the exit status.
-
--If offsets are given, comparison starts at the designated byte position of the corresponding file.
--Offsets that begin with 0x are hexadecimal; with 0, octal; with anything else, decimal.
-*/
-
+//If offsets are given, comparison starts at the designated byte position of the corresponding file.
+//Offsets that begin with 0x are hexadecimal; with 0, octal; with anything else, decimal.
 package main
 
 import (
 	"bufio"
 	"flag"
-	"fmt"
+	"log"
 	"os"
 	"strconv"
 )
 
-var long = flag.Bool("l", false, "print the byte number (decimal) and the differing bytes (hexadecimal) for each difference")
+var long = flag.Bool("l", false, "print the byte number (decimal) and the differing bytes (octal) for each difference")
 var line = flag.Bool("L", false, "print the line number of the first differing byte")
 var silent = flag.Bool("s", false, "print nothing for differing files, but set the exit status")
 
-func emit(f *os.File, c chan byte, offset int64) (error) {
+func emit(f *os.File, c chan byte, offset int64) error {
 	if offset > 0 {
 		f.Seek(offset, 0)
 	}
@@ -46,6 +36,23 @@ func emit(f *os.File, c chan byte, offset int64) (error) {
 	}
 }
 
+func openFile(name string) *os.File {
+	var f *os.File
+	var err error
+	
+	if name == "-" {
+		f, err = os.Open(os.Stdin.Name())
+	} else {
+		f, err = os.Open(name)
+	}
+
+	if err != nil {
+		log.Fatalf("error opening %s: %v", name, err)
+	}
+
+	return f
+}
+
 func main() {
 	flag.Parse()
 	var offset1, offset2 int64
@@ -53,18 +60,17 @@ func main() {
 
 	fnames := flag.Args()
 	if len(fnames) != 2 && len(fnames) != 4 {
-		fmt.Fprintf(os.Stderr, "expected two filenames (and two optional offsets), got %d", len(fnames))
-		os.Exit(1)
+		log.Fatalf("expected two filenames (and two optional offsets), got %d", len(fnames))
 	}
 	if len(fnames) == 4 {
 		offset1, err = strconv.ParseInt(fnames[2], 0, 64)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "bad offset1: %s: %v\n", fnames[2], err)
+			log.Printf("bad offset1: %s: %v\n", fnames[2], err)
 			return
 		}
 		offset2, err = strconv.ParseInt(fnames[3], 0, 64)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "bad offset2: %s: %v\n", fnames[3], err)
+			log.Printf("bad offset2: %s: %v\n", fnames[3], err)
 			return
 		}
 	}
@@ -72,18 +78,10 @@ func main() {
 	c1 := make(chan byte, 8192)
 	c2 := make(chan byte, 8192)
 
-	f, err := os.Open(fnames[0])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error opening %s: %v", fnames[0], err)
-		os.Exit(1)
-	}
+	f := openFile(fnames[0])
 	go emit(f, c1, offset1)
 
-	f, err = os.Open(fnames[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error opening %s: %v", fnames[1], err)
-		os.Exit(2)
-	}
+	f = openFile(fnames[1])
 	go emit(f, c2, offset2)
 
 	lineno, charno := int64(1), int64(1)
@@ -97,23 +95,19 @@ func main() {
 				os.Exit(1)
 			}
 			if *line {
-				fmt.Fprintf(os.Stderr, "%s %s differ: char %d line %d\n", fnames[0], fnames[1], charno, lineno)
-				os.Exit(1)
+				log.Fatalf("%s %s differ: char %d line %d\n", fnames[0], fnames[1], charno, lineno)
 			}
 			if *long {
 				if b1 == '\u0000' {
-					fmt.Fprintf(os.Stderr, "EOF on %s\n", fnames[0])
-					os.Exit(1)
+					log.Fatalf("EOF on %s\n", fnames[0])
 				}
 				if b2 == '\u0000' {
-					fmt.Fprintf(os.Stderr, "EOF on %s\n", fnames[1])
-					os.Exit(1)
+					log.Fatalf("EOF on %s\n", fnames[1])
 				}
-				fmt.Fprintf(os.Stderr, "%8d %#.2o %#.2o\n", charno, b1, b2)
+				log.Printf("%8d %#.2o %#.2o\n", charno, b1, b2)
 				goto skip
 			}
-			fmt.Fprintf(os.Stderr, "%s %s differ: char %d\n", fnames[0], fnames[1], charno)
-			os.Exit(1)
+			log.Fatalf("%s %s differ: char %d\n", fnames[0], fnames[1], charno)
 		}
 	skip:
 		charno++
