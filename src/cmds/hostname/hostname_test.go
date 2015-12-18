fc66d14c88714e74459425ad6c25e09a0c362cba
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-18 10:51:09 -0300

    Add test for hostname
    Change-Id: I2dd6a0f6d8b5f32ee44fe48008887e34dbea0d8c
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

---
+++ src/cmds/hostname/hostname_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func Test_hostname(t *testing.T) {
+	var buf bytes.Buffer
+	var host string
+	var err error
+
+	if err = hostname(&buf); err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if host, err = os.Hostname(); err != nil {
+		t.Errorf("%v", err)
+	}
+
+
+	if bytes.Compare(buf.Bytes(), []byte(host)) != 0 {
+		t.Fatalf("want %v, got %v", []byte(host), buf)
+	}
+}
