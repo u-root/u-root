f7ba9e0a74b9174f8c95020f867e33536aad02dd
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-18 10:51:28 -0300

    Add test for echo
    Change-Id: I9eb3748df4ec1e039150be2376f2d66127613bcb
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

---
+++ src/cmds/echo/echo_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+
+	type test struct {
+		s string
+		nonewline bool
+	}
+	
+	tests := []test{{s:"simple test", nonewline:false}, {s:"another one",nonewline:true}}
+	bufs := make([]bytes.Buffer, len(tests))
+
+	for i, v := range tests {
+		if err := echo(&bufs[i], v.s); err != nil {
+			t.Errorf("%s", err)
+		}
+		if !*nonewline {
+			v.s += "\n"
+		}
+		if string(bufs[i].Bytes()) != v.s {
+			t.Fatalf("Want %v, got %v", v.s, string(bufs[i].Bytes()))
+		}
+	}
+
+}
