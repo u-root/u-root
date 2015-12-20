738c57a428d728a9d020649db65a8bf4384f8666
Manoel Vilela <manoel_vilela@engineer.com>
2015-12-20 03:26:35 -0300

    Date.go +format POSIX compliant and added tests
    A total of 29 flags of format was implemented
    
    Change-Id: I8a77cc02e98bc55d71482b067821f39b548c22e7
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

--- src/cmds/date/date_test.go
+++ src/cmds/date/date_test.go
@@ -35,6 +35,7 @@ func TestDateUniversal(t *testing.T) {
 	flags.universal = true
 	t.Log("::  Printing date with UTC (using -u flag)...")
 	d, err := date()
+	flags.universal = false // avoid conflict, variable global default value
 	t.Logf("Date: %v\n", d)
 	if err != nil {
 		t.Error(err)
@@ -46,5 +47,57 @@ func TestDateUniversal(t *testing.T) {
 	dTest := dParsed.Format(time.UnixDate)
 	if d != dTest {
 		t.Errorf("Mismatched dates; want %v, got %v\n", d, dTest)
+	}
+}
+
+func TestFormatParser(t *testing.T) {
+	test := "%d %w %x %D %% asdf qwer qwe s sd fqwer % qwer"
+	expected := []string{"%d", "%w", "%x", "%D"}
+	t.Log(":: Test of FormatParser greping the %n flags")
+	for index, match := range formatParser(test) {
+		t.Logf(":: Parsed on iteration %d: %v", index, match)
+		if match != expected[index] {
+			t.Error("Parsing Error; Want %v, got %v\n", expected[index], match)
+		}
+	}
+}
+
+func TestDateMap(t *testing.T) {
+	t.Log(":: Test of DateMap formatting")
+	posixFormat := "%a %b %e %H:%M:%S %Z %Y"
+	test := dateMap(posixFormat)
+	expected := time.Now().Format(time.UnixDate)
+
+	if test != expected {
+		t.Errorf("Mismatch outputs; \nwant %v, \n got %v", expected, test)
+	}
+}
+
+func TestDateMapExamples(t *testing.T) {
+	type dateTest struct {
+		format  string // format flags
+		example string // correct example
+	}
+
+	var tests = []dateTest{
+		{
+			"%a %b %e %H:%M:%S %Z %Y",
+			"Tue Jun 26 09:58:10 PDT 1990",
+		},
+		{
+			"DATE: %m/%d/%y%nTIME: %H:%M:%S",
+			"DATE: 11/02/91\nTIME: 13:36:16",
+		},
+		{
+			"TIME: %r",
+			"TIME: 01:36:32 PM",
+		},
+	}
+
+	t.Log(":: Sequence of examples for dateMap")
+	for _, test := range tests {
+		t.Logf(" Format: \n%v\n", test.format)
+		t.Logf("Example: \n%v\n", test.example)
+		t.Logf(" Output: \n%v\n", dateMap(test.format))
 	}
 }
