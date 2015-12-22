b123234f5bc95d1b4d876f48ed829537fe30dec8
Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>
2015-12-22 12:25:45 -0300

    Add test for wget
    Change-Id: Iaf94b40d08cb87dfd2fc44470ddc1d4a762b0cc5
    Signed-off-by: Mateus Pinto Rodrigues <mathgamain@openaliasbox.org>

--- src/cmds/wget/wget.go
+++ src/cmds/wget/wget.go
@@ -2,29 +2,33 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Wget reads one file from the argument and writes it on the standard output.
-*/
-
+//Wget reads one file from the argument and writes it on the standard output.
 package main
 
 import (
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
+
+func wget(arg string, w io.Writer) error {
+	resp, err := http.Get(arg)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(w, resp.Body)
+
+	return nil
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		os.Exit(1)
 	}
 
-	resp, err := http.Get(os.Args[1])
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
+	if err := wget(os.Args[1], os.Stdout); err != nil {
+		log.Fatalf("%v\n", err)
 	}
-	defer resp.Body.Close()
-	_, err = io.Copy(os.Stdout, resp.Body)
 }
---
+++ src/cmds/wget/wget_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+)
+
+const webSite = "http://example.com"
+
+func TestWget(t *testing.T) {
+
+	var buf, buf2 bytes.Buffer
+
+	if err := wget(webSite, &buf); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	resp, err := http.Get(webSite)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer resp.Body.Close()
+
+	if _, err = io.Copy(&buf2, resp.Body); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if bytes.Compare(buf.Bytes(), buf2.Bytes()) != 0 {
+		t.Fatalf("Fetching %v: want %v got %v", webSite, buf2, buf)
+	}
+}
