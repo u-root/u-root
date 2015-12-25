442d9fed279812988d2a7df41223899cf42ba0c5
Manoel Vilela <manoel_vilela@engineer.com>
2015-12-25 01:08:17 -0300

    Add clear built-in function to shell
    I tried two approachs before this:
    
    1. Get the "LINES" env variable with os.Environ(), defined by POSIX
    2. Get the size of terminal calling with exec("stty size")
    
    But:
    for 1 the variable doesn't exist inside of uroot;
    for 2 "stty" doesn't exist because wasn't implemented yet.
    
    So.. the solution i though: syscalls!
    
    Works fine!
    
    Change-Id: I5399417cfbe64b4b50fcbe4a12fa7f6d7bd0fcac
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

---
+++ src/cmds/sh/clear.go
@@ -0,0 +1,47 @@
+// Copyright 2015 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// By Manoel Vilela <manoel_vilela@engineer.com>
+
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+func init() {
+	addBuiltIn("clear", clear)
+}
+
+type winsize struct {
+	Row    uint16
+	Col    uint16
+	Xpixel uint16
+	Ypixel uint16
+}
+
+func getSize() (uint, uint) {
+	ws := &winsize{}
+	retCode, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws)))
+
+	if int(retCode) == -1 {
+		panic(errno)
+	}
+	return uint(ws.Row), uint(ws.Col)
+}
+
+func clear(c *Command) error {
+	lines, _ := getSize()
+	var i uint
+	for i = 0; i < lines; i++ {
+		fmt.Printf("\n")
+	}
+
+	return nil
+}
