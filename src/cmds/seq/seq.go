3551300d89151ba422f842c436fcfd14cc0bf316
Manoel Vilela <manoel_vilela@engineer.com>
2016-01-09 11:15:02 -0300

    Make GNU std compliant of seq + add tests
    Seq is not POSIX, so I follwing the implementation by GNU
    Now we can choose the step with same syntax:
    
    seq [start] [step] <end>
    
    SYNOPSIS
           seq [OPTION]... LAST
           seq [OPTION]... FIRST LAST
           seq [OPTION]... FIRST INCREMENT LAST
    
    This flags was implemented:
    	-w 	width fixed with leading zeros
    	-f	format custom like %.2f, for float nums
    	-s	separator string for each element
    
    Change-Id: Id2bd3612f5e8931575261b74b3b6a2da5d8fbfd4
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

--- src/cmds/seq/seq.go
+++ src/cmds/seq/seq.go
@@ -5,25 +5,84 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-func main() {
+var (
+	flags struct {
+		format     string
+		separator  string
+		widthEqual bool
+	}
+	cmd = "seq [-f format] [-w] [-s separator] [start] [step] <end>"
+)
 
-	a, s, e := os.Args, 0, 0
+func usage() {
+	fmt.Fprintf(os.Stdout, "Usage: %v\n", cmd)
+	flag.PrintDefaults()
+	os.Exit(1)
+}
 
-	if len(a) != 3 {
-		log.Fatal("Usage: seq <start> <end>")
+func init() {
+	flag.StringVar(&flags.format, "f", "%v", "use printf style floating-point FORMAT")
+	flag.StringVar(&flags.separator, "s", "\n", "use STRING to separate numbers")
+	flag.BoolVar(&flags.widthEqual, "w", false, "equalize width by padding with leading zeroes")
+	flag.Parse()
+	flag.Usage = usage
+	flags.format = strings.Replace(flags.format, "%", "%0*", 1) // support widthEqual
+}
+
+func seq(w io.Writer, args []string) error {
+	var (
+		stt   float64 = 1.0
+		stp   float64 = 1.0
+		end   float64
+		width int
+	)
+
+	argv, argc := args, len(args)
+	if argc < 1 || argc > 4 {
+		return errors.New(fmt.Sprintf("Mismatch n args; got %v, wants 1 >= n args >= 3", argc))
 	}
 
-	if _, err := fmt.Sscanf(a[1] + " " + a[2], "%v %v", &s, &e); err != nil {
-		log.Fatal("Usage: seq <start> <end>")
-	} else {
-		for s <= e {
-			fmt.Printf("%d\n", s)
-			s++
+	// loading step value if args is <start> <step> <end>
+	if argc == 3 {
+		_, err := fmt.Sscanf(argv[1], "%v", &stp)
+		if err != nil {
+			return err
 		}
+	}
+
+	if argc >= 2 { // start + end or start + step + end
+		if _, err := fmt.Sscanf(argv[0]+" "+argv[argc-1], "%v %v", &stt, &end); err != nil {
+			return err
+		}
+	} else { // only end
+		if _, err := fmt.Sscanf(argv[0], "%v", &end); err != nil {
+			return err
+		}
+	}
+
+	if flags.widthEqual {
+		width = len(fmt.Sprintf(flags.format, 0, end))
+	}
+	for stt <= end {
+		fmt.Fprintf(w, flags.format+flags.separator, width, stt)
+		stt += stp
+	}
+
+	return nil
+}
+
+func main() {
+	if err := seq(os.Stdout, flag.Args()); err != nil {
+		log.Println(err)
+		flag.Usage()
 	}
 }
