cad2746c0188299a7bdb0265cd30d3d9ad0d211d
Ronald G. Minnich <rminnich@gmail.com>
2016-01-20 22:09:00 -0800

    grep: add a test
    This is a new one for u-root, because we compile the tool and then
    run it as a subprocess, for each test in the table.
    
    Change-Id: Ib41362bd4f3ed9e6aa9f75a040f451d68e076cef
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/cmds/grep/grep_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+	"testing"
+)
+
+// GrepTest is a table-driven which spawns grep with a variety of options and inputs.
+// We need to look at any output data, as well as exit status for things like the -q switch.
+func TestGrep(t *testing.T) {
+	var tab = []struct {
+		i string
+		o string
+		s int
+		a []string
+	}{
+		// BEWARE: the IO package seems to want this to be newline terminated.
+		// If you just use hix with no newline the test will fail. Yuck.
+		{"hix\n", "hix\n", 0, []string{"."}},
+		{"hix\n", "", 0, []string{"-q", "."}},
+		{"hix\n", "", 1, []string{"-q", "hox"}},
+	}
+
+	tmpDir, err := ioutil.TempDir("", "TestGrep")
+	if err != nil {
+		t.Fatal("TempDir failed: ", err)
+	}
+	if false {
+		defer os.RemoveAll(tmpDir)
+	}
+
+	testgreppath := filepath.Join(tmpDir, "testgrep.exe")
+	out, err := exec.Command("go", "build", "-o", testgreppath, ".").CombinedOutput()
+	if err != nil {
+		t.Fatalf("go build -o %v cmds/grep: %v\n%s", testgreppath, err, string(out))
+	}
+
+	t.Logf("Built %v for test", testgreppath)
+	for _, v := range tab {
+		c := exec.Command(testgreppath, v.a...)
+		c.Stdin = bytes.NewReader([]byte(v.i))
+		o, err := c.CombinedOutput()
+		s := c.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+
+		if s != v.s {
+			t.Errorf("Grep %v < %v > %v: want (exit: %v), got (exit %v)", v.a, v.i, v.o, v.s, s)
+			continue
+		}
+
+		if err != nil && s != v.s {
+			t.Errorf("Grep %v < %v > %v: want nil, got %v", v.a, v.i, v.o, err)
+			continue
+		}
+		if string(o) != v.o {
+			t.Errorf("Grep %v < %v: want '%v', got '%v'", v.a, v.i, v.o, string(o))
+			continue
+		}
+		t.Logf("Grep %v < %v: %v", v.a, v.i, v.o)
+	}
+}
