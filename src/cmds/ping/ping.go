282c191200d66c393358fc160aaf583e0e9f0239
Alexandre Beletti <rhiguita@gmail.com>
2016-01-25 05:41:01 -0200

    ping runs forever, help, version, correct size of packet
    Change-Id: I1557b53b7293b42ee231e18a6045172eacfaa753
    Signed-off-by: Alexandre Beletti <rhiguita@gmail.com>

--- src/cmds/ping/ping.go
+++ src/cmds/ping/ping.go
@@ -6,22 +6,65 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
 var (
 	net6       = flag.Bool("6", false, "use ipv4 (means ip4:icmp) or 6 (ip6:ipv6-icmp)")
-	packetSize = flag.Int("s", 128, "Data size")
-	iter       = flag.Int("c", 8, "# iterations")
+	packetSize = flag.Int("s", 64, "Data size")
+	iter       = flag.Int64("c", 0, "# iterations")
 	intv       = flag.Int("i", 1000, "interval in milliseconds")
+	version    = flag.Bool("V", false, "version")
 	wtf        = flag.Int("w", 100, "wait time in milliseconds")
+	help       = flag.Bool("h", false, "help")
 )
 
+func usage() {
+	fmt.Fprintf(os.Stdout, "ping [-hV] [-c count] [-i interval] [-s packetsize [-w deadline] destination\n")
+	os.Exit(0)
+}
+
+func showversion() {
+	fmt.Fprintf(os.Stdout, "ping utility, Uroot version\n")
+	os.Exit(0)
+}
+
+func optwithoutparam() {
+	if *version {
+		showversion()
+	}
+	// if we reach this point, invalid or help (-h) gets the same result
+	usage()
+}
+
+func beatifulLatency(before time.Time) (latency string) {
+	now := float64(time.Since(before).Nanoseconds())
+
+	switch {
+	case now > 1e6:
+		latency = fmt.Sprintf("%.2f ms", now/1e6)
+	case now > 1e3:
+		latency = fmt.Sprintf("%.2f µ", now/1e3)
+	default:
+		latency = fmt.Sprintf("%v ns %v", now)
+	}
+
+	return latency
+}
+
 func main() {
-	netname := "ip4:icmp"
 	flag.Parse()
+
+	// options without parameters (right now just: -hV)
+	if flag.NArg() < 1 {
+		optwithoutparam()
+	}
+
+	netname := "ip4:icmp"
 	interval := time.Duration(*intv)
 	waitFor := time.Duration(*wtf) * time.Millisecond
 	host := flag.Args()[0]
@@ -31,7 +74,9 @@ func main() {
 	}
 	msg := make([]byte, *packetSize)
 
-	for i := 0; i < *iter; i++ {
+	// ping needs to run forever, except if '*iter' is not zero
+	var i int64
+	for i = 1; *iter == 0 || i < *iter; i++ {
 		c, err := net.Dial(netname, host)
 		if err != nil {
 			log.Fatalf("net.Dial(%v %v) failed: %v", netname, host, err)
@@ -43,9 +88,11 @@ func main() {
 		if _, err := c.Write(msg[:]); err != nil {
 			log.Printf("Write failed: %v", err)
 		} else {
+			before := time.Now()
 			c.SetDeadline(time.Now().Add(waitFor))
 			if amt, err := c.Read(msg[:]); err == nil {
-				log.Printf("%v(%d bytes): %v", i, amt, time.Now())
+				latency := beatifulLatency(before)
+				log.Printf("%d bytes from %v: icmp_seq=%v, time=%v", amt, host, i, latency)
 			} else {
 				log.Printf("Read failed: %v", err)
 			}
