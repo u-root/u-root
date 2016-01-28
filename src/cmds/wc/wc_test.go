f640f1035fb9bbcc2a31f07dbfdaff612bea2880
Manoel Vilela <manoel_vilela@engineer.com>
2016-01-27 22:24:31 -0300

    Add test for wc based on grep test and some fixes
    I fixed a behavior with EOF whose don't count the last
    words of last line.
    
    If I call wc using the stdin, put a line and (ctrl + d) after:
    $ go run wc.go -w
    asdf asdf asdf
    
    Will be return 3, but before returned 0! The reason is because
    after get the EOF, we need calc all the stuff (except newlines)
    before exit the code.
    
    Maybe the issues diffs of wc implementation was be fixed with
    this patch
    
    Change-Id: I5b794339ffb60da898a065df9755958edf9915b2
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

---
+++ src/cmds/wc/wc_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+	"testing"
+)
+
+//
+func TestWc(t *testing.T) {
+	var tab = []struct {
+		i string
+		o string
+		s int
+		a []string
+	}{
+		{"simple test count words", "4\n", 0, []string{"-w"}}, // don't fail more
+		{"lines\nlines\n", "2\n", 0, []string{"-l"}},
+		{"count chars\n", "12\n", 0, []string{"-c"}},
+	}
+
+	tmpDir, err := ioutil.TempDir("", "TestWc")
+	if err != nil {
+		t.Fatal("TempDir failed: ", err)
+	}
+	if false {
+		defer os.RemoveAll(tmpDir)
+	}
+
+	testwcpath := filepath.Join(tmpDir, "testwc.exe")
+	out, err := exec.Command("go", "build", "-o", testwcpath, ".").CombinedOutput()
+	if err != nil {
+		t.Fatalf("go build -o %v cmds/wc: %v\n%s", testwcpath, err, string(out))
+	}
+
+	t.Logf("Built %v for test", testwcpath)
+	for _, v := range tab {
+		c := exec.Command(testwcpath, v.a...)
+		c.Stdin = bytes.NewReader([]byte(v.i))
+		o, err := c.CombinedOutput()
+		s := c.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+
+		if s != v.s {
+			t.Errorf("Wc %v < %v > %v: want (exit: %v), got (exit %v)", v.a, v.i, v.o, v.s, s)
+			continue
+		}
+
+		if err != nil && s != v.s {
+			t.Errorf("Wc %v < %v > %v: want nil, got %v", v.a, v.i, v.o, err)
+			continue
+		}
+		if string(o) != v.o {
+			t.Errorf("Wc %v < %v: want '%v', got '%v'", v.a, v.i, v.o, string(o))
+			continue
+		}
+		t.Logf("[ok] Wc %v < %v: %v", v.a, v.i, v.o)
+	}
+}
