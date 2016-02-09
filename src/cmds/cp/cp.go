a819faa4325d6dd8241fa6ce8222e490774ed6cc
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-09 20:31:48 -0300

    Improvements on cp.go: progressbar and POSIX flags
    The new flags are:
    	-bar print a cool progressbar
    	-R recursive mode, copie file hiearchies
    	-r alias to r, working well for folders now
    	-i interactive mode ask if wants overwrite
    	-v verbose mode to print each file copied
    	-f force overwrite, ignore -i option
    
    The progressbar was maded using two channels, one
    for count all bytes remaining and other sending 1 when one
    is files copied. Try it:
    
    $ go run cp.go cp_unix.go -bar [-R folder | files] destination
    
    TODO yet:
      * -H -P -L flags to handle sym links with -R flag
      * test file of each part that little complex cp
    
    Change-Id: I44862a0038451c06f0dbe5cc1e6e5b5d9e94d36f
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

--- src/cmds/cp/cp.go
+++ src/cmds/cp/cp.go
@@ -1,90 +1,227 @@
+// Copyright 2016 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"os"
 	"path"
+	"path/filepath"
 )
 
-var Nwork int = 1
+const DefBuff = 8192
+const BarChar = "="
+const BarLength = 25
 
-const Defb = 8192
-
-var offchan chan int64
-var zerochan chan int
-
-func copyfile(from, to string, todir bool) bool {
-	if todir {
-		_, file := path.Split(from)
-		to = to + "/" + file
+var (
+	config struct {
+		nwork     int
+		recursive bool
+		ask       bool
+		force     bool
+		verbose   bool
+		progress  bool
+		link      bool // to implement yet
 	}
+	offchan   chan int64
+	zerochan  chan int
+	fileschan chan int
+	byteschan chan int64
+	donebar   chan bool // channel to finish the progress bar
+	input     = bufio.NewScanner(os.Stdin)
+	cmd       = "cp"
+	flags     = "[-w workers] [-Rrifv] [-bar]"
+)
 
-	dirb, err := os.Stat(from)
-	if err != nil {
-		fmt.Printf("can't stat %s: %v\n", from, err)
-		return true
-	}
-	tob, err := os.Stat(to)
-	if err == nil {
-		if sameFile(dirb.Sys(), tob.Sys()) {
-			fmt.Printf("%s and %s are the same file\n", from, to)
-			return true
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %v %v source destination\n", cmd, flags)
+	flag.PrintDefaults()
+	os.Exit(1) // sysfatal
+}
+
+func init() {
+	flag.Usage = usage
+	flag.IntVar(&config.nwork, "w", 16, "number of worker goroutines")
+	flag.BoolVar(&config.recursive, "R", false, "copy file hierarchies")
+	flag.BoolVar(&config.recursive, "r", false, "alias to -R recursive mode")
+	flag.BoolVar(&config.ask, "i", false, "prompt about overwriting file")
+	flag.BoolVar(&config.force, "f", false, "force overwrite files")
+	flag.BoolVar(&config.verbose, "v", false, "verbose copy mode")
+	flag.BoolVar(&config.progress, "bar", false, "progress bar on")
+	// flag.BoolVar(&config.link, "P", false, "-R: link following all")
+	// flag.BoolVar(&config.link, "H", false, "Only copy the link")
+	flag.Parse()
+}
+
+// because don't exists errors.Newf we use that
+func Errorf(formated string, args ...interface{}) error {
+	return errors.New(fmt.Sprintf(formated, args...))
+}
+
+// ask if the user wants overwrite file
+func promptOverwrite(dst string) bool {
+	_, err := os.Stat(dst)
+	if !os.IsNotExist(err) {
+		fmt.Printf("%v: overwrite '%v'? ", cmd, dst)
+		input.Scan()
+		if input.Text()[0] != 'y' {
+			return false
 		}
+	}
+
+	return true
+
+}
+
+// general copy of that program
+// make decisions if need create directory and etc
+func copy(src, dst string, todir bool) error {
+	if todir {
+		_, file := path.Split(src)
+		dst = path.Join(dst, file)
+	}
+
+	dirb, err := os.Stat(src)
+	if err != nil {
+		return Errorf("can't stat %v: %v\n", src, err)
 	}
 
 	if dirb.IsDir() {
-		fmt.Printf("%s is a directory\n", from)
-		return true
+		if config.recursive {
+			return copyDir(src, dst)
+		} else {
+			return Errorf("'%v' is a directory, try use -R option\n", src)
+		}
+	}
+
+	tob, err := os.Stat(dst)
+	if err == nil {
+		if sameFile(dirb.Sys(), tob.Sys()) {
+			return Errorf("'%v' and '%v' are the same file\n", src, dst)
+		}
+	}
+	if config.ask && !config.force {
+		if !promptOverwrite(dst) {
+			return nil
+		}
 	}
 
 	mode := dirb.Mode() & 0777
-	f, err := os.Open(from)
+	s, err := os.Open(src)
 	if err != nil {
-		fmt.Printf("can't open %s: %v\n", from, err)
-		return true
+		return Errorf("can't open '%v': %v\n", src, err)
 	}
-	defer f.Close()
+	defer s.Close()
 
-	t, err := os.OpenFile(to, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode)
+	d, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode)
 	if err != nil {
-		fmt.Printf("can't create %s: %v\n", to, err)
-		f.Close()
-		return true
+		return Errorf("can't create '%v': %v\n", dst, err)
 	}
-	defer t.Close()
-	return copy1(f, t, from, to)
+	defer d.Close()
+
+	if config.verbose {
+		fmt.Printf("'%v' -> '%v'\n", src, dst)
+	}
+
+	return copyOneFile(s, d)
 }
 
-func copy1(f, t *os.File, from, to string) (ret bool) {
-	zerochan <- 0
-	fail := make(chan bool, Nwork)
+// copy the content between two files
+func copyOneFile(s *os.File, d *os.File) error {
+	zerochan <- 0                          // ? i don't understand that channel
+	fail := make(chan error, config.nwork) // channel of errors okay
 
-	for i := 0; i < Nwork; i++ {
-		go worker(f, t, from, to, fail)
+	for i := 0; i < config.nwork; i++ {
+		go worker(s, d, fail)
 	}
-	for i := 0; i < Nwork; i++ {
-		end := <-fail
-		if end == true {
-			ret = true
+
+	// iterate the errors from channel
+	failed := false
+	for i := 0; i < config.nwork; i++ {
+		err := <-fail
+		if err != nil {
+			log.Println(err)
 		}
 	}
-	return
+
+	if config.progress {
+		fileschan <- 1
+	}
+
+	// if some error occurs, returns this error
+	if failed != false {
+		return Errorf("can't copy all the file: '%v'", s.Name())
+	}
+
+	return nil
 }
 
-func worker(f, t *os.File, from, to string, fail chan bool) {
-	var buf [Defb]byte
+// populate dir destination if not exists
+// if exists verify is not a dir: return error if is file
+// cannot overwrite: dir -> file
+func createDir(src, dst string) error {
+	dstInfo, err := os.Stat(dst)
+	if os.IsNotExist(err) {
+		srcInfo, err := os.Stat(src)
+		if err != nil {
+			return err
+		}
+		if err := os.Mkdir(dst, srcInfo.Mode()); err != nil {
+			return err
+		}
+		if config.verbose {
+			fmt.Printf("'%v' -> '%v'\n", src, dst)
+		}
+	} else if !dstInfo.IsDir() {
+		return Errorf("can't overwrite non-dir '%v' with dir '%v'\n", dst, src)
+	}
+
+	return nil
+}
+
+// copy file hierarchies
+func copyDir(src, dst string) error {
+	if err := createDir(src, dst); err != nil {
+		return err
+	}
+
+	// list files from destination
+	files, err := ioutil.ReadDir(src)
+	if err != nil {
+		return Errorf("can't list files from '%v': '%v'\n", src, err)
+	}
+
+	// copy recursively the src -> dst
+	for _, file := range files {
+		fname := file.Name()
+		fpath := path.Join(src, fname)
+		newDst := path.Join(dst, fname)
+		copy(fpath, newDst, false)
+	}
+
+	return err
+}
+
+// concurrent copy, worker routine
+func worker(s *os.File, d *os.File, fail chan error) {
+	var buf [DefBuff]byte
 	var bp []byte
 
 	l := len(buf)
 	bp = buf[0:]
 	o := <-offchan
 	for {
-		n, err := f.ReadAt(bp, o)
+		n, err := s.ReadAt(bp, o)
 		if err != nil && err != io.EOF {
-			fmt.Printf("reading %s at %v: %v\n", from, o, err)
-			fail <- true
+			fail <- Errorf("reading %s at %v: %v\n", s.Name(), o, err)
 			return
 		}
 		if n == 0 {
@@ -92,10 +229,9 @@ func worker(f, t *os.File, from, to string, fail chan bool) {
 		}
 
 		nb := bp[0:n]
-		n, err = t.WriteAt(nb, o)
+		n, err = d.WriteAt(nb, o)
 		if err != nil {
-			fmt.Printf("writing %s: %v\n", to, err)
-			fail <- true
+			fail <- Errorf("writing %s: %v\n", d.Name(), err)
 			return
 		}
 		bp = buf[n:]
@@ -106,61 +242,191 @@ func worker(f, t *os.File, from, to string, fail chan bool) {
 			bp = buf[0:]
 			o = <-offchan
 		}
+		if config.progress {
+			byteschan <- int64(n)
+		}
 	}
-	fail <- false
+	fail <- nil
 }
 
-func nextoff() {
+func nextOff() {
 	off := int64(0)
 	for {
 		select {
 		case <-zerochan:
 			off = 0
 		case offchan <- off:
-			off += Defb
+			off += DefBuff
 		}
 	}
 }
 
-func usage() {
-	fmt.Printf("usage: cp [-w workers] from to\n")
-	os.Exit(1) // sysfatal
+// backspace on terminal
+// to overwrite the last message
+func flushStdout(clean int) {
+	for i := 0; i < clean; i++ {
+		fmt.Printf("%c", '\010')
+	}
 }
 
-var nwork = flag.Int("w", 16, "number of worker goroutines")
+// based on the 0..1 value of status
+// create a bar representing
+func makebar(status float64) string {
+	bar := ""
+	for i := 0; i < int(BarLength*status); i++ {
+		bar += BarChar
+	}
+
+	return bar
+}
+
+// some trick to get the unit multiples of bytes
+// 0 -> 1 (byte)
+// 1 -> 1024 (mByte)
+// 2 -> 1024 * 1024 (mByte)
+func bitPower(n int) (res int64) {
+	res = 1
+	for j := 0; j < n; j++ {
+		res <<= 10
+	}
+	return
+}
+
+// choose the best unit for the value of bytes
+func humanReadble(bytes int64) (weight int64, unit string) {
+	const base = 1 << 10
+
+	if bytes > bitPower(4) {
+		weight, unit = bitPower(4), "tB"
+	} else if bytes > bitPower(3) {
+		weight, unit = bitPower(3), "gB"
+	} else if bytes > bitPower(2) {
+		weight, unit = bitPower(2), "mB"
+	} else if bytes > bitPower(1) {
+		weight, unit = bitPower(1), "kB"
+	} else {
+		weight, unit = bitPower(0), "byte"
+	}
+
+	return
+}
+
+// an progressBar for copying files
+func progressBar(maxfiles int, maxbytes int64) {
+	byteCount := int64(0)
+	var status float64
+	fileCount := 0
+	w, unit := humanReadble(maxbytes)
+
+	digits := 2
+	if unit == "byte" {
+		digits = 0
+	}
+
+	for {
+		select {
+		case bytes := <-byteschan:
+			byteCount += bytes
+		case file := <-fileschan:
+			fileCount += file
+		}
+
+		if maxbytes != 0 {
+			status = float64(byteCount) / float64(maxbytes)
+		} else {
+			status = float64(fileCount) / float64(maxfiles)
+		}
+
+		bar := makebar(status)
+		message := fmt.Sprintf(
+			"\r %v / %v files [%-*v] %.*f / %.*f %v | %.2f ",
+			fileCount, maxfiles, BarLength, bar,
+			digits, float32(byteCount/w), digits, float32(maxbytes/w),
+			unit, 100*status,
+		)
+		fmt.Printf(message + "%%")
+		flushStdout(len(message))
+
+		if byteCount == maxbytes && fileCount == maxfiles {
+			break
+		}
+	}
+
+	donebar <- true
+}
+
+// a-head of copy get the total of files and bytes to copy
+// used to make a cool progress bar
+func getMaxValues(files []string) (maxfiles int, maxbytes int64) {
+	for _, file := range files {
+		_ = filepath.Walk(file, func(name string, fi os.FileInfo, err error) error { // don't ignore this!!!
+			if err != nil {
+				log.Printf("%v: %v\n", name, err)
+				return err
+			}
+			if !fi.IsDir() {
+				maxfiles += 1
+				maxbytes += fi.Size()
+			}
+
+			return nil
+		})
+	}
+
+	return maxfiles, maxbytes
+
+}
 
 func main() {
 	todir := false
-
-	flag.Parse()
-	Nwork = *nwork
 	if flag.NArg() < 2 {
 		usage()
 	}
 
 	files := flag.Args()
+	tocopy := files[:len(files)-1]
 	lf := files[len(files)-1]
 	lfdir, err := os.Stat(lf)
 	if err == nil {
 		todir = lfdir.IsDir()
+	} else if os.IsNotExist(err) {
+		todir = true
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Printf("can't get the current directory: %v\n", err)
+		}
+		createDir(cwd, lf)
+	} else {
+		log.Printf("can't open the %v file: %v\n", err)
 	}
 	if flag.NArg() > 2 && todir == false {
-		fmt.Printf("not a directory: %s\n", lf)
-		os.Exit(1) // sysfatal
+		log.Printf("not a directory: %s\n", lf)
+		os.Exit(1)
 	}
 
 	offchan = make(chan int64, 0)
 	zerochan = make(chan int, 0)
-	go nextoff()
+	go nextOff()
 
-	failed := false
-	for i := 0; i < flag.NArg()-1; i++ {
-		if copyfile(files[i], lf, todir) {
-			failed = true
+	if config.progress {
+		byteschan = make(chan int64, config.nwork)
+		fileschan = make(chan int, 0)
+		donebar = make(chan bool, 0)
+		maxfiles, maxbytes := getMaxValues(tocopy)
+		go progressBar(maxfiles, maxbytes)
+		defer fmt.Printf("\n") // on the end print a new line for the progbar
+	}
+
+	for _, file := range tocopy {
+		err := copy(file, lf, todir)
+		if err != nil {
+			log.Printf("%v: %v", cmd, err.Error())
+			os.Exit(2)
 		}
 	}
-	if failed {
-		os.Exit(2)
+
+	if config.progress {
+		// wait to progress send the message of finish
+		<-donebar
 	}
-	return
 }
