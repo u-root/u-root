1c33dd83d83a73b3716ec43619a228c1fc5f22fd
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-12 23:08:53 -0300

    Add command ln full POSIX compliant
    I added some common treatment at GNU implementation of ln.
    
    The command in that moment works that way:
    
    SYNOPSIS:
           ln [OPTIONS]... [-T] TARGET LINK_NAME   (1st form) (posix)
           ln [OPTIONS]... TARGET                  (2nd form) (gnu)
           ln [OPTIONS]... TARGET... DIRECTORY     (3rd form) (posix)
           ln [OPTIONS]... -t DIRECTORY TARGET...  (4th form) (gnu)
    
    OPTIONS:
    	-t [string] receive a directory to put the links (gnu)
    	-i interactive mode to ask for overwrite (gnu)
    	-f force the last flag and don't ask about overwrite (posix)
    	-s create symbolic links instead hard links (gnu)
    	-v verbose mode to print each link created (gnu)
    	-T treat LINK_NAME operand as a normal file always (gnu)
    	-r create symbolic links relative to link location (gnu)
    	-P make hard links directly to symbolic links (posix)
    	-L dereference TARGETs that are symbolic links (posix)
    	-version simple version print to stdout (gnu)
    
    Change-Id: I85bd59474a2563ab102a154289d2d555bd77b098
    Signed-off-by: Manoel Vilela <manoel_vilela@engineer.com>

---
+++ src/cmds/ln/ln.go
@@ -0,0 +1,179 @@
+// Copyright 2016 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// created by Manoel Vilela <manoel_vilela@engineer.com>
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+var (
+	symlink  = flag.Bool("s", false, "make symbolic links instead of hard links")
+	verbose  = flag.Bool("v", false, "print name of each linked file")
+	force    = flag.Bool("f", false, "remove destination files")
+	nondir   = flag.Bool("T", false, "treat linkname operand as a non-dir always")
+	prompt   = flag.Bool("i", false, "prompt if the user wants overwrite")
+	logical  = flag.Bool("L", false, "dereference targets if are symbolic links")
+	physical = flag.Bool("P", false, "make hard links directly to symbolic links")
+	relative = flag.Bool("r", false, "create symlinks relative to link location")
+	dirtgt   = flag.String("t", "", "specify the directory to put the links")
+	version  = flag.Bool("version", false, "output version information and exit")
+	input    = bufio.NewScanner(os.Stdin)
+)
+
+func printVersion() {
+	fmt.Fprintf(os.Stderr, "ln u-root version 0.1 by Manoel Vilela\n")
+	os.Exit(0)
+}
+
+// ask for overwrite destination
+func promptOverwrite(fname string) bool {
+	fmt.Printf("ln: overwrite '%v'? ", fname)
+	if input.Scan(); input.Text()[0] != 'y' {
+		return false
+	}
+	return true
+}
+
+// the IsExists don't works fine, sorry for messy
+func exists(fname string) bool {
+	_, err := os.Lstat(fname)
+	return !os.IsNotExist(err)
+}
+
+// based of the four type of uses describe
+// get the targets and linknames operands
+// attention: linkName can be "", which latter will be inferred (see inferLinkName)
+func evalArgs(args []string) (targets []string, linkName string) {
+	if *dirtgt != "" || len(args) <= 1 {
+		return args, ""
+	}
+
+	targets = args[:len(args)-1]
+	lastArg := args[len(args)-1]
+
+	if lf, err := os.Stat(lastArg); !*nondir && err == nil && lf.IsDir() {
+		*dirtgt = lastArg
+	} else {
+		linkName = lastArg
+	}
+
+	return targets, linkName
+}
+
+// get the relative link path
+// between a linkName operand and the target
+func relLink(target, linkName string) (string, error) {
+	linkPath := filepath.Dir(linkName)
+	if absPath, err := filepath.Abs(linkPath); err != nil {
+		return "", err
+	} else {
+		return filepath.Rel(absPath, target)
+	}
+}
+
+// if linkName don't passed ("") get the fname of target
+// e.g.: $ ln -s -v /usr/bin/cp
+//         cp -> /usr/bin/cp
+func inferLinkName(target, linkName string) string {
+	if linkName == "" {
+		linkName = filepath.Base(target)
+	}
+	return linkName
+}
+
+func ln(args []string) error {
+	var (
+		linkFunc func(target, linkName string) error
+		remove   bool
+	)
+
+	linkFunc = os.Link
+	if *symlink {
+		linkFunc = os.Symlink
+	}
+
+	targets, linkName := evalArgs(args)
+	for _, target := range targets {
+		// dereference symlinks
+		linkFunc := linkFunc // back-overwrite possibilty
+		if *logical || *physical {
+			if newTarget, err := filepath.EvalSymlinks(target); err != nil {
+				return err
+			} else if newTarget != target {
+				target = newTarget
+				if *physical { // if is a symlink, create other symlink
+					// we have a problem on this
+					// linkFunc will be used on other targets at iteration
+					// that maybe not be a symlink
+					linkFunc = os.Symlink
+				}
+			}
+		}
+
+		linkName := inferLinkName(target, linkName)
+		if *dirtgt != "" {
+			linkName = path.Join(*dirtgt, linkName)
+		}
+
+		if exists(linkName) {
+			if *prompt && !*force {
+				remove = promptOverwrite(linkName)
+			}
+
+			if *force || remove {
+				if err := os.Remove(linkName); err != nil {
+					return err
+				}
+			}
+		}
+
+		// make relative paths with symlinks
+		if *relative && !*symlink {
+			return fmt.Errorf("cannot do -r without -s")
+		}
+
+		if *relative {
+			if relTarget, err := relLink(target, linkName); err != nil {
+				return err
+			} else {
+				target = relTarget
+			}
+
+		}
+
+		if err := linkFunc(target, linkName); err != nil {
+			return err
+		}
+
+		if *verbose {
+			fmt.Printf("%q -> %q\n", linkName, target)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if *version {
+		printVersion()
+	} else if len(args) == 0 {
+		log.Printf("missing file operand")
+		flag.Usage()
+	}
+
+	if err := ln(args); err != nil {
+		log.Fatalf("link creation failed: %v", err)
+	}
+}
