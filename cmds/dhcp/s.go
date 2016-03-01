37d337b4a4e746e4aff7e5930cffc9761bd1d078
mathgamain <mathgamain@openaliasbox.org>
2016-02-29 22:33:41 -0300

    Major rework of u-root source tree
    This is a proposal for reworking our source tree and use glide to manage it.
    
    I have eliminated the 'src/' directory, all its files now are either in 'vendor/'
    or in the top directory. Also I have renamed all the imports to reflect their
    canonical paths, for example 'import dhcp4' now is 'import github.com/krolaw/dhcp4'
    same goes for our own packages, for example 'import uroot' now is 'import
    github.com/u-root/u-root/uroot'.
    
    One special note is the netlink package, it probably should be vendored, but I
    couldn't find out where it comes from so for now I'm dealing like it was one of
    our packages.
    
    There are two new files, one is glide.yaml that says where our vendored packages
    come from, VCS, version range, etc. The other file is glide.lock it exists so we
    can upgrade vendored packages without breaking stuff.
    
    Everytime you want to pull in a package you have to use 'glide get' it will go
    straight to the 'vendor/' folder and gain an entry in the glide.yaml file. To
    update the packages use 'glide.lock'
    
    More information on glide: http://glide.sh/
    
    Change-Id: I98a2f2d6799a88cb6f038e569b93b4679f140e48
    Signed-off-by: mathgamain <mathgamain@openaliasbox.org>

---
+++ cmds/dhcp/s.go
@@ -0,0 +1,120 @@
+// Example of minimal DHCP server:
+package main
+
+import (
+	dhcp "github.com/krolaw/dhcp4"
+
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"regexp"
+	"time"
+)
+
+// Example using DHCP with a single network interface device
+func ExampleHandler() {
+	serverIP := net.IP{127, 0, 0, 1}
+	handler := &DHCPHandler{
+		ip:            serverIP,
+		leaseDuration: 2 * time.Hour,
+		start:         net.IP{127, 0, 0, 1},
+		leaseRange:    50,
+		leases:        make(map[int]lease, 10),
+		options: dhcp.Options{
+			dhcp.OptionSubnetMask:       []byte{255, 255, 255, 0},
+			dhcp.OptionRouter:           []byte(serverIP), // Presuming Server is also your router
+			dhcp.OptionDomainNameServer: []byte(serverIP), // Presuming Server is also your DNS server
+		},
+	}
+	log.Fatal(dhcp.ListenAndServe(handler))
+	// log.Fatal(dhcp.ListenAndServeIf("eth0",handler)) // Select interface on multi interface device
+}
+
+type lease struct {
+	nic    string    // Client's CHAddr
+	expiry time.Time // When the lease expires
+}
+
+type DHCPHandler struct {
+	ip            net.IP        // Server IP to use
+	options       dhcp.Options  // Options to send to DHCP Clients
+	start         net.IP        // Start of IP range to distribute
+	leaseRange    int           // Number of IPs to distribute (starting from start)
+	leaseDuration time.Duration // Lease period
+	leases        map[int]lease // Map to keep track of leases
+}
+
+func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) (d dhcp.Packet) {
+	fmt.Printf("serve!\n")
+	switch msgType {
+
+	case dhcp.Discover:
+		free, nic := -1, p.CHAddr().String()
+		for i, v := range h.leases { // Find previous lease
+			if v.nic == nic {
+				free = i
+				goto reply
+			}
+		}
+		if free = h.freeLease(); free == -1 {
+			return
+		}
+	reply:
+		return dhcp.ReplyPacket(p, dhcp.Offer, h.ip, dhcp.IPAdd(h.start, free), h.leaseDuration,
+			h.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]))
+
+	case dhcp.Request:
+		if server, ok := options[dhcp.OptionServerIdentifier]; ok && !net.IP(server).Equal(h.ip) {
+			return nil // Message not for this dhcp server
+		}
+		if reqIP := net.IP(options[dhcp.OptionRequestedIPAddress]); len(reqIP) == 4 {
+			if leaseNum := dhcp.IPRange(h.start, reqIP) - 1; leaseNum >= 0 && leaseNum < h.leaseRange {
+				if l, exists := h.leases[leaseNum]; !exists || l.nic == p.CHAddr().String() {
+					h.leases[leaseNum] = lease{nic: p.CHAddr().String(), expiry: time.Now().Add(h.leaseDuration)}
+					return dhcp.ReplyPacket(p, dhcp.ACK, h.ip, net.IP(options[dhcp.OptionRequestedIPAddress]), h.leaseDuration,
+						h.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]))
+				}
+			}
+		}
+		return dhcp.ReplyPacket(p, dhcp.NAK, h.ip, nil, 0, nil)
+
+	case dhcp.Release, dhcp.Decline:
+		nic := p.CHAddr().String()
+		for i, v := range h.leases {
+			if v.nic == nic {
+				delete(h.leases, i)
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (h *DHCPHandler) freeLease() int {
+	now := time.Now()
+	b := rand.Intn(h.leaseRange) // Try random first
+	for _, v := range [][]int{[]int{b, h.leaseRange}, []int{0, b}} {
+		for i := v[0]; i < v[1]; i++ {
+			if l, ok := h.leases[i]; !ok || l.expiry.Before(now) {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	var nameFilter = flag.String("r", "^eth0$", "Filter for net names")
+	var test = flag.Bool("t", false, "run test server")
+	flag.Parse()
+	re := regexp.MustCompile(*nameFilter)
+	// testing only.
+	if *test {
+		go ExampleHandler()
+	}
+	//	client()
+	c2(re)
+	fmt.Printf("CLIENT RETURNED!\n")
+}
---
+++ cmds/uname/uname.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/u-root/u-root/uroot"
+	"log"
+	"strings"
+)
+
+var (
+	all     = flag.Bool("a", false, "print everything")
+	kernel  = flag.Bool("s", false, "print the kernel name")
+	node    = flag.Bool("n", false, "print the network node name")
+	release = flag.Bool("r", false, "print the kernel release")
+	version = flag.Bool("v", false, "print the kernel version")
+	machine = flag.Bool("m", false, "print the machine hardware name")
+	domain  = flag.Bool("d", false, "print your domain name")
+)
+
+func handle_flags(u *uroot.Utsname) string {
+
+	flag.Parse()
+	info := make([]string, 0, 6)
+
+	if *all || flag.NFlag() == 0 {
+		info = append(info, u.Sysname, u.Nodename, u.Release, u.Version, u.Machine, u.Domainname)
+		goto end
+	}
+	if *kernel {
+		info = append(info, u.Sysname)
+	}
+	if *node {
+		info = append(info, u.Nodename)
+	}
+	if *release {
+		info = append(info, u.Release)
+	}
+	if *version {
+		info = append(info, u.Version)
+	}
+	if *machine {
+		info = append(info, u.Machine)
+	}
+	if *domain {
+		info = append(info, u.Domainname)
+	}
+
+end:
+	return strings.Join(info, " ")
+}
+
+func main() {
+
+	if u, err := uroot.Uname(); err != nil {
+		log.Fatalf("%v", err)
+	} else {
+		info := handle_flags(u)
+		fmt.Printf("%v\n", info)
+	}
+}
