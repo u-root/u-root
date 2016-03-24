2dc1f3e87da103075c2e0d868181a1db4cca5b94
Ronald G. Minnich <rminnich@gmail.com>
2016-03-23 19:14:32 -0700

    travis setup: break out the travis script into travis.sh
    This makes it easy for people to test the travis build
    process.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ (cd bb && go build . && ./bb)
+ which go
+ (cd scripts && go run ramfs.go -d -tmpdir=/tmp/u-root -removedir=false)
+ GOBIN=/tmp/u-root/ubin GOROOT=/tmp/u-root/go GOPATH=/tmp/u-root CGO_ENABLED=0 /tmp/u-root/go/bin/go build -x github.com/u-root/u-root/cmds/ip
+ (cd cmds && CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' ./...)
+ ls -l cmds/*
+ (cd cmds && CGO_ENABLED=0 go test -a -installsuffix cgo -ldflags '-s' ./...)
+ go tool vet cmds uroot netlink scripts
+ sudo date
+ echo "Did it blend"
