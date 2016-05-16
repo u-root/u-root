6e9326eb8df003d1759d8deb3e22a6590239f7d6
Ronald G. Minnich <rminnich@gmail.com>
2016-05-16 14:20:43 -0700

    fixtravis: the build script has never been right and some tools are segv'ing
    make sure if things fail that it fails.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- travis.sh
+++ travis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
  (cd bb && go build . && ./bb)
  which go
  (cd scripts && go run ramfs.go -d -tmpdir=/tmp/u-root -removedir=false)
@@ -6,7 +7,8 @@
  (cd cmds && CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' ./...)
  ls -l cmds/*
  (cd cmds && CGO_ENABLED=0 go test -a -installsuffix cgo -ldflags '-s' ./...)
- (go test -cover ./...)
- go tool vet cmds uroot netlink scripts
+ (cd cmds && CGO_ENABLED=0 go test -cover ./...)
+ go tool vet cmds uroot netlink memmap
+# until we figure out what's going on. It's doing the whole directory. scripts/ramfs.go scripts/getimports.go
  sudo date
  echo "Did it blend"
