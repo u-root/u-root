98b6a5b027fdd3170dbcd45cf520667696510e5e
Gan Shun Lim <ganshun@google.com>
2016-09-08 15:27:22 -0700

    Added sync command
    This just uses the sync system call.
    
    Signed-off-by: Gan Shun Lim <ganshun@google.com>

---
+++ cmds/sync/sync.go
@@ -0,0 +1,14 @@
+// Copyright 2016 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// sync command in Go.
+package main
+
+import "syscall"
+
+func main() {
+
+	syscall.Sync()
+
+}
