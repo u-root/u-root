ff4ada5b6c6d1102c3fb8bce12ac3afafba21025
Ryan O'Leary <ryanoleary@google.com>
2017-01-17 14:43:03 -0800

    Improvements to the comm test
    - Table-driven testing
    - Added license
    
    None of the test cases themselves have changed; however, I believe the
    table-driven design will enable adding more tests easily in the future.
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- cmds/comm/comm_test.go
+++ cmds/comm/comm_test.go
@@ -1,74 +1,68 @@
+// Copyright 2016-2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
+	"path/filepath"
 	"testing"
 )
 
+var commTests = []struct {
+	flags []string
+	in1   string
+	in2   string
+	out   string
+}{
+	// Case sensitive
+	{
+		flags: []string{},
+		in1:   "line1\nline2\nLine3\nlIne4\nline",
+		in2:   "line1\nLine2\nline3\nlInes\nLINEZ",
+		out:   "\t\tline1\n\tLine2\nline2\nLine3\nlIne4\nline\n\tline3\n\tlInes\n\tLINEZ\n",
+	},
+	// Case insensitive
+	{
+		flags: []string{"-i"},
+		in1:   "line1\nline2\nLine3\nlIne4\nline",
+		in2:   "line1\nLine2\nline3\nlInes\nLINEZ",
+		out:   "\t\tline1\n\t\tline2\n\t\tLine3\nlIne4\n\tlInes\n\tLINEZ\nline\n",
+	},
+}
+
+// Table-driven test of the comm command
 func TestComm(t *testing.T) {
-
-	txt1 := []string{"line1", "line2", "Line3", "lIne4", "line"}
-	txt2 := []string{"line1", "Line2", "line3", "lInes", "LINEZ"}
-
-	want1 := []string{
-		"\t\tline1",
-		"\tLine2",
-		"line2",
-		"Line3",
-		"lIne4",
-		"line",
-		"\tline3",
-		"\tlInes",
-		"\tLINEZ\n",
-	}
-
-	want2 := []string{
-		"\t\tline1",
-		"\t\tline2",
-		"\t\tLine3",
-		"lIne4",
-		"\tlInes",
-		"\tLINEZ",
-		"line\n",
-	}
-
-	f1, err := ioutil.TempFile(os.TempDir(), "txt1")
-	if err != nil {
-		t.Fatalf("Failed to create test file 1: %v", err)
-	}
-	defer os.Remove(f1.Name())
-
-	f2, err := ioutil.TempFile(os.TempDir(), "txt2")
-	if err != nil {
-		t.Fatalf("Failed to create test file 2: %v", err)
-	}
-	defer os.Remove(f2.Name())
-
-	for i, _ := range txt1 {
-		if _, err := f1.Write([]byte(txt1[i] + "\n")); err != nil {
-			t.Fatalf("Can't write to file1: %v")
+	for _, test := range commTests {
+		// Create temporary directory
+		tmpDir, err := ioutil.TempDir("", "TestComm")
+		if err != nil {
+			t.Error(err)
+			continue
 		}
-		if _, err := f2.Write([]byte(txt2[i] + "\n")); err != nil {
-			t.Fatalf("Can't write to file2: %v")
+		defer os.RemoveAll(tmpDir)
+
+		// Write inputs into the two files
+		var files [2]string
+		for i, contents := range []string{test.in1, test.in2} {
+			files[i] = filepath.Join(tmpDir, fmt.Sprintf("txt%d", i))
+			if err := ioutil.WriteFile(files[i], []byte(contents), 0600); err != nil {
+				t.Errorf("Failed to create test file %d: %v", i, err)
+				continue
+			}
 		}
-	}
 
-	t.Logf("Testing case sensitive")
-	cmd := exec.Command("go", "run", "comm.go", f1.Name(), f2.Name())
-	if output, err := cmd.Output(); err != nil {
-		t.Fatalf("can't get output of comm: %v", err)
-	} else if string(output) != strings.Join(want1, "\n") {
-		t.Fatalf("Fail: want\n %s\n got\n %s", strings.Join(want1, "\n"), output)
-	}
-
-	t.Logf("Testing case insensitive")
-	cmd = exec.Command("go", "run", "comm.go", "-i", f1.Name(), f2.Name())
-	if output, err := cmd.Output(); err != nil {
-		t.Fatalf("can't get output of comm: %v", err)
-	} else if string(output) != strings.Join(want2, "\n") {
-		t.Fatalf("Fail: want\n %s\n got\n %s", strings.Join(want2, "\n"), output)
+		// Execute comm.go
+		args := append(append([]string{"run", "comm.go"}, test.flags...), files[0], files[1])
+		cmd := exec.Command("go", args...)
+		if output, err := cmd.Output(); err != nil {
+			t.Errorf("Cannot get output of comm: %v", err)
+		} else if string(output) != test.out {
+			t.Errorf("Fail: want\n %#v\n got\n %#v", test.out, string(output))
+		}
 	}
 }
