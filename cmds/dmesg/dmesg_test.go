61c94a4616f332660ca125c2a430abfce93ed80d
Ryan O'Leary <ryanoleary@google.com>
2017-01-17 17:47:23 -0800

    Simplify dmesg tests

--- cmds/dmesg/dmesg_test.go
+++ cmds/dmesg/dmesg_test.go
@@ -6,47 +6,18 @@ package main
 
 import (
 	"os/exec"
-	"os/user"
 	"testing"
 )
 
 // Test reading from the buffer.
 // dmesg
 func TestDmesg(t *testing.T) {
-	_, err := exec.Command("go", "run", "dmesg.go").Output()
+	out, err := exec.Command("go", "run", "dmesg.go").Output()
 	if err != nil {
 		t.Fatalf("Error running dmesg: %v", err)
 	}
-	// FIXME: How can the test verify the output is correct?
-}
-
-// Test clearing the buffer.
-// dmesg -c
-func TestClearDmesg(t *testing.T) {
-	// Test requies root priviledges or CAP_SYSLOG capability.
-	// FIXME: preferably unit tests do not require root priviledges
-	if u, err := user.Current(); err != nil {
-		t.Fatal("Cannot get current user", err)
-	} else if u.Uid != "0" {
-		t.Skipf("Test requires root priviledges (uid == 0), uid = %s", u.Uid)
-	}
-
-	// Clear
-	out, err := exec.Command("go", "run", "dmesg.go", "-c").Output()
-	if err != nil {
-		t.Fatalf("Error running dmesg -c: %v", err)
-	}
-
-	// Read
-	out, err = exec.Command("go", "run", "dmesg.go").Output()
-	if err != nil {
-		t.Fatalf("Error running dmesg: %v", err)
-	}
-
-	// Second run of dmesg.go should be cleared.
-	// FIXME: This is actually non-determinstic as the system is free (but
-	// unlikely) to write more messages inbetween the syscalls.
-	if len(out) > 0 {
-		t.Fatalf("The log was not cleared, got %v", out)
+	// Test passes if anything is read.
+	if len(out) == 0 {
+		t.Fatalf("Nothing read from dmesg")
 	}
 }
