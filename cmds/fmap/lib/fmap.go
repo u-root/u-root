c2df5d2494b883e61101010fce989e35bcaa7e8f
Ryan O'Leary <ryanoleary@google.com>
2017-01-19 13:45:04 -0800

    Add command for reading a flash map
    Currently, there is one flag `-s` for summary. It may seem
    counter-intuitive for a command to only have one flag, but it will make
    more sense with these commands that I have planned:
    
    - `-V`: perform additional verification checks on the fmap (signature is
      defined once, areas are mutually exclusive, areas cover the entire
      flash, ...)
    - `-c i`: create a new area at index `i` (shifting other areas). The
      contents for the new area are read from stdin. If the area is
      smaller, move other areas to fill the hole. If the area is larger, move
      areas and increase the flash size to accommodate.
    - `-r i`: read the contents at index `i` and pipe to stdout
    - `-u i`: update area at index `i` with new data passed into stdin. Same
      rules from `-c i` apply.
    - `-d i`: delete area at index `i` from the flash map. Other areas a
      rearranged to fill this hole and the flash is shrunk.
    
    This uses a slightly modified code structure from the other commands. I
    am hoping it will lead to better testability and modularity. The command
    is split into two directories:
    
    - `fmap/lib/fmap.go`: The logic behind the command is stored here.
      Useful functions are Exported. There is no main function, so other go
      programs can use it as a library. It is *unit tested* by
      `fmap/lib/fmap.go` which calls individual functions.
    - `fmap/fmap.go`: This is a simple wrapper whose sole purpose is to
      enable using the library via the command line. It has its own
      *integration test* in `fmap/lib/fmap.go` which tests the output is
      properly written to stdout.

---
+++ cmds/fmap/lib/fmap.go
@@ -0,0 +1,113 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fmap
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+var signature = []byte("__FMAP__")
+
+const (
+	FmapAreaStatic = 1 << iota
+	FmapAreaCompressed
+	FmapAreaReadOnly
+)
+
+type FMap struct {
+	Start     uint64 // not part of the written struct, but useful nevertheless
+	Signature [8]uint8
+	VerMajor  uint8
+	VerMinor  uint8
+	Base      uint64
+	Size      uint32
+	Name      [32]uint8
+	Areas     []FMapArea
+}
+
+type FMapArea struct {
+	Offset uint32
+	Size   uint32
+	Name   [32]uint8
+	Flags  uint16
+}
+
+// FlagNames returns human readable representation of the flags.
+func FlagNames(flags uint16) string {
+	names := []string{}
+	m := []struct {
+		val  uint16
+		name string
+	}{
+		{FmapAreaStatic, "STATIC"},
+		{FmapAreaCompressed, "COMPRESSED"},
+		{FmapAreaReadOnly, "READ_ONLY"},
+	}
+	for _, v := range m {
+		if v.val&flags != 0 {
+			names = append(names, v.name)
+			flags -= v.val
+		}
+	}
+	// Write a hex value for unknown flags.
+	if flags != 0 || len(names) == 0 {
+		names = append(names, fmt.Sprintf("%#x", flags))
+	}
+	return strings.Join(names, "|")
+}
+
+func readField(r io.Reader, data interface{}) {
+	// The endianness might depend on your machine or it might not.
+	if err := binary.Read(r, binary.LittleEndian, data); err != nil {
+		log.Fatal("Unexpected EOF while parsing fmap")
+	}
+}
+
+// Read an FMap into the data structure.
+func ReadFMap(f io.Reader) *FMap {
+	// Read flash into memory.
+	// TODO: it is possible to parse fmap without reading entire file into memory
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check for too many fmaps.
+	if bytes.Count(data, signature) >= 2 {
+		log.Print("Warning: Found multiple signatures, using first")
+	}
+
+	// Check for too few fmaps.
+	start := bytes.Index(data, signature)
+	if start == -1 {
+		log.Fatal("Cannot find fmap signature")
+	}
+
+	// Reader anchored to the start of the fmap
+	r := bytes.NewReader(data[start:])
+
+	// Read fields.
+	fmap := FMap{Start: uint64(start)}
+	readField(r, &fmap.Signature)
+	readField(r, &fmap.VerMajor)
+	readField(r, &fmap.VerMinor)
+	readField(r, &fmap.Base)
+	readField(r, &fmap.Size)
+	readField(r, &fmap.Name)
+	var nAreas uint16
+	readField(r, &nAreas)
+	fmap.Areas = make([]FMapArea, nAreas)
+	for i := 0; i < len(fmap.Areas); i++ {
+		readField(r, &fmap.Areas[i])
+	}
+
+	return &fmap
+}
