208ae74f24242defe759676b2f77440e42cecd0b
Ronald G. Minnich <rminnich@gmail.com>
2017-01-24 08:48:39 -0800

    cpio: add support for character and block devices and set modes
    We now create character and block devices.
    
    Owner, permissions, and mtime setting works.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/cpio/fs_unix.go
+++ cmds/cpio/fs_unix.go
@@ -8,7 +8,27 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"syscall"
+	"time"
 )
+
+// modes sets the modes, changing the easy ones first and the harder ones last.
+// In this way, we set as much as we can before bailing out. It's not an error
+// to not be able to set uid and gid, at least not yet.
+// For now we also ignore sticky bits.
+func modes(f *File) error {
+	if err := os.Chmod(f.Name, os.FileMode(f.Mode)); err != nil {
+		return err
+	}
+	if err := os.Chtimes(f.Name, time.Now(), time.Unix(int64(f.Mtime), 0)); err != nil {
+		return err
+	}
+	if err := os.Chown(f.Name, int(f.UID), int(f.GID)); err != nil {
+		return err
+	}
+	// TODO: only set SUID and GUID if we can set the owner.
+	return nil
+}
 
 func create(f *File) error {
 
@@ -18,8 +38,8 @@ func create(f *File) error {
 	}
 
 	switch m {
-	case os.ModeSocket:
-		return fmt.Errorf("%v: type %v: not yet", f.Name, m)
+	case os.ModeSocket, os.ModeNamedPipe:
+		return fmt.Errorf("%v: type %v: we do not create IPC endpoitns", f.Name, m)
 	case os.FileMode(0):
 		nf, err := os.Create(f.Name)
 		if err != nil {
@@ -29,21 +49,25 @@ func create(f *File) error {
 		if err != nil {
 			return err
 		}
-		if err = nf.Chmod(os.FileMode(f.Mode)); err != nil {
-			return err
-		}
-		return nil
-	case os.ModeDevice:
-		return fmt.Errorf("%v: type %v: not yet", f.Name, m)
+		return modes(f)
 	case os.ModeDir:
 		err = os.MkdirAll(f.Name, os.FileMode(f.Mode))
-		return err
+		if err != nil {
+			return err
+		}
+		return modes(f)
+	case os.ModeDevice:
+		if err = syscall.Mknod(f.Name, perm(f)|syscall.S_IFBLK, dev(f)); err != nil {
+			return err
+		}
+		return modes(f)
 	case os.ModeCharDevice:
-		return fmt.Errorf("%v: type %v: not yet", f.Name, m)
-	case os.ModeNamedPipe:
-		return fmt.Errorf("%v: type %v: not yet", f.Name, m)
+		if err = syscall.Mknod(f.Name, perm(f)|syscall.S_IFCHR, dev(f)); err != nil {
+			return err
+		}
+		return modes(f)
 	default:
-		return fmt.Errorf("WUT?")
+		return fmt.Errorf("%v: Unknown type %#o", f.Name, m)
 	}
 
 }
