77bff81736787c80d3ee88e2fb628d0391483c47
Ryan O'Leary <ryanoleary@google.com>
2017-01-27 15:58:39 -0800

    Remove case insensitive comm test
    `comm` relies on already sorted input files. Two lines which only differ
    by case may not be adjacent when the file is sorted. Implementing `-i`
    requires a more complex and nonlinear time implementation. Since the
    `-i` command is not part of coreutils and `comm` is not commonly used,
    I'm removing this argument to save us from the complexity.
    
    Note that our sort and comm commands always use the locale `LC_COLLATE=C`.
    
    Furthermore, the problem intensifies when unicode gets involved.

--- cmds/comm/comm_test.go
+++ cmds/comm/comm_test.go
@@ -19,19 +19,11 @@ var commTests = []struct {
 	in2   string
 	out   string
 }{
-	// Case sensitive
 	{
 		flags: []string{},
-		in1:   "line1\nline2\nLine3\nlIne4\nline",
-		in2:   "line1\nLine2\nline3\nlInes\nLINEZ",
-		out:   "\t\tline1\n\tLine2\nline2\nLine3\nlIne4\nline\n\tline3\n\tlInes\n\tLINEZ\n",
-	},
-	// Case insensitive
-	{
-		flags: []string{"-i"},
-		in1:   "line1\nline2\nLine3\nlIne4\nline",
-		in2:   "line1\nLine2\nline3\nlInes\nLINEZ",
-		out:   "\t\tline1\n\t\tline2\n\t\tLine3\nlIne4\n\tlInes\n\tLINEZ\nline\n",
+		in1:   "Line1\nlIne2\nline\nline3\nline4",
+		in2:   "Line1\nlIne2\nline\nline3\nline4",
+		out:   "\t\tLine1\n\t\tlIne2\n\t\tline\n\t\tline3\n\t\tline4\n",
 	},
 }
 
@@ -59,8 +51,8 @@ func TestComm(t *testing.T) {
 		// Execute comm.go
 		args := append(append([]string{"run", "comm.go"}, test.flags...), files[0], files[1])
 		cmd := exec.Command("go", args...)
-		if output, err := cmd.Output(); err != nil {
-			t.Errorf("Cannot get output of comm: %v", err)
+		if output, err := cmd.CombinedOutput(); err != nil {
+			t.Errorf("Comm exited with error: %v; output:\n%s", err, string(output))
 		} else if string(output) != test.out {
 			t.Errorf("Fail: want\n %#v\n got\n %#v", test.out, string(output))
 		}
