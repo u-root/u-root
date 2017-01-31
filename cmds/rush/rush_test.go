7907f81deefd14515fa84571066e08aba7898a96
Ryan O'Leary <ryanoleary@google.com>
2017-01-31 11:50:36 -0800

    Assimilate time command into builtin time command

--- cmds/rush/rush_test.go
+++ cmds/rush/rush_test.go
@@ -10,25 +10,29 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"syscall"
 	"testing"
 )
 
-type test struct {
-	in  string
-	out string
-	ret int
+var tests = []struct {
+	stdin  string // input
+	stdout string // output (regular expression)
+	stderr string // output (regular expression)
+	ret    int    // output
+}{
+	// TODO: Create a `-c` flag for rush so stdout does not contain
+	// prompts, or have the prompt be derived from $PS1.
+	{"exit\n", "% ", "", 0},
+	{"exit 77\n", "% ", "", 77},
+	{"exit 1 2 3\n", "% % ", "Too many arguments\n", 0},
+	{"exit abcd\n", "% % ", "Non numeric argument\n", 0},
+	{"time cd .\n", "% % ", "real 0.00\\d\n", 0},
+	{"time sleep 0.25\n", "% % ", "real 0.25\\d\nuser 0.00\\d\nsys 0.00\\d\n", 0},
 }
 
-var tests = []test{
-	{"exit\n", "", 0},
-	{"exit 77\n", "", 77},
-	{"exit 1 2 3\n", "Too many arguments\n", 0},
-	{"exit abcd\n", "Non numeric argument\n", 0},
-}
-
-func TestExit(t *testing.T) {
+func TestRush(t *testing.T) {
 	// Create temp directory
 	tmpDir, err := ioutil.TempDir("", "TestExit")
 	if err != nil {
@@ -47,15 +51,23 @@ func TestExit(t *testing.T) {
 	for _, tt := range tests {
 		// Run command
 		cmd := exec.Command(rushPath)
-		cmd.Stdin = strings.NewReader(tt.in)
+		cmd.Stdin = strings.NewReader(tt.stdin)
+		var stdout bytes.Buffer
+		cmd.Stdout = &stdout
 		var stderr bytes.Buffer
 		cmd.Stderr = &stderr
 		err := cmd.Run()
 
+		// Check stdout
+		strout := string(stdout.Bytes())
+		if !regexp.MustCompile("^" + tt.stdout + "$").MatchString(strout) {
+			t.Errorf("Want: %#v; Got: %#v", tt.stdout, strout)
+		}
+
 		// Check stderr
-		out := string(stderr.Bytes())
-		if out != tt.out {
-			t.Errorf("Want: %#v; Got: %#v", tt.out, out)
+		strerr := string(stderr.Bytes())
+		if !regexp.MustCompile("^" + tt.stderr + "$").MatchString(strerr) {
+			t.Errorf("Want: %#v; Got: %#v", tt.stderr, strerr)
 		}
 
 		// Check return code
