2e6e3bff65a5fcd1e79d4abff95a7bdc0fd84d0e
Ryan O'Leary <ryanoleary@google.com>
2017-02-03 18:23:42 -0800

    Implement sleep command

---
+++ cmds/sleep/sleep.go
@@ -0,0 +1,54 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Delay for the specified amount of time.
+//
+// Synopsis:
+//     sleep DURATION
+//
+// Description:
+//     If no units are given, the duration is assumed to be measured in
+//     seconds, otherwise any format parsed by Go's `time.ParseDuration` is
+//     accepted.
+//
+// Examples:
+//     sleep 2.5
+//     sleep 300ms
+//     sleep 2h45m
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+	"time"
+)
+
+var invalidDuration = errors.New("invalid duration")
+
+func parseDuration(s string) (time.Duration, error) {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		d, err = time.ParseDuration(s + "s")
+	}
+	if err != nil || d < 0 {
+		return time.Duration(0), invalidDuration
+	}
+	return d, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		log.Fatal("Incorrect number of arguments")
+	}
+
+	d, err := parseDuration(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(d)
+}
---
+++ cmds/sleep/sleep_test.go
@@ -0,0 +1,36 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseDuration(t *testing.T) {
+	var tests = []struct {
+		in  string
+		out time.Duration
+		err error
+	}{
+		{"", time.Duration(0), invalidDuration},
+		{"xyz", time.Duration(0), invalidDuration},
+		{"-2.5", time.Duration(0), invalidDuration},
+		{"-2.5s", time.Duration(0), invalidDuration},
+		{"2.5", time.Duration(2500 * time.Millisecond), nil},
+		{"2.5s", time.Duration(2500 * time.Millisecond), nil},
+		{"300m", time.Duration(300 * time.Minute), nil},
+		{"2h45m", time.Duration(2*time.Hour + 45*time.Minute), nil},
+	}
+
+	// Table-driven testing
+	for _, tt := range tests {
+		out, err := parseDuration(tt.in)
+		if out != tt.out || err != tt.err {
+			t.Errorf("parseDuration(%#v) = %v, %v; want %v, %v",
+				tt.in, out, err, tt.out, tt.err)
+		}
+	}
+}
