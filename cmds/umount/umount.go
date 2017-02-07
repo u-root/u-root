2f3e1c8babfbdcae23511d5675c96e6fe65ecdde
Gan Shun Lim <ganshun@google.com>
2017-02-07 11:00:13 -0800

    Umount implementation for Linux
    Signed-off-by: Gan Shun Lim <ganshun@google.com>

---
+++ cmds/umount/umount.go
@@ -0,0 +1,21 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Unmount a filesystem at the specified path.
+//
+// Synopsis:
+//     umount [-f | -l] PATH
+//
+// Options:
+//     -f: force unmount
+//     -l: lazy unmount
+package main
+
+import "log"
+
+func main() {
+	if err := umount(); err != nil {
+		log.Fatal(err)
+	}
+}
