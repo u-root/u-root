631a710dfcffc416f0b9eced832500a4ef07eb69
Ryan O'Leary <ryanoleary@google.com>
2017-02-10 11:43:13 -0800

    Improvements to ls command
    - The starting directory is printed as a dot.
    - List mode now prints device major, minor, group name and user name.
    - List and quotes mode can be used at the same time.
    - Unprintable characters and newlines are printed as a ? mark.
    - tests
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

---
+++ cmds/ls/fileinfo.go
@@ -0,0 +1,128 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"regexp"
+	"strings"
+	"syscall"
+	"time"
+)
+
+// From Linux header: /include/uapi/linux/kdev_t.h
+const (
+	minorBits = 8
+	minorMask = (1 << minorBits) - 1
+)
+
+// Matches characters which would interfere with ls's formatting.
+var unprintableRe = regexp.MustCompile("[[:cntrl:]\n]")
+
+// Since `os.FileInfo` is an interface, it is difficult to tweak some of its
+// internal values. For example, replacing the starting directory with a dot.
+// `extractImportantParts` populates our own struct which we can modify at will
+// before printing.
+type fileInfo struct {
+	name         string
+	mode         os.FileMode
+	major, minor uint64
+	uid, gid     uint32
+	size         int64
+	modTime      time.Time
+}
+
+func extractImportantParts(fi os.FileInfo) fileInfo {
+	s := fi.Sys().(*syscall.Stat_t)
+	return fileInfo{
+		name:    fi.Name(),
+		mode:    fi.Mode(),
+		major:   s.Rdev & minorMask,
+		minor:   s.Rdev >> minorBits,
+		uid:     s.Uid,
+		gid:     s.Gid,
+		size:    fi.Size(),
+		modTime: fi.ModTime(),
+	}
+}
+
+// Without this cache, `ls -l` is orders of magnitude slower.
+var (
+	uidCache = map[uint32]string{}
+	gidCache = map[uint32]string{}
+)
+
+// Convert uid to username, or return uid on error.
+func lookupUserName(id uint32) string {
+	if s, ok := uidCache[id]; ok {
+		return s
+	}
+	s := fmt.Sprint(id)
+	if u, err := user.LookupId(s); err == nil {
+		s = u.Username
+	}
+	uidCache[id] = s
+	return s
+}
+
+// Convert gid to group name, or return gid on error.
+func lookupGroupName(id uint32) string {
+	if s, ok := gidCache[id]; ok {
+		return s
+	}
+	s := fmt.Sprint(id)
+	if g, err := user.LookupGroupId(s); err == nil {
+		s = g.Name
+	}
+	gidCache[id] = s
+	return s
+}
+
+// The default stringer. Return only the filename. Unprintable characters are
+// replaced with '?'.
+func (fi fileInfo) String() string {
+	return unprintableRe.ReplaceAllLiteralString(fi.name, "?")
+}
+
+// Two alternative stringers
+type quotedStringer struct {
+	fileInfo
+}
+type longStringer struct {
+	fileInfo
+	comp fmt.Stringer // decorator pattern
+}
+
+// Return the name surrounded by quotes with escaped control characters.
+func (fi quotedStringer) String() string {
+	return fmt.Sprintf("%#v", fi.name)
+}
+
+// The long and quoted stringers can be combined like so:
+//     longStringer{fi, quotedStringer{fi}}
+func (fi longStringer) String() string {
+	// Golang's FileMode.String() is almost sufficient, except we would
+	// rather use b and c for devices.
+	replacer := strings.NewReplacer("Dc", "c", "D", "b")
+
+	// Ex: crw-rw-rw-  root  root  1, 3  Feb 6 09:31  null
+	pattern := "%[1]s\t%[2]s\t%[3]s\t%[4]d, %[5]d\t%[7]v\t%[8]s"
+	if fi.major == 0 && fi.minor == 0 {
+		// Ex: -rw-rw----  myuser  myuser  1256  Feb 6 09:31  recipes.txt
+		pattern = "%[1]s\t%[2]s\t%[3]s\t%[6]d\t%[7]v\t%[8]s"
+	}
+
+	return fmt.Sprintf(pattern,
+		replacer.Replace(fi.mode.String()),
+		lookupUserName(fi.uid),
+		lookupGroupName(fi.gid),
+		fi.minor,
+		fi.major,
+		fi.size,
+		fi.modTime.Format("Jan _2 15:04"),
+		fi.comp.String())
+}
---
+++ cmds/ls/ls_test.go
@@ -0,0 +1,74 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/u-root/u-root/shared/testutil"
+)
+
+var tests = []struct {
+	flags []string
+	out   string
+}{
+	{
+		flags: []string{},
+		out: `.
+d1
+f1
+f2
+f3?line 2
+`,
+	}, {
+		flags: []string{"-Q"},
+		out: `"."
+"d1"
+"f1"
+"f2"
+"f3\nline 2"
+`,
+	}, {
+		flags: []string{"-R"},
+		out: `.
+d1
+d1/f4
+f1
+f2
+f3?line 2
+`,
+	},
+}
+
+func TestLs(t *testing.T) {
+	tmpDir, execPath := testutil.CompileInTempDir(t)
+	defer os.RemoveAll(tmpDir)
+
+	// Create an empty directory.
+	testDir := filepath.Join(tmpDir, "testDir")
+	os.Mkdir(testDir, 0700)
+	os.Chdir(testDir)
+
+	// Create some files.
+	os.Create("f1")
+	os.Create("f2")
+	os.Create("f3\nline 2")
+	os.Mkdir("d1", 0740)
+	os.Create("d1/f4")
+
+	// Table-driven testing
+	for _, tt := range tests {
+		out, err := exec.Command(execPath, tt.flags...).Output()
+		if err != nil {
+			t.Error(err)
+		}
+		if string(out) != tt.out {
+			t.Errorf("got:\n%s\nwant:\n%s", string(out), tt.out)
+		}
+	}
+}
