939b99ac8c6600359989ea6ded460c3d6cc13f37
Ryan O'Leary <ryanoleary@google.com>
2017-02-10 11:43:13 -0800

    Improvements to ls command
    - The starting directory is printed as a dot.
    - List mode now prints device major, minor, group name and user name.
    - List and quotes mode can be used at the same time.
    - Unprintable characters and newlines are printed as a ? mark.
    - tests
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- cmds/ls/ls.go
+++ cmds/ls/ls.go
@@ -8,67 +8,93 @@
 //     ls [OPTIONS] [DIRS]...
 //
 // Options:
-//     -l: Long form.
-//     -r: raw (%v) form
-//     -R: recurse
+//     -l: long form
+//     -Q: quoted
+//     -R: equivalent to findutil's find
+//
+// Bugs:
+//     With the `-R` flag, directories are only ever printed once.
 package main
 
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"text/tabwriter"
 )
 
 var (
-	long      = flag.Bool("l", false, "Long form")
-	raw       = flag.Bool("r", false, "raw struct")
-	recursive = flag.Bool("R", false, "Recurse")
+	long    = flag.Bool("l", false, "long form")
+	quoted  = flag.Bool("Q", false, "quoted")
+	recurse = flag.Bool("R", false, "equivalent to findutil's find")
 )
 
-func show(fullpath string, fi os.FileInfo) {
-	switch {
-	case *raw == true:
-		fmt.Printf("%v\n", fi)
-	case *long == false:
-		fmt.Printf("%v\n", fi.Name())
-	// -rw-r--r-- 1 root root 174 Aug 18 17:18 /etc/hosts
-	case *long == true:
-		fmt.Printf("%v\t%v\t%v\t%v", fi.Mode(), fi.Size(), fi.Name(), fi.ModTime())
-		if link, err := os.Readlink(fullpath); err == nil {
-			fmt.Printf(" -> %v", link)
-		}
-		fmt.Printf("\n")
+func stringer(fi fileInfo) fmt.Stringer {
+	var s fmt.Stringer = fi
+	if *quoted {
+		s = quotedStringer{fi}
 	}
+	if *long {
+		s = longStringer{fi, s}
+	}
+	return s
+}
 
+func listDir(d string, w io.Writer) error {
+	return filepath.Walk(d, func(path string, osfi os.FileInfo, err error) error {
+		// Soft error. Useful when a permissions are insufficient to
+		// stat one of the files.
+		if err != nil {
+			log.Printf("%s: %v\n", path, err)
+			return nil
+		}
+
+		fi := extractImportantParts(osfi)
+
+		if *recurse {
+			// Mimic find command
+			fi.name = path
+		} else if path == d {
+			// Starting directory is a dot when non-recursive
+			fi.name = "."
+		}
+
+		// Print the file in the proper format.
+		fmt.Fprintln(w, stringer(fi))
+
+		// Skip directories when non-recursive.
+		if path != d && fi.mode.IsDir() && !*recurse {
+			return filepath.SkipDir
+		}
+		return nil
+	})
 }
 
 func main() {
 	flag.Parse()
 
-	dirs := flag.Args()
+	// Write output in tabular form.
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 0, 1, ' ', 0)
+	defer w.Flush()
 
+	// Array of directories to list.
+	dirs := flag.Args()
 	if len(dirs) == 0 {
 		dirs = []string{"."}
 	}
-	for _, v := range dirs {
-		if len(dirs) > 1 {
-			fmt.Printf("%v:\n", v)
-		}
-		err := filepath.Walk(v, func(path string, fi os.FileInfo, err error) error {
-			if err != nil {
-				fmt.Printf("%v: %v\n", path, err)
-				return err
-			}
-			show(path, fi)
-			if fi.IsDir() && !*recursive && path != v {
-				return filepath.SkipDir
-			}
 
-			return err
-		})
-		if err != nil {
-			fmt.Printf("%s: %v\n", v, err)
+	// List each directory in its own section.
+	for _, d := range dirs {
+		if len(dirs) > 1 {
+			fmt.Printf("%s:\n", d)
 		}
+		if err := listDir(d, w); err != nil {
+			log.Printf("error while listing %#v: %v", d, err)
+		}
+		w.Flush()
 	}
 }
