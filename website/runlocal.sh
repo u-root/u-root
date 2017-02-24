836eb6a507582a2de6b56c87f127bf7f1bd95a5d
Ryan O'Leary <ryanoleary@google.com>
2017-02-23 16:25:13 -0800

    Update runlocal.sh and add exec bit
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- website/runlocal.sh
+++ website/runlocal.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-[ x"$U-root" != x ] && cd $U-root/web
-mkdocs serve && exit 0 
+# Must run in website directory.
+mkdocs serve && exit 0
 echo "#### we need mkdocs: use pip install mkdocs"
-
-
