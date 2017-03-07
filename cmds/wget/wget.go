ad0880b041d4d8b5c5ee1037cb0923f58e5820ff
Ryan O'Leary <ryanoleary@google.com>
2017-03-06 18:14:14 -0800

    Add improvements to the wget command
    - More complete man page
    - Return 1 if the HTTP status is not 200
    - `-O` flag
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- cmds/wget/wget.go
+++ cmds/wget/wget.go
@@ -5,17 +5,34 @@
 // Wget reads one file from a url and writes to stdout.
 //
 // Synopsis:
-//     wget URL
+//     wget [ARGS] URL
+//
+// Description:
+//     Returns a non-zero code on failure.
+//
+// Options:
+//     -O: output filename, defaults to '-' (stdout)
+//
+// Notes:
+//     There are a few differences with GNU wget:
+//     - `-O` defaults to `-`.
+//     - The protocol (http/https) is mandatory.
 //
 // Example:
-//     wget http://google.com/ | tee e100.html
+//     wget -O e100.html http://google.com/
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+)
+
+var (
+	outFilename = flag.String("O", "-", "output filename, '-' for stdout")
 )
 
 func wget(arg string, w io.Writer) error {
@@ -24,18 +41,37 @@ func wget(arg string, w io.Writer) error {
 		return err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("non-200 HTTP status: %d", resp.StatusCode)
+	}
 	_, err = io.Copy(w, resp.Body)
-
 	return nil
 }
 
+func usage() {
+	log.Printf("Usage: %s [ARGS] URL\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
 func main() {
-	// TODO: use flags library
-	if len(os.Args) < 2 {
-		os.Exit(1)
+	flag.Parse()
+	if flag.NArg() != 1 {
+		usage()
 	}
 
-	if err := wget(os.Args[1], os.Stdout); err != nil {
+	url := flag.Arg(0)
+	w := os.Stdout
+	if *outFilename != "-" {
+		var err error
+		w, err = os.Create(*outFilename)
+		if err != nil {
+			log.Fatalln("Cannot create file:", err)
+		}
+		defer w.Close()
+	}
+
+	if err := wget(url, w); err != nil {
 		log.Fatalf("%v\n", err)
 	}
 }
