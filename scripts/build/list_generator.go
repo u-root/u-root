245bbb7437068a85d7f28469382918aa1cd277c4
Ryan O'Leary <ryan@ryanoleary.ca>
2017-04-27 05:27:42 -0400

    Replace []byte with io.Reader for file data
    This prevents too many large files from filling up memory and OOM the
    process. Files stick on the disk until they are needed. This might cause
    a problem with too many open fds, but we'll see.
    
    Signed-off-by: Ryan O'Leary <ryan@ryanoleary.ca>

--- scripts/build/list_generator.go
+++ scripts/build/list_generator.go
@@ -7,6 +7,7 @@ package build
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 )
 
 func init() {
@@ -19,12 +20,20 @@ type listGenerator struct {
 // Rather than creating an archive, this simply outputs a list for debugging.
 func (g listGenerator) generate(config Config, files []file) error {
 	totalSize := 0
+	// TODO: use "text/tabwriter" for nicer alignment
 	for _, f := range files {
-		if f.rdev == 0 {
-			fmt.Printf("%v\t%d\t%q\n", f.mode, len(f.data), f.path)
-			totalSize += len(f.data)
-		} else {
+		if f.data != nil {
+			// TODO: Can we get the length of a Reader without reading its entirety into memory?
+			data, err := ioutil.ReadAll(f.data)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%v\t%d\t%q\n", f.mode, len(data), f.path)
+			totalSize += len(data)
+		} else if f.rdev != 0 {
 			fmt.Printf("%v\t%d, %d\t%q\n", f.mode, major(f.rdev), minor(f.rdev), f.path)
+		} else {
+			fmt.Printf("%v\t\t%q\n", f.mode, f.path)
 		}
 	}
 	fmt.Println("Number of files:", len(files))
