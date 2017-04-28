69bf1816cf0dfa58ce013570cf1ea1858254d40c
ron minnich <rminnich@gmail.com>
2017-04-28 06:55:09 -0700

    Revert "Remove linux-specific types"
    This reverts commit 9c913f82b0d24abdbdf1ee5f4c5319427979a5fa.

--- scripts/build/types.go
+++ scripts/build/types.go
@@ -32,27 +32,6 @@ type file struct {
 	rdev uint64
 }
 
-// From Linux header: /include/uapi/linux/kdev_t.h
-const (
-	minorBits = 8
-	minorMask = (1 << minorBits) - 1
-)
-
-// dev returns the device number given the major and minor numbers.
-func dev(major, minor uint64) uint64 {
-	return major<<minorBits + minor
-}
-
-// major returns the device number's major number.
-func major(dev uint64) uint64 {
-	return dev >> minorBits
-}
-
-// minor returns the device number's minor number.
-func minor(dev uint64) uint64 {
-	return dev & minorMask
-}
-
 // Generate files for inclusion into the archive.
 type builder interface {
 	generate(Config) ([]file, error)
