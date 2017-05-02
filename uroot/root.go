dd2c4374e7ba2f8a3929a22507d7898b8fc90f7b
Ronald G. Minnich <rminnich@google.com>
2017-05-02 15:10:54 -0700

    WIP: ramfs and installcommand: support external commands
    External commands like /src/github.com/elves/elvish
    don't follow standard rules.
    
    This change allows finding the source if it's not
    in the standard place. I also took the opportunity to
    tidy things up a bit and also remove what looked like an error,
    namely if the command was found but noforce was set, we'd do
    nothing, which is not correct.
    
    For testing the build on darwin, I also moved some things
    around in uroot/. These are not seperate out but I hope
    that's ok.
    
    But I can now do this:
    go run scripts/ramfs.go -test 'src/github.com/u-root/u-root/cmds/[a-z]*' src/github.com/elves/elvish
    
    And I get the elvish shell if I just type
    elvish
    
    I took the opportunity to do a lot of cleanup. The text/template stuff had to go.
    It was really essential in the early Go days, when there were lots of names like 5c, etc.
    but it had no value now. Some usages of cpio would not work on OSX, due to weird OSX
    naming around /tmp and their oddball not-quite-symlinks, and this fixes that problem.
    I still can't quite build an initramfs on OSX that works, but that's for another day.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- uroot/root.go
+++ uroot/root.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux
+
 // package uroot contains various functions that might be needed more than
 // one place.
 package uroot
