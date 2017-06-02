6c6bd5e9ba78c64d370b57959d2fa2e8153301b0
Ronald G. Minnich <rminnich@gmail.com>
2017-06-02 16:57:13 -0700

    add genvendor framework
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/pci/genvendor.go
@@ -0,0 +1,14 @@
+
+// Copyright 2012-2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("done\n")
+}
