afbf291121af51dde057f9fb61a2ae9a36b0e04c
Christopher Koch <chrisko@google.com>
2017-06-28 12:24:49 -0700

    Make kexec compatible with standard Linux kexec(8).
    Signed-off-by: Christopher Koch <chrisko@google.com>

--- cmds/kexec/kexec.go
+++ cmds/kexec/kexec.go
@@ -5,95 +5,139 @@
 // kexec executes a new kernel over the running kernel (u-root).
 //
 // Synopsis:
-//     kexec [--dryrun] [--cmdline=] [--i=] [FILES...]
+//     kexec [--initrd=FILE] [--command-line=STRING] [-l] [-e] KERNELIMAGE
 //
 // Description:
 //     This is only intended to be used with kexec_load_files, not the older
 //     kexec.
 //
 // Options:
-//     -dryrun:   do not do kexec system calls
-//     --cmdline: command line for kernel
-//     --i:       initramfs
+//     --cmdline=STRING:       command line for kernel
+//     --command-line=STRING:  command line for kernel
+//     --append=STRING:        command line for kernel
+//
+//     --reuse-commandline:    reuse command line from running system
+//
+//     --i=FILE:       initramfs file
+//     --initrd=FILE:  initramfs file
+//     --ramdisk=FILE: initramfs file
+//
+//     -l or --load:   only load the kernel
+//     -e or --exec:   reboot with the currently loaded kernel
 package main
-
-// N.B. /**/ comments are verbatim from uapi/linux/kexec.h.
-/* kexec system call -  It loads the new kernel to boot into.
- * kexec does not sync, or unmount filesystems so if you need
- * that to happen you need to do that yourself.
- */
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"syscall"
 	"unsafe"
 )
 
-/* kexec flags for different usage scenarios */
+type options struct {
+	cmdline      string
+	reuseCmdline bool
+	initramfs    string
+	load         bool
+	exec         bool
+}
+
+func registerFlags(f *flag.FlagSet) *options {
+	o := &options{}
+	f.StringVar(&o.cmdline, "cmdline", "", "Set the kernel command line")
+	f.StringVar(&o.cmdline, "command-line", "", "Set the kernel command line")
+	f.StringVar(&o.cmdline, "append", "", "Set the kernel command line")
+
+	f.BoolVar(&o.reuseCmdline, "reuse-cmdline", false, "Use the kernel command line from running system")
+
+	f.StringVar(&o.initramfs, "i", "", "Use file as the kernel's initial ramdisk")
+	f.StringVar(&o.initramfs, "initrd", "", "Use file as the kernel's initial ramdisk")
+	f.StringVar(&o.initramfs, "ramdisk", "", "Use file as the kernel's initial ramdisk")
+
+	f.BoolVar(&o.load, "l", false, "Load the new kernel into the current kernel.")
+	f.BoolVar(&o.load, "load", false, "Load the new kernel into the current kernel.")
+
+	f.BoolVar(&o.exec, "e", false, "Execute a currently loaded kernel.")
+	f.BoolVar(&o.exec, "exec", false, "Execute a currently loaded kernel.")
+	return o
+}
+
+// Syscall number for file kexec.
+const _SYS_KEXEC_FILE_LOAD = 320
+
+// Linux kexec syscall flags.
 const (
-	KEXEC_FILE_UNLOAD       = 0x1
-	KEXEC_FILE_ON_CRASH     = 0x2
-	KEXEC_FILE_NO_INITRAMFS = 0x4
+	_KEXEC_FILE_UNLOAD       = 0x1
+	_KEXEC_FILE_ON_CRASH     = 0x2
+	_KEXEC_FILE_NO_INITRAMFS = 0x4
 )
 
-var (
-	dryrun        = flag.Bool("dryrun", false, "Do not do kexec system calls")
-	cmdline       = flag.String("cmdline", "", "Command line for kernel")
-	initramfs     = flag.String("i", "", "initramfs")
-	kern      int = -1
-	ramfs     int = -1
-	flags     uintptr
-)
+func kexec(kernelfd, ramfsfd int, cmdline string, flags int) error {
+	cmd := append([]byte(cmdline), 0)
+	if e1, e2, errno := syscall.Syscall6(
+		_SYS_KEXEC_FILE_LOAD,
+		uintptr(kernelfd),
+		uintptr(ramfsfd),
+		uintptr(len(cmd)),
+		uintptr(unsafe.Pointer(&cmd[0])),
+		uintptr(flags),
+		0); errno != 0 {
+		return fmt.Errorf("sys_kexec(%d, %d, %s, %x) = (%d, %d, errno %v)", kernelfd, ramfsfd, cmdline, flags, e1, e2, errno)
+	}
+	return nil
+}
 
 func main() {
-	var err error
-	var b []byte
-	var l uintptr
-
+	opts := registerFlags(flag.CommandLine)
 	flag.Parse()
+
 	if len(flag.Args()) < 1 {
 		flag.PrintDefaults()
 		log.Fatalf("usage: kexec [flags] kernelname")
 	}
 	kernel := flag.Args()[0]
 
-	if *cmdline != "" {
-		b = append(b, []byte(*cmdline)...)
-		l = uintptr(len(b)) + 1
-	} else {
-		b, err = ioutil.ReadFile("/proc/cmdline")
+	if opts.cmdline != "" && opts.reuseCmdline {
+		flag.PrintDefaults()
+		log.Fatalf("--reuse-cmdline and other command line options are mutually exclusive")
+	}
+
+	if opts.load == false && opts.exec == false {
+		opts.load = true
+		opts.exec = true
+	}
+
+	cmdline := opts.cmdline
+	if opts.reuseCmdline {
+		procCmdline, err := ioutil.ReadFile("/proc/cmdline")
 		if err != nil {
 			log.Fatalf("%v", err)
 		}
-		b[len(b)-1] = 0
-		l = uintptr(len(b))
+		cmdline = string(procCmdline)
 	}
 
-	p := uintptr(unsafe.Pointer(&b[0]))
+	if opts.load {
+		var flags int
+		log.Printf("Loading %s for kernel\n", kernel)
+		kernelfd, err := syscall.Open(kernel, syscall.O_RDONLY, 0)
+		if err != nil {
+			log.Fatalf("open(%s): %v", kernel, err)
+		}
 
-	log.Printf("Loading %v\n", kernel)
+		ramfsfd, err := syscall.Open(opts.initramfs, syscall.O_RDONLY, 0)
+		if err != nil {
+			flags |= _KEXEC_FILE_NO_INITRAMFS
+		}
 
-	if kern, err = syscall.Open(kernel, syscall.O_RDONLY, 0); err != nil {
-		log.Fatalf("%v", err)
+		if err := kexec(kernelfd, ramfsfd, cmdline, flags); err != nil {
+			log.Fatalf("%v", err)
+		}
 	}
 
-	if ramfs, err = syscall.Open(*initramfs, syscall.O_RDONLY, 0); err != nil {
-		flags |= KEXEC_FILE_NO_INITRAMFS
-	}
-
-	log.Printf("command line: '%v'", string(b))
-	log.Printf("%v %v %v %v %v %v", 320, uintptr(kern), uintptr(ramfs), p, l, flags)
-	if *dryrun {
-		log.Printf("Dry run -- exiting now")
-		return
-	}
-	if e1, e2, err := syscall.Syscall6(320, uintptr(kern), uintptr(ramfs), l, p, flags, uintptr(0)); err != 0 {
-		log.Fatalf("a %v b %v err %v", e1, e2, err)
-	}
-
-	if e1, e2, err := syscall.Syscall6(syscall.SYS_REBOOT, syscall.LINUX_REBOOT_MAGIC1, syscall.LINUX_REBOOT_MAGIC2, syscall.LINUX_REBOOT_CMD_KEXEC, 0, 0, 0); err != 0 {
-		log.Fatalf("a %v b %v err %v", e1, e2, err)
+	if opts.exec {
+		if e1, e2, errno := syscall.Syscall(syscall.SYS_REBOOT, syscall.LINUX_REBOOT_MAGIC1, syscall.LINUX_REBOOT_MAGIC2, syscall.LINUX_REBOOT_CMD_KEXEC); errno != 0 {
+			log.Fatalf("sys_reboot(..., kexec) = (%d, %d, errno %v)", e1, e2, errno)
+		}
 	}
 }
