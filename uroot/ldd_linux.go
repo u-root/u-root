9d0e5ee1cfb8ddf74b3619f55c8df34c781298d5
Ronald G. Minnich <rminnich@gmail.com>
2017-07-01 22:21:00 -0700

    ldd: working ldd command
    The format is not the same as GNU ldd.
    We don't add the useless decorations such as =>; don't add
    the useless information about where it's mapped (in ASLR world,
    who cares?); and just show the full path. The output
    is hence far easier to parse by programs.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- uroot/ldd_linux.go
+++ uroot/ldd_linux.go
@@ -21,28 +21,44 @@ package uroot
 
 import (
 	"debug/elf"
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
+type FileInfo struct {
+	FullName string
+	os.FileInfo
+}
+
 // Follow starts at a pathname and adds it
 // to a map if it is not there.
 // If the pathname is a symlink, indicated by the Readlink
 // succeeding, links repeats and continues
 // for as long as the name is not found in the map.
-// It is not an error for a file not to exist; sometimes
-// links point off into the air intentionally.
-func follow(l string, names map[string]bool) {
+func follow(l string, names map[string]*FileInfo) error {
 	for {
-		if names[l] {
-			return
+		if names[l] != nil {
+			return nil
 		}
-		names[l] = true
+		i, err := os.Lstat(l)
+		if err != nil {
+			return fmt.Errorf("%v", err)
+		}
+
+		names[l] = &FileInfo{FullName: l, FileInfo: i}
+		if i.Mode().IsRegular() {
+			return nil
+		}
+		// If it's a symlink, the read works; if not, it fails.
+		// we can skip testing the type, since we still have to
+		// handle any error if it's a link.
 		next, err := os.Readlink(l)
 		if err != nil {
-			return
+			return err
 		}
 		// It may be a relative link, so we need to
 		// make it abs.
@@ -87,11 +103,11 @@ func runinterp(interp, file string) ([]string, error) {
 // It's not an error for a file to not be an ELF, as
 // this function should be convenient and the list might
 // include non-ELF executables (a.out format, scripts)
-func Ldd(names []string) ([]string, error) {
+func Ldd(names []string) ([]*FileInfo, error) {
 	var (
-		list    = make(map[string]bool)
-		interps = make(map[string]bool)
-		libs    []string
+		list    = make(map[string]*FileInfo)
+		interps = make(map[string]*FileInfo)
+		libs    []*FileInfo
 	)
 	for _, n := range names {
 		r, err := os.Open(n)
@@ -122,9 +138,11 @@ func Ldd(names []string) ([]string, error) {
 		interp := string(i[:len(i)-1])
 		// We could just append the interp but people
 		// expect to see that first.
-		if !interps[interp] {
-			interps[interp] = true
-			libs = append(libs, interp)
+		if interps[interp] == nil {
+			err := follow(interp, interps)
+			if err != nil {
+				return nil, err
+			}
 		}
 		// oh boy. Now to run the interp and get more names.
 		n, err := runinterp(interp, n)
@@ -132,13 +150,31 @@ func Ldd(names []string) ([]string, error) {
 			return nil, err
 		}
 		for i := range n {
-			follow(n[i], list)
+			if err := follow(n[i], list); err != nil {
+				log.Fatalf("ldd: %v", err)
+			}
 		}
 	}
 
+	for i := range interps {
+		libs = append(libs, interps[i])
+	}
+
 	for i := range list {
-		libs = append(libs, i)
+		libs = append(libs, list[i])
 	}
 
 	return libs, nil
+}
+
+func LddList(names []string) ([]string, error) {
+	var list []string
+	l, err := Ldd(names)
+	if err != nil {
+		return nil, err
+	}
+	for i := range l {
+		list = append(list, l[i].FullName)
+	}
+	return list, nil
 }
