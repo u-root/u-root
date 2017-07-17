63816c30cf97de1871c17a266aa83d49e50545c1
Ronald G. Minnich <rminnich@gmail.com>
2017-07-17 16:35:49 -0700

    bb: create dircomponents of extra paths
    this may be the last bit of having to generate the intermediate
    paths.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- bb/ramfs.go
+++ bb/ramfs.go
@@ -50,6 +50,21 @@ func sanity() {
 	}
 }
 
+// dirComponents takes a string and returns an array of strings,
+// such that we can create the directory records for intermediate
+// directories.
+func dirComponents(dir string) []string {
+	var dirlist []string
+	if filepath.Dir(dir) == "." {
+		return []string{}
+	}
+	for d := filepath.Dir(dir); d != "/"; d = filepath.Dir(d) {
+		dirlist = append([]string{d}, dirlist...)
+	}
+	dirlist = append(dirlist, dir)
+	return dirlist
+}
+
 // copyCommands takes a list of commands, generates the list of libs,
 // and creates cpio records, including directory records.
 func copyCommands(w cpio.Writer, cmds []string) {
@@ -62,12 +77,7 @@ func copyCommands(w cpio.Writer, cmds []string) {
 	cmds = append(cmds, libs...)
 	for _, n := range cmds {
 		debug("copyCommands: file %v", n)
-		var dirlist []string
-		for d := filepath.Dir(n); d != "/"; d = filepath.Dir(d) {
-			dirlist = append([]string{d}, dirlist...)
-		}
-		dirlist = append(dirlist, n)
-		for _, n := range dirlist {
+		for _, n := range dirComponents(n) {
 			debug("copyCommands: %v", n)
 			r, err := cpio.GetRecord(n)
 			if err != nil {
@@ -116,6 +126,23 @@ func ramfs() {
 	// For all the 'roots' in paths, start walking at the name.
 	debug("PATHS: %v", paths)
 	for r, list := range paths {
+		debug("PATHS: root %v", r)
+		// we need to make all the path prefix directories.
+		for _, n := range list {
+			debug("\troot %v, name %v", r, n)
+			for _, d := range dirComponents(n) {
+				debug("\t\troot %v, name %v, component %v", r, n, d)
+				rec, err := cpio.GetRecord(d)
+				if err != nil {
+					log.Fatalf("Getting record of %q failed: %v", d, err)
+				}
+				recs := []cpio.Record{rec}
+				cpio.MakeReproducible(recs)
+				if err := w.WriteRecords(recs); err != nil {
+					log.Fatalf("%v\n", err)
+				}
+			}
+		}
 		for _, n := range list {
 			if err := filepath.Walk(filepath.Join(r, n), func(name string, fi os.FileInfo, err error) error {
 				if err != nil {
