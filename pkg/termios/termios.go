83e2a6c13b6a3267bed613e9ea2a589d36fa8daf
Ronald G. Minnich <rminnich@gmail.com>
2017-07-20 08:30:46 -0700

    termios package
    Termios is the full termios struct; WinSize is just the window size.
    
    New() gets a new Termios; WinSize gets a new Winsize; Raw is like
    New() but sets raw mode and returns a Termios that can be used to
    go back to the previous mode.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ pkg/termios/termios.go
@@ -0,0 +1,28 @@
+// Copyright 2015-2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package termios implements basic termios operations including getting
+// a tty struct, termio struct, a winsize struct, and setting raw mode.
+// To get a TTY, call termios.New.
+// To get a Termios, call tty.Get(); to set it, call tty.Set(*Termios)
+// To set raw mode and then restore, one can do:
+// tty := termios.NewTTY()
+// restorer, err := tty.Raw()
+// do things
+// tty.Set(restorer)
+package termios
+
+func (t *TTY) Raw() (*Termios, error) {
+	restorer, err := t.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	raw := MakeRaw(restorer)
+
+	if err := t.Set(raw); err != nil {
+		return nil, err
+	}
+	return restorer, nil
+}
