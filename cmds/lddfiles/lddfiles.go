91759a5e4d905d241c296ba2a5b858644abe447d
Ronald G. Minnich <rminnich@gmail.com>
2017-07-24 12:50:24 -0700

    lddfiles: print dependencies of executables
    We've adopted a principle for u-root: if a command has the same name
    as a gnubin command it should act like that command.
    
    Given that ld.so does what we want, and ldd as defined by gnu is
    not that useful in many ways, we're moving ldd to lddfiles and making it useful.
    
    lddfiles will print out the executables and all dependencies, suitable for
    use in scripts:
    scp `lddfiles /bin/date` root@host:/
    or for cppio
    lddfiles `which xterm` | cpio -H newc -o > /tmp/x
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/lddfiles/lddfiles.go
@@ -0,0 +1,52 @@
+// Copyright 2009-2017 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// lddfiles prints the arguments and all .so dependencies of those arguments
+//
+// Description:
+//	lddfiles prints the arguments on the command line and all .so's
+//	on which they depend. In some cases, those .so's are actually symlinks;
+//	in that case, the symlink and its value are printed.
+//	lddfiles can be used to package up a command for tranporation to
+//	another machine, e.g.
+//	scp `lddfiles /usr/bin/*` remotehost:/
+//	will let you copy all of /usr/bin, and all needed libraries. to a remote
+//	host.
+//	lddfiles /usr/bin/* | cpio -H newc -o > /tmp/x.cpio
+//	lets you easily prepare cpio archives, which can be included in a kernel
+//	or similarly scp'ed to another machine.
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/u-root/u-root/pkg/ldd"
+)
+
+func usage() {
+	log.Fatalf("usage: lddfiles file [file...]")
+}
+
+func ldd(o io.Writer, s ...string) error {
+	l, err := uroot.Ldd(s)
+	if err != nil {
+		return err
+	}
+	for i := range s {
+		fmt.Fprintf(o, "%s\n", s[i])
+	}
+	for i := range l {
+		fmt.Fprintf(o, "%s\n", l[i].FullName)
+	}
+	return nil
+}
+
+func main() {
+	if err := ldd(os.Stdout, os.Args[1:]...); err != nil {
+		log.Fatalf("ldd: %v", err)
+	}
+}
