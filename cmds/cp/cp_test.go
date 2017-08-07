7e8a36b7d5d9c8973232e9113ed874b73f01c38a
Ananya Joshi <ananya@ananya.co>
2017-08-07 09:07:31 -0700

    changed path.Join to filepath.Join

--- cmds/cp/cp_test.go
+++ cmds/cp/cp_test.go
@@ -12,6 +12,7 @@ import (
 	"math/rand"
 	"os"
 	"path"
+	"path/filepath"
 	"reflect"
 	"testing"
 	"time"
@@ -94,7 +95,7 @@ func readDirs(pathToRead string, mapFiles map[string][]string) error {
 	for _, file := range pathFiles {
 		fname := file.Name()
 		_, exists := mapFiles[fname]
-		fpath := path.Join(pathToRead, fname)
+		fpath := filepath.Join(pathToRead, fname)
 		if !exists {
 			slc := []string{fpath}
 			mapFiles[fname] = slc
@@ -200,7 +201,7 @@ func TestCpSimple(t *testing.T) {
 	srcFpath := f.Name()
 
 	dstFname := fmt.Sprintf("cpfile_%v_dst_copied", indentifier)
-	dstFpath := path.Join(tempDir, dstFname)
+	dstFpath := filepath.Join(tempDir, dstFname)
 
 	if err := copyFile(srcFpath, dstFpath, false); err != nil {
 		t.Fatalf("copyFile %v -> %v failed: %v", srcFpath, dstFpath, err)
@@ -280,7 +281,7 @@ func TestCpRecursiveNew(t *testing.T) {
 		t.Fatalf("cannot create files tree on directory %q: %v", srcTest, err)
 	}
 
-	dstTest := path.Join(tempDir, dstPrefix)
+	dstTest := filepath.Join(tempDir, dstPrefix)
 	copyFile(srcTest, dstTest, false)
 	isEqual, err := isEqualTree(srcTest, dstTest)
 	if err != nil {
@@ -340,7 +341,7 @@ func TestCpRecursiveMultiple(t *testing.T) {
 	}
 	for _, src := range srcDirs {
 		_, srcFile := path.Split(src)
-		dst := path.Join(dstTest, srcFile)
+		dst := filepath.Join(dstTest, srcFile)
 		if equal, err := isEqualTree(src, dst); !equal || err != nil {
 			t.Fatalf("The copy %q -> %q failed, trees are different", src, dst)
 		}
