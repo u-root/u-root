9e5ea2ec81c3fa42356406b984bd06680d3e6ddf
laconicpneumonic <tonyprolland@gmail.com>
2017-08-17 16:21:02 -0700

    Write initial spec of ID

---
+++ cmds/id/id.go
@@ -0,0 +1,23 @@
+// Copyright 2013-2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Print process information.
+//
+// Synopsis:
+//     id
+//
+// Description:
+//     id displays the uid, guid and groups of the calling process
+//
+// Options:
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
