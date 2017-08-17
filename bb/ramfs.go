fda40d8eb9acf200ab2ff64fd61f8c2a51b907e9
Ananya Joshi <ananya@ananya.co>
2017-08-17 10:30:05 -0700

    removed unnecessary comments

--- bb/ramfs.go
+++ bb/ramfs.go
@@ -29,9 +29,6 @@ var (
 	// starting point for the walk as lib/modules/4.04. That way we only preserve
 	// as much of the path as we need, but we can preserve it all.
 	paths = map[string][]string{}
-	/*extraPaths = flag.String("extra", "", `Extra paths to add in the form root:start, e.g. /:etc/hosts.
-	The path before the : is used as a starting point for a walk; the path after the : selects what things to put
-	into the initramfs starting at /. E.g., /tmp/prototype:/ will install the prototype file system into / of the initramfs`)*/	
 	extraCmds = flag.String("cmds", "", "Extra commands to add (full path, comma-separated string)")
 	extraCpio = flag.String("cpio", "", "A list of cpio archives to include in the output")
 )
