f8fd90be9be1aae49c12c0e939a90734f5676c64
laconicpneumonic <tonyprolland@gmail.com>
2017-08-17 18:44:13 -0700

    Parse /etc/group for ID num and name

--- cmds/id/id.go
+++ cmds/id/id.go
@@ -14,12 +14,76 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"syscall"
 )
 
-var ()
+var (
+	GROUP_FILE  = "/etc/group"
+	PASSWD_FILE = "/etc/passwd"
+)
+
+type Group struct {
+	Name   string
+	Number int
+}
+
+type User struct {
+	Name   string
+	Uid    int
+	Euid   int
+	Groups []*Group
+}
+
+func (u *User) getUid() {
+	u.Uid = syscall.Getuid()
+}
+
+func (u *User) getEuid() {
+	u.Uid = syscall.Getieuid()
+}
+
+func (u *User) getGroups() {
+	groupsNumbers, err := syscall.Getgroups()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	groupsMap := readGroups()
+
+	for _, groupNum := range groupsNumbers {
+		u.Groups = append(u.Groups, Group{
+			Name:   groupsMap[groupNum],
+			Number: groupNum,
+		})
+	}
+
+}
+
+func readGroups() (map[int]string, error) {
+	groupFile, err := os.Open(GROUP_FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	var line string
+	var groupInfo []string
+
+	groupsMap := make(map[int]string)
+	groupScanner := bufio.NewScanner(groupFile)
+
+	for groupScanner.Scan() {
+		groupInfo = strings.Split(groupScanner.Text(), ":")
+		groupsMap[strconv.Atoi(groupInfo[2])] = groupInfo[0]
+	}
+
+	return groupMap, nil
+}
 
 func main() {
 	uid := syscall.Getuid()
