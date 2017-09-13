bdf88b4f92a0dd9c0229f8e008b4eecb65c27591
Ronald G. Minnich <rminnich@gmail.com>
2017-09-13 09:12:39 -0700

    Fix something lint just found.
    Not sure how I missed this before.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/xinit/xinit.go
+++ cmds/xinit/xinit.go
@@ -166,13 +166,13 @@ func xSetup() error {
 	time.Sleep(5 * time.Second)
 	for _, f := range []string{"flwm", "opera-12"} {
 		log.Printf("Run %v", f)
-		go func() {
-			cmd := exec.Command(f, "-display", ":0")
+		go func(n string) {
+			cmd := exec.Command(n, "-display", ":0")
 			cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
 			if err := cmd.Run(); err != nil {
-				log.Printf("%v: %v", f, err)
+				log.Printf("%v: %v", n, err)
 			}
-		}()
+		}(f)
 	}
 	cmd := exec.Command("aterm", "-display", ":0")
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
