d2df773482934e72ee4b3a2ddc112051616a748e
Ronald G. Minnich <rminnich@gmail.com>
2017-10-11 09:57:51 -0700

    Fix ldd_test to test shared library dependency checking
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/ldd/ldd_test.go
+++ pkg/ldd/ldd_test.go
@@ -1,8 +1,10 @@
 package uroot
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -20,28 +22,54 @@ func TestLdd(t *testing.T) {
 	}
 }
 
-// TestLddList tests that the LddList is the
-// same as the info returned by Ldd
-func TestLddList(t *testing.T) {
+// lddOne is a helper that runs Ldd on one file. It returns
+// the list so we can use elements from the list on another
+// test. We do it this way because, unlike /bin/date, there's
+// almost nothing else we can assume exists, e.g. /lib/libc.so
+// is a different name on almost every *ix* system.
+func lddOne(name string) ([]string, error) {
 	var libMap = make(map[string]bool)
-	n, err := Ldd([]string{"/bin/date"})
+	n, err := Ldd([]string{name})
 	if err != nil {
-		t.Fatalf("Ldd on /bin/date: want nil, got %v", err)
+		return nil, fmt.Errorf("Ldd on %v: want nil, got %v", name, err)
 	}
-	l, err := LddList([]string{"/bin/date"})
+	l, err := LddList([]string{name})
 	if err != nil {
-		t.Fatalf("LddList on /bin/date: want nil, got %v", err)
+		return nil, fmt.Errorf("LddList on %v: want nil, got %v", name, err)
 	}
 	if len(n) != len(l) {
-		t.Fatalf("Len of Ldd(%v) and LddList(%v): want same, got different", len(n), len(l))
+		return nil, fmt.Errorf("%v: Len of Ldd(%v) and LddList(%v): want same, got different", name, len(n), len(l))
 	}
 	for i := range n {
 		libMap[n[i].FullName] = true
 	}
 	for i := range n {
 		if !libMap[l[i]] {
-			t.Errorf("%v was in LddList but not in Ldd", l[i])
+			return nil, fmt.Errorf("%v: %v was in LddList but not in Ldd", name, l[i])
 		}
+	}
+	return l, nil
+}
+
+// TestLddList tests that the LddList is the
+// same as the info returned by Ldd
+func TestLddList(t *testing.T) {
+	n, err := lddOne("/bin/date")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Find the first name in the array that contains "lib"
+	// Test 'em all
+	for _, f := range n {
+		if !strings.Contains(f, "lib") {
+			continue
+		}
+		t.Logf("Test %v", f)
+		n, err := lddOne(f)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("%v has deps of %v", f, n)
 	}
 }
 
