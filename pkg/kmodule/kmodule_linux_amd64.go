04b273f02128093d3cab34632d2a374c6897d9da
Christopher Koch <chrisko@google.com>
2017-10-29 22:28:57 -0700

    rmmod/insmod: define as package
    Signed-off-by: Christopher Koch <chrisko@google.com>

---
+++ pkg/kmodule/kmodule_linux_amd64.go
@@ -0,0 +1,6 @@
+package kmodule
+
+const (
+	// x86-64 syscall number for finit_module(2).
+	_SYS_FINIT_MODULE = 313
+)
---
+++ pkg/kmodule/kmodule_linux_ppc64le.go
@@ -0,0 +1,6 @@
+package kmodule
+
+const (
+	// powerpc syscall number for finit_module(2).
+	_SYS_FINIT_MODULE = 353
+)
