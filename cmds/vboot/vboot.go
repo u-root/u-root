8b046a8b07f8db0531571a6e2c5c3fc7cc181cf0
Christopher Koch <chrisko@google.com>
2017-10-30 14:29:45 -0700

    vboot: cleanup
    Fixes #358.
    
    Signed-off-by: Christopher Koch <chrisko@google.com>

--- cmds/vboot/vboot.go
+++ cmds/vboot/vboot.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/zaolin/go-tpm/tpm"
 	"golang.org/x/crypto/ed25519"
-	"golang.org/x/sys/unix"
 )
 
 const (
@@ -21,52 +20,45 @@ const (
 	filesystem string = "ext3"
 )
 
-func dieHard() {
-	if e1, e2, err := syscall.Syscall6(syscall.SYS_REBOOT, syscall.LINUX_REBOOT_MAGIC1, syscall.LINUX_REBOOT_MAGIC2, syscall.LINUX_REBOOT_CMD_POWER_OFF, 0, 0, 0); err != 0 {
-		log.Fatalf("a %v b %v err %v", e1, e2, err)
+var (
+	publicKey            = flag.String("pubkey", "/etc/sig.pub", "A public key which should verify the signature.")
+	pcr                  = flag.Uint("pcr", 12, "The pcr index used for measuring the kernel before kexec.")
+	bootDev              = flag.String("boot-device", "/dev/sda1", "The boot device which is used to kexec into a signed kernel.")
+	linuxKernel          = flag.String("kernel", "/mnt/vboot/kernel", "Kernel image file path.")
+	linuxKernelSignature = flag.String("kernel-sig", "/mnt/vboot/kernel.sig", "Kernel image signature file path.")
+	initrd               = flag.String("initrd", "/mnt/vboot/initrd", "Initrd file path.")
+	initrdSignature      = flag.String("initrd-sig", "/mnt/vboot/initrd.sig", "Initrd signature file path.")
+	debug                = flag.Bool("debug", false, "Enables debug mode.")
+	noTPM                = flag.Bool("no-tpm", false, "Disables tpm measuring process.")
+)
+
+func die(err error) {
+	if *debug {
+		panic(err)
+	}
+	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_POWER_OFF); err != nil {
+		log.Fatalf("reboot err: %v", err)
 	}
 }
 
 func main() {
-	var publicKey = flag.String("pubkey", "/etc/sig.pub", "A public key which should verify the signature.")
-	var pcr = flag.Uint("pcr", 12, "The pcr index used for measuring the kernel before kexec.")
-	var bootDev = flag.String("boot-device", "/dev/sda1", "The boot device which is used to kexec into a signed kernel.")
-	var linuxKernel = flag.String("kernel", "/mnt/vboot/kernel", "Kernel image file path.")
-	var linuxKernelSignature = flag.String("kernel-sig", "/mnt/vboot/kernel.sig", "Kernel image signature file path.")
-	var initrd = flag.String("initrd", "/mnt/vboot/initrd", "Initrd file path.")
-	var initrdSignature = flag.String("initrd-sig", "/mnt/vboot/initrd.sig", "Initrd signature file path.")
-	var debug = flag.Bool("debug", false, "Enables debug mode.")
-	var noTPM = flag.Bool("no-tpm", false, "Disables tpm measuring process.")
-
 	flag.Parse()
 
 	if err := os.MkdirAll(mountPath, os.ModePerm); err != nil {
-		if *debug {
-			panic(err)
-		} else {
-			dieHard()
-		}
+		die(err)
 	}
 
-	if err := unix.Mount(*bootDev, mountPath, filesystem, unix.MS_RDONLY, ""); err != nil {
-		if *debug {
-			panic(err)
-		} else {
-			dieHard()
-		}
+	if err := syscall.Mount(*bootDev, mountPath, filesystem, syscall.MS_RDONLY, ""); err != nil {
+		die(err)
 	}
 
-	paths := [...]string{*publicKey, *linuxKernel, *linuxKernelSignature, *initrd, *initrdSignature}
+	paths := []string{*publicKey, *linuxKernel, *linuxKernelSignature, *initrd, *initrdSignature}
 	files := make(map[string][]byte)
 
 	for _, element := range paths {
 		data, err := ioutil.ReadFile(element)
 		if err != nil {
-			if *debug {
-				panic(err)
-			} else {
-				dieHard()
-			}
+			die(err)
 		} else {
 			files[element] = data
 		}
@@ -81,42 +73,29 @@ func main() {
 	kernelSuccess := ed25519.Verify(files[*publicKey], kernelDigest[:], files[*linuxKernelSignature])
 	initrdSuccess := ed25519.Verify(files[*publicKey], initrdDigest[:], files[*linuxKernelSignature])
 
-	if kernelSuccess && initrdSuccess {
-		if !*noTPM {
-			rwc, err := tpm.OpenTPM(tpmDevice)
-			if err != nil {
-				if *debug {
-					panic(err)
-				} else {
-					dieHard()
-				}
-			}
+	if !kernelSuccess || !initrdSuccess {
+		die(nil)
+	}
 
-			tpm.PcrExtend(rwc, uint32(*pcr), pcrDigestKernel)
-			tpm.PcrExtend(rwc, uint32(*pcr), pcrDigestInitrd)
+	if !*noTPM {
+		rwc, err := tpm.OpenTPM(tpmDevice)
+		if err != nil {
+			die(err)
 		}
 
-		binary, lookErr := exec.LookPath("kexec")
-		if lookErr != nil {
-			if *debug {
-				panic(lookErr)
-			} else {
-				dieHard()
-			}
-		}
+		tpm.PcrExtend(rwc, uint32(*pcr), pcrDigestKernel)
+		tpm.PcrExtend(rwc, uint32(*pcr), pcrDigestInitrd)
+	}
 
-		args := []string{"kexec", "-initrd", *initrd, *linuxKernel}
-		env := os.Environ()
+	binary, lookErr := exec.LookPath("kexec")
+	if lookErr != nil {
+		die(lookErr)
+	}
 
-		execErr := syscall.Exec(binary, args, env)
-		if execErr != nil {
-			if *debug {
-				panic(execErr)
-			} else {
-				dieHard()
-			}
-		}
-	} else {
-		dieHard()
+	args := []string{"kexec", "-initrd", *initrd, *linuxKernel}
+	env := os.Environ()
+
+	if execErr := syscall.Exec(binary, args, env); execErr != nil {
+		die(execErr)
 	}
 }
