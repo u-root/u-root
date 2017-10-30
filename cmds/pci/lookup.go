5f5cac7e240032dfd9bb76fe34d4449f84732a91
Christopher Koch <chrisko@google.com>
2017-10-30 14:35:37 -0700

    pci: convert to log package
    Signed-off-by: Christopher Koch <chrisko@google.com>

--- cmds/pci/lookup.go
+++ cmds/pci/lookup.go
@@ -7,14 +7,14 @@ package main
 import (
 	"bufio"
 	"bytes"
+
+	"github.com/u-root/u-root/pkg/log"
 )
 
 const (
 	FindingVendor = iota
 	FindingDevice
 )
-
-var debug = func(s string, arg ...interface{}) {} //{log.Printf(s, arg...)}
 
 func isHex(b byte) bool {
 	return ('a' <= b && b <= 'f') || ('A' <= b && b <= 'F') || ('0' <= b && b <= '9')
@@ -26,30 +26,36 @@ func lookup(vendor, device string) (string, string) {
 	var lineno int
 	vendorName := vendor
 	deviceName := device
-	s := bufio.NewScanner(bytes.NewReader(pciids))
 
+	s := bufio.NewScanner(bytes.NewReader(pciids))
 	for s.Scan() {
 		line = s.Text()
 		lineno++
-		debug("Line %d: %v\n", lineno, line)
+
+		log.Printf("Line %d: %v", lineno, line)
+
 		switch {
 		case len(line) < 7, line[0] == '#':
-			debug("discard")
+			log.Printf("discard")
 			continue
+
 		case state == FindingVendor && isHex(line[0]) && isHex(line[1]):
-			debug("vendor check %s against %s", vendor[:4], line[:4])
+			log.Printf("vendor check %s against %s", vendor[:4], line[:4])
 			if vendor[:4] == line[:4] {
 				vendorName = line[6:]
 				state = FindingDevice
 			}
+
 		// There are subdevices, ignore them.
 		case state == FindingDevice && line[0:2] == "\t\t":
-			debug("Subdevice")
+			log.Printf("Subdevice")
+
 		case state == FindingDevice && (line[0] != '\t' || !isHex(line[1])):
-			debug("Finding device: no more devices for this vendor")
+			log.Printf("Finding device: no more devices for this vendor")
 			return vendorName, deviceName
+
 		case state == FindingDevice && line[0] == '\t' && isHex(line[1]):
-			debug("device check %s against %s", device[:4], line[1:5])
+			log.Printf("device check %s against %s", device[:4], line[1:5])
 			if device[:4] == line[1:5] {
 				deviceName = line[7:]
 				return vendorName, deviceName
