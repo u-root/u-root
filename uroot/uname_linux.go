b49077481ef35807cb6dd7c1dd7b6e6a491d538f
Tobias Klauser <tklauser@distanz.ch>
2017-11-07 17:16:21 +0100

    uroot: simplify Utsname handling
    Use Utsname from golang.org/x/sys/unix which provides a consistent type
    Utsname with byte array members (instead of int/uint8 depending on
    platform) on all Unix-like platforms. This greatly simplifies conversion
    to strings.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- uroot/uname_linux.go
+++ uroot/uname_linux.go
@@ -1,4 +1,4 @@
-// Copyright 2017 the u-root Authors. All rights reserved
+// Copyright 2015-2017 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -7,7 +7,9 @@
 package uroot
 
 import (
-	"syscall"
+	"bytes"
+
+	"golang.org/x/sys/unix"
 )
 
 type Utsname struct {
@@ -19,21 +21,14 @@ type Utsname struct {
 	Domainname string
 }
 
-func toString(d []int8) string {
-	s := ""
-	for _, c := range d {
-		if c == 0 {
-			break
-		}
-		s = s + string(byte(c))
-	}
-	return s
+func toString(d []byte) string {
+	return string(d[:bytes.IndexByte(d[:], 0)])
 }
 
 // uname does a uname and returns a uroot.Utsname
 func Uname() (*Utsname, error) {
-	var u syscall.Utsname
-	if err := syscall.Uname(&u); err != nil {
+	var u unix.Utsname
+	if err := unix.Uname(&u); err != nil {
 		return nil, err
 	}
 	return &Utsname{Sysname: toString(u.Sysname[:]), Nodename: toString(u.Nodename[:]), Release: toString(u.Release[:]), Version: toString(u.Version[:]), Machine: toString(u.Machine[:]), Domainname: toString(u.Domainname[:])}, nil
