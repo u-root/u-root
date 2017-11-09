77f5d41e3e41afaeebad21bfe513704646abf13a
Tobias Klauser <tklauser@distanz.ch>
2017-11-09 15:34:30 +0100

    pkg/termios: use termios functions and types from x/sys/unix
    Use the Ioctl{Get,Set}{Termios,Winsize} functions, types and constants
    from golang.org/x/sys/unix instead of locally defined copies. Over all
    this reduces code size and makes it more portable across different
    architectures.
    
    Also ann an additional GetTermios function which will help to implement
    termios functionality in cmd/pflask using pkg/termios.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- pkg/termios/termios_linux.go
+++ pkg/termios/termios_linux.go
@@ -5,68 +5,14 @@
 package termios
 
 import (
-	"fmt"
 	"os"
-	"syscall"
-	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
-// NCCS is the size of the control character array, which is basically
-// ^@ to ^Z.
-const NCCS = 32
-
-type (
-	ControlChar byte
-	Speed       uint32
-	TCFlag      uint32
-
-	Termios struct {
-		Iflag, Oflag, Cflag, Lflag TCFlag
-		Line                       uint8
-		CC                         [NCCS]ControlChar
-		Ispeed, Ospeed             Speed
-	}
-
-	WinSize struct {
-		Row, Col       uint16
-		Xpixel, Ypixel uint16
-	}
-
-	TTY struct {
-		f *os.File
-	}
-)
-
-// termios constants
-const (
-	IGNBRK = TCFlag(0000001)
-	BRKINT = TCFlag(0000002)
-	PARMRK = TCFlag(0000010)
-	ISTRIP = TCFlag(0000040)
-	INLCR  = TCFlag(0000100)
-	IGNCR  = TCFlag(0000200)
-	ICRNL  = TCFlag(0000400)
-	IXON   = TCFlag(0002000)
-	OPOST  = TCFlag(0000001)
-	ECHO   = TCFlag(0000010)
-	ECHONL = TCFlag(0000100)
-	ICANON = TCFlag(0000002)
-	ISIG   = TCFlag(0000001)
-	IEXTEN = TCFlag(0100000)
-	CSIZE  = TCFlag(0000060)
-	CS8    = TCFlag(0000060)
-	PARENB = TCFlag(0000400)
-	VTIME  = 5
-	VMIN   = 6
-)
-
-// ioctl constants
-const (
-	TCGETS     = 0x5401
-	TCSETS     = 0x5402
-	TIOCGWINSZ = 0x5413
-	TIOCSWINSZ = 0x5414
-)
+type TTY struct {
+	f *os.File
+}
 
 func New() (*TTY, error) {
 	f, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
@@ -78,58 +24,48 @@ func New() (*TTY, error) {
 	return t, nil
 }
 
-func (t *TTY) Get() (*Termios, error) {
-	var ti = &Termios{}
-	r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL, t.f.Fd(), uintptr(TCGETS), uintptr(unsafe.Pointer(ti)))
-	if errno != 0 || r1 != 0 {
-		return nil, fmt.Errorf("termios.get: r1 %v, errno %v", r1, errno)
-	}
-	return ti, nil
+func GetTermios(fd uintptr) (*unix.Termios, error) {
+	return unix.IoctlGetTermios(int(fd), unix.TCGETS)
 }
 
-func (t *TTY) Set(ti *Termios) error {
-	if r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL, t.f.Fd(), uintptr(TCSETS), uintptr(unsafe.Pointer(ti))); errno != 0 || r1 != 0 {
-		return fmt.Errorf("Termios.Set: r1 %v, errno %v", r1, errno)
-	}
-
-	return nil
+func (t *TTY) Get() (*unix.Termios, error) {
+	return GetTermios(t.f.Fd())
 }
 
-func MakeRaw(term *Termios) *Termios {
+func SetTermios(fd uintptr, ti *unix.Termios) error {
+	return unix.IoctlSetTermios(int(fd), unix.TCSETS, ti)
+}
+
+func (t *TTY) Set(ti *unix.Termios) error {
+	return SetTermios(t.f.Fd(), ti)
+}
+
+func MakeRaw(term *unix.Termios) *unix.Termios {
 	raw := *term
-	raw.Iflag &= ^(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON)
-	raw.Oflag &= ^OPOST
-	raw.Lflag &= ^(ECHO | ECHONL | ICANON | ISIG | IEXTEN)
-	raw.Cflag &= ^(CSIZE | PARENB)
-	raw.Cflag |= CS8
+	raw.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.ICRNL | unix.IXON
+	raw.Oflag &^= unix.OPOST
+	raw.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON | unix.ISIG | unix.IEXTEN
+	raw.Cflag &^= unix.CSIZE | unix.PARENB
+	raw.Cflag |= unix.CS8
 
-	raw.CC[VMIN] = 1
-	raw.CC[VTIME] = 0
+	raw.Cc[unix.VMIN] = 1
+	raw.Cc[unix.VTIME] = 0
 
 	return &raw
 }
 
-func GetWinSize(fd uintptr) (*WinSize, error) {
-	var w = &WinSize{}
-	if r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(TIOCGWINSZ), uintptr(unsafe.Pointer(w))); errno != 0 || r1 != 0 {
-		return nil, fmt.Errorf("WinSize.Get: r1 %v, errno %v", r1, errno)
-	}
-
-	return w, nil
+func GetWinSize(fd uintptr) (*unix.Winsize, error) {
+	return unix.IoctlGetWinsize(int(fd), unix.TIOCGWINSZ)
 }
 
-func (t *TTY) GetWinSize() (*WinSize, error) {
+func (t *TTY) GetWinSize() (*unix.Winsize, error) {
 	return GetWinSize(t.f.Fd())
 }
 
-func SetWinSize(fd uintptr, w *WinSize) error {
-	if r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(TIOCSWINSZ), uintptr(unsafe.Pointer(w))); errno != 0 || r1 != 0 {
-		return fmt.Errorf("WinSize.Set: r1 %v, errno %v", r1, errno)
-	}
-
-	return nil
+func SetWinSize(fd uintptr, w *unix.Winsize) error {
+	return unix.IoctlSetWinsize(int(fd), unix.TIOCSWINSZ, w)
 }
 
-func (t *TTY) SetWinSize(w *WinSize) error {
+func (t *TTY) SetWinSize(w *unix.Winsize) error {
 	return SetWinSize(t.f.Fd(), w)
 }
