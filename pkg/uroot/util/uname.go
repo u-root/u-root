e688d64922cc8f3a328c043b3ffec87943694c06
Tobias Klauser <tklauser@distanz.ch>
2017-11-14 14:55:18 +0100

    pkg/uroot: use unix.Uname on Darwin
    Unify the Uname implementation by using golang.org/x/sys/unix, now that
    it supports Uname on Darwin.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- pkg/uroot/util/uname.go
+++ pkg/uroot/util/uname.go
@@ -4,6 +4,12 @@
 
 package util
 
+import (
+	"bytes"
+
+	"golang.org/x/sys/unix"
+)
+
 type Utsname struct {
 	Sysname    string
 	Nodename   string
@@ -11,4 +17,24 @@ type Utsname struct {
 	Version    string
 	Machine    string
 	Domainname string
+}
+
+func toString(d []byte) string {
+	return string(d[:bytes.IndexByte(d[:], 0)])
+}
+
+// Uname returns name and information about the current OS kernel
+func Uname() (*Utsname, error) {
+	var u unix.Utsname
+	if err := unix.Uname(&u); err != nil {
+		return nil, err
+	}
+	return &Utsname{
+		Sysname:    toString(u.Sysname[:]),
+		Nodename:   toString(u.Nodename[:]),
+		Release:    toString(u.Release[:]),
+		Version:    toString(u.Version[:]),
+		Machine:    toString(u.Machine[:]),
+		Domainname: toString(u.Domainname[:]),
+	}, nil
 }
