c612ffc0211cb63e491c90fa7163fb9b855f0a15
alexander <git@systemstart.net>
2017-11-15 16:32:16 +0100

    added -R and --reference to chmod

--- cmds/chmod/chmod.go
+++ cmds/chmod/chmod.go
@@ -13,32 +13,89 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
+var (
+	recursive bool
+	reference string
+)
+
+func init() {
+	flag.BoolVar(&recursive,
+		"R",
+		false,
+		"do changes recursively")
+
+	flag.BoolVar(&recursive,
+		"recursive",
+		false,
+		"do changes recursively")
+
+	flag.StringVar(&reference,
+		"reference",
+		"",
+		"use mode from reference file")
+}
+
 func main() {
 	flag.Parse()
-	if len(flag.Args()) < 2 {
+	if len(flag.Args()) < 1 {
+		fmt.Fprintf(os.Stderr, "Usage of %s: [mode] filepath\n", os.Args[0])
 		flag.PrintDefaults()
-		log.Fatalf("usage: chmod mode filepath")
+		os.Exit(1)
 	}
 
-	mode := flag.Args()[0]
+	if len(flag.Args()) < 2 && reference == "" {
+		fmt.Fprintf(os.Stderr, "Usage of %s: [mode] filepath\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-	octval, err := strconv.ParseUint(mode, 8, 32)
-	if err != nil {
-		log.Fatalf("Unable to decode mode %q. Please use an octal value: %v", mode, err)
-	} else if octval > 0777 {
-		log.Fatalf("Invalid octal value %0o. Value should be less than or equal to 0777.", octval)
+	var mode os.FileMode
+	var fileList []string
+
+	if reference != "" {
+		fi, err := os.Stat(reference)
+		if err != nil {
+			log.Fatalf("bad reference file: %v", err)
+
+		}
+		mode = fi.Mode()
+		fileList = flag.Args()
+	} else {
+		modeString := flag.Args()[0]
+		octval, err := strconv.ParseUint(modeString, 8, 32)
+		if err != nil {
+			log.Fatalf("Unable to decode mode %q. Please use an octal value: %v", modeString, err)
+		} else if octval > 0777 {
+			log.Fatalf("Invalid octal value %0o. Value should be less than or equal to 0777.", octval)
+		}
+		mode = os.FileMode(octval)
+		fileList = flag.Args()[1:]
 	}
 
 	var exitError bool
-	for _, name := range flag.Args()[1:] {
-		if err := os.Chmod(name, os.FileMode(octval)); err != nil {
-			log.Printf("%v", err)
-			exitError = true
+	for _, name := range fileList {
+		if recursive {
+			err := filepath.Walk(name, func(path string,
+				info os.FileInfo,
+				err error) error {
+				return os.Chmod(path, mode)
+			})
+			if err != nil {
+				log.Printf("%v", err)
+				exitError = true
+			}
+		} else {
+			if err := os.Chmod(name, mode); err != nil {
+				log.Printf("%v", err)
+				exitError = true
+			}
 		}
 	}
 	if exitError {
--- cmds/chmod/chmod_test.go
+++ cmds/chmod/chmod_test.go
@@ -62,6 +62,122 @@ func TestChmodSimple(t *testing.T) {
 	}
 }
 
+func checkPath(t *testing.T, path string, want os.FileMode) {
+	info, err := os.Stat(path)
+	if err != nil {
+		t.Fatalf("stat(%q) failed: %v", path, err)
+	}
+	if got := info.Mode().Perm(); got != want {
+		t.Fatalf("Wrong file permissions on file %q: got %0o, want %0o",
+			path, got, want)
+	}
+}
+func TestChmodRecursive(t *testing.T) {
+	// Temporary directories.
+	tempDir, err := ioutil.TempDir("", "TestChmodRecursive")
+	if err != nil {
+		t.Fatalf("cannot create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	var targetFiles []string
+	var targetDirectories []string
+	for _, dir := range []string{"L1_A", "L1_B", "L1_C",
+		filepath.Join("L1_A", "L2_A"),
+		filepath.Join("L1_A", "L2_B"),
+		filepath.Join("L1_A", "L2_C"),
+		filepath.Join("L1_B", "L2_A"),
+		filepath.Join("L1_B", "L2_B"),
+		filepath.Join("L1_B", "L2_C"),
+		filepath.Join("L1_C", "L2_A"),
+		filepath.Join("L1_C", "L2_B"),
+		filepath.Join("L1_C", "L2_C"),
+	} {
+		dir = filepath.Join(tempDir, dir)
+		err := os.Mkdir(dir, os.FileMode(0700))
+		if err != nil {
+			t.Fatalf("cannot create test directory: %v", err)
+		}
+		targetDirectories = append(targetDirectories, dir)
+		targetFile, err := os.Create(filepath.Join(dir, "X"))
+		if err != nil {
+			t.Fatalf("cannot create temporary file: %v", err)
+		}
+		targetFiles = append(targetFiles, targetFile.Name())
+
+	}
+
+	// Build chmod binary.
+	testpath := filepath.Join(tempDir, "testchmod.exe")
+	out, err := exec.Command("go", "build", "-o", testpath, ".").CombinedOutput()
+	if err != nil {
+		t.Fatalf("go build -o %v cmds/chmod: %v\n%s", testpath, err, string(out))
+	}
+
+	for _, perm := range []os.FileMode{0707, 0770} {
+		// Set target file permissions using chmod.
+		c := exec.Command(testpath,
+			"-R",
+			fmt.Sprintf("%0o", perm),
+			tempDir)
+		c.Run()
+
+		// Check that it worked.
+		for _, dir := range targetDirectories {
+			checkPath(t, dir, perm)
+		}
+	}
+}
+
+func TestChmodReference(t *testing.T) {
+	// Temporary directories.
+	tempDir, err := ioutil.TempDir("", "TestChmodReference")
+	if err != nil {
+		t.Fatalf("cannot create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	sourceFile, err := ioutil.TempFile(tempDir, "BLAH1")
+	if err != nil {
+		t.Fatalf("cannot create temporary file: %v", err)
+	}
+	defer sourceFile.Close()
+
+	targetFile, err := ioutil.TempFile(tempDir, "BLAH2")
+	if err != nil {
+		t.Fatalf("cannot create temporary file: %v", err)
+	}
+	defer targetFile.Close()
+
+	// Build chmod binary.
+	testpath := filepath.Join(tempDir, "testchmod.exe")
+	out, err := exec.Command("go", "build", "-o", testpath, ".").CombinedOutput()
+	if err != nil {
+		t.Fatalf("go build -o %v cmds/chmod: %v\n%s", testpath, err, string(out))
+	}
+
+	for _, perm := range []os.FileMode{0777, 0644} {
+		os.Chmod(sourceFile.Name(), perm)
+
+		// Set target file permissions using chmod.
+		c := exec.Command(testpath,
+			"--reference",
+			sourceFile.Name(),
+			targetFile.Name())
+		c.Run()
+
+		// Check that it worked.
+		info, err := os.Stat(targetFile.Name())
+		if err != nil {
+			t.Fatalf("stat(%q) failed: %v", targetFile.Name(), err)
+		}
+		if got := info.Mode().Perm(); got != perm {
+			t.Fatalf("Wrong file permissions on file %q: got %0o, want %0o",
+				targetFile.Name(), got, perm)
+		}
+	}
+}
+
 func TestInvocationErrors(t *testing.T) {
 	tempDir, err := ioutil.TempDir("", "TestInvocationErrors")
 	if err != nil {
@@ -82,35 +198,50 @@ func TestInvocationErrors(t *testing.T) {
 	}
 
 	for _, v := range []struct {
-		args []string
-		want string
+		args     []string
+		want     string
+		skipTo   int
+		skipFrom int
 	}{
 
 		{
-			args: []string{f.Name()},
-			want: "usage: chmod mode filepath\n",
+			args:     []string{f.Name()},
+			want:     "Usage",
+			skipTo:   0,
+			skipFrom: len("Usage"),
 		},
 		{
-			args: []string{""},
-			want: "usage: chmod mode filepath\n",
+			args:     []string{""},
+			want:     "Usage",
+			skipTo:   0,
+			skipFrom: len("Usage"),
 		},
 		{
-			args: []string{"01777", f.Name()},
-			want: "Invalid octal value 1777. Value should be less than or equal to 0777.\n",
+			args:     []string{"01777", f.Name()},
+			want:     "Invalid octal value 1777. Value should be less than or equal to 0777.\n",
+			skipTo:   20,
+			skipFrom: -1,
 		},
 		{
-			args: []string{"0abas", f.Name()},
-			want: "Unable to decode mode \"0abas\". Please use an octal value: strconv.ParseUint: parsing \"0abas\": invalid syntax\n",
+			args:     []string{"0abas", f.Name()},
+			want:     "Unable to decode mode \"0abas\". Please use an octal value: strconv.ParseUint: parsing \"0abas\": invalid syntax\n",
+			skipTo:   20,
+			skipFrom: -1,
 		},
 		{
-			args: []string{"0777", "blah1234"},
-			want: "chmod blah1234: no such file or directory\n",
+			args:     []string{"0777", "blah1234"},
+			want:     "chmod blah1234: no such file or directory\n",
+			skipTo:   20,
+			skipFrom: -1,
 		},
 	} {
 		cmd := exec.Command(testpath, v.args...)
 		_, stderr, err := run(cmd)
+		if v.skipFrom == -1 {
+			v.skipFrom = len(stderr)
+		}
 		// Ignore the date and time because we're using Log.Fatalf
-		if got := stderr[20:]; got != v.want {
+		if got := stderr[v.skipTo:v.skipFrom]; got != v.want {
 			t.Errorf("Chmod for %q failed: got %q, want %q", v.args, got, v.want)
 		}
 		if err == nil {
