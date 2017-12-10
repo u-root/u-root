09bfe044dfb479c5f064e7ce87e48da74987cc23
Ronald G. Minnich <rminnich@gmail.com>
2017-12-09 20:51:29 -0800

    pci: allow for dumping and setting of config registers
    Back in the day, this pci command would have been incredibly handy.
    
    Suppose you want to dump 16 bits @ 0 and 2, and 32 bits at 4, and 8 bits at 4.
    This is device and vendor id, and 32 bits @ 4, and 8 bits of command register.
    And you want it for all devices at device 18, a root port.
    
    pci -s *18* 0.w 2.w 4.l 4.b
    
    0000:00:18.0: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.1: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.2: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.3: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.4: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.5: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.6: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    0000:00:18.7: VMware PCI Express Root Port
    00000000:15ad
    00000002:07a0
    00000004:00100007
    00000004:07
    
    Here is how you can set, and then see what value was really set:
     sudo ./pci -s 0000:00:18.7 4.b=7 4.b
    0000:00:18.7: VMware PCI Express Root Port
    00000004:07
    
    The *18* version of this command, shown above, would also work here. I've been using bash scripts
    looping over TBFN pci notation for 15 years; no longer.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/pci/pci.go
+++ pkg/pci/pci.go
@@ -5,9 +5,11 @@
 package pci
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -45,4 +47,75 @@ func (p *PCI) ReadConfig() error {
 	}
 	p.ExtraInfo = append(p.ExtraInfo, hex.Dump(c))
 	return nil
+}
+
+type barreg struct {
+	offset int64
+	*os.File
+}
+
+func (r *barreg) Read(b []byte) (int, error) {
+	return r.ReadAt(b, r.offset)
+}
+
+func (w *barreg) Write(b []byte) (int, error) {
+	return w.WriteAt(b, w.offset)
+}
+
+// ReadConfigRegister reads a configuration register of size 8, 16, 32, or 64.
+// It will only work on little-endian machines.
+func (p *PCI) ReadConfigRegister(offset, size int64) (uint64, error) {
+	f, err := os.Open(filepath.Join(p.FullPath, "config"))
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	var reg uint64
+	r := &barreg{offset: offset, File: f}
+	switch size {
+	default:
+		return 0, fmt.Errorf("%d is not valid: only options are 8, 16, 32, 64", size)
+	case 64:
+		err = binary.Read(r, binary.LittleEndian, &reg)
+	case 32:
+		var val uint32
+		err = binary.Read(r, binary.LittleEndian, &val)
+		reg = uint64(val)
+	case 16:
+		var val uint16
+		err = binary.Read(r, binary.LittleEndian, &val)
+		reg = uint64(val)
+	case 8:
+		var val uint8
+		err = binary.Read(r, binary.LittleEndian, &val)
+		reg = uint64(val)
+	}
+	return reg, err
+}
+
+// WriteConfigRegister writes a configuration register of size 8, 16, 32, or 64.
+// It will only work on little-endian machines.
+func (p *PCI) WriteConfigRegister(offset, size int64, val uint64) error {
+	f, err := os.OpenFile(filepath.Join(p.FullPath, "config"), os.O_WRONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	w := &barreg{offset: offset, File: f}
+	switch size {
+	default:
+		return fmt.Errorf("%d is not valid: only options are 8, 16, 32, 64", size)
+	case 64:
+		err = binary.Write(w, binary.LittleEndian, &val)
+	case 32:
+		var v = uint32(val)
+		err = binary.Write(w, binary.LittleEndian, &v)
+	case 16:
+		var v = uint16(val)
+		err = binary.Write(w, binary.LittleEndian, &v)
+	case 8:
+		var v = uint8(val)
+		err = binary.Write(w, binary.LittleEndian, &v)
+	}
+	return err
 }
