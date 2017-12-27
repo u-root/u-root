u-root/u-root

commit b7cae605bd439eeef5c169a6f65819ddae3d6e55

uroot package: add : syntax back for extra files
The : syntax lets you do stuff like this:
-files /path/to/prototype/root:etc

It will source from /path/to/prototype/root/etc but
put it into the cpio archive as etc. It's impossible to
live without, I keep learning.

I used SplitN, not Split, as there are valid paths (maybe?)
with : in them. That said, we may find we want a more robust
way to do this.

Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

pkg/uroot/uroot.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 
 	"github.com/u-root/u-root/pkg/cpio"
 	"github.com/u-root/u-root/pkg/golang"
@@ -139,11 +140,15 @@ func CreateInitramfs(opts Opts) error {
 
 	// Add files from command line.
 	for _, file := range opts.ExtraFiles {
-		path, err := filepath.Abs(file)
+		parts := strings.SplitN(file, ":", 2)
+		path, err := filepath.Abs(filepath.Join(parts...))
+		if len(parts) != 2 {
+			parts = append(parts, parts[0][1:])
+		}
 		if err != nil {
 			return fmt.Errorf("couldn't find absolute path for %q: %v", file, err)
 		}
-		if err := archive.AddFile(path, path[1:]); err != nil {
+		if err := archive.AddFile(path, parts[1]); err != nil {
 			return fmt.Errorf("couldn't add %q to archive: %v", file, err)
 		}
 

commit d4dade3e10d68f854a14468b656f3d1d8b521c25

uroot package: fix: syntax for extra files
The previous commit b7cae605bd439eeef5c169a6f65819ddae3d6e55 introduced
a regression where using the old syntax (e.g. `-files something.txt`) is
truncating the first character (e.g. the resulting rootfs will contain
"omething.txt"). This diff avoids the truncation when the old syntax is
used.

pkg/uroot/uroot.go
@@ -140,21 +140,28 @@ func CreateInitramfs(opts Opts) error {
 
 	// Add files from command line.
 	for _, file := range opts.ExtraFiles {
+		var src, dst string
 		parts := strings.SplitN(file, ":", 2)
-		path, err := filepath.Abs(filepath.Join(parts...))
-		if len(parts) != 2 {
-			parts = append(parts, parts[0][1:])
+		if len(parts) == 2 {
+			// treat the entry with the new src:dst syntax
+			src = parts[0]
+			dst = parts[1]
+		} else {
+			// plain old syntax
+			src = file
+			dst = file
 		}
+		src, err := filepath.Abs(src)
 		if err != nil {
-			return fmt.Errorf("couldn't find absolute path for %q: %v", file, err)
+			return fmt.Errorf("couldn't find absolute path for %q: %v", src, err)
 		}
-		if err := archive.AddFile(path, parts[1]); err != nil {
+		if err := archive.AddFile(src, dst); err != nil {
 			return fmt.Errorf("couldn't add %q to archive: %v", file, err)
 		}
 
 		// Pull dependencies in the case of binaries. If `path` is not
 		// a binary, `libs` will just be empty.
-		libs, err := ldd.List([]string{path})
+		libs, err := ldd.List([]string{src})
 		if err != nil {
 			return fmt.Errorf("couldn't list ldd dependencies for %q: %v", file, err)
 		}