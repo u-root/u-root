c6f10be306005da39ffc22273944b08bbdef1a54
Andrea Barberio <insomniac@slackware.it>
2018-01-09 17:09:03 +0000

    cmds/free: refactored linux-specific functions

--- cmds/free/free.go
+++ cmds/free/free.go
@@ -21,16 +21,11 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"strconv"
 )
-
-const meminfoFile = "/proc/meminfo"
 
 var humanOutput = flag.Bool("h", false, "Human output: show automatically the shortest three-digits unit")
 var inBytes = flag.Bool("b", false, "Express the values in bytes")
@@ -90,39 +85,6 @@ type MemInfo struct {
 
 type meminfomap map[string]uint64
 
-// meminfo returns a mapping that represents the fields contained in
-// /proc/meminfo
-func meminfo() (meminfomap, error) {
-	buf, err := ioutil.ReadFile(meminfoFile)
-	if err != nil {
-		return nil, err
-	}
-	return meminfoFromBytes(buf)
-}
-
-// meminfoFromBytes returns a mapping that represents the fields contained in a
-// byte stream with a content compatible with /proc/meminfo
-func meminfoFromBytes(buf []byte) (meminfomap, error) {
-	ret := make(meminfomap, 0)
-	for _, line := range bytes.Split(buf, []byte{'\n'}) {
-		kv := bytes.SplitN(line, []byte{':'}, 2)
-		if len(kv) != 2 {
-			// invalid line?
-			continue
-		}
-		key := string(kv[0])
-		tokens := bytes.SplitN(bytes.TrimSpace(kv[1]), []byte{' '}, 2)
-		if len(tokens) > 0 {
-			value, err := strconv.ParseUint(string(tokens[0]), 10, 64)
-			if err != nil {
-				return nil, err
-			}
-			ret[key] = value
-		}
-	}
-	return ret, nil
-}
-
 // missingRequiredFields checks if any of the specified fields are present in
 // the input map.
 func missingRequiredFields(m meminfomap, fields []string) bool {
@@ -137,7 +99,8 @@ func missingRequiredFields(m meminfomap, fields []string) bool {
 
 // humanReadableValue returns a string representing the input value, treated as
 // a size in bytes, interpreted in a human readable form. E.g. the number 10240
-// woud return the string "10 kB"
+// woud return the string "10 kB". Note that the decimal part is truncated, not
+// rounded, so the values are guaranteed to be "at least X"
 func humanReadableValue(value uint64) string {
 	v := value
 	// bits to shift. 0 means bytes, 10 means kB, and so on. 40 is the highest
@@ -157,7 +120,7 @@ func humanReadableValue(value uint64) string {
 	var decimal uint64
 	if shift > 0 {
 		// no rounding. Is there a better way to do this?
-		decimal = ((value - (value >> shift << shift)) >> (shift - 10)) / 100
+		decimal = ((value - (value >> shift << shift)) >> (shift - 10)) * 1000 / 1024 / 100
 	}
 	return fmt.Sprintf("%v.%v%v",
 		value>>shift,
@@ -175,69 +138,6 @@ func formatValueByConfig(value uint64, config *FreeConfig) string {
 	}
 	// units and decimal part are not printed when a unit is explicitly specified
 	return fmt.Sprintf("%v", value>>config.Unit)
-}
-
-// getMainMemInfo prints the physical memory information in the specified units. Only
-// the relevant fields will be used from the input map.
-func getMainMemInfo(m meminfomap, config *FreeConfig) (*mainMemInfo, error) {
-	fields := []string{
-		"MemTotal",
-		"MemFree",
-		"Buffers",
-		"Cached",
-		"Shmem",
-		"SReclaimable",
-		"MemAvailable",
-	}
-	if missingRequiredFields(m, fields) {
-		return nil, fmt.Errorf("Missing required fields from meminfo")
-	}
-
-	// These values are expressed in kibibytes, convert to the desired unit
-	memTotal := m["MemTotal"] << KB
-	memFree := m["MemFree"] << KB
-	memShared := m["Shmem"] << KB
-	memCached := (m["Cached"] + m["SReclaimable"]) << KB
-	memBuffers := (m["Buffers"]) << KB
-	memUsed := memTotal - memFree - memCached - memBuffers
-	if memUsed < 0 {
-		memUsed = memTotal - memFree
-	}
-	memAvailable := m["MemAvailable"] << KB
-
-	mmi := mainMemInfo{
-		Total:     memTotal,
-		Used:      memUsed,
-		Free:      memFree,
-		Shared:    memShared,
-		Cached:    memCached,
-		Buffers:   memBuffers,
-		Available: memAvailable,
-	}
-	return &mmi, nil
-}
-
-// getSwapInfo prints the swap space information in the specified units. Only the
-// relevant fields will be used from the input map.
-func getSwapInfo(m meminfomap, config *FreeConfig) (*swapInfo, error) {
-	fields := []string{
-		"SwapTotal",
-		"SwapFree",
-	}
-	if missingRequiredFields(m, fields) {
-		return nil, fmt.Errorf("Missing required fields from meminfo")
-	}
-	// These values are expressed in kibibytes, convert to the desired unit
-	swapTotal := m["SwapTotal"] << KB
-	swapUsed := (m["SwapTotal"] - m["SwapFree"]) << KB
-	swapFree := m["SwapFree"] << KB
-
-	si := swapInfo{
-		Total: swapTotal,
-		Used:  swapUsed,
-		Free:  swapFree,
-	}
-	return &si, nil
 }
 
 // Free prints physical memory and swap space information. The fields will be
