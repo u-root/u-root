1aba96e8f518660c45dfcf69973fd3786ebc0b49
Ronald G. Minnich <rminnich@gmail.com>
2018-02-16 09:17:51 -0800

    basic forth command line
    simple usage
    ./forth
    5 5 +
    
    this will leave 10 on TOS. You can then
    5 +
    
    todo: add iteration, io ops, all that jazz. If anyone cares.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/forth/forth.go
+++ pkg/forth/forth.go
@@ -1,26 +1,30 @@
-// the forth package is designed for use by programs 
-// needing to evaluate command-line arguments or simple 
-// expressions to set program variables. It is designed 
-// to map host names to numbers. We use it to 
-// easily convert host names and IP addresses into 
-// parameters. 
+// Copyright 2010-2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// the forth package is designed for use by programs
+// needing to evaluate command-line arguments or simple
+// expressions to set program variables. It is designed
+// to map host names to numbers. We use it to
+// easily convert host names and IP addresses into
+// parameters.
 // The language
-// is a Forth-like postfix notation. Elements are 
-// either commands or strings. Strings are 
+// is a Forth-like postfix notation. Elements are
+// either commands or strings. Strings are
 // immediately pushed. Commands consume stack variables
-// and produce new ones. 
-// Simple examples: 
+// and produce new ones.
+// Simple examples:
 // push hostname, strip alpha characters to produce a number. If your
-// hostname is sb47, top of stack will be left with 47. 
-// hostname  hostbase 
-// Get the hostbase, if it is 0 mod 20, return the hostbase / 20, 
+// hostname is sb47, top of stack will be left with 47.
+// hostname  hostbase
+// Get the hostbase, if it is 0 mod 20, return the hostbase / 20,
 // else return hostbase mod 20
 //
 // hostname hostbase dup 20 / swap 20 % dup ifelse
 //
-// At the end of the evaluation the stack should have one element 
+// At the end of the evaluation the stack should have one element
 // left; that element is popped and returned. It is an error (currently)
-// to return with a non-empty stack. 
+// to return with a non-empty stack.
 // This package was used for real work at Sandia National Labs from 2010 to 2012 and possibly later.
 // Some of the use of error may seem a bit weird but the creation of this package predates the
 // creation of the error type (it was still an os thing back then).
@@ -57,7 +61,7 @@ var opmap = map[string]forthop{
 
 // Forth is an interface used by the package. The interface
 // requires definition of Push, Pop, Length, Empty (convenience function
-// meaning Length is 0), Newop (insert a new or replacement operator), 
+// meaning Length is 0), Newop (insert a new or replacement operator),
 // and Reset (clear the stack, mainly diagnostic)
 type Forth interface {
 	Push(string)
@@ -66,7 +70,7 @@ type Forth interface {
 	Empty() bool
 	Newop(string, forthop)
 	Reset()
-	Stack()[]string
+	Stack() []string
 }
 
 // New creates a new stack
@@ -81,26 +85,28 @@ func (f *forthstack) Newop(n string, op forthop) {
 	opmap[n] = op
 }
 
-func  Ops() (map[string] forthop){
+func Ops() map[string]forthop {
 	return opmap
 }
+
 // Reset resets the stack to empty
 func (f *forthstack) Reset() {
 	f.stack = f.stack[0:0]
 }
+
 // Return the stack as a []string
-func (f *forthstack) Stack() []string{
+func (f *forthstack) Stack() []string {
 	return f.stack
 }
 
-// Push pushes the string on the stack. 
+// Push pushes the string on the stack.
 func (f *forthstack) Push(s string) {
 	f.stack = append(f.stack, s)
 	//fmt.Printf("push: %v: stack: %v\n", s, f.stack)
 }
 
-// Pop pops the stack. If the stack is Empty Pop will panic. 
-// Eval recovers() the panic. 
+// Pop pops the stack. If the stack is Empty Pop will panic.
+// Eval recovers() the panic.
 func (f *forthstack) Pop() (ret string) {
 
 	if len(f.stack) < 1 {
@@ -112,7 +118,7 @@ func (f *forthstack) Pop() (ret string) {
 	return ret
 }
 
-// Length returns the stack length. 
+// Length returns the stack length.
 func (f *forthstack) Length() int {
 	return len(f.stack)
 }
@@ -123,7 +129,7 @@ func (f *forthstack) Empty() bool {
 }
 
 // errRecover converts panics to errstr iff it is an os.Error, panics
-// otherwise. 
+// otherwise.
 func errRecover(errp *error) {
 	e := recover()
 	if e != nil {
@@ -135,28 +141,31 @@ func errRecover(errp *error) {
 }
 
 /* iEval takes a Forth and strings and splits the string on space
- * characters, pushing each element on the stack or invoking the 
- * operator if it is found in the opmap. 
+ * characters, pushing each element on the stack or invoking the
+ * operator if it is found in the opmap.
  */
 func iEval(f Forth, s string) {
-	for _, val := range strings.Split(s, " ") {
-		/* two or more spaces can have odd results */
-		if len(val) == 0 || val == " " {
-			continue
-		}
+	// TODO: create a separator list based on isspace and all the
+	// non alpha numberic characters in the opmap.
+	for _, val := range strings.Fields(s) {
+		//fmt.Printf("eval %s stack %v", val, f.Stack())
 		fun := opmap[val]
 		if fun != nil {
+			//fmt.Printf("Eval ...:")
 			fun(f)
+			//fmt.Printf("Stack now %v", f.Stack())
 		} else {
 			f.Push(val)
+			//fmt.Printf("push %s, stack %v", val, f.Stack())
 		}
 	}
 	return
 }
+
 /* Eval takes a Forth and strings and splits the string on space
- * characters, pushing each element on the stack or invoking the 
- * operator if it is found in the opmap. It returns TOS when it is done. 
- * it is an error to leave the stack non-Empty. 
+ * characters, pushing each element on the stack or invoking the
+ * operator if it is found in the opmap. It returns TOS when it is done.
+ * it is an error to leave the stack non-Empty.
  */
 func Eval(f Forth, s string) (ret string, err error) {
 	defer errRecover(&err)
@@ -166,8 +175,9 @@ func Eval(f Forth, s string) (ret string, err error) {
 
 }
 
-func toInt(f Forth) int {
-	i, err := strconv.Atoi(f.Pop())
+// toInt converts to int64.
+func toInt(f Forth) int64 {
+	i, err := strconv.ParseInt(f.Pop(), 0, 64)
 	if err != nil {
 		panic(err)
 	}
@@ -178,42 +188,42 @@ func plus(f Forth) {
 	x := toInt(f)
 	y := toInt(f)
 	z := x + y
-	f.Push(strconv.Itoa(z))
+	f.Push(strconv.FormatInt(z, 10))
 }
 
 func times(f Forth) {
 	x := toInt(f)
 	y := toInt(f)
 	z := x * y
-	f.Push(strconv.Itoa(z))
+	f.Push(strconv.FormatInt(z, 10))
 }
 
 func sub(f Forth) {
 	x := toInt(f)
 	y := toInt(f)
 	z := y - x
-	f.Push(strconv.Itoa(z))
+	f.Push(strconv.FormatInt(z, 10))
 }
 
 func div(f Forth) {
 	x := toInt(f)
 	y := toInt(f)
 	z := y / x
-	f.Push(strconv.Itoa(z))
+	f.Push(strconv.FormatInt(z, 10))
 }
 
 func mod(f Forth) {
 	x := toInt(f)
 	y := toInt(f)
 	z := y % x
-	f.Push(strconv.Itoa(z))
+	f.Push(strconv.FormatInt(z, 10))
 }
 
 func roundup(f Forth) {
 	rnd := toInt(f)
 	v := toInt(f)
 	v = ((v + rnd - 1) / rnd) * rnd
-	f.Push(strconv.Itoa(v))
+	f.Push(strconv.FormatInt(v, 10))
 }
 
 func swap(f Forth) {
@@ -267,4 +277,3 @@ func NewWord(f Forth, name, command string) {
 	opmap[name] = newword
 	return
 }
-
