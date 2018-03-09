15ef5717e16a426ed9483951e23b8dd625c6e910
Thien Hoang <thienhoang@google.com>
2018-03-09 15:05:11 -0800

    SoS: Register and Unregister work for unique services
    Signed-off-by: Thien Hoang <thienhoang@google.com>

--- cmds/sos/sos.go
+++ cmds/sos/sos.go
@@ -4,6 +4,42 @@
 
 package main
 
+import "sync"
+
+var (
+	RWLock   sync.RWMutex
+	Registry = make(map[string]int)
+)
+
+func read(serviceName string) (port int, exists bool) {
+	RWLock.RLock()
+	defer RWLock.RUnlock()
+	port, exists = Registry[serviceName]
+	return
+}
+
+func register(serviceName string, portNum int) {
+	RWLock.Lock()
+	defer RWLock.Unlock()
+	Registry[serviceName] = portNum
+}
+
+func unregister(serviceName string) {
+	RWLock.Lock()
+	defer RWLock.Unlock()
+	delete(Registry, serviceName)
+}
+
+func snapshotRegistry() map[string]int {
+	RWLock.RLock()
+	defer RWLock.RUnlock()
+	snapshot := make(map[string]int)
+	for name, port := range Registry {
+		snapshot[name] = port
+	}
+	return snapshot
+}
+
 func main() {
 	startServer()
 }
