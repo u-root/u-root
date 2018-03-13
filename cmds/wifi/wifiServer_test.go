fefeb552a735d300ca51716ee1c3c11a40c7c9ee
Thien Hoang <thienhoang@google.com>
2018-03-13 16:45:58 -0700

    Added test to wifiServer_test.go
    Signed-off-by: Thien Hoang <thienhoang@google.com>

--- cmds/wifi/wifiServer_test.go
+++ cmds/wifi/wifiServer_test.go
@@ -5,8 +5,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http/httptest"
 	"reflect"
+	"sync"
 	"testing"
 )
 
@@ -64,6 +68,11 @@ var (
 	}
 )
 
+func turnOnTestingMode() {
+	t := true
+	test = &t
+}
+
 func TestUserInputValidation(t *testing.T) {
 	for _, test := range userInputValidationTestcases {
 		out, err := userInputValidation(test.essid, test.pass, test.id)
@@ -73,4 +82,84 @@ func TestUserInputValidation(t *testing.T) {
 			t.Errorf("%s\ngot:[%v, %v]\nwant:[%v, %v]", fncCall, out, err, test.exp, test.err)
 		}
 	}
+}
+
+func TestConnectHandle(t *testing.T) {
+	// Set Up
+	turnOnTestingMode()
+	connectWifiArbitratorSetup("", "", 2)
+	defer close(ConnectReqChan)
+
+	m := ConnectJsonMsg{EssidStub, PassStub, IdStub}
+	b, err := json.Marshal(m)
+	if err != nil {
+		t.Errorf("Setup Fails")
+		return
+	}
+
+	r := httptest.NewRequest("GET", "localhost:"+PortNum+"/connect", bytes.NewBuffer(b))
+	w := httptest.NewRecorder()
+	connectHandle(w, r)
+	if CurEssid != EssidStub {
+		t.Errorf("\ngot:%v\nwant:%v", CurEssid, EssidStub)
+	}
+}
+
+func TestConnectHandleOneAfterAnother(t *testing.T) {
+	// Set Up
+	turnOnTestingMode()
+	connectWifiArbitratorSetup("", "", 2)
+	defer close(ConnectReqChan)
+
+	m1 := ConnectJsonMsg{"stub1", PassStub, IdStub}
+	b1, err := json.Marshal(m1)
+	if err != nil {
+		t.Errorf("Setup Fails")
+		return
+	}
+
+	m2 := ConnectJsonMsg{"stub2", PassStub, IdStub}
+	b2, err := json.Marshal(m2)
+	if err != nil {
+		t.Errorf("Setup Fails")
+		return
+	}
+
+	r1 := httptest.NewRequest("GET", "localhost:"+PortNum+"/connect", bytes.NewBuffer(b1))
+	w1 := httptest.NewRecorder()
+	connectHandle(w1, r1)
+
+	r2 := httptest.NewRequest("GET", "localhost:"+PortNum+"/connect", bytes.NewBuffer(b2))
+	w2 := httptest.NewRecorder()
+	connectHandle(w2, r2)
+
+	if CurEssid != "stub2" {
+		t.Errorf("\ngot:%v\nwant:%v", CurEssid, "stub2")
+	}
+}
+
+func TestConnectHandleRace(t *testing.T) {
+	// Set Up
+	turnOnTestingMode()
+	numGoRoutines := 100
+	connectWifiArbitratorSetup("", "", numGoRoutines)
+	defer close(ConnectReqChan)
+
+	var wg sync.WaitGroup
+	for i := 0; i < numGoRoutines; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			m := ConnectJsonMsg{EssidStub, PassStub, IdStub}
+			b, err := json.Marshal(m)
+			if err != nil {
+				t.Errorf("Setup Fails")
+				return
+			}
+			r := httptest.NewRequest("GET", "localhost:"+PortNum+"/connect", bytes.NewBuffer(b))
+			w := httptest.NewRecorder()
+			connectHandle(w, r)
+		}()
+	}
+	wg.Wait()
 }
