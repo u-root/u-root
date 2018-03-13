7363b458f00648ea22eeb31072d6e61ad9a4a1d8
Christopher Koch <chrisko@google.com>
2018-03-13 15:12:11 -0700

    vendor: update all dependencies.

--- vendor/github.com/u-root/dhcp4/dhcp4opts/options.go
+++ vendor/github.com/u-root/dhcp4/dhcp4opts/options.go
@@ -1,3 +1,7 @@
+// Copyright 2018 the u-root Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package dhcp4opts implements Options parsing for DHCPv4 options as described in RFC 2132.
 //
 // Not all options are currently implemented.
