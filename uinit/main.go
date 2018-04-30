9b17daa6ad24479042e67ebc3aa673ddabba4060
Andrea Barberio <insomniac@slackware.it>
2018-04-30 21:29:59 +0100

    Replaced debug flag with quiet flag

--- uinit/main.go
+++ uinit/main.go
@@ -10,8 +10,8 @@ import (
 // TODO allow to specify boot sequence
 
 var (
-	doDebug  = flag.Bool("d", false, "Enable debug mode for subcommands")
-	interval = flag.Int("I", 0, "Interval in seconds before looping to the next boot command")
+	doQuiet  = flag.Bool("q", false, "Disable verbose output")
+	interval = flag.Int("I", 1, "Interval in seconds before looping to the next boot command")
 )
 
 var bootsequence = [][]string{
@@ -24,14 +24,11 @@ func main() {
 
 	log.Print("Starting boot sequence, press CTRL-C within 5 seconds to drop into a shell")
 	time.Sleep(5 * time.Second)
-	if *doDebug {
-		log.Print("Debug mode enabled")
-	}
 
 	sleepInterval := time.Duration(*interval) * time.Second
 	for {
 		for _, bootcmd := range bootsequence {
-			if *doDebug {
+			if !*doQuiet {
 				bootcmd = append(bootcmd, "-d")
 			}
 			log.Printf("Running boot command: %v", bootcmd)
@@ -40,7 +37,7 @@ func main() {
 				log.Printf("Error executing %s: %v", cmd, err)
 			}
 		}
-		if *doDebug {
+		if !*doQuiet {
 			log.Printf("Sleeping %v before attempting next boot command", sleepInterval)
 		}
 		time.Sleep(sleepInterval)
