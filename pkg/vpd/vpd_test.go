991415dc680f04121bf14b58d9297dcb19ced54d
insomniac <insomniacslk@users.noreply.github.com>
2018-05-05 01:06:20 +0200

    Added simple VPD library (#7)
    Implemented a simple VPD library, using the sysfs interface at /sys/firmware/vpd . See https://chromium.googlesource.com/chromiumos/platform/vpd/ for details.
    
    There are three methods, Get, Set, and GetAll. Unfortunately Set doesn't currently work, because the sysfs interface does not support writing. To write, this library needs a backend able to write to flash chips, like the command line tool flashrom[1] or flashtools[2].
    
    [1] https://www.flashrom.org
    [2] https://github.com/osresearch/flashtools

---
+++ pkg/vpd/vpd_test.go
@@ -0,0 +1,42 @@
+package vpd
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetReadOnly(t *testing.T) {
+	VpdDir = "./tests"
+	value, err := Get("key1", true)
+	require.NoError(t, err)
+	require.Equal(t, value, []byte("value1\n"))
+}
+
+func TestGetReadWrite(t *testing.T) {
+	VpdDir = "./tests"
+	value, err := Get("mysecretpassword", false)
+	require.NoError(t, err)
+	require.Equal(t, value, []byte("passw0rd\n"))
+}
+
+func TestGetReadBinary(t *testing.T) {
+	VpdDir = "./tests"
+	value, err := Get("binary1", true)
+	require.NoError(t, err)
+	require.Equal(t, value, []byte("some\x00binary\ndata"))
+}
+
+func TestGetAllReadOnly(t *testing.T) {
+	VpdDir = "./tests"
+	expected := map[string][]byte{
+		"binary1": []byte("some\x00binary\ndata"),
+		"key1":    []byte("value1\n"),
+	}
+	vpdMap, err := GetAll(true)
+	require.NoError(t, err)
+	if !reflect.DeepEqual(vpdMap, expected) {
+		t.FailNow()
+	}
+}
