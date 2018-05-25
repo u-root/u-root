16260fbc8a807e536814e7eec0be9eba47722cfb
Andrea Barberio <insomniac@slackware.it>
2018-05-25 21:12:08 +0100

    Linter

--- pkg/tpm/tpm.go
+++ pkg/tpm/tpm.go
@@ -1,5 +1,8 @@
 package tpm
 
+// TPM is an interface that both TPM1 and TPM2 have to implement. It requires a
+// common subset of methods that both TPM versions have to implement.
+// Version-specific methods have to be implemented in the relevant object.
 type TPM interface {
 	Version() string
 	SetupTPM() error
