3b11141de94692ebf26abe1b38be32541613d151
Ronald G. Minnich <rminnich@gmail.com>
2018-05-25 19:45:04 -0700

    WIP: enable special processing of inito
    Some systems need special processing of inito
    
    Define an empty function, osInitGo, that can be written
    at init time, and move some special Linux setup there.
    Note that on non-Linux systems, osInitGo will do nothing.
    
    As an example, add init_linux.go
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/init/init_linux.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"syscall"
+
+	"github.com/u-root/u-root/pkg/cmdline"
+)
+
+func init() {
+	osInitGo = runOSInitGo
+}
+
+func runOSInitGo() {
+	// systemd is "special". If we are supposed to run systemd, we're
+	// going to exec, and if we're going to exec, we're done here.
+	// systemd uber alles.
+	initFlags := cmdline.GetInitFlagMap()
+	// systemd gets upset when it discovers it isn't really process 1, so
+	// we can't start it in its own namespace. I just love systemd.
+	systemd, present := initFlags["systemd"]
+	systemdEnabled, boolErr := strconv.ParseBool(systemd)
+	if present && boolErr == nil && systemdEnabled == true {
+		v := cmdList[0]
+		debug("Exec %v", v)
+		if err := syscall.Exec(v, []string{v}, envs); err != nil {
+			log.Printf("Lucky you, systemd failed: %v", err)
+		}
+		// well, what a shame.
+		cmdList = cmdList[1:]
+		cmdCount++
+	}
+}
