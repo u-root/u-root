bdf1401ce082c6b0cb59ebab04a7dce0f9a7f693
Trevor Farrelly <trevorfarrelly@google.com>
2018-06-08 15:42:30 -0700

    Added basic Upspin frontend for future SoS integration
    Signed-off-by: Trevor Farrelly <trevorfarrelly@google.com>

---
+++ cmds/upspin/upspin.go
@@ -0,0 +1,14 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+func main() {
+	NewUpspinServer().Start()
+}
