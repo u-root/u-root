2c2931d836eff33b2aea085a9206efa29199f90c
Ryan O'Leary <ryanoleary@google.com>
2018-08-09 17:44:27 -0700

    integration: Run more tests in QEMU
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- integration/gotest_test.go
+++ integration/gotest_test.go
@@ -12,8 +12,53 @@ import (
 	"path"
 	"path/filepath"
 	"sort"
+	"strings"
 	"testing"
+	"time"
 )
+
+// testPkgs returns a slice of tests to run.
+func testPkgs(t *testing.T) []string {
+	// Packages which do not contain tests (or do not contain tests for the
+	// build target) will still compile a test binary which vacuously pass.
+	cmd := exec.Command("go", "list",
+		"github.com/u-root/u-root/cmds/...",
+		// TODO: only running tests in cmds because tests in pkg have
+		// duplicate names which confuses the test runner. This should
+		// get fixed.
+		// "github.com/u-root/u-root/xcmds/...",
+		// "github.com/u-root/u-root/pkg/...",
+	)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pkgs := strings.Fields(strings.TrimSpace(string(out)))
+
+	// TODO: Some tests do not run properly in QEMU at the moment. They are
+	// blacklisted. These tests fail for mostly two reasons:
+	// 1. either it requires networking (not enabled in the kernel)
+	// 2. or it depends on some test files (for example /bin/sleep)
+	blacklist := []string{
+		"github.com/u-root/u-root/cmds/cmp",
+		"github.com/u-root/u-root/cmds/dhclient",
+		"github.com/u-root/u-root/cmds/fmap",
+		"github.com/u-root/u-root/cmds/kill",
+		"github.com/u-root/u-root/cmds/tail",
+		"github.com/u-root/u-root/cmds/wget",
+		"github.com/u-root/u-root/cmds/which",
+		"github.com/u-root/u-root/cmds/wifi",
+	}
+	for i := 0; i < len(pkgs); i++ {
+		for _, b := range blacklist {
+			if pkgs[i] == b {
+				pkgs = append(pkgs[:i], pkgs[i+1:]...)
+			}
+		}
+	}
+
+	return pkgs
+}
 
 // TestGoTest effectively runs "go test ./..." inside a QEMU instance. The
 // tests run as root and can do all sorts of things not possible otherwise.
@@ -24,19 +69,13 @@ func TestGoTest(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Create list of packages to test.
-	// TODO: include all packages in cmds
-	pkgs := []string{
-		"github.com/u-root/u-root/cmds/ls",
-		"github.com/u-root/u-root/cmds/mknod",
-	}
-
 	// Statically build tests and add them to the temporary directory.
+	pkgs := testPkgs(t)
 	os.Setenv("CGO_ENABLED", "0")
 	testDir := filepath.Join(tmpDir, "tests")
 	for _, pkg := range pkgs {
 		testFile := filepath.Join(testDir, path.Base(pkg))
-		cmd := exec.Command("go", "test", "-c", pkg, "-o", testFile)
+		cmd := exec.Command("go", "test", "-ldflags", "-s -w", "-c", pkg, "-o", testFile)
 		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 		if err := cmd.Run(); err != nil {
 			t.Fatalf("could not build %s: %v", pkg, err)
