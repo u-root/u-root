a36ddde122c7c71f0cf25faef8e70eb28bfebbed
Andrea Barberio <insomniac@slackware.it>
2018-08-28 14:17:50 +0100

    pkg/bootconfig: Added Manifest.GetBootConfig, and tests

--- pkg/bootconfig/bootconfig.go
+++ pkg/bootconfig/bootconfig.go
@@ -1,16 +1,46 @@
 package bootconfig
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+)
 
 // BootConfig is a general-purpose boot configuration. It draws some
 // characteristics from FIT but it's not compatible with it. It uses
 // JSON for interoperability.
+// If you add or remove fields, remember to update UnmarshalJSON.
 type BootConfig struct {
 	Name       string `json:"name"`
 	Kernel     string `json:"kernel"`
 	Initramfs  string `json:"initramfs,omitempty"`
 	KernelArgs string `json:"kernel_args,omitempty"`
 	DeviceTree string `json:"devicetree,omitempty"`
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface to enforce the
+// presence of required fields.
+func (bc *BootConfig) UnmarshalJSON(data []byte) error {
+	// Alias is needed to avoid an infinite loop of inherited and overridden
+	// UnmarshalJSON
+	type Alias BootConfig
+	newBc := Alias{}
+	err := json.Unmarshal(data, &newBc)
+	if err != nil {
+		return err
+	}
+	if newBc.Name == "" {
+		return errors.New("Name field cannot be empty or missing")
+	}
+	if newBc.Kernel == "" {
+		return errors.New("Kernel field cannot be empty or missing")
+	}
+	// either this, or reflection :(
+	bc.Name = newBc.Name
+	bc.Kernel = newBc.Kernel
+	bc.Initramfs = newBc.Initramfs
+	bc.KernelArgs = newBc.KernelArgs
+	bc.DeviceTree = newBc.DeviceTree
+	return nil
 }
 
 // NewBootConfig parses a boot configuration in JSON format and returns a
---
+++ pkg/bootconfig/bootconfig_test.go
@@ -0,0 +1,56 @@
+package bootconfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewBootConfig(t *testing.T) {
+	data := []byte(`{
+	"name": "some_conf",
+	"kernel": "/path/to/kernel",
+	"initramfs": "/path/to/initramfs",
+	"kernel_args": "init=/bin/bash",
+	"devicetree": "some data here"
+}`)
+	c, err := NewBootConfig(data)
+	require.NoError(t, err)
+	require.Equal(t, "some_conf", c.Name)
+	require.Equal(t, "/path/to/kernel", c.Kernel)
+	require.Equal(t, "/path/to/initramfs", c.Initramfs)
+	require.Equal(t, "init=/bin/bash", c.KernelArgs)
+	require.Equal(t, "some data here", c.DeviceTree)
+}
+
+func TestNewBootConfigInvalidJSON(t *testing.T) {
+	data := []byte(`{
+	"name": "broken
+}`)
+	_, err := NewBootConfig(data)
+	require.Error(t, err)
+}
+
+func TestNewBootConfigMissingName(t *testing.T) {
+	data := []byte(`{
+	"name_is_missing": "some_conf",
+	"kernel": "/path/to/kernel",
+	"initramfs": "/path/to/initramfs",
+	"kernel_args": "init=/bin/bash",
+	"devicetree": "some data here"
+}`)
+	_, err := NewBootConfig(data)
+	require.Error(t, err)
+}
+
+func TestNewBootConfigMissingKernel(t *testing.T) {
+	data := []byte(`{
+	"name": "some_conf",
+	"kernel_is_missing": "/path/to/kernel",
+	"initramfs": "/path/to/initramfs",
+	"kernel_args": "init=/bin/bash",
+	"devicetree": "some data here"
+}`)
+	_, err := NewBootConfig(data)
+	require.Error(t, err)
+}
