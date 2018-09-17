6cb5e5badd8a742c9dff8a6dd1a07a024c81181b
Daniel Schaefer <git@danielschaefer.me>
2018-09-17 12:24:57 +0200

    mv: Call command in testsuite, not the function
    This checks the entire command including argparsing. Previously it would
    directly call an intermediate function.

--- cmds/mv/mv_test.go
+++ cmds/mv/mv_test.go
@@ -12,6 +12,8 @@ import (
 	"path/filepath"
 	"testing"
 	"time"
+
+	"github.com/u-root/u-root/pkg/testutil"
 )
 
 type makeit struct {
@@ -67,11 +69,6 @@ func setup() (string, error) {
 	return d, nil
 }
 
-func resetFlags() {
-	// TODO: Is it possible to extract the default out of the declaration?
-	*update = false
-}
-
 func TestMv(t *testing.T) {
 	d, err := setup()
 	if err != nil {
@@ -80,27 +77,35 @@ func TestMv(t *testing.T) {
 	defer os.RemoveAll(d)
 
 	fmt.Println("Renaming file...")
-	files1 := []string{filepath.Join(d, "hi1.txt"), filepath.Join(d, "hi4.txt")}
-	if err := mv(files1, false); err != nil {
-		t.Error(err)
+	{
+		files := []string{filepath.Join(d, "hi1.txt"), filepath.Join(d, "hi4.txt")}
+		res := testutil.Command(t, files...)
+		_, err = res.CombinedOutput()
+		if err = testutil.IsExitCode(err, 0); err != nil {
+			t.Error(err)
+		}
 	}
 
 	dsub := filepath.Join(d, "hi.sub.dir")
 
 	fmt.Println("Moving files to directory...")
-	files2 := []string{filepath.Join(d, "hi2.txt"), filepath.Join(d, "hi4.txt"), dsub}
-	if err := mv(files2, true); err != nil {
-		t.Error(err)
+	{
+		files := []string{filepath.Join(d, "hi2.txt"), filepath.Join(d, "hi4.txt"), dsub}
+		res := testutil.Command(t, files...)
+		_, err = res.CombinedOutput()
+		if err = testutil.IsExitCode(err, 0); err != nil {
+			t.Error(err)
+		}
 	}
 }
 
 func TestMvUpdate(t *testing.T) {
 	*update = true
-	defer resetFlags()
 	d, err := setup()
 	if err != nil {
 		t.Error(err)
 	}
+	defer os.RemoveAll(d)
 	fmt.Println("Testing mv -u...")
 
 	// Ensure that the newer file actually has a newer timestamp
@@ -116,31 +121,43 @@ func TestMvUpdate(t *testing.T) {
 	}
 
 	// Check that it doesn't downgrade files with -u switch
-	var files1 = []string{filepath.Join(d, old.n), filepath.Join(d, new.n)}
-	if err := mv(files1, false); err != nil {
-		t.Error(err)
-	}
-	newContent1, err := ioutil.ReadFile(filepath.Join(d, new.n))
-	if err != nil {
-		t.Error(err)
-	}
-	if bytes.Equal(newContent1, old.c) {
-		t.Error("Newer file was overwritten by older file. Should not happen with -u.")
+	{
+		files := []string{"-u", filepath.Join(d, old.n), filepath.Join(d, new.n)}
+		res := testutil.Command(t, files...)
+		_, err = res.CombinedOutput()
+		if err = testutil.IsExitCode(err, 0); err != nil {
+			t.Error(err)
+		}
+		newContent, err := ioutil.ReadFile(filepath.Join(d, new.n))
+		if err != nil {
+			t.Error(err)
+		}
+		if bytes.Equal(newContent, old.c) {
+			t.Error("Newer file was overwritten by older file. Should not happen with -u.")
+		}
 	}
 
 	// Check that it does update files with -u switch
-	var files2 = []string{filepath.Join(d, new.n), filepath.Join(d, old.n)}
-	if err := mv(files2, false); err != nil {
-		t.Error(err)
+	{
+		files := []string{"-u", filepath.Join(d, new.n), filepath.Join(d, old.n)}
+		res := testutil.Command(t, files...)
+		_, err = res.CombinedOutput()
+		if err = testutil.IsExitCode(err, 0); err != nil {
+			t.Error(err)
+		}
+		newContent, err := ioutil.ReadFile(filepath.Join(d, old.n))
+		if err != nil {
+			t.Error(err)
+		}
+		if !bytes.Equal(newContent, new.c) {
+			t.Error("Older file was not overwritten by newer file. Should happen with -u.")
+		}
+		if _, err := os.Lstat(filepath.Join(d, old.n)); err != nil {
+			t.Error("The new file shouldn't be there anymore.")
+		}
 	}
-	newContent2, err := ioutil.ReadFile(filepath.Join(d, old.n))
-	if err != nil {
-		t.Error(err)
-	}
-	if !bytes.Equal(newContent2, new.c) {
-		t.Error("Older file was not overwritten by newer file. Should happen with -u.")
-	}
-	if _, err := os.Lstat(filepath.Join(d, old.n)); err != nil {
-		t.Error("The new file shouldn't be there anymore.")
-	}
+}
+
+func TestMain(m *testing.M) {
+	testutil.Run(m, main)
 }
