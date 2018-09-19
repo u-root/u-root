u-root/u-root

commit 29cb33b6981c16d5ca8c7f8687a3bee6f2121ed3

initramfs: add directories' children directly.
Previously, directories' children were added when the archive was
written out to OutputFile.

This change was made so conflicting files can be found before writing
the archive. E.g. take the following directory tree:

  -/bin
  -/bin/foo (file)
  -/bin/bar (file)

Then, make the following calls:

  AddFile(/bin, bin)
  AddFile(/bin/foo, bin/bar)

This conflict wouldn't be caught until writing out the archive, when it
could be caught much earlier.

pkg/uroot/initramfs/files.go
@@ -12,12 +12,9 @@ import (
 	"sort"
 
 	"github.com/u-root/u-root/pkg/cpio"
-	"golang.org/x/sys/unix"
 )
 
-// Files are host files and cpio records to add to an initramfs.
-//
-// The initramfs can be written out using Files.WriteTo.
+// Files are host files and records to add to the resulting initramfs.
 type Files struct {
 	// Files is a map of relative archive path -> absolute host file path.
 	Files map[string]string
@@ -31,15 +28,15 @@ type Files struct {
 }
 
 // NewFiles returns a new archive files map.
-func NewFiles() Files {
-	return Files{
+func NewFiles() *Files {
+	return &Files{
 		Files:   make(map[string]string),
 		Records: make(map[string]cpio.Record),
 	}
 }
 
-// SortedKeys returns a list of sorted paths in the archive.
-func (af Files) SortedKeys() []string {
+// sortedKeys returns a list of sorted paths in the archive.
+func (af *Files) sortedKeys() []string {
 	keys := make([]string, 0, len(af.Files)+len(af.Records))
 	for dest := range af.Files {
 		keys = append(keys, dest)
@@ -51,8 +48,13 @@ func (af Files) SortedKeys() []string {
 	return keys
 }
 
-// AddFile adds a host file at `src` into the archive at `dest`.
-func (af Files) AddFile(src string, dest string) error {
+// AddFile adds a host file at src into the archive at dest.
+//
+// If src is a directory, it and its children will be added to the archive
+// relative to dest.
+//
+// Duplicate files with identical content will be silently ignored.
+func (af *Files) AddFile(src string, dest string) error {
 	src = filepath.Clean(src)
 	dest = path.Clean(dest)
 	if path.IsAbs(dest) {
@@ -62,9 +64,32 @@ func (af Files) AddFile(src string, dest string) error {
 		return fmt.Errorf("source file %q (-> %q) must be absolute", src, dest)
 	}
 
-	if _, ok := af.Records[dest]; ok {
-		return fmt.Errorf("record for %q already exists in archive", dest)
+	// We check if it's a directory first. If a directory already exists as
+	// a record or file, we want to include its children anyway.
+	sInfo, err := os.Stat(src)
+	if err != nil {
+		return fmt.Errorf("adding file %q to archive failed: %v", src, err)
 	}
+
+	// Recursively add children.
+	if sInfo.Mode().IsDir() {
+		err := children(src, func(name string) error {
+			return af.AddFile(filepath.Join(src, name), filepath.Join(dest, name))
+		})
+		if err != nil {
+			return err
+		}
+
+		// Only override an existing directory if all children were
+		// added successfully.
+		af.Files[dest] = src
+		return nil
+	}
+
+	if record, ok := af.Records[dest]; ok {
+		return fmt.Errorf("record for %q already exists in archive: %v", dest, record)
+	}
+
 	if srcFile, ok := af.Files[dest]; ok {
 		// Just a duplicate.
 		if src == srcFile {
@@ -78,20 +103,20 @@ func (af Files) AddFile(src string, dest string) error {
 }
 
 // AddRecord adds a cpio.Record into the archive at `r.Name`.
-func (af Files) AddRecord(r cpio.Record) error {
+func (af *Files) AddRecord(r cpio.Record) error {
 	r.Name = path.Clean(r.Name)
 	if filepath.IsAbs(r.Name) {
 		return fmt.Errorf("record name %q must not be absolute", r.Name)
 	}
 
-	if _, ok := af.Files[r.Name]; ok {
-		return fmt.Errorf("record for %q already exists in archive", r.Name)
+	if src, ok := af.Files[r.Name]; ok {
+		return fmt.Errorf("record for %q already exists in archive: file %q", r.Name, src)
 	}
 	if rr, ok := af.Records[r.Name]; ok {
 		if rr.Info == r.Info {
 			return nil
 		}
-		return fmt.Errorf("record for %q already exists in archive", r.Name)
+		return fmt.Errorf("record for %q already exists in archive: %v", r.Name, rr)
 	}
 
 	af.Records[r.Name] = r
@@ -99,14 +124,14 @@ func (af Files) AddRecord(r cpio.Record) error {
 }
 
 // Contains returns whether path `dest` is already contained in the archive.
-func (af Files) Contains(dest string) bool {
+func (af *Files) Contains(dest string) bool {
 	_, fok := af.Files[dest]
 	_, rok := af.Records[dest]
 	return fok || rok
 }
 
 // Rename renames a file in the archive.
-func (af Files) Rename(name string, newname string) {
+func (af *Files) Rename(name string, newname string) {
 	if src, ok := af.Files[name]; ok {
 		delete(af.Files, name)
 		af.Files[newname] = src
@@ -119,7 +144,7 @@ func (af Files) Rename(name string, newname string) {
 }
 
 // addParent recursively adds parent directory records for `name`.
-func (af Files) addParent(name string) {
+func (af *Files) addParent(name string) {
 	parent := path.Dir(name)
 	if parent == "." {
 		return
@@ -131,7 +156,7 @@ func (af Files) addParent(name string) {
 }
 
 // fillInParents adds parent directory records for unparented files in `af`.
-func (af Files) fillInParents() {
+func (af *Files) fillInParents() {
 	for name := range af.Files {
 		af.addParent(name)
 	}
@@ -141,13 +166,13 @@ func (af Files) fillInParents() {
 }
 
 // WriteTo writes all records and files in `af` to `w`.
-func (af Files) WriteTo(w Writer) error {
+func (af *Files) WriteTo(w Writer) error {
 	// Add parent directories when not added specifically.
 	af.fillInParents()
 
 	// Reproducible builds: Files should be added to the archive in the
 	// same order.
-	for _, path := range af.SortedKeys() {
+	for _, path := range af.sortedKeys() {
 		if record, ok := af.Records[path]; ok {
 			if err := w.WriteRecord(record); err != nil {
 				return err
@@ -174,16 +199,7 @@ func writeFile(w Writer, src, dest string) error {
 
 	// Fix the name.
 	record.Name = dest
-	if err := w.WriteRecord(cpio.MakeReproducible(record)); err != nil {
-		return err
-	}
-
-	if record.Info.Mode&unix.S_IFMT == unix.S_IFDIR {
-		return children(src, func(name string) error {
-			return writeFile(w, filepath.Join(src, name), filepath.Join(dest, name))
-		})
-	}
-	return nil
+	return w.WriteRecord(cpio.MakeReproducible(record))
 }
 
 // children calls `fn` on all direct children of directory `dir`.

pkg/uroot/builder/bb.go
@@ -41,7 +41,7 @@ func (BBBuilder) DefaultBinaryDir() string {
 }
 
 // Build is an implementation of Builder.Build for a busybox-like initramfs.
-func (BBBuilder) Build(af initramfs.Files, opts Opts) error {
+func (BBBuilder) Build(af *initramfs.Files, opts Opts) error {
 	// Build the busybox binary.
 	bbPath := filepath.Join(opts.TempDir, "bb")
 	if err := bb.BuildBusybox(opts.Env, opts.Packages, bbPath); err != nil {

pkg/uroot/builder/binary.go
@@ -25,7 +25,7 @@ func (BinaryBuilder) DefaultBinaryDir() string {
 }
 
 // Build implements Builder.Build.
-func (BinaryBuilder) Build(af initramfs.Files, opts Opts) error {
+func (BinaryBuilder) Build(af *initramfs.Files, opts Opts) error {
 	result := make(chan error, len(opts.Packages))
 	var wg sync.WaitGroup
 

pkg/uroot/builder/builder.go
@@ -48,7 +48,7 @@ type Opts struct {
 type Builder interface {
 	// Build uses the given options to build Go packages and adds its files
 	// to be included in the initramfs to the given ArchiveFiles.
-	Build(initramfs.Files, Opts) error
+	Build(*initramfs.Files, Opts) error
 
 	// DefaultBinaryDir is the initramfs' default directory for binaries
 	// built using this builder.

pkg/uroot/builder/source.go
@@ -35,7 +35,7 @@ func (SourceBuilder) DefaultBinaryDir() string {
 }
 
 // Build is an implementation of Builder.Build.
-func (SourceBuilder) Build(af initramfs.Files, opts Opts) error {
+func (SourceBuilder) Build(af *initramfs.Files, opts Opts) error {
 	// TODO: this is a failure to collect the correct dependencies.
 	if err := af.AddFile(filepath.Join(opts.Env.GOROOT, "pkg/include"), "go/pkg/include"); err != nil {
 		return err
@@ -51,7 +51,7 @@ func (SourceBuilder) Build(af initramfs.Files, opts Opts) error {
 		}
 
 		// Add high-level packages' src files to archive.
-		p := goListPkg(opts, pkg, &af)
+		p := goListPkg(opts, pkg, af)
 		if p == nil {
 			continue
 		}
@@ -73,7 +73,7 @@ func (SourceBuilder) Build(af initramfs.Files, opts Opts) error {
 
 	// Add src files of dependencies to archive.
 	for dep := range deps {
-		goListPkg(opts, dep, &af)
+		goListPkg(opts, dep, af)
 	}
 
 	// Add Go toolchain.

pkg/uroot/initramfs/archive.go
@@ -65,7 +65,7 @@ type Opts struct {
 	//
 	// Files here generally have priority over files in DefaultRecords or
 	// BaseArchive.
-	Files
+	*Files
 
 	// OutputFile is the file to write to.
 	OutputFile Writer

pkg/uroot/initramfs/files_test.go
@@ -5,7 +5,6 @@
 package initramfs
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -16,38 +15,56 @@ import (
 	"testing"
 
 	"github.com/u-root/u-root/pkg/cpio"
-	"golang.org/x/sys/unix"
 )
 
 func TestFilesAddFile(t *testing.T) {
+	regularFile, err := ioutil.TempFile("", "archive-files-add-file")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(regularFile.Name())
+
+	dir, err := ioutil.TempDir("", "archive-add-files")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(dir)
+
+	os.Create(filepath.Join(dir, "foo"))
+	os.Create(filepath.Join(dir, "foo2"))
+
 	for i, tt := range []struct {
-		af          Files
+		name        string
+		af          *Files
 		src         string
 		dest        string
-		result      Files
+		result      *Files
 		errContains string
 	}{
 		{
+			name: "just add a file",
 			af:   NewFiles(),
-			src:  "/foo/bar",
+
+			src:  regularFile.Name(),
 			dest: "bar/foo",
 
-			result: Files{
+			result: &Files{
 				Files: map[string]string{
-					"bar/foo": "/foo/bar",
+					"bar/foo": regularFile.Name(),
 				},
 				Records: map[string]cpio.Record{},
 			},
 		},
 		{
-			af: Files{
+			name: "add file that exists in Files",
+			af: &Files{
 				Files: map[string]string{
 					"bar/foo": "/some/other/place",
 				},
 			},
-			src:  "/foo/bar",
+			src:  regularFile.Name(),
 			dest: "bar/foo",
-			result: Files{
+			result: &Files{
 				Files: map[string]string{
 					"bar/foo": "/some/other/place",
 				},
@@ -55,14 +72,15 @@ func TestFilesAddFile(t *testing.T) {
 			errContains: "already exists in archive",
 		},
 		{
-			af: Files{
+			name: "add a file that exists in Records",
+			af: &Files{
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", "/some/other/place"),
 				},
 			},
-			src:  "/foo/bar",
+			src:  regularFile.Name(),
 			dest: "bar/foo",
-			result: Files{
+			result: &Files{
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", "/some/other/place"),
 				},
@@ -70,31 +88,88 @@ func TestFilesAddFile(t *testing.T) {
 			errContains: "already exists in archive",
 		},
 		{
-			af: Files{
+			name: "add a file that already exists in Files, but is the same one",
+			af: &Files{
 				Files: map[string]string{
-					"bar/foo": "/foo/bar",
+					"bar/foo": regularFile.Name(),
 				},
 			},
-			src:  "/foo/bar",
+			src:  regularFile.Name(),
 			dest: "bar/foo",
-			result: Files{
+			result: &Files{
 				Files: map[string]string{
-					"bar/foo": "/foo/bar",
+					"bar/foo": regularFile.Name(),
 				},
 			},
 		},
 		{
-			src:         "/foo/bar",
+			name:        "destination path must not be absolute",
+			src:         regularFile.Name(),
 			dest:        "/bar/foo",
 			errContains: "must not be absolute",
 		},
 		{
+			name:        "source path must be absolute",
 			src:         "foo/bar",
 			dest:        "bar/foo",
 			errContains: "must be absolute",
 		},
+		{
+			name: "add a directory",
+			af: &Files{
+				Files: map[string]string{},
+			},
+			src:  dir,
+			dest: "bar/foo",
+			result: &Files{
+				Files: map[string]string{
+					"bar/foo":      dir,
+					"bar/foo/foo":  filepath.Join(dir, "foo"),
+					"bar/foo/foo2": filepath.Join(dir, "foo2"),
+				},
+			},
+		},
+		{
+			name: "add a different directory to the same destination, no overlapping children",
+			af: &Files{
+				Files: map[string]string{
+					"bar/foo":     "/some/place/real",
+					"bar/foo/zed": "/some/place/real/zed",
+				},
+			},
+			src:  dir,
+			dest: "bar/foo",
+			result: &Files{
+				Files: map[string]string{
+					"bar/foo":      dir,
+					"bar/foo/foo":  filepath.Join(dir, "foo"),
+					"bar/foo/foo2": filepath.Join(dir, "foo2"),
+					"bar/foo/zed":  "/some/place/real/zed",
+				},
+			},
+		},
+		{
+			name: "add a different directory to the same destination, overlapping children",
+			af: &Files{
+				Files: map[string]string{
+					"bar/foo":      "/some/place/real",
+					"bar/foo/foo2": "/some/place/real/zed",
+				},
+			},
+			src:  dir,
+			dest: "bar/foo",
+			result: &Files{
+				Files: map[string]string{
+					// This is weird. Half-done operation.
+					"bar/foo/foo":  filepath.Join(dir, "foo"),
+					"bar/foo":      "/some/place/real",
+					"bar/foo/foo2": "/some/place/real/zed",
+				},
+			},
+			errContains: "already exists in archive",
+		},
 	} {
-		t.Run(fmt.Sprintf("Test %02d", i), func(t *testing.T) {
+		t.Run(fmt.Sprintf("Test %02d: %s", i, tt.name), func(t *testing.T) {
 			err := tt.af.AddFile(tt.src, tt.dest)
 			if err != nil && !strings.Contains(err.Error(), tt.errContains) {
 				t.Errorf("Error is %v, does not contain %v", err, tt.errContains)
@@ -112,16 +187,16 @@ func TestFilesAddFile(t *testing.T) {
 
 func TestFilesAddRecord(t *testing.T) {
 	for i, tt := range []struct {
-		af     Files
+		af     *Files
 		record cpio.Record
 
-		result      Files
+		result      *Files
 		errContains string
 	}{
 		{
 			af:     NewFiles(),
 			record: cpio.Symlink("bar/foo", ""),
-			result: Files{
+			result: &Files{
 				Files: map[string]string{},
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", ""),
@@ -129,13 +204,13 @@ func TestFilesAddRecord(t *testing.T) {
 			},
 		},
 		{
-			af: Files{
+			af: &Files{
 				Files: map[string]string{
 					"bar/foo": "/some/other/place",
 				},
 			},
 			record: cpio.Symlink("bar/foo", ""),
-			result: Files{
+			result: &Files{
 				Files: map[string]string{
 					"bar/foo": "/some/other/place",
 				},
@@ -143,13 +218,13 @@ func TestFilesAddRecord(t *testing.T) {
 			errContains: "already exists in archive",
 		},
 		{
-			af: Files{
+			af: &Files{
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", "/some/other/place"),
 				},
 			},
 			record: cpio.Symlink("bar/foo", ""),
-			result: Files{
+			result: &Files{
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", "/some/other/place"),
 				},
@@ -157,13 +232,13 @@ func TestFilesAddRecord(t *testing.T) {
 			errContains: "already exists in archive",
 		},
 		{
-			af: Files{
+			af: &Files{
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", "/some/other/place"),
 				},
 			},
 			record: cpio.Symlink("bar/foo", "/some/other/place"),
-			result: Files{
+			result: &Files{
 				Records: map[string]cpio.Record{
 					"bar/foo": cpio.Symlink("bar/foo", "/some/other/place"),
 				},
@@ -192,16 +267,16 @@ func TestFilesAddRecord(t *testing.T) {
 
 func TestFilesfillInParent(t *testing.T) {
 	for i, tt := range []struct {
-		af     Files
-		result Files
+		af     *Files
+		result *Files
 	}{
 		{
-			af: Files{
+			af: &Files{
 				Records: map[string]cpio.Record{
 					"foo/bar": cpio.Directory("foo/bar", 0777),
 				},
 			},
-			result: Files{
+			result: &Files{
 				Records: map[string]cpio.Record{
 					"foo/bar": cpio.Directory("foo/bar", 0777),
 					"foo":     cpio.Directory("foo", 0755),
@@ -209,7 +284,7 @@ func TestFilesfillInParent(t *testing.T) {
 			},
 		},
 		{
-			af: Files{
+			af: &Files{
 				Files: map[string]string{
 					"baz/baz/baz": "/somewhere",
 				},
@@ -217,7 +292,7 @@ func TestFilesfillInParent(t *testing.T) {
 					"foo/bar": cpio.Directory("foo/bar", 0777),
 				},
 			},
-			result: Files{
+			result: &Files{
 				Files: map[string]string{
 					"baz/baz/baz": "/somewhere",
 				},
@@ -230,8 +305,8 @@ func TestFilesfillInParent(t *testing.T) {
 			},
 		},
 		{
-			af:     Files{},
-			result: Files{},
+			af:     &Files{},
+			result: &Files{},
 		},
 	} {
 		t.Run(fmt.Sprintf("Test %02d", i), func(t *testing.T) {
@@ -298,99 +373,6 @@ func sameNameModeContent(r1 cpio.Record, r2 cpio.Record) bool {
 	return cpio.ReaderAtEqual(r1.ReaderAt, r2.ReaderAt)
 }
 
-func TestWriteFile(t *testing.T) {
-	unix.Umask(0)
-
-	for i, tt := range []struct {
-		ma   *MockArchiver
-		src  func() string
-		dest string
-		err  error
-		want Records
-	}{
-		{
-			ma: &MockArchiver{
-				Records: make(Records),
-			},
-			src: func() string {
-				f, err := ioutil.TempFile("", "foo")
-				if err != nil {
-					panic(err)
-				}
-				n := f.Name()
-				f.Close()
-				return n
-			},
-			dest: "foo/whatever",
-			want: Records{
-				"foo/whatever": cpio.Record{
-					Info: cpio.Info{
-						Name:  "foo/whatever",
-						Mode:  unix.S_IFREG | 0600,
-						UID:   uint64(os.Geteuid()),
-						GID:   uint64(os.Getegid()),
-						NLink: 1,
-						Major: 253,
-						Minor: 1,
-					},
-				},
-			},
-		},
-		{
-			ma: &MockArchiver{
-				Records: make(Records),
-			},
-			src: func() string {
-				f, err := ioutil.TempDir("", "foo")
-				if err != nil {
-					panic(err)
-				}
-				if err := ioutil.WriteFile(filepath.Join(f, "bla"), []byte("foo"), 0644); err != nil {
-					panic(err)
-				}
-				if err := ioutil.WriteFile(filepath.Join(f, "bla2"), []byte("foo2"), 0644); err != nil {
-					panic(err)
-				}
-				return f
-			},
-			dest: "etc",
-			want: Records{
-				"etc": cpio.Record{
-					Info: cpio.Info{
-						Name: "etc",
-						Mode: unix.S_IFDIR | 0700,
-					},
-				},
-				"etc/bla": cpio.Record{
-					Info: cpio.Info{
-						Name: "etc/bla",
-						Mode: unix.S_IFREG | 0644,
-					},
-					ReaderAt: bytes.NewReader([]byte("foo")),
-				},
-				"etc/bla2": cpio.Record{
-					Info: cpio.Info{
-						Name: "etc/bla2",
-						Mode: unix.S_IFREG | 0644,
-					},
-					ReaderAt: bytes.NewReader([]byte("foo2")),
-				},
-			},
-		},
-	} {
-		t.Run(fmt.Sprintf("Test %02d", i), func(t *testing.T) {
-			src := tt.src()
-			defer os.RemoveAll(src)
-			if err := writeFile(tt.ma, src, tt.dest); err != tt.err {
-				t.Errorf("writeFile() = %v, want %v", err, tt.err)
-			}
-			if !RecordsEqual(tt.ma.Records, tt.want, sameNameModeContent) {
-				t.Errorf("writeFile() = %v, want %v", tt.ma.Records, tt.want)
-			}
-		})
-	}
-}
-
 func TestOptsWrite(t *testing.T) {
 	for i, tt := range []struct {
 		desc string
@@ -402,7 +384,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "no conflicts, just records",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"foo": cpio.Symlink("foo", "elsewhere"),
 					},
@@ -422,9 +404,9 @@ func TestOptsWrite(t *testing.T) {
 			},
 		},
 		{
-			desc: "base archive file already exists",
+			desc: "default already exists",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"etc": cpio.Symlink("etc", "whatever"),
 					},
@@ -443,7 +425,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "no conflicts, missing parent automatically created",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"foo/bar/baz": cpio.Symlink("foo/bar/baz", "elsewhere"),
 					},
@@ -461,7 +443,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "parent only automatically created if not already exists",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"foo/bar":     cpio.Directory("foo/bar", 0444),
 						"foo/bar/baz": cpio.Symlink("foo/bar/baz", "elsewhere"),
@@ -480,7 +462,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "base archive",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"foo/bar": cpio.Symlink("foo/bar", "elsewhere"),
 						"exists":  cpio.Directory("exists", 0777),
@@ -505,7 +487,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "base archive with init, no user init",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{},
 				},
 			},
@@ -522,7 +504,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "base archive with init and user init",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"init": cpio.StaticFile("init", "bar", 0444),
 					},
@@ -542,7 +524,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "base archive with init, use existing init",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{},
 				},
 				UseExistingInit: true,
@@ -560,7 +542,7 @@ func TestOptsWrite(t *testing.T) {
 		{
 			desc: "base archive with init and user init, use existing init",
 			opts: &Opts{
-				Files: Files{
+				Files: &Files{
 					Records: map[string]cpio.Record{
 						"init": cpio.StaticFile("init", "huh", 0111),
 					},
@@ -584,13 +566,13 @@ func TestOptsWrite(t *testing.T) {
 			tt.opts.OutputFile = tt.ma
 
 			if err := Write(tt.opts); err != tt.err {
-				t.Errorf("Write(%v) = %v, want %v", tt.opts, err, tt.err)
+				t.Errorf("Write() = %v, want %v", err, tt.err)
 			} else if err == nil && !tt.ma.FinishCalled {
 				t.Errorf("Finish wasn't called on archive")
 			}
 
 			if !RecordsEqual(tt.ma.Records, tt.want, sameNameModeContent) {
-				t.Errorf("Write(%v) = %v, want %v", tt.opts, tt.ma.Records, tt.want)
+				t.Errorf("Write() = %v, want %v", tt.ma.Records, tt.want)
 			}
 		})
 	}

pkg/uroot/uroot.go
@@ -333,7 +333,7 @@ func ResolvePackagePaths(env golang.Environ, pkgs []string) ([]string, error) {
 //   - `justAPath` is added to the archive under justAPath.
 //
 // ParseExtraFiles will also add ldd-listed dependencies if lddDeps is true.
-func ParseExtraFiles(archive initramfs.Files, extraFiles []string, lddDeps bool) error {
+func ParseExtraFiles(archive *initramfs.Files, extraFiles []string, lddDeps bool) error {
 	var err error
 	// Add files from command line.
 	for _, file := range extraFiles {

commit c4872772b7dcfa28202e72fbf4ea63cd77be2438

uroot package: use Lstat, not Stat
Stat will try to follow the symlink, which is not what
we want; we want the symlink, not what it points at.

Use Lstat, not Stat.

This fixes the breakage of NiChrome from 29cb33b69.

Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

pkg/uroot/initramfs/files.go
@@ -66,9 +66,9 @@ func (af *Files) AddFile(src string, dest string) error {
 
 	// We check if it's a directory first. If a directory already exists as
 	// a record or file, we want to include its children anyway.
-	sInfo, err := os.Stat(src)
+	sInfo, err := os.Lstat(src)
 	if err != nil {
-		return fmt.Errorf("adding file %q to archive failed: %v", src, err)
+		return fmt.Errorf("Adding %q to archive failed because Lstat failed: %v", src, err)
 	}
 
 	// Recursively add children.