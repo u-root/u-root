f73dcc4dce341c18f926df72f63251488eaca471
Ronald G. Minnich <rminnich@gmail.com>
2018-10-25 13:13:17 -0700

    Implement multiprocess tracing
    To test:
    strace /bin/sh
    and see what shakes.
    
    or strace /bin/sh -c /bin/date
    
    TODO: more tests
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/strace/tracer.go
+++ pkg/strace/tracer.go
@@ -22,6 +22,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// Debug is a do-nothing function which can be replaced by, e.g., log.Printf
 var Debug = func(string, ...interface{}) {}
 
 // TraceRecord has information about a ptrace event.
@@ -43,21 +44,26 @@ type TraceRecord struct {
 // starting a command, or attaching. Attaching is not supported yet.
 type Tracer struct {
 	Pid     int
+	EX      EventType
 	Records chan *TraceRecord
 	Count   int
 	Raw     bool // Set by the user, it disables pretty printing
 	Name    string
 	Printer func(t *Tracer, r *TraceRecord)
+	Last    *TraceRecord
 	// We save the output from the previous Enter so Exit handling
 	// can both use and adjust it.
 	output []string
 }
 
+// New returns a new Tracer.
 func New() (*Tracer, error) {
-	return &Tracer{Pid: -1, Records: make(chan *TraceRecord), Printer: SysCall}, nil
+	return &Tracer{Pid: -1, Records: make(chan *TraceRecord, 1), Printer: SysCall}, nil
 }
 
-// StartTracerFromCommand runs a Tracer given an exec.Cmd.
+// RunTracerFromCommand runs a Tracer given an exec.Cmd.
+// It locks itself down with LockOSThread and will unlock itself
+// when it returns, after the command and all its children exit.
 func (t *Tracer) RunTracerFromCmd(c *exec.Cmd) {
 	defer close(t.Records)
 	if c.SysProcAttr == nil {
@@ -84,80 +90,152 @@ func (t *Tracer) RunTracerFromCmd(c *exec.Cmd) {
 	}
 	t.Pid = c.Process.Pid
 	t.Name = fmt.Sprintf("%s(%d)", c.Args[0], t.Pid)
-	t.Run()
+	t.EX = Exit
+	Run(t)
 }
 
-func (t *Tracer) Step(e EventType) error {
+// NewTracerChild creates a tracer from a tracer.
+func NewTracerChild(pid int) (*Tracer, error) {
+	nt, err := New()
+	if err != nil {
+		return nil, err
+	}
+	nt.Pid = pid
+	nt.Name = fmt.Sprintf("%d", pid)
+	nt.EX = Exit
+	return nt, nil
+}
+
+// Step steps a Tracer by issuing a PtraceSyscall to it and then doing a Wait.
+// Note that Step waits for any child to return, not just the one we are stepping.
+func (t *Tracer) Step(e EventType) (int, error) {
+	Debug("Step %d", t.Pid)
 	if err := unix.PtraceSyscall(t.Pid, 0); err != nil {
 		r := &TraceRecord{Serial: t.Count, EX: e, Pid: t.Pid}
 		Debug("ptracesyscall for %d gets %v", t.Pid, err)
-		r.Err = err
+		r.Err = fmt.Errorf("unix.PtraceSyscall: %d: %s: %v", t.Pid, t.Name, err)
 		t.Records <- r
-		return err
+		return -1, r.Err
 	}
-	if w, err := Wait(t.Pid); err != nil {
+	Debug("Stepped %d, now Wait", t.Pid)
+	pid, w, err := Wait(-1)
+	Debug("Wait returns (%d, %v, %v)", pid, w, err)
+	if err != nil {
 		r := &TraceRecord{Serial: t.Count, EX: e, Pid: t.Pid}
+		r.Err = fmt.Errorf("unix.Wait: %d: %s: %v, %v", t.Pid, t.Name, w, err)
 		Debug("wait4 for %d gets %v, %v", t.Pid, w, err)
-		r.Err = err
 		t.Records <- r
-		return err
+		return -1, r.Err
 	}
-	return nil
+	Debug("Step %d: back from wait", pid)
+	return pid, nil
 }
 
-func (t *Tracer) Run() error {
+// Run runs a set of processes as defined by a Tracer. Because of Unix restrictions
+// around which processes which can trace other processes, Run gets a tad involved.
+// It is implemented as a simple loop, driving events via ptrace commands to processes;
+// and responding to events returned by a Wait.
+// It has to handle a few events specially:
+// o if a wait fails, the process has exited, and must no longer be commanded
+//   this is indicated by a wait followed by an error on PtraceGetRegs
+// o if a process forks successully, we must add it to our set of traced processes.
+//   We attach that process, wait for it, then issue a ptrace system call command
+//   to it. We don't use the Linux SEIZE command as we can do this in a more generic
+//   Unix way.
+// We create a map of our traced processes and run until it is empty.
+// The initial value of the map is just the one process we start with.
+func Run(root *Tracer) error {
+	var nextEX EventType
+	var procs = map[int]*Tracer{
+		root.Pid: root,
+	}
+	Debug("procs %v", procs)
 	var tm time.Time
 	var a SyscallArguments
 	var sysno = syscall.SYS_EXECVE
-	for {
+	Debug("Run %v", root.Pid)
+	pid := root.Pid
+	var err error
+	var count int
+	for len(procs) > 0 {
+		t := procs[pid]
 		t.Count++
-		x := &TraceRecord{Serial: t.Count, EX: Exit, Pid: t.Pid, Args: a}
-		if err := unix.PtraceGetRegs(t.Pid, &x.Regs); err != nil {
-			Debug("ptracegetregs for %d gets %v", t.Pid, err)
-			x.Err = err
+		count++
+		Debug("Get regs for %d", pid)
+		x := &TraceRecord{Serial: t.Count, EX: t.EX, Pid: pid, Args: a}
+		if err := unix.PtraceGetRegs(pid, &x.Regs); err != nil {
+			Debug("ptracegetregs for %d gets %v", pid, err)
+			x.Err = fmt.Errorf("ptracegetregs for %d gets %v", pid, err)
 			t.Records <- x
-			break
+			delete(procs, pid)
+			pid, _, _ = Wait(-1)
+			continue
 		}
+		Debug("GOT regs for %d", pid)
 		x.FillArgs()
-		x.FillRet()
-		x.Sysno = sysno
-		x.Time = time.Since(tm)
+		if t.EX == Exit {
+			x.FillRet()
+			x.Sysno = sysno
+			x.Time = time.Since(tm)
+			nextEX = Enter
+		} else {
+			tm = time.Now()
+			x.FillArgs()
+			a = x.Args
+			sysno = x.Sysno
+			t.Last = x
+			nextEX = Exit
+		}
 
-		// in the original version, we did the various probes of memory
-		// in the strace command. Oops: on Unix, you can't look at process
-		// memory when it's not stopped (that's not true on all systems,
-		// but it's the Unix model). So we do this potentially expensive step
-		// here, not knowing if we will not need it.
 		if !t.Raw {
 			SysCall(t, x)
 		}
+		Debug("Push %v", x)
 		t.Records <- x
-		if err := t.Step(Enter); err != nil {
+		// Was there a clone? Capture the child. Don't forget the child has an exit
+		// record for the clone too, so don't get confused.
+		p := int(x.Ret[0].Int())
+		Debug("Check for new pid: tracer pid %d, ret %d", t.Pid, p)
+		if x.Sysno == unix.SYS_CLONE && x.EX == Exit && p > 0 && p != t.Pid {
+			nt, err := NewTracerChild(int(x.Ret[0].Int()))
+			if err != nil {
+				Debug("Setting up child: %v", err)
+			} else {
+				nt.Records = t.Records
+				Debug("New child: %v", nt)
+				// The result of the attach gets picked up by the wait()
+				if err := unix.PtraceAttach(nt.Pid); err != nil {
+					r := &TraceRecord{Serial: nt.Count, EX: Enter, Pid: nt.Pid}
+					Debug("RunTracerChild: attach for %d gets %v", nt.Pid, err)
+					nt.Records <- r
+				}
+				pid, w, err := Wait(nt.Pid)
+				Debug("Wait returns (%d, %v, %v)", pid, w, err)
+				if err != nil || pid != nt.Pid {
+					r := &TraceRecord{Serial: nt.Count, EX: Exit, Pid: nt.Pid}
+					r.Err = fmt.Errorf("unix.Wait: %d: %s: %v, %v", nt.Pid, nt.Name, w, err)
+					Debug("wait4 for %d gets %v, %v", nt.Pid, w, err)
+					t.Records <- r
+				}
+				Debug("Step %d", nt.Pid)
+				if err := unix.PtraceSyscall(nt.Pid, 0); err != nil {
+					r := &TraceRecord{Serial: nt.Count, EX: Exit, Pid: nt.Pid}
+					Debug("ptracesyscall for %d gets %v", nt.Pid, err)
+					r.Err = fmt.Errorf("unix.PtraceSyscall: %d: %s: %v", nt.Pid, nt.Name, err)
+					t.Records <- r
+				}
+				procs[nt.Pid] = nt
+			}
+		}
+
+		Debug("Step after exit")
+		t.EX = nextEX
+		if pid, err = t.Step(t.EX); err != nil {
 			return err
 		}
-		e := &TraceRecord{Serial: t.Count, EX: Enter, Pid: t.Pid}
-		if err := unix.PtraceGetRegs(t.Pid, &e.Regs); err != nil {
-			Debug("ptracegetregs for %d gets %v", t.Pid, err)
-			e.Err = err
-			t.Records <- e
-			break
-		}
-		tm = time.Now()
-		e.FillArgs()
-		if !t.Raw {
-			SysCall(t, e)
-		}
-		a = e.Args
-		sysno = e.Sysno
-		t.Records <- e
-
-		if err := t.Step(Exit); err != nil {
-			return err
-		}
-
-		t.Count++
 	}
-	Debug("Pushed %d records", t.Count)
+
+	Debug("Pushed %d records", count)
 	return nil
 }
 
@@ -179,16 +257,21 @@ func (t *TraceRecord) String() string {
 	return fmt.Sprintf("%s %v", pre, t.Regs)
 }
 
+// A ProcIO is used to implement io.Reader and io.Writer.
+// it contains a pid, which is unchanging; and an
+// addr and byte count which change as IO proceeds.
 type ProcIO struct {
 	pid   int
 	addr  uintptr
 	bytes int
 }
 
+// NewProcReader returns an io.Reader for a ProcIO.
 func NewProcReader(pid int, addr uintptr) io.Reader {
 	return &ProcIO{pid: pid, addr: addr}
 }
 
+// Read implements io.Read for a ProcIO.
 func (p *ProcIO) Read(b []byte) (int, error) {
 	n, err := unix.PtracePeekData(p.pid, p.addr, b)
 	if err != nil {
@@ -199,10 +282,12 @@ func (p *ProcIO) Read(b []byte) (int, error) {
 	return n, nil
 }
 
+// NewProcWriter returns an io.Writer for a ProcIO.
 func NewProcWriter(pid int, addr uintptr) io.Writer {
 	return &ProcIO{pid: pid, addr: addr}
 }
 
+// Write implements io.Write for a ProcIO.
 func (p *ProcIO) Write(b []byte) (int, error) {
 	n, err := unix.PtracePokeData(p.pid, p.addr, b)
 	if err != nil {
@@ -213,12 +298,16 @@ func (p *ProcIO) Write(b []byte) (int, error) {
 	return n, nil
 }
 
+// Read reads from the process at Addr to the interface{}
+// and returns a byte count and error.
 func (t *Tracer) Read(addr Addr, v interface{}) (int, error) {
 	p := NewProcReader(t.Pid, uintptr(addr))
 	err := binary.Read(p, binary.LittleEndian, v)
 	return p.(*ProcIO).bytes, err
 }
 
+// ReadString reads a null-terminated string from the process
+// at Addr and any errors.
 func (t *Tracer) ReadString(addr Addr, max int) (string, error) {
 	if addr == 0 {
 		return "<nil>", nil
@@ -238,6 +327,8 @@ func (t *Tracer) ReadString(addr Addr, max int) (string, error) {
 	return s, nil
 }
 
+// ReadStringVector takes an address, max string size, and max number of string to read,
+// and returns a string slice or error.
 func (t *Tracer) ReadStringVector(addr Addr, maxsize, maxno int) ([]string, error) {
 	var v []Addr
 	if addr == 0 {
@@ -272,12 +363,15 @@ func (t *Tracer) ReadStringVector(addr Addr, maxsize, maxno int) ([]string, erro
 	return vs, nil
 }
 
+// Write writes to the process address sapce and returns a count and error.
 func (t *Tracer) Write(addr Addr, v interface{}) (int, error) {
 	p := NewProcWriter(t.Pid, uintptr(addr))
 	err := binary.Write(p, binary.LittleEndian, v)
 	return p.(*ProcIO).bytes, err
 }
 
+// CaptureAddress pulls a socket address from the process as a byte slice.
+// It returns any errors.
 func CaptureAddress(t *Tracer, addr Addr, addrlen uint32) ([]byte, error) {
 	b := make([]byte, addrlen)
 	if _, err := t.Read(addr, b); err != nil {
