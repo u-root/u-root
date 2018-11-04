e0b1e14fb647b72dde708fd7ebfb5f4c8938e05e
Christopher Koch <chrisko@google.com>
2018-11-04 00:20:14 -0700

    pkg/loop: cleanup names and types.

--- pkg/loop/losetup_linux.go
+++ pkg/loop/losetup_linux.go
@@ -7,13 +7,12 @@ package loop
 import (
 	"fmt"
 	"os"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 const (
-	/*
-	 * IOCTL commands --- we will commandeer 0x4C ('L')
-	 */
+	// Loop ioctl commands --- we will commandeer 0x4C ('L')
 	LOOP_SET_CAPACITY = 0x4C07
 	LOOP_CHANGE_FD    = 0x4C06
 	LOOP_GET_STATUS64 = 0x4C05
@@ -24,58 +23,46 @@ const (
 	LOOP_SET_FD       = 0x4C00
 	LO_NAME_SIZE      = 64
 	LO_KEY_SIZE       = 32
-	/* /dev/loop-control interface */
+
+	// /dev/loop-control interface
 	LOOP_CTL_ADD      = 0x4C80
 	LOOP_CTL_REMOVE   = 0x4C81
 	LOOP_CTL_GET_FREE = 0x4C82
 )
 
-// FindDevice finds an unused loop device.
-func FindDevice() (name string, err error) {
+// FindDevice finds an unused loop device and returns its /dev/loopN path.
+func FindDevice() (string, error) {
 	cfd, err := os.OpenFile("/dev/loop-control", os.O_RDWR, 0644)
 	if err != nil {
 		return "", err
 	}
 	defer cfd.Close()
 
-	number, err := CtlGetFree(cfd.Fd())
+	number, err := GetFree(int(cfd.Fd()))
 	if err != nil {
 		return "", err
 	}
 	return fmt.Sprintf("/dev/loop%d", number), nil
 }
 
-// ClearFd clears the loop device associated with filedescriptor fd.
-func ClearFd(fd uintptr) error {
-	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, LOOP_CLR_FD, 0); err != 0 {
-		return err
-	}
-
-	return nil
+// ClearFD clears the loop device associated with file descriptor fd.
+func ClearFD(fd int) error {
+	return unix.IoctlSetInt(fd, LOOP_CLR_FD, 0)
 }
 
-// CtlGetFree finds a free loop device querying the loop control device pointed
-// by fd. It returns the number of the free loop device /dev/loopX
-func CtlGetFree(fd uintptr) (uintptr, error) {
-	number, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, LOOP_CTL_GET_FREE, 0)
-	if err != 0 {
-		return 0, err
-	}
-	return number, nil
+// GetFree finds a free loop device querying the loop control device pointed
+// by fd. It returns the number of the free loop device /dev/loopX.
+func GetFree(fd int) (int, error) {
+	return unix.IoctlGetInt(fd, LOOP_CTL_GET_FREE)
 }
 
-// SetFd associates a loop device pointed by lfd with a regular file pointed by ffd.
-func SetFd(lfd, ffd uintptr) error {
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, lfd, LOOP_SET_FD, ffd)
-	if err != 0 {
-		return err
-	}
-
-	return nil
+// SetFD associates a loop device lfd with a regular file ffd.
+func SetFD(lfd, ffd int) error {
+	return unix.IoctlSetInt(lfd, LOOP_SET_FD, ffd)
 }
 
-// SetFdFiles associates loop device "devicename" with regular file "filename"
-func SetFdFiles(devicename, filename string) error {
+// SetFile associates loop device "devicename" with regular file "filename"
+func SetFile(devicename, filename string) error {
 	mode := os.O_RDWR
 	file, err := os.OpenFile(filename, mode, 0644)
 	if err != nil {
@@ -93,16 +80,16 @@ func SetFdFiles(devicename, filename string) error {
 	}
 	defer device.Close()
 
-	return SetFd(device.Fd(), file.Fd())
+	return SetFD(int(device.Fd()), int(file.Fd()))
 }
 
-// ClearFdFile clears the loop device "devicename"
-func ClearFdFile(devicename string) error {
+// ClearFile clears the fd association of the loop device "devicename".
+func ClearFile(devicename string) error {
 	device, err := os.Open(devicename)
 	if err != nil {
 		return err
 	}
 	defer device.Close()
 
-	return ClearFd(device.Fd())
+	return ClearFD(int(device.Fd()))
 }
