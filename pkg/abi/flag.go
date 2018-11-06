2a859cf31d05b728afd34d8f2cc1191d84ae0621
Ronald G. Minnich <rminnich@gmail.com>
2018-11-06 08:03:17 -0800

    abi: change flag types
    Flag is now an interface, with Match(), String(), and Mask()
    functions. We provide three implementations:
    BitSet, wherein a single bit, if set, matches the value
    Value, where the entire uint64 is used for the match
    Field, where a subset of bits, one or more, is used to match and
           then prettyprint the value as a hex number.
    
    Each has a different model for clearing bits.
    In BitSet, the bit is the mask
    In Value, the mask is a constant MaxUint64
    In Field, a set of bits (not necessarily continguous) provides the mask
    
    I can now have a FlagSet consisting of, e.g., intermixed BitSet and Field
    components. This will suffice to print all the weird cases I can think of.
    
    We may, in future, need more sophisticated field extractors but we don't yet.
    
    This change is sufficient to allow me to parse ChromeOS GPT extra fields.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/abi/flag.go
+++ pkg/abi/flag.go
@@ -21,56 +21,121 @@ import (
 	"strings"
 )
 
-// A FlagSet is a slice of bit-flags and their name.
-type FlagSet []struct {
-	Flag uint64
-	Name string
+// The Flag interface has three functions:
+// Match, to see if a given value is covered by the Flag.
+// Mask, to return which bits in the flag are covered.
+// String, to print a string determined by the flag and possibly the value.
+type Flag interface {
+	Match(uint64) bool
+	Mask() uint64
+	String(uint64) string
 }
 
-// Parse returns a pretty version of val, using the flag names for known flags.
-// Unknown flags remain numeric.
+// A FlagSet is a slice of Flags
+type FlagSet []Flag
+
+// A BitFlag is one or more bits and a name.
+// The name is printed if all bits are matched
+// (i.e. enabled) in a value. Typically, the Value
+// is only one bit.
+type BitFlag struct {
+	Name  string
+	Value uint64
+}
+
+// Match returns true if the val and the BitFlag Value
+// are the same, i.e. the flag is set.
+func (f *BitFlag) Match(val uint64) bool {
+	return f.Value&val == f.Value
+}
+
+// String returns the string value of the BitFlag.
+// The parameter is ignored.
+func (f *BitFlag) String(_ uint64) string {
+	return f.Name
+}
+
+// Mask returns a mask for the BitFlag, namely, the value.
+func (f *BitFlag) Mask() uint64 {
+	return f.Value
+}
+
+// Field is used to extract named fields, e.g. the
+// Tries field in the ChromeOS GPT. The value
+// is masked and shifted right. We may at some
+// point want a format string.
+type Field struct {
+	Name    string
+	BitMask uint64
+	Shift   uint64
+}
+
+// Match always matches in a Field, regardless of value.
+func (f *Field) Match(val uint64) bool {
+	return true
+}
+
+// String returns the part or the val covered by this Field.
+// The bits are extracted, shifted, and printed as hex.
+func (f *Field) String(val uint64) string {
+	return fmt.Sprintf("%s=%#x", f.Name, (val&f.BitMask)>>f.Shift)
+}
+
+// Mask returns the bits covered by the Field.
+func (f *Field) Mask() uint64 {
+	return f.BitMask
+}
+
+// Value is the simplest implementation for Flags.
+// If an entire uint64 matches the Value, then Match
+// will be true. Note that Value could be implemented
+// by a Field with a Mask of MaxUint64, but Value
+// is more convenient to use.
+type Value struct {
+	Name  string
+	Value uint64
+}
+
+// String returns the name of the value associated with `val`.
+func (e *Value) String(_ uint64) string {
+	return e.Name
+}
+
+// Match determines if a Value matches the argument, meaning the
+// Value and the arg are the same.
+func (e *Value) Match(val uint64) bool {
+	return e.Value == val
+}
+
+// Mask returns the bits covered by a Value, in this case MaxUint64.
+func (e *Value) Mask() uint64 {
+	return math.MaxUint64
+}
+
+// Parse returns a pretty version of a FlagSet, using the flag names for known flags.
+// Unknown flags are printed as numeric if the Flagset did not cover all the bits
+// in the argument.
 func (s FlagSet) Parse(val uint64) string {
 	var flags []string
+	var clr uint64
 
 	for _, f := range s {
-		if val&f.Flag == f.Flag {
-			flags = append(flags, f.Name)
-			val &^= f.Flag
+		if f.Match(val) {
+			flags = append(flags, f.String(val))
+			val &^= f.Mask()
+			clr |= f.Mask()
 		}
 	}
 
-	if val != 0 {
+	// If there are bits we did not check, then print out
+	// whatever is left, *if it is non-zero*. This is a bit
+	// hokey, but at the same time, it seems the most usable.
+	// We may, later, want to print out the bits not covered.
+	// It's possible to miss bits and have val be 0 if the bits
+	// we missed are 0, as in the earlier code.
+	if clr != math.MaxUint64 && val != 0 {
 		flags = append(flags, "0x"+strconv.FormatUint(val, 16))
 	}
 
 	return strings.Join(flags, "|")
-}
-
-// ValueSet is a slice of syscall values and their name. Parse will replace
-// values that exactly match an entry with its name.
-type ValueSet []struct {
-	Value uint64
-	Name  string
-}
-
-// Parse returns the name of the value associated with `val`. Unknown values
-// are converted to hex.
-func (e ValueSet) Parse(val uint64) string {
-	for _, f := range e {
-		if val == f.Value {
-			return f.Name
-		}
-	}
-	return fmt.Sprintf("%#x", val)
-}
-
-// ParseName returns the flag value associated with 'name'. Returns false
-// if no value is found.
-func (e ValueSet) ParseName(name string) (uint64, bool) {
-	for _, f := range e {
-		if name == f.Name {
-			return f.Value, true
-		}
-	}
-	return math.MaxUint64, false
 }
---
+++ pkg/abi/flag_test.go
@@ -0,0 +1,62 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package abi
+
+import (
+	"testing"
+
+	"golang.org/x/sys/unix"
+)
+
+// From the cros docs
+// Bits	 Content	 Notes
+// 63-57	 Unused
+// 56	 Successful Boot Flag
+//Set to 1 the first time the system has successfully booted from this partition (see the File System/Autoupdate design document for the definition of success).
+// 55-52	 Tries Remaining	Number of times to attempt booting this partition. Used only when the Successful Boot Flag is 0.
+// 51-48	 Priority	4-bit number: 15 = highest, 1 = lowest, 0 = not bootable.
+// 47-0	 Reserved by EFI Spec
+func TestCrosGPT(t *testing.T) {
+	var tests = []struct {
+		n string
+		f FlagSet
+		v uint64
+		o string
+	}{
+		{n: "Basic ChromeOS GPT flags",
+			f: FlagSet{
+				&BitFlag{Name: "Suc", Value: 1 << 56},
+				&Field{Name: "Tries", BitMask: 15 << 52, Shift: 52},
+				&Field{Name: "prio", BitMask: 15 << 48, Shift: 48},
+			},
+			v: 1<<56 | 8<<52 | 3<<48,
+			o: "Suc|Tries=0x8|prio=0x3",
+		},
+		{n: "Basic ChromeOS GPT flags with standard GPT value",
+			f: FlagSet{
+				&BitFlag{Name: "Suc", Value: 1 << 56},
+				&Field{Name: "Tries", BitMask: 15 << 52, Shift: 52},
+				&Field{Name: "prio", BitMask: 15 << 48, Shift: 48},
+			},
+			v: 1<<56 | 8<<52 | 3<<48 | 1,
+			o: "Suc|Tries=0x8|prio=0x3|0x1",
+		},
+		{n: "Simple system call",
+			f: FlagSet{
+				&Value{Name: "write", Value: unix.SYS_WRITE},
+			},
+			v: unix.SYS_WRITE,
+			o: "write",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.n, func(t *testing.T) {
+			s := tc.f.Parse(tc.v)
+			if s != tc.o {
+				t.Fatalf("Got %s, want %s", s, tc.o)
+			}
+		})
+	}
+}
