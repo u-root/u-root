8b5364010ddd6ff0e69ca8589d2111c73f2fe04f
Ryan O'Leary <ryanoleary@google.com>
2018-11-21 11:41:06 -0800

    Strace supports forking
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- cmds/strace/strace.go
+++ cmds/strace/strace.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// strace is a simple single-process tracer.
+// strace is a simple multi-process tracer.
 // It starts the comand and lets the strace.Run() do all the work.
 //
 // Synopsis:
