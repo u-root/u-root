95295e8e74041e56f1a6f8f0e9110f579f8b951e
Luis Alberto Herrera <luisalberto@google.com>
2018-11-27 15:32:15 -0800

    Add support for pxeboot to get DHCP configuration on IPv6.
    This commit updates pxeboot to fire DHCP requests on IPv4 and IPv6 in
    parallel. The first response response will be use as DHCP configuration
    and the other will be discarded.
    
    Signed-off-by: Luis Alberto Herrera <luisalberto@google.com>

--- cmds/pxeboot/pxeboot.go
+++ cmds/pxeboot/pxeboot.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -17,6 +18,7 @@ import (
 	"github.com/u-root/dhcp4/dhcp4client"
 	"github.com/u-root/u-root/pkg/boot"
 	"github.com/u-root/u-root/pkg/dhclient"
+	"github.com/u-root/u-root/pkg/dhcp6client"
 	"github.com/u-root/u-root/pkg/ipxe"
 	"github.com/u-root/u-root/pkg/pxe"
 	"github.com/vishvananda/netlink"
@@ -28,23 +30,118 @@ var (
 	debug   = func(string, ...interface{}) {}
 )
 
-func attemptDHCPLease(iface netlink.Link, timeout time.Duration, retry int) (*dhclient.Packet4, error) {
-	if _, err := dhclient.IfUp(iface.Attrs().Name); err != nil {
-		return nil, err
-	}
+const (
+	kDhcpTimeout = 30 * time.Second
+	kDhcpTries   = 5
+)
 
+func leaseAndConfigureV4(iface netlink.Link, leased chan bool) (*url.URL, net.IP, error) {
 	client, err := dhcp4client.New(iface,
-		dhcp4client.WithTimeout(timeout),
-		dhcp4client.WithRetry(retry))
+		dhcp4client.WithTimeout(kDhcpTimeout),
+		dhcp4client.WithRetry(kDhcpTries))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
+	log.Printf("Attempting to get DHCPv4 lease on %s", iface.Attrs().Name)
 	p, err := client.Request()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return dhclient.NewPacket4(p), nil
+	packet := dhclient.NewPacket4(p)
+	uri, err := packet.Boot()
+	if err != nil {
+		log.Printf("Got DHCPv4 lease, but no valid PXE information.")
+		return nil, nil, err
+	}
+
+	_, ok := <-leased
+	if !ok {
+		return nil, nil, errors.New("another DHCP request offer was acepted")
+	}
+	close(leased)
+
+	log.Printf("Got DHCPv4 lease on %s", iface.Attrs().Name)
+	return uri, packet.Lease().IP, dhclient.Configure4(iface, packet.P)
+}
+
+func leaseAndConfigureV6(iface netlink.Link, leased chan bool) (*url.URL, net.IP, error) {
+	client, err := dhcp6client.New(iface,
+		dhcp6client.WithTimeout(kDhcpTimeout),
+		dhcp6client.WithRetry(kDhcpTries))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	log.Printf("Attempting to get DHCPv6 lease on %s", iface.Attrs().Name)
+	iana, p, err := client.RapidSolicit()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	packet := dhclient.NewPacket6(p, iana)
+	uri, _, err := packet.Boot()
+	if err != nil {
+		log.Printf("Got DHCPv6 lease, but no valid PXE information.")
+		return nil, nil, err
+	}
+
+	_, ok := <-leased
+	if !ok {
+		return nil, nil, errors.New("another DHCP request offer was acepted")
+	}
+	close(leased)
+
+	log.Printf("Got DHCPv6 lease on %s", iface.Attrs().Name)
+
+	return uri, packet.Lease().IP, dhclient.Configure6(iface, p, iana)
+}
+
+func leaseAndConfigure(iface netlink.Link) (*url.URL, net.IP, error) {
+	// leased has a message if no lease had been accepted. It is closed otherwise.
+	leased := make(chan bool, 1)
+	leased <- true
+
+	type dhcpResponse struct {
+		u   *url.URL
+		ip  net.IP
+		err error
+	}
+
+	response := make(chan dhcpResponse)
+
+	leaseFunctions := []func(iface netlink.Link, leased chan bool) (*url.URL, net.IP, error){
+		leaseAndConfigureV4,
+		leaseAndConfigureV6,
+	}
+
+	for _, f := range leaseFunctions {
+		f := f
+		go func() {
+			u, ip, err := f(iface, leased)
+			response <- dhcpResponse{
+				u:   u,
+				ip:  ip,
+				err: err,
+			}
+		}()
+	}
+
+	errCount := 0
+
+	for r := range response {
+		// If we got a lease, we are all set.
+		// Note that one go routine will leak, but eventually timeout.
+		if r.err != nil {
+			return r.u, r.ip, nil
+		}
+		errCount++
+		// All attempts failed, report to caller.
+		if errCount == len(leaseFunctions) {
+			return nil, nil, errors.New("unable to get DHCP lease on IPv4 nor IPv6")
+		}
+	}
+	return nil, nil, errors.New("BUG: unreachable code")
 }
 
 // getBootImage attempts to parse the file at uri as an ipxe config and returns
@@ -86,34 +183,15 @@ func Netboot() error {
 			continue
 		}
 
-		log.Printf("Attempting to get DHCP lease on %s", iface.Attrs().Name)
-		packet, err := attemptDHCPLease(iface, 30*time.Second, 5)
+		uri, ip, err := leaseAndConfigure(iface)
 		if err != nil {
-			log.Printf("No lease on %s: %v", iface.Attrs().Name, err)
-			continue
-		}
-		log.Printf("Got lease on %s", iface.Attrs().Name)
-		if err := dhclient.Configure4(iface, packet.P); err != nil {
-			log.Printf("shit: %v", err)
-			continue
-		}
-
-		// We may have to make this DHCPv6 and DHCPv4-specific anyway.
-		// Only tested with v4 right now; and assuming the uri points
-		// to a pxelinux.0.
-		//
-		// Or rather, we need to make this option-specific. DHCPv6 has
-		// options for passing a kernel and cmdline directly. v4
-		// usually just passes a pxelinux.0. But what about an initrd?
-		uri, err := packet.Boot()
-		if err != nil {
-			log.Printf("Got DHCP lease, but no valid PXE information.")
+			log.Printf("error while attempting DHCP on interface %v: %v", iface.Attrs().Name, err)
 			continue
 		}
 
 		log.Printf("Boot URI: %s", uri)
 
-		img, err := getBootImage(uri, iface.Attrs().HardwareAddr, packet.Lease().IP)
+		img, err := getBootImage(uri, iface.Attrs().HardwareAddr, ip)
 		if err != nil {
 			return err
 		}
