64c10ca071e5d19758abf5b2053dd0e09552b874
Luis Alberto Herrera <luisalberto@google.com>
2018-11-30 13:58:14 -0800

    Append acpi_rsdp on kexec.
    This commits automatically detects the value of location of the ACPI
    Root System Description Table, and passes that value to the kexec'd
    kernel as acpi_rsdp.
    
    Signed-off-by: Luis Alberto Herrera <luisalberto@google.com>

--- pkg/uroot/util/root.go
+++ pkg/uroot/util/root.go
@@ -8,12 +8,15 @@
 package util
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/u-root/u-root/pkg/cmdline"
@@ -229,4 +232,32 @@ func Rootfs() {
 		create(cgroupsnamespace)
 	}
 
+}
+
+func GetRSDP() (string, error) {
+	file, err := os.Open("/sys/firmware/efi/systab")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	const (
+		acpi20 = "ACPI20="
+		acpi   = "ACPI="
+	)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, acpi20) {
+			return strings.TrimPrefix(line, acpi20), nil
+		}
+		if strings.HasPrefix(line, acpi) {
+			return strings.TrimPrefix(line, acpi), nil
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("error while reading EFI systab: %v", err)
+	}
+	return "", errors.New("invalid efi/systab file")
 }
