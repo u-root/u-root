8ae3e19f8348ab904bf924284ec5792507dd00f1
Andrea Barberio <insomniac@slackware.it>
2018-12-01 18:25:30 +0000

    logging TPM opening errors

--- pkg/crypto/measure.go
+++ pkg/crypto/measure.go
@@ -21,39 +21,45 @@ const (
 // TryMeasureBootConfig measures bootconfig contents
 func TryMeasureBootConfig(name string, kernel string, initramfs string, kernelArgs string, deviceTree string) {
 	TPMInterface, err := tpm.NewTPM()
-	if err == nil {
-		TryMeasureData(BootConfig, []byte(name), &name)
-		TryMeasureData(BootConfig, []byte(kernel), &kernel)
-		TryMeasureData(BootConfig, []byte(initramfs), &initramfs)
-		TryMeasureData(BootConfig, []byte(kernelArgs), &kernelArgs)
-		TryMeasureData(BootConfig, []byte(deviceTree), &deviceTree)
-		TryMeasureFiles(kernel, initramfs, deviceTree)
-		TPMInterface.Close()
+	if err != nil {
+		log.Printf("Cannot open TPM: %v", err)
+		return
 	}
+	TryMeasureData(BootConfig, []byte(name), &name)
+	TryMeasureData(BootConfig, []byte(kernel), &kernel)
+	TryMeasureData(BootConfig, []byte(initramfs), &initramfs)
+	TryMeasureData(BootConfig, []byte(kernelArgs), &kernelArgs)
+	TryMeasureData(BootConfig, []byte(deviceTree), &deviceTree)
+	TryMeasureFiles(kernel, initramfs, deviceTree)
+	TPMInterface.Close()
 }
 
 // TryMeasureData measures a byte array with additional information
 func TryMeasureData(pcr uint32, data []byte, info *string) {
 	TPMInterface, err := tpm.NewTPM()
-	if err == nil {
-		log.Println("Measuring blob: " + *info)
-		TPMInterface.Measure(pcr, data)
-		TPMInterface.Close()
+	if err != nil {
+		log.Printf("Cannot open TPM: %v", err)
+		return
 	}
+	log.Println("Measuring blob: " + *info)
+	TPMInterface.Measure(pcr, data)
+	TPMInterface.Close()
 }
 
 // TryMeasureFiles measures a variable amount of files
 func TryMeasureFiles(files ...string) {
 	TPMInterface, err := tpm.NewTPM()
-	if err == nil {
-		for _, file := range files {
-			log.Println("Measuring file: " + file)
-			data, err := ioutil.ReadFile(file)
-			if err != nil {
-				continue
-			}
-			TPMInterface.Measure(Blob, data)
-		}
-		TPMInterface.Close()
+	if err != nil {
+		log.Printf("Cannot open TPM: %v", err)
+		return
 	}
+	for _, file := range files {
+		log.Println("Measuring file: " + file)
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			continue
+		}
+		TPMInterface.Measure(Blob, data)
+	}
+	TPMInterface.Close()
 }
