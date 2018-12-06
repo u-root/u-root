eca06f3f973df99c65047c1691952b5b43e278cf
Ronald G. Minnich <rminnich@gmail.com>
2018-12-06 07:52:09 -0800

    Add integration test for tcz
    The checking for mounts fails, not sure why, it's disabled.
    It would almost be nicer if I could suck out all the output from qemu
    and then look for the matches. Not sure that's possible but it
    would be easier.
    
    Actual mounting deferred at present since the test kernel does not
    include loop (!!!!)
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- integration/dhclient_test.go
+++ integration/dhclient_test.go
@@ -13,11 +13,15 @@ import (
 
 func TestDhclient(t *testing.T) {
 	network := qemu.NewNetwork()
+	var sb, cb wc
 	_, scleanup := QEMUTest(t, &Options{
-		Name: "TestDhclient_Server",
+		Name:         "TestDhclient_Server",
+		SerialOutput: &sb,
 		Cmds: []string{
+			"github.com/u-root/u-root/cmds/echo",
 			"github.com/u-root/u-root/cmds/ip",
 			"github.com/u-root/u-root/cmds/init",
+			"github.com/u-root/u-root/cmds/sleep",
 			"github.com/u-root/u-root/cmds/shutdown",
 			"github.com/u-root/u-root/integration/testcmd/pxeserver",
 		},
@@ -25,7 +29,10 @@ func TestDhclient(t *testing.T) {
 			"ip addr add 192.168.0.1/24 dev eth0",
 			"ip link set eth0 up",
 			"ip route add 255.255.255.255/32 dev eth0",
+			"echo RUN THAT PXE SERVER",
 			"pxeserver",
+			"echo ALL DONE",
+			"sleep 15",
 			"shutdown -h",
 		},
 		Network: network,
@@ -33,16 +40,21 @@ func TestDhclient(t *testing.T) {
 	defer scleanup()
 
 	dhcpClient, ccleanup := QEMUTest(t, &Options{
-		Name: "TestDhclient_Client",
+		Name:         "TestDhclient_Client",
+		SerialOutput: &cb,
 		Cmds: []string{
+			"github.com/u-root/u-root/cmds/echo",
 			"github.com/u-root/u-root/cmds/ip",
 			"github.com/u-root/u-root/cmds/init",
 			"github.com/u-root/u-root/cmds/dhclient",
 			"github.com/u-root/u-root/cmds/shutdown",
 		},
 		Uinit: []string{
+			"echo DO THAT DHCLIENT",
 			"dhclient -ipv6=false -verbose",
+			"echo BACK, WHAT IP",
 			"ip a",
+			"echo OK, ALL DONE",
 			"shutdown -h",
 		},
 		Network: network,
@@ -50,13 +62,16 @@ func TestDhclient(t *testing.T) {
 	})
 	defer ccleanup()
 
+	t.Logf("Now we wait!")
+
 	if err := dhcpClient.Expect("err from done <nil>"); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	if err := dhcpClient.Expect("inet 192.168.1.0"); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
+	t.Logf("Server: %s\nClient: %s", sb.String(), cb.String())
 }
 
 func TestPxeboot(t *testing.T) {
