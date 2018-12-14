0e5fcbc00dd94b76bb7c5eeaaee1379327ed85bd
Max Shegai <max.shegai@gmail.com>
2018-12-14 13:16:51 -0800

    kexec: routines to work with kexec_segment
    This is the start of multiboot v1.
    This change allows parsing memory map provided by firmware,
    parsing segments from ELF files, adding new kexec segments.
    
    Signed-off-by: Max Shegai <max.shegai@gmail.com>

---
+++ pkg/kexec/memory_test.go
@@ -0,0 +1,251 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package kexec
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"reflect"
+	"testing"
+)
+
+func TestParseMemoryMap(t *testing.T) {
+	var mem Memory
+	root, err := ioutil.TempDir("", "memmap")
+	if err != nil {
+		t.Fatalf("Cannot create test dir: %v", err)
+	}
+	defer os.RemoveAll(root)
+
+	old := memoryMapRoot
+	memoryMapRoot = root
+	defer func() { memoryMapRoot = old }()
+
+	create := func(dir string, start, end uintptr, typ RangeType) error {
+		p := path.Join(root, dir)
+		if err := os.Mkdir(p, 0755); err != nil {
+			return err
+		}
+		if err := ioutil.WriteFile(path.Join(p, "start"), []byte(fmt.Sprintf("%#x\n", start)), 0655); err != nil {
+			return err
+		}
+		if err := ioutil.WriteFile(path.Join(p, "end"), []byte(fmt.Sprintf("%#x\n", end)), 0655); err != nil {
+			return err
+		}
+		return ioutil.WriteFile(path.Join(p, "type"), append([]byte(typ), '\n'), 0655)
+	}
+
+	if err := create("0", 0, 50, RangeRAM); err != nil {
+		t.Fatal(err)
+	}
+	if err := create("1", 100, 150, RangeNVACPI); err != nil {
+		t.Fatal(err)
+	}
+	if err := create("2", 200, 250, RangeACPI); err != nil {
+		t.Fatal(err)
+	}
+	if err := create("3", 300, 350, RangeNVS); err != nil {
+		t.Fatal(err)
+	}
+
+	want := []TypedAddressRange{
+		{Range: Range{Start: 0, Size: 50}, Type: RangeRAM},
+		{Range: Range{Start: 100, Size: 50}, Type: RangeNVACPI},
+		{Range: Range{Start: 200, Size: 50}, Type: RangeACPI},
+		{Range: Range{Start: 300, Size: 50}, Type: RangeNVS},
+	}
+
+	if err := mem.ParseMemoryMap(); err != nil {
+		t.Fatalf("ParseMemoryMap() error: %v", err)
+	}
+	if !reflect.DeepEqual(mem.Phys, want) {
+		t.Errorf("ParseMemoryMap() got %v, want %v", mem.Phys, want)
+	}
+}
+
+func TestAvailableRAM(t *testing.T) {
+	var mem Memory
+	mem.Phys = []TypedAddressRange{
+		TypedAddressRange{Range: Range{Start: 0, Size: 100}, Type: RangeRAM},
+		TypedAddressRange{Range: Range{Start: 200, Size: 100}, Type: RangeRAM},
+		TypedAddressRange{Range: Range{Start: 400, Size: 100}, Type: RangeRAM},
+		TypedAddressRange{Range: Range{Start: 600, Size: 100}, Type: RangeRAM},
+	}
+
+	mem.Segments = []Segment{
+		Segment{Phys: Range{Start: 0, Size: 50}},
+		Segment{Phys: Range{Start: 100, Size: 100}},
+		Segment{Phys: Range{Start: 250, Size: 50}},
+		Segment{Phys: Range{Start: 410, Size: 80}},
+		Segment{Phys: Range{Start: 599, Size: 102}},
+	}
+
+	want := []TypedAddressRange{
+		TypedAddressRange{Range: Range{Start: 50, Size: 50}, Type: RangeRAM},
+		TypedAddressRange{Range: Range{Start: 200, Size: 50}, Type: RangeRAM},
+		TypedAddressRange{Range: Range{Start: 400, Size: 10}, Type: RangeRAM},
+		TypedAddressRange{Range: Range{Start: 490, Size: 10}, Type: RangeRAM},
+	}
+
+	got := mem.availableRAM()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("availableRAM() got %+v, want %+v", got, want)
+	}
+}
+
+func TestAlignPhys(t *testing.T) {
+	for _, test := range []struct {
+		name      string
+		seg, want Segment
+	}{
+		{
+			name: "aligned",
+			seg: Segment{
+				Buf:  Range{Start: 0x1000, Size: 0x1000},
+				Phys: Range{Start: 0x2000, Size: 0x1000},
+			},
+			want: Segment{
+				Buf:  Range{Start: 0x1000, Size: 0x1000},
+				Phys: Range{Start: 0x2000, Size: 0x1000},
+			},
+		},
+		{
+			name: "unaligned",
+			seg: Segment{
+				Buf:  Range{Start: 0x1011, Size: 0x1022},
+				Phys: Range{Start: 0x2011, Size: 0x1022},
+			},
+			want: Segment{
+				Buf:  Range{Start: 0x1000, Size: 0x1033},
+				Phys: Range{Start: 0x2000, Size: 0x2000},
+			},
+		},
+		{
+			name: "empty_buf",
+			seg: Segment{
+				Buf:  Range{Start: 0x1011, Size: 0},
+				Phys: Range{Start: 0x2011, Size: 0},
+			},
+			want: Segment{
+				Buf:  Range{Start: 0x1000, Size: 0},
+				Phys: Range{Start: 0x2000, Size: 0x1000},
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			got := AlignPhys(test.seg)
+			if got != test.want {
+				t.Errorf("AlignPhys() got %v, want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestTryMerge(t *testing.T) {
+	for _, test := range []struct {
+		name   string
+		phys   Range
+		merged bool
+		want   Range
+	}{
+		{
+			name:   "disjunct",
+			phys:   Range{Start: 100, Size: 150},
+			merged: false,
+		},
+		{
+			name:   "superset",
+			phys:   Range{Start: 0, Size: 80},
+			merged: true,
+			want:   Range{Start: 0, Size: 100},
+		},
+		{
+			name:   "superset",
+			phys:   Range{Start: 10, Size: 80},
+			merged: true,
+			want:   Range{Start: 0, Size: 100},
+		},
+		{
+			name:   "superset",
+			phys:   Range{Start: 10, Size: 90},
+			merged: true,
+			want:   Range{Start: 0, Size: 100},
+		},
+		{
+			name:   "superset",
+			phys:   Range{Start: 0, Size: 100},
+			merged: true,
+			want:   Range{Start: 0, Size: 100},
+		},
+		{
+			name:   "overlap",
+			phys:   Range{Start: 0, Size: 150},
+			merged: true,
+			want:   Range{Start: 0, Size: 150},
+		},
+		{
+			name:   "overlap",
+			phys:   Range{Start: 50, Size: 100},
+			merged: true,
+			want:   Range{Start: 0, Size: 150},
+		},
+		{
+			name:   "overlap",
+			phys:   Range{Start: 99, Size: 51},
+			merged: true,
+			want:   Range{Start: 0, Size: 150},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			a := NewSegment([]byte("aaaa"), Range{Start: 0, Size: 100})
+			b := NewSegment([]byte("bbbb"), test.phys)
+
+			merged := a.tryMerge(b)
+			if merged != test.merged {
+				t.Fatalf("tryMerge() got %v, want %v", merged, test.merged)
+			}
+			if !merged {
+				return
+			}
+			if a.Phys != test.want {
+				t.Fatalf("Wrong merge result: got %+v, want %+v", a.Phys, test.want)
+			}
+
+			got := a.Buf.toSlice()
+			want := []byte("aaaabbbb")
+			if !bytes.Equal(got, want) {
+				t.Errorf("Wrong buf: got %s, want %s", got, want)
+			}
+		})
+	}
+}
+
+func TestDedup(t *testing.T) {
+	s := []Segment{
+		NewSegment([]byte("test"), Range{Start: 0, Size: 100}),
+		NewSegment([]byte("test"), Range{Start: 100, Size: 100}),
+		NewSegment([]byte("test"), Range{Start: 200, Size: 100}),
+		NewSegment([]byte("test"), Range{Start: 250, Size: 50}),
+		NewSegment([]byte("test"), Range{Start: 300, Size: 100}),
+		NewSegment([]byte("test"), Range{Start: 350, Size: 100}),
+	}
+	want := []Range{
+		Range{Start: 0, Size: 100},
+		Range{Start: 100, Size: 100},
+		Range{Start: 200, Size: 100},
+		Range{Start: 300, Size: 150},
+	}
+
+	got := Dedup(s)
+	for i := range got {
+		if got[i].Phys != want[i] {
+			t.Errorf("Dedup() got %v, want %v", got[i].Phys, want[i])
+		}
+	}
+
+}
