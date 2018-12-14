7bbfe1ca36191e6e3790135729936a72f688c65b
Max Shegai <max.shegai@gmail.com>
2018-12-14 13:19:25 -0800

    multiboot: multiboot v1
    This package is a multiboot v1 implementation
    https://www.gnu.org/software/grub/manual/multiboot/multiboot.html.
    
    Trampoline code segment is included as blob, this should
    be fixed in a future releases.
    
    Signed-off-by: Max Shegai <max.shegai@gmail.com>

---
+++ pkg/multiboot/info.go
@@ -0,0 +1,121 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Multiboot info as defined in
+// https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#Boot-information-format
+package multiboot
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/u-root/u-root/pkg/ubinary"
+)
+
+var sizeofInfo = uint32(binary.Size(Info{}))
+
+type Flag uint32
+
+const (
+	flagInfoMemory Flag = 1 << iota
+	flagInfoBootDev
+	flagInfoCmdLine
+	flagInfoMods
+	flagInfoAoutSyms
+	flagInfoElfSHDR
+	flagInfoMemMap
+	flagInfoDriveInfo
+	flagInfoConfigTable
+	flagInfoBootLoaderName
+	flagInfoAPMTable
+	flagInfoVideoInfo
+	flagInfoFrameBuffer
+)
+
+// Info represents the Multiboot v1 info passed to the loaded kernel.
+type Info struct {
+	Flags    Flag
+	MemLower uint32
+	MemUpper uint32
+
+	// BootDevice is not supported, always zero.
+	BootDevice uint32
+
+	CmdLine uint32
+
+	ModsCount uint32
+	ModsAddr  uint32
+
+	// Syms is not supported, always zero array.
+	Syms [4]uint32
+
+	MmapLength uint32
+	MmapAddr   uint32
+
+	// Following fields except BootLoaderName are not suppoted yet,
+	// the values are always set to zeros.
+
+	DriversLength uint32
+	DrivesrAddr   uint32
+
+	ConfigTable uint32
+
+	BootLoaderName uint32
+
+	APMTable uint32
+
+	VBEControlInfo  uint32
+	VBEModeInfo     uint32
+	VBEMode         uint16
+	VBEInterfaceSeg uint16
+	VBEInterfaceOff uint16
+	VBEInterfaceLen uint16
+
+	FramebufferAddr   uint16
+	FramebufferPitch  uint16
+	FramebufferWidth  uint32
+	FramebufferHeight uint32
+	FramebufferBPP    byte
+	FramebufferType   byte
+	ColorInfo         [6]byte
+}
+
+type infoWrapper struct {
+	Info
+
+	CmdLine        string
+	BootLoaderName string
+}
+
+// marshal writes out the exact bytes of multiboot info
+// expected by the kernel being loaded.
+func (iw infoWrapper) marshal(base uintptr) ([]byte, error) {
+	offset := sizeofInfo + uint32(base)
+	iw.Info.CmdLine = offset
+	offset += uint32(len(iw.CmdLine)) + 1
+	iw.Info.BootLoaderName = offset
+
+	buf := bytes.Buffer{}
+	if err := binary.Write(&buf, ubinary.NativeEndian, iw.Info); err != nil {
+		return nil, err
+	}
+
+	for _, s := range []string{iw.CmdLine, iw.BootLoaderName} {
+		if _, err := buf.WriteString(s); err != nil {
+			return nil, err
+		}
+		if err := buf.WriteByte(0); err != nil {
+			return nil, err
+		}
+	}
+
+	size := (buf.Len() + 3) &^ 3
+	_, err := buf.Write(bytes.Repeat([]byte{0}, size-buf.Len()))
+	return buf.Bytes(), err
+}
+
+func (iw infoWrapper) size() (uint, error) {
+	b, err := iw.marshal(0)
+	return uint(len(b)), err
+}
---
+++ pkg/multiboot/multiboot_test.go
@@ -0,0 +1,120 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package multiboot
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+)
+
+func createFile(hdr *Header, offset, size int) (io.Reader, error) {
+	buf := bytes.Repeat([]byte{0xDE, 0xAD, 0xBE, 0xEF}, (size+4)/4)
+	buf = buf[:size]
+	if hdr != nil {
+		w := bytes.Buffer{}
+		if err := binary.Write(&w, binary.LittleEndian, *hdr); err != nil {
+			return nil, err
+		}
+		copy(buf[offset:], w.Bytes())
+	}
+	return bytes.NewReader(buf), nil
+}
+
+type flag string
+
+const (
+	flagGood        flag = "good"
+	flagUnsupported      = "unsup"
+	flagBad              = "bad"
+)
+
+func createHeader(fl flag) Header {
+	flags := Flag(0x00000002)
+	var checksum uint32
+	switch fl {
+	case flagGood:
+		checksum = 0xFFFFFFFF - headerMagic - uint32(flags) + 1
+	case flagBad:
+		checksum = 0xDEADBEEF
+	case flagUnsupported:
+		flags = 0x0000FFFC
+		checksum = 0xFFFFFFFF - headerMagic - uint32(flags) + 1
+	}
+
+	return Header{
+		mandatory: mandatory{
+			Magic:    headerMagic,
+			Flags:    flags,
+			Checksum: checksum,
+		},
+		optional: optional{
+			HeaderAddr:  1,
+			LoadAddr:    2,
+			LoadEndAddr: 3,
+			BSSEndAddr:  4,
+			EntryAddr:   5,
+
+			ModeType: 6,
+			Width:    7,
+			Height:   8,
+			Depth:    9,
+		},
+	}
+}
+
+func TestParseHeader(t *testing.T) {
+	mandatorySize := binary.Size(mandatory{})
+	optionalSize := binary.Size(optional{})
+	sizeofHeader := mandatorySize + optionalSize
+
+	for _, test := range []struct {
+		flags  flag
+		offset int
+		size   int
+		err    error
+	}{
+		{flags: flagGood, offset: 0, size: 8192, err: nil},
+		{flags: flagGood, offset: 2048, size: 8192, err: nil},
+		{flags: flagGood, offset: 8192 - sizeofHeader - 4, size: 8192, err: nil},
+		{flags: flagGood, offset: 8192 - sizeofHeader - 1, size: 8192, err: ErrHeaderNotFound},
+		{flags: flagGood, offset: 8192 - sizeofHeader, size: 8192, err: nil},
+		{flags: flagGood, offset: 8192 - 4, size: 8192, err: ErrHeaderNotFound},
+		{flags: flagGood, offset: 8192, size: 16384, err: ErrHeaderNotFound},
+		{flags: flagGood, offset: 0, size: 10, err: io.ErrUnexpectedEOF},
+		{flags: flagBad, offset: 0, size: 8192, err: ErrHeaderNotFound},
+		{flags: flagUnsupported, offset: 0, size: 8192, err: ErrFlagsNotSupported},
+		{flags: flagGood, offset: 8192 - mandatorySize, size: 8192, err: nil},
+	} {
+		t.Run(fmt.Sprintf("flags:%v,off:%v,sz:%v,err:%v", test.flags, test.offset, test.size, test.err), func(t *testing.T) {
+			want := createHeader(test.flags)
+			r, err := createFile(&want, test.offset, test.size)
+			if err != nil {
+				t.Fatalf("Cannot create test file: %v", err)
+			}
+			got, err := parseHeader(r)
+			if err != test.err {
+				t.Fatalf("parseHeader() got error: %v, want: %v", err, test.err)
+			}
+
+			if err != nil {
+				return
+			}
+			if test.size-test.offset > mandatorySize {
+				if !reflect.DeepEqual(got, want) {
+					t.Errorf("parseHeader() got %+v, want %+v", got, want)
+				}
+			} else {
+				if !reflect.DeepEqual(got.mandatory, want.mandatory) {
+					t.Errorf("parseHeader() got %+v, want %+v", got.mandatory, want.mandatory)
+				}
+			}
+
+		})
+	}
+}
