a43947751bc31a7b30a0510c90a18de34fb9589e
Ryan O'Leary <ryanoleary@google.com>
2019-01-15 17:09:35 -0800

    io: Create UintN interface
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/io/seek.go
+++ pkg/io/seek.go
@@ -8,7 +8,7 @@ package io
 
 import (
 	"encoding/binary"
-	"fmt"
+	"io"
 	"os"
 
 	"github.com/u-root/u-root/pkg/ubinary"
@@ -16,52 +16,40 @@ import (
 
 var memPath = "/dev/mem"
 
-func pathRead(path string, addr int64, data interface{}) error {
+func pathRead(path string, addr int64, data UintN) error {
 	f, err := os.OpenFile(path, os.O_RDONLY, 0)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	if _, err := f.Seek(addr, os.SEEK_SET); err != nil {
+	if _, err := f.Seek(addr, io.SeekCurrent); err != nil {
 		return err
 	}
 	return binary.Read(f, ubinary.NativeEndian, data)
 }
 
-// Read reads data from physical memory at address addr. data must be one of:
-// *uint8, *uint16, *uint32, or *uint64. On x86 platforms, this uses the
-// seek+read syscalls. On arm platforms, this uses mmap.
-func Read(addr int64, data interface{}) error {
-	switch data.(type) {
-	case *uint8, *uint16, *uint32, *uint64:
-	default:
-		return fmt.Errorf("cannot read type %T", data)
-	}
+// Read reads data from physical memory at address addr. On x86 platforms,
+// this uses the seek+read syscalls. On arm platforms, this uses mmap.
+func Read(addr int64, data UintN) error {
 	return pathRead(memPath, addr, data)
 }
 
-func pathWrite(path string, addr int64, data interface{}) error {
+func pathWrite(path string, addr int64, data UintN) error {
 	f, err := os.OpenFile(path, os.O_WRONLY, 0)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	if _, err := f.Seek(addr, os.SEEK_SET); err != nil {
+	if _, err := f.Seek(addr, io.SeekCurrent); err != nil {
 		return err
 	}
 	return binary.Write(f, ubinary.NativeEndian, data)
 }
 
-// Write writes data to physical memory at address addr. data must be one of:
-// uint8, uint16, uint32, or uint64. On x86 platforms, this uses the seek+read
-// syscalls. On arm platforms, this uses mmap.
-func Write(addr int64, data interface{}) error {
-	switch data.(type) {
-	case uint8, uint16, uint32, uint64:
-	default:
-		return fmt.Errorf("cannot write type %T", data)
-	}
+// Write writes data to physical memory at address addr. On x86 platforms, this
+// uses the seek+read syscalls. On arm platforms, this uses mmap.
+func Write(addr int64, data UintN) error {
 	return pathWrite(memPath, addr, data)
 }
