46c5858b37793ffd2c04f87fe8a433024f475582
Claudio Zumbo <claudioz@fb.com>
2019-01-22 03:32:57 -0800

    Fixes

--- cmds/hwclock/hwclock.go
+++ cmds/hwclock/hwclock.go
@@ -23,6 +23,7 @@ import (
 	"flag"
 	"fmt"
 	"golang.org/x/sys/unix"
+	"log"
 	"os"
 	"syscall"
 	"time"
@@ -31,29 +32,14 @@ import (
 
 const startYear = 1900
 
-type tm struct {
-	tmSec   int32
-	tmMin   int32
-	tmHour  int32
-	tmMday  int32
-	tmMon   int32
-	tmYear  int32
-	tmWday  int32
-	tmYday  int32
-	tmIsdst int32
-}
+var write = flag.Bool("w", false, "Set hwclock from system clock in UTC")
 
-func readRtc() (time.Time, error) {
+func readRtc(rtcFile *os.File) (time.Time, error) {
 
 	var (
-		ptm     tm
-		retTime time.Time
+		ptm     unix.RTCTime
+		ret time.Time
 	)
-
-	rtcFile, err := os.Open("/dev/rtc")
-	if err != nil {
-		return retTime, err
-	}
 
 	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
 		uintptr(rtcFile.Fd()),
@@ -61,37 +47,37 @@ func readRtc() (time.Time, error) {
 		uintptr(unsafe.Pointer(&ptm)),
 	)
 	if errno != 0 {
-		return retTime, fmt.Errorf("ioctl RTC_RD_TIME returned with errno: %v", errno)
+		return ret, fmt.Errorf("%v", errno)
 	}
 
-	retTime = time.Date(int(ptm.tmYear)+startYear,
-		time.Month(ptm.tmMon),
-		int(ptm.tmMday),
-		int(ptm.tmHour),
-		int(ptm.tmMin),
-		int(ptm.tmSec),
+	ret = time.Date(int(ptm.Year)+startYear,
+		time.Month(ptm.Mon),
+		int(ptm.Mday),
+		int(ptm.Hour),
+		int(ptm.Min),
+		int(ptm.Sec),
 		0,
 		time.UTC)
 
-	return retTime, nil
+	return ret, nil
 }
 
-func setRtcFromSysClock() error {
+func setRtcFromSysClock(rtcFile *os.File) error {
 	timeUTC := time.Now().UTC()
 
 	rtc, err := os.Open("/dev/rtc")
 	if err != nil {
 		return err
 	}
-	stm := tm{tmSec: int32(timeUTC.Second()),
-		tmMin:   int32(timeUTC.Minute()),
-		tmHour:  int32(timeUTC.Hour()),
-		tmMday:  int32(timeUTC.Day()),
-		tmMon:   int32(timeUTC.Month()),
-		tmYear:  int32(timeUTC.Year() - startYear),
-		tmWday:  int32(0),
-		tmYday:  int32(0),
-		tmIsdst: int32(0)}
+	stm := unix.RTCTime{Sec: int32(timeUTC.Second()),
+		Min:   int32(timeUTC.Minute()),
+		Hour:  int32(timeUTC.Hour()),
+		Mday:  int32(timeUTC.Day()),
+		Mon:   int32(timeUTC.Month()),
+		Year:  int32(timeUTC.Year() - startYear),
+		Wday:  int32(0),
+		Yday:  int32(0),
+		Isdst: int32(0)}
 
 	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
 		rtc.Fd(),
@@ -99,24 +85,30 @@ func setRtcFromSysClock() error {
 		uintptr(unsafe.Pointer(&stm)),
 	)
 	if errno != 0 {
-		return fmt.Errorf("ioctl RTC_SET_TIME returned with errno: %v", errno)
+		return fmt.Errorf("%v", errno)
 	}
 	return nil
 }
 
 func main() {
-	writePtr := flag.Bool("w", false, "Set hwclock from system clock in UTC")
+
 	flag.Parse()
 
-	if *writePtr {
-		if err := setRtcFromSysClock(); err != nil {
-			panic(err)
-		}
-	} else {
-		t, err := readRtc()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(t)
+	rtcFile, err := os.Open("/dev/rtc")
+	if err != nil {
+		log.Fatalf("%v\n", err)
 	}
+
+	if *write {
+		if err := setRtcFromSysClock(rtcFile); err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		return
+	}
+
+	t, err := readRtc(rtcFile)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	fmt.Println(t)
 }
