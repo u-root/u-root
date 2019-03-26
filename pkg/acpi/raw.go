1a3d6710ffaebed2edef0c5bb18ef1e0184e6908
Ronald G. Minnich <rminnich@gmail.com>
2019-03-26 21:55:39 +0000

    raw might be able to read
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/acpi/raw.go
+++ pkg/acpi/raw.go
@@ -3,6 +3,8 @@ package acpi
 import (
 	"encoding/binary"
 	"fmt"
+
+	"github.com/u-root/u-root/pkg/io"
 )
 
 // Raw is just a table embedded in a []byte.  Operations on Raw are
@@ -17,6 +19,25 @@ var _ = Tabler(&Raw{})
 func NewRaw(b []byte) (Tabler, error) {
 	u := binary.LittleEndian.Uint32(b[LengthOffset : LengthOffset+4])
 	return &Raw{data: b[0:u]}, nil
+}
+
+// ReadRaw reads a full table in, given an address.
+func ReadRaw(addr uint64) (Tabler, error) {
+	var u io.Uint32
+	// Read the table size at a+4
+	a := int64(addr)
+	if err := io.Read(int64(a+4), &u); err != nil {
+		return nil, err
+	}
+	dat := make([]byte, u)
+	for i := range dat {
+		var d io.Uint8
+		if err := io.Read(a+int64(i), &d); err != nil {
+			return nil, err
+		}
+		dat[i] = uint8(d)
+	}
+	return &Raw{data: dat}, nil
 }
 
 func (r *Raw) Len() int {
