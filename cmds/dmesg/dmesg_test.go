883d683dd2a8ef9e81897b124f819339f6631caf
Christopher Koch <chrisko@google.com>
2019-04-02 23:27:42 -0700

    dmesg: require root for test. fixes #1160
    Kind of sucks, because only CircleCI requires root access for that. Oh
    well.
    
    Signed-off-by: Christopher Koch <chrisko@google.com>

--- cmds/dmesg/dmesg_test.go
+++ cmds/dmesg/dmesg_test.go
@@ -5,13 +5,17 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/u-root/u-root/pkg/testutil"
 )
 
-// TODO(https://github.com/u-root/u-root/issues/1160): This test has been disabled
-func testDmesg(t *testing.T) {
+func TestDmesg(t *testing.T) {
+	if uid := os.Getuid(); uid != 0 {
+		t.Skipf("test requires root on CircleCI, your uid is %d", uid)
+	}
+
 	cmd := testutil.Command(t)
 	out, err := cmd.Output()
 	if err != nil || len(out) == 0 {
