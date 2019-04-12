dbf227b6bc8fa22cf99e62419c8967d81b117a77
Ryan O'Leary <ryanoleary@google.com>
2019-04-12 11:13:58 -0700

    Add sh.Run function and documentation
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/sh/run.go
+++ pkg/sh/run.go
@@ -10,12 +10,19 @@ import (
 	"os/exec"
 )
 
-func RunOrDie(arg0 string, args ...string) {
+// Run runs a command with stdin, stdout and stderr.
+func Run(arg0 string, args ...string) error {
 	cmd := exec.Command(arg0, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
+	return cmd.Run()
+}
+
+// RunOrDie runs a commands with stdin, stdout and stderr. If there is a an
+// error, it is fatally logged.
+func RunOrDie(arg0 string, args ...string) {
+	if err := Run(arg0, args...); err != nil {
 		log.Fatal(err)
 	}
 }
