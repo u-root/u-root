c14c37cbb8361fe1af576a72ef76bec2591e03f7
Ryan O'Leary <ryanoleary@google.com>
2019-04-12 12:29:49 -0700

    Add RunWithLogs function to sh
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/sh/run.go
+++ pkg/sh/run.go
@@ -19,6 +19,21 @@ func Run(arg0 string, args ...string) error {
 	return cmd.Run()
 }
 
+// RunWithLogs runs a command with stdin, stdout and stderr. This function is
+// more verbose than log.Run.
+func RunWithLogs(arg0 string, args ...string) error {
+	log.Printf("executing command %q with args %q", arg0, args)
+	cmd := exec.Command(arg0, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("command %q with args %q failed: %v", arg0, args, err)
+	}
+	return err
+}
+
 // RunOrDie runs a commands with stdin, stdout and stderr. If there is a an
 // error, it is fatally logged.
 func RunOrDie(arg0 string, args ...string) {
