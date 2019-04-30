872d1ce3795d40a4e2946e30bc06b1a2f19485c0
Andrea Barberio <insomniac@slackware.it>
2019-04-30 11:48:53 +0100

    [checker] Explicitly set stdin/out/err in CommandExecutor
    Also use Run instead of Start + Wait
    
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- pkg/checker/commandexecutor.go
+++ pkg/checker/commandexecutor.go
@@ -1,15 +1,16 @@
 package checker
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+)
 
 // CommandExecutor returns a check that runs the provided command and arguments.
 func CommandExecutor(prog string, args ...string) Checker {
 	return func() error {
 		cmd := exec.Command(prog, args...)
-		if err := cmd.Start(); err != nil {
-			return err
-		}
-		if err := cmd.Wait(); err != nil {
+		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+		if err := cmd.Run(); err != nil {
 			return err
 		}
 		return nil
