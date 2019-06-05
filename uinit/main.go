1695fcdf4b4a885d3a76cc1bc6f60f8ebe761d90
tfg13 <tobifleig@gmail.com>
2019-06-05 12:43:45 +0100

    invert interactive flag, only forbig user interaction, don't change sleep behavior
    Signed-off-by: tfg13 <tobifleig@gmail.com>

--- uinit/main.go
+++ uinit/main.go
@@ -12,10 +12,10 @@ import (
 )
 
 var (
-	doQuiet       = flag.Bool("q", false, "Disable verbose output")
-	interactive   = flag.Bool("i", true, "Enable interactive mode")
-	interval      = flag.Int("I", 1, "Interval in seconds before looping to the next boot command")
-	noDefaultBoot = flag.Bool("nodefault", false, "Do not attempt default boot entries if regular ones fail")
+	allowInteractive = flag.Bool("i", true, "Allow user to interrupt boot process and run commands")
+	doQuiet          = flag.Bool("q", false, "Disable verbose output")
+	interval         = flag.Int("I", 1, "Interval in seconds before looping to the next boot command")
+	noDefaultBoot    = flag.Bool("nodefault", false, "Do not attempt default boot entries if regular ones fail")
 )
 
 var defaultBootsequence = [][]string{
@@ -26,18 +26,6 @@ var defaultBootsequence = [][]string{
 func main() {
 	flag.Parse()
 
-	var sleepInterval time.Duration
-	if *interactive {
-		time.Sleep(5 * time.Second)
-		sleepInterval = time.Duration(*interval) * time.Second
-
-		log.Printf("**************************************************************************")
-		log.Print("Starting boot sequence, press CTRL-C within 5 seconds to drop into a shell")
-		log.Printf("**************************************************************************")
-	} else {
-		signal.Ignore()
-	}
-
 	log.Print(`
                      ____            _                 _                 _   
                     / ___| _   _ ___| |_ ___ _ __ ___ | |__   ___   ___ | |_ 
@@ -46,6 +34,17 @@ func main() {
                     |____/ \__, |___/\__\___|_| |_| |_|_.__/ \___/ \___/ \__|
                            |___/
 `)
+
+	sleepInterval := time.Duration(*interval) * time.Second
+
+	if *allowInteractive {
+		log.Printf("**************************************************************************")
+		log.Print("Starting boot sequence, press CTRL-C within 5 seconds to drop into a shell")
+		log.Printf("**************************************************************************")
+		time.Sleep(5 * time.Second)
+	} else {
+		signal.Ignore()
+	}
 
 	// Get and show boot entries
 	bootEntries := booter.GetBootEntries()
@@ -58,12 +57,10 @@ func main() {
 		if err := entry.Booter.Boot(); err != nil {
 			log.Printf("Warning: failed to boot with configuration: %+v", entry)
 		}
-		if *interactive {
-			if !*doQuiet {
-				log.Printf("Sleeping %v before attempting next boot command", sleepInterval)
-			}
-			time.Sleep(sleepInterval)
+		if !*doQuiet {
+			log.Printf("Sleeping %v before attempting next boot command", sleepInterval)
 		}
+		time.Sleep(sleepInterval)
 	}
 
 	// if boot entries failed, use the default boot sequence
@@ -84,13 +81,10 @@ func main() {
 					log.Printf("Error executing %v: %v", cmd, err)
 				}
 			}
-
-			if *interactive {
-				if !*doQuiet {
-					log.Printf("Sleeping %v before attempting next boot command", sleepInterval)
-				}
-				time.Sleep(sleepInterval)
+			if !*doQuiet {
+				log.Printf("Sleeping %v before attempting next boot command", sleepInterval)
 			}
+			time.Sleep(sleepInterval)
 		}
 	}
 }
