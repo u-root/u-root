735afdc821567904cead9c5d5204051310814c9c
Chris Koch <chrisko@google.com>
2019-06-07 06:05:18 -0700

    Add license declaration to systemboot files
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/recovery/recovery.go
+++ pkg/recovery/recovery.go
@@ -1,3 +1,7 @@
+// Copyright 2017-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package recovery
 
 // Recoverer interface offers recovering
