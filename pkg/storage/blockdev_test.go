e4e63e3580095a8be43d47009f9deaaad7ed6238
Chris Koch <chrisko@google.com>
2019-06-07 06:05:18 -0700

    Add license declaration to systemboot files
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/storage/blockdev_test.go
+++ pkg/storage/blockdev_test.go
@@ -1,3 +1,7 @@
+// Copyright 2017-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package storage
 
 import (
@@ -24,26 +28,26 @@ func TestFindMountPointValid(t *testing.T) {
 }
 
 func TestBlockStatFromBytes15Fields(t *testing.T) {
-    // dummy values, don't judge me
-    input := []byte("       0        1        2        3        4        5        6        7        8        9        10        11        12        13        14\n")
-    bs, err := BlockStatFromBytes(input)
-    require.NoError(t, err)
-    require.Equal(t, uint64(5), bs.WriteMerges)
-    require.Equal(t, uint64(14), bs.DiscardTicks)
+	// dummy values, don't judge me
+	input := []byte("       0        1        2        3        4        5        6        7        8        9        10        11        12        13        14\n")
+	bs, err := BlockStatFromBytes(input)
+	require.NoError(t, err)
+	require.Equal(t, uint64(5), bs.WriteMerges)
+	require.Equal(t, uint64(14), bs.DiscardTicks)
 }
 
 func TestBlockStatFromBytes11Fields(t *testing.T) {
-    // dummy values, don't judge me
-    input := []byte("       0        1        2        3        4        5        6        7        8        9        10\n")
-    bs, err := BlockStatFromBytes(input)
-    require.NoError(t, err)
-    require.Equal(t, uint64(5), bs.WriteMerges)
-    require.Equal(t, uint64(0), bs.DiscardTicks)
+	// dummy values, don't judge me
+	input := []byte("       0        1        2        3        4        5        6        7        8        9        10\n")
+	bs, err := BlockStatFromBytes(input)
+	require.NoError(t, err)
+	require.Equal(t, uint64(5), bs.WriteMerges)
+	require.Equal(t, uint64(0), bs.DiscardTicks)
 }
 
 func TestBlockStatFromBytesNotEnoughFields(t *testing.T) {
-    // dummy values, don't judge me
-    input := []byte("       0        1        2        3        4        5        6        7        8\n")
-    _, err := BlockStatFromBytes(input)
-    require.Error(t, err)
+	// dummy values, don't judge me
+	input := []byte("       0        1        2        3        4        5        6        7        8\n")
+	_, err := BlockStatFromBytes(input)
+	require.Error(t, err)
 }
