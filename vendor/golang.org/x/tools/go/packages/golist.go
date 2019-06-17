cffac1edc279df6eaa6d25df72e9e750953f3368
Ronald G. Minnich <rminnich@gmail.com>
2019-06-17 22:04:01 +0000

    dep update
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- vendor/golang.org/x/tools/go/packages/golist.go
+++ vendor/golang.org/x/tools/go/packages/golist.go
@@ -229,7 +229,13 @@ func runContainsQueries(cfg *Config, driver driver, response *responseDeduper, q
 		}
 		dirResponse, err := driver(cfg, pattern)
 		if err != nil {
-			return err
+			// Couldn't find a package for the directory. Try to load the file as an ad-hoc package.
+			var queryErr error
+			dirResponse, err = driver(cfg, query)
+			if queryErr != nil {
+				// Return the original error if the attempt to fall back failed.
+				return err
+			}
 		}
 		isRoot := make(map[string]bool, len(dirResponse.Roots))
 		for _, root := range dirResponse.Roots {
