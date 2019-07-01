fa70321d6d4c236e1ea50f5b7ff80e9a9f54f600
Devon Bautista <devontbautista@gmail.com>
2019-07-01 14:54:33 -0600

    hwclock: Add _darwin extension files
    Signed-off-by: Devon Bautista <devontbautista@gmail.com>

---
+++ cmds/core/hwclock/hwclock_darwin.go
@@ -0,0 +1,51 @@
+// Copyright 2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Read or set the hardware clock (RTC) in UTC format.
+//
+// Synopsis:
+//     hwclock [-w]
+//
+// Description:
+//     It prints the current hwclock time in UTC if called without any flags.
+//     It sets the hwclock to the system clock in UTC if called with -w.
+//
+// Options:
+//     -w: set hwclock to system clock in UTC
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/u-root/u-root/pkg/rtc"
+)
+
+var write = flag.Bool("w", false, "Set hwclock from system clock in UTC")
+
+func main() {
+	flag.Parse()
+
+	r, err := rtc.OpenRTC()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *write {
+		tu := time.Now().UTC()
+		if err := r.Set(tu); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	t, err := r.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print local time. Match the format of util-linux' hwclock.
+	fmt.Println(t.Local().Format("Mon 2 Jan 2006 15:04:05 AM MST"))
+}
