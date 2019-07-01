54ffce8a1bf50cb0a6e542bc3246a61d8a4cf681
Devon Bautista <devontbautista@gmail.com>
2019-07-01 15:03:50 -0600

    switch_root: Add _darwin extension files
    Signed-off-by: Devon Bautista <devontbautista@gmail.com>

---
+++ cmds/core/switch_root/switch_root_darwin.go
@@ -0,0 +1,51 @@
+// Copyright 2012-2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/u-root/u-root/pkg/mount"
+)
+
+var (
+	help    = flag.Bool("h", false, "Help")
+	version = flag.Bool("V", false, "Version")
+)
+
+func usage() string {
+	return "switch_root [-h] [-V]\nswitch_root newroot init"
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		fmt.Println(usage())
+		os.Exit(0)
+	}
+
+	if *help {
+		fmt.Println(usage())
+		os.Exit(0)
+	}
+
+	if *version {
+		fmt.Println("Version XX")
+		os.Exit(0)
+	}
+
+	newRoot := flag.Args()[0]
+	init := flag.Args()[1]
+
+	if err := mount.SwitchRoot(newRoot, init); err != nil {
+		log.Fatalf("switch_root failed %v\n", err)
+	}
+}
