d18e28385192f3325a1b3039adf552b085fdb10b
Christopher Koch <c@chrisko.ch>
2019-07-29 02:52:10 +0000

    pkg/kmodule: add package comment
    Signed-off-by: Christopher Koch <c@chrisko.ch>

--- pkg/kmodule/kmodule_linux.go
+++ pkg/kmodule/kmodule_linux.go
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package kmodule interfaces with Linux kernel modules.
+//
+// kmodule allows loading and unloading kernel modules with dependencies, as
+// well as locating them through probing.
 package kmodule
 
 import (
