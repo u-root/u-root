adddb1212b96df43e542e8c90a194f8473a333c6
Christopher Koch <c@chrisko.ch>
2019-07-29 02:48:33 +0000

    pkg/golang: add package comment
    Signed-off-by: Christopher Koch <c@chrisko.ch>

--- pkg/golang/build.go
+++ pkg/golang/build.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package golang is an API to the Go compiler.
 package golang
 
 import (
