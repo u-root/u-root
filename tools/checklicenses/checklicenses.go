71b0fa61fda1040652eef3bfd6c4a7c61b575d60
Andrea Barberio <insomniac@slackware.it>
2019-08-01 17:55:56 +0100

    [scripts/checlicences] Generalized tool to be used in other projects
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- tools/checklicenses/checklicenses.go
+++ tools/checklicenses/checklicenses.go
@@ -8,6 +8,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -18,23 +19,10 @@ import (
 	"strings"
 )
 
-var absPath = flag.Bool("a", false, "Print absolute paths")
-
-const uroot = "$GOPATH/src/github.com/u-root/u-root"
-
-var oklicenses = []*regexp.Regexp{
-	regexp.MustCompile(
-		`^// Copyright [\d\-, ]+ the u-root Authors\. All rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file\.
-
-`),
-	regexp.MustCompile(
-		`^// Copyright [\d\-, ]+ Google LLC.
-//
-// Licensed under the Apache License, Version 2.0.*
-`),
-}
+var (
+	absPath    = flag.Bool("a", false, "Print absolute paths")
+	configFile = flag.String("c", "", "Configuration file in JSON format")
+)
 
 type rule struct {
 	*regexp.Regexp
@@ -55,21 +43,67 @@ func reject(s string) rule {
 	}
 }
 
-// A file is checked iff all the accepts and none of the rejects match.
-var rules = []rule{
-	accept(`.*\.go`),
-	reject(`vendor/.*`),           // Various authors
-	reject(`cmds/core/elvish/.*`), // elvish developers and contributors
-	reject(`cmds/core/ping/.*`),   // Go authors
-	reject(`cmds/exp/ectool/.*`),  // Chromium authors
+// Config contains the rules for license checking.
+type Config struct {
+	// Licenses is a list of acceptable license headers. Each license is
+	// represented by an array of strings, one string per line, without the
+	// trailing \n .
+	Licenses        [][]string
+	licensesRegexps []*regexp.Regexp
+	// GoPkg is the Go package name to check for licenses
+	GoPkg string
+	// Accept is a list of file patterns to include in the license checking
+	Accept []string
+	accept []rule
+	// Reject is a list of file patterns to exclude from the license checking
+	Reject []string
+	reject []rule
+}
 
-	reject(`cmds/core/man/data/data.go`),       // generated
-	reject(`pkg/diskboot/entrytype_string.go`), // generated
+// CompileRegexps compiles the regular expressions coming from the JSON
+// configuration, and returns an error if an invalid regexp is found.
+func (c *Config) CompileRegexps() error {
+	for _, licenseRegexps := range c.Licenses {
+		licenseRegexp := strings.Join(licenseRegexps, "\n")
+		re, err := regexp.Compile(licenseRegexp)
+		if err != nil {
+			return err
+		}
+		c.licensesRegexps = append(c.licensesRegexps, re)
+	}
+
+	c.accept = make([]rule, 0, len(c.Accept))
+	for _, rule := range c.Accept {
+		c.accept = append(c.accept, accept(rule))
+	}
+
+	c.reject = make([]rule, 0, len(c.Reject))
+	for _, rule := range c.Reject {
+		c.reject = append(c.reject, reject(rule))
+	}
+
+	return nil
 }
 
 func main() {
 	flag.Parse()
-	uroot := os.ExpandEnv(uroot)
+
+	if *configFile == "" {
+		log.Fatal("Config file name cannot be empty")
+	}
+	buf, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		log.Fatalf("Failed to read file %s: %v", *configFile, err)
+	}
+	var config Config
+	if err := json.Unmarshal(buf, &config); err != nil {
+		log.Fatalf("Cannot unmarshal JSON from config file %s: %v", *configFile, err)
+	}
+	if err := config.CompileRegexps(); err != nil {
+		log.Fatalf("Failed to compile regexps from JSON config: %v", err)
+	}
+
+	pkgPath := os.ExpandEnv(config.GoPkg)
 	incorrect := []string{}
 
 	// List files added to u-root.
@@ -79,11 +113,13 @@ func main() {
 	}
 	files := strings.Fields(string(out))
 
+	rules := append(config.accept, config.reject...)
+
 	// Iterate over files.
 outer:
 	for _, file := range files {
 		// Test rules.
-		trimmedPath := strings.TrimPrefix(file, uroot)
+		trimmedPath := strings.TrimPrefix(file, pkgPath)
 		for _, r := range rules {
 			if r.MatchString(trimmedPath) == r.invert {
 				continue outer
@@ -110,7 +146,7 @@ outer:
 			log.Fatalln("cannot read", file, err)
 		}
 		var foundone bool
-		for _, l := range oklicenses {
+		for _, l := range config.licensesRegexps {
 			if l.Match(contents) {
 				foundone = true
 				break
