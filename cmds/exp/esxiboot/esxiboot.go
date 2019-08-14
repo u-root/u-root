cd2bb440cb394d482a64d9e607d5e7fe9be2b656
Chris Koch <chrisko@google.com>
2019-08-13 17:54:02 -0700

    pkg/esxi: implement bootbank logic to locate the right partition
    Goes something like this:
    
    - Locate both <device>5/boot.cfg and <device>6/boot.cfg.
    - If parsable, choose partition with bootstate=(0|2|empty) and greater
      updated=N.
    - Try to load ESXi into memory from chosen partition.
    - Try loading from the other if it fails.
    
    Sometimes, an ESXi partition can contain a valid boot.cfg, but not
    actually any of the named modules. Hence it is important to try fully
    loading ESXi into memory, and only then falling back to the other
    partition.
    
    Only boots partitions with bootstate=0, bootstate=2, bootstate=(empty)
    will boot at all.
    
    Implemented the logic at
    https://github.com/vmware/esx-boot/blob/abe46c4b2edb507e5486c3996075d9bd7ade4ba1/safeboot/bootbank.c
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- cmds/exp/esxiboot/esxiboot.go
+++ cmds/exp/esxiboot/esxiboot.go
@@ -5,7 +5,7 @@
 // esxiboot executes ESXi kernel over the running kernel.
 //
 // Synopsis:
-//     esxiboot [-d --device -p --partition] [-c --config] [-r --cdrom]
+//     esxiboot [-d --device] [-c --config] [-r --cdrom]
 //
 // Description:
 //     Loads and executes ESXi kernel.
@@ -14,7 +14,6 @@
 //     --config=FILE or -c=FILE: set the ESXi config
 //     --device=FILE or -d=FILE: set an ESXi disk to boot from
 //     --cdrom=FILE or -r=FILE: set an ESXI CDROM to boot from
-//     --partition=NUM or -p=NUM: which partition to boot ESXi from (either 5 or 6), only used with --device
 //
 // --device is required to kexec installed ESXi instance.
 // You don't need it if you kexec ESXi installer.
@@ -30,7 +29,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 
@@ -41,48 +39,54 @@ import (
 )
 
 var (
-	cfg       = flag.StringP("config", "c", "", "ESXi config file")
-	cdrom     = flag.StringP("cdrom", "r", "", "ESXi CDROM boot device")
-	diskDev   = flag.StringP("device", "d", "", "ESXi disk boot device")
-	partition = flag.IntP("partition", "p", 5, "ESXi boot partition")
+	cfg     = flag.StringP("config", "c", "", "ESXi config file")
+	cdrom   = flag.StringP("cdrom", "r", "", "ESXi CDROM boot device")
+	diskDev = flag.StringP("device", "d", "", "ESXi disk boot device")
 )
 
 func main() {
 	flag.Parse()
 	if *diskDev == "" && *cfg == "" && *cdrom == "" {
-		log.Printf("Either --config, --device, or --cdrom must not be empty")
+		log.Printf("Either --config, --device, or --cdrom must be specified")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	var mi *boot.MultibootImage
-	var err error
-	if len(*cfg) > 0 {
-		mi, err = esxi.LoadConfig(*cfg)
-	} else if len(*cdrom) > 0 {
-		// This is where the ESXi disk will be mounted.
-		mountPoint, xerr := ioutil.TempDir("", "esxicdrom")
-		if xerr != nil {
-			log.Fatal(xerr)
+	if len(*diskDev) > 0 {
+		imgs, err := esxi.LoadDisk(*diskDev)
+		if err != nil {
+			log.Fatalf("Failed to load ESXi configuration: %v", err)
 		}
 
-		mi, err = esxi.LoadCDROM(mountPoint, *cdrom)
+		loaded := false
+		for _, img := range imgs {
+			if err := img.Load(false); err != nil {
+				log.Printf("Failed to load ESXi image (%v) into memory: %v", img, err)
+			} else {
+				// We loaded one, that's it.
+				loaded = true
+				break
+			}
+		}
+		if !loaded {
+			log.Fatalf("Failed to load all ESXi images found.")
+		}
 	} else {
-		// This is where the ESXi disk will be mounted.
-		mountPoint, xerr := ioutil.TempDir("", "esxidisk")
-		if xerr != nil {
-			log.Fatal(xerr)
+		var err error
+		var img *boot.MultibootImage
+		if len(*cfg) > 0 {
+			img, err = esxi.LoadConfig(*cfg)
+		} else if len(*cdrom) > 0 {
+			img, err = esxi.LoadCDROM(*cdrom)
 		}
+		if err != nil {
+			log.Fatalf("Failed to load ESXi configuration: %v", err)
+		}
+		if err := img.Load(false); err != nil {
+			log.Fatalf("Failed to load ESXi image (%v) into memory: %v", img, err)
+		}
+	}
 
-		mi, err = esxi.LoadOS(mountPoint, *diskDev, *partition)
-	}
-	if err != nil {
-		log.Fatalf("Failed to find ESXi: %v", err)
-	}
-
-	if err := mi.Load(false); err != nil {
-		log.Fatalf("Failed to load ESXi into memory: %v", err)
-	}
 	if err := boot.Execute(); err != nil {
 		log.Fatalf("Failed to boot image: %v", err)
 	}
--- pkg/esxi/esxi.go
+++ pkg/esxi/esxi.go
@@ -2,9 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package esxi contains an ESXi boot config parser.
+// Package esxi contains an ESXi boot config parser for disks and CDROMs.
 //
-// This package can read ESXi boot configurations from disks or CDROMs.
+// For CDROMs, it parses the boot.cfg found in the root directory and tries to
+// boot from it.
+//
+// For disks, there may be multiple boot partitions:
+//
+// - Locates both <device>5/boot.cfg and <device>6/boot.cfg.
+//
+// - If parsable, chooses partition with bootstate=(0|2|empty) and greater
+// updated=N.
+//
+// Sometimes, an ESXi partition can contain a valid boot.cfg, but not actually
+// any of the named modules. Hence it is important to try fully loading ESXi
+// into memory, and only then falling back to the other partition.
+//
+// Only boots partitions with bootstate=0, bootstate=2, bootstate=(empty) will
+// boot at all.
+//
+// Most of the parsing logic in this package comes from
+// https://github.com/vmware/esx-boot/blob/master/safeboot/bootbank.c
 package esxi
 
 import (
@@ -12,8 +30,10 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"golang.org/x/sys/unix"
@@ -23,45 +43,105 @@ import (
 	"github.com/u-root/u-root/pkg/mount"
 )
 
-// LoadOS loads an ESXi multiboot kernel from disk device's partition.
+// LoadDisk loads the right ESXi multiboot kernel from partitions 5 or 6 of the
+// given device.
 //
-// If device is /dev/sda, and partition is 5, /dev/sda5 will be mounted at
-// mountPoint.
-func LoadOS(mountPoint string, device string, partition int) (*boot.MultibootImage, error) {
-	partitionDev := fmt.Sprintf("%s%d", device, partition)
-	if err := mount.Mount(partitionDev, mountPoint, "vfat", "", unix.MS_RDONLY|unix.MS_NOATIME); err != nil {
-		return nil, err
+// The kernels are returned in the priority order according to the bootstate
+// and updated values in their boot configurations.
+//
+// The caller should try loading all returned images in order, as some of them
+// may not be valid.
+//
+// device5 and device6 will be mounted at temporary directories.
+func LoadDisk(device string) ([]*boot.MultibootImage, error) {
+	opts5, err5 := mountPartition(fmt.Sprintf("%s5", device))
+	opts6, err6 := mountPartition(fmt.Sprintf("%s6", device))
+	if err5 != nil && err6 != nil {
+		return nil, fmt.Errorf("could not mount or read either partition 5 (%v) or partition 6 (%v)", err5, err6)
 	}
-	return loadConfig(filepath.Join(mountPoint, "boot.cfg"), device, partition)
+	return getImages(device, opts5, opts6)
+}
+
+func getImages(device string, opts5, opts6 *options) ([]*boot.MultibootImage, error) {
+	var (
+		img5, img6 *boot.MultibootImage
+		err5, err6 error
+	)
+	if opts5 != nil {
+		img5, err5 = getBootImage(*opts5, device, 5)
+	}
+	if opts6 != nil {
+		img6, err6 = getBootImage(*opts6, device, 6)
+	}
+	if img5 == nil && img6 == nil {
+		return nil, fmt.Errorf("could not read boot configs on partition 5 (%v) or partition 6 (%v)", err5, err6)
+	}
+
+	if img5 != nil && img6 != nil {
+		if opts6.updated > opts5.updated {
+			return []*boot.MultibootImage{img6, img5}, nil
+		}
+		return []*boot.MultibootImage{img5, img6}, nil
+	} else if img5 != nil {
+		return []*boot.MultibootImage{img5}, nil
+	}
+	return []*boot.MultibootImage{img6}, nil
 }
 
 // LoadCDROM loads an ESXi multiboot kernel from a CDROM at device.
 //
 // device will be mounted at mountPoint.
-func LoadCDROM(mountPoint string, device string) (*boot.MultibootImage, error) {
+func LoadCDROM(device string) (*boot.MultibootImage, error) {
+	mountPoint, err := ioutil.TempDir("", "esxi-mount-")
+	if err != nil {
+		return nil, err
+	}
 	if err := mount.Mount(device, mountPoint, "iso9660", "", unix.MS_RDONLY|unix.MS_NOATIME); err != nil {
 		return nil, err
 	}
 	// Don't pass the device to ESXi. It doesn't need it.
-	return loadConfig(filepath.Join(mountPoint, "boot.cfg"), "", 0)
+	return LoadConfig(filepath.Join(mountPoint, "boot.cfg"))
 }
 
 // LoadConfig loads an ESXi configuration from configFile.
 func LoadConfig(configFile string) (*boot.MultibootImage, error) {
-	return loadConfig(configFile, "", 0)
-}
-
-func loadConfig(configFile, device string, partition int) (*boot.MultibootImage, error) {
 	opts, err := parse(configFile)
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse config at %s: %v", configFile, err)
 	}
+	return getBootImage(opts, "", 0)
+}
+
+func mountPartition(dev string) (*options, error) {
+	base := filepath.Base(dev)
+	mountPoint, err := ioutil.TempDir("", fmt.Sprintf("%s-", base))
+	if err != nil {
+		return nil, err
+	}
+	if err := mount.Mount(dev, mountPoint, "vfat", "", unix.MS_RDONLY|unix.MS_NOATIME); err != nil {
+		return nil, err
+	}
+
+	configFile := filepath.Join(mountPoint, "boot.cfg")
+	opts, err := parse(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse config at %s: %v", configFile, err)
+	}
+	return &opts, nil
+}
+
+func getBootImage(opts options, device string, partition int) (*boot.MultibootImage, error) {
+	// esx-boot/safeboot/bootbank.c:bank_scan says only "valid" and "dirty"
+	// are bootable partitions.
+	if opts.bootstate != bootValid && opts.bootstate != bootDirty {
+		return nil, fmt.Errorf("boot state %d invalid", opts.bootstate)
+	}
+
 	if len(device) > 0 {
 		if err := opts.addUUID(device, partition); err != nil {
 			return nil, fmt.Errorf("cannot add boot uuid of %s: %v", device, err)
 		}
 	}
-
 	return &boot.MultibootImage{
 		Path:    opts.kernel,
 		Cmdline: opts.args,
@@ -69,27 +149,30 @@ func loadConfig(configFile, device string, partition int) (*boot.MultibootImage,
 	}, nil
 }
 
+type options struct {
+	kernel    string
+	args      string
+	modules   []string
+	updated   int
+	bootstate bootstate
+}
+
+type bootstate int
+
+// From safeboot.c
 const (
-	kernel  = "kernel"
-	args    = "kernelopt"
-	modules = "modules"
-
-	comment = '#'
-	sep     = "---"
-
-	uuidMagic = "VMWARE FAT16    "
-	uuidSize  = 32
+	bootValid     bootstate = 0
+	bootUpgrading bootstate = 1
+	bootDirty     bootstate = 2
+	bootInvalid   bootstate = 3
 )
 
-type options struct {
-	kernel  string
-	args    string
-	modules []string
-}
+// So tests can replace this and don't have to have actual block devices.
+var getBlockSize = gpt.GetBlockSize
 
 func getUUID(device string, partition int) (string, error) {
 	device = strings.TrimRight(device, "/")
-	blockSize, err := gpt.GetBlockSize(device)
+	blockSize, err := getBlockSize(device)
 	if err != nil {
 		return "", err
 	}
@@ -130,6 +213,14 @@ func (o *options) addUUID(device string, partition int) error {
 	return nil
 }
 
+const (
+	comment = '#'
+	sep     = "---"
+
+	uuidMagic = "VMWARE FAT16    "
+	uuidSize  = 32
+)
+
 func parse(configFile string) (options, error) {
 	dir := filepath.Dir(configFile)
 
@@ -139,7 +230,15 @@ func parse(configFile string) (options, error) {
 	}
 	defer f.Close()
 
-	var opt options
+	// An empty or missing updated value is always 0, so we can let the
+	// ints be initialized to 0.
+	//
+	// see esx-boot/bootlib/parse.c:parse_config_file.
+	opt := options{
+		// Default value taken from
+		// esx-boot/safeboot/bootbank.c:bank_scan.
+		bootstate: bootInvalid,
+	}
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -157,11 +256,41 @@ func parse(configFile string) (options, error) {
 		key := strings.TrimSpace(tokens[0])
 		val := strings.TrimSpace(tokens[1])
 		switch key {
-		case kernel:
+		case "kernel":
 			opt.kernel = filepath.Join(dir, val)
-		case args:
+		case "kernelopt":
 			opt.args = val
-		case modules:
+		case "updated":
+			if len(val) == 0 {
+				// Explicitly setting to 0, as in
+				// esx-boot/bootlib/parse.c:parse_config_file,
+				// in case this value is specified twice.
+				opt.updated = 0
+			} else {
+				n, err := strconv.Atoi(val)
+				if err != nil {
+					return options{}, err
+				}
+				opt.updated = n
+			}
+		case "bootstate":
+			if len(val) == 0 {
+				// Explicitly setting to valid, as in
+				// esx-boot/bootlib/parse.c:parse_config_file,
+				// in case this value is specified twice.
+				opt.bootstate = bootValid
+			} else {
+				n, err := strconv.Atoi(val)
+				if err != nil {
+					return options{}, err
+				}
+				if n < 0 || n > 3 {
+					opt.bootstate = bootInvalid
+				} else {
+					opt.bootstate = bootstate(n)
+				}
+			}
+		case "modules":
 			for _, tok := range strings.Split(val, sep) {
 				// Each module is "filename arg0 arg1 arg2" and
 				// the filename is relative to the directory
