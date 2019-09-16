e28c99a1d58699d499be9cee0109581d9238eefd
Deomid "rojer" Ryabkov <rojer9@fb.com>
2019-09-15 17:40:09 -0700

    dmidecode: Add --dump-bin
    Signed-off-by: Deomid "rojer" Ryabkov <rojer9@fb.com>

--- cmds/exp/dmidecode/dmidecode.go
+++ cmds/exp/dmidecode/dmidecode.go
@@ -17,6 +17,7 @@ import (
 )
 
 var (
+	flagDumpBin  = flag.String("dump-bin", "", `Do not decode the entries, instead dump the DMI data to a file in binary form. The generated file is suitable to pass to --from-dump later.`)
 	flagFromDump = flag.String("from-dump", "", `Read the DMI data from a binary file previously generated using --dump-bin.`)
 	flagType     = flag.StringSliceP("type", "t", nil, `Only  display  the  entries of type TYPE. TYPE can be either a DMI type number, or a comma-separated list of type numbers, or a keyword from the following list: bios, system, baseboard, chassis, processor, memory, cache, connector, slot. If this option is used more than once, the set of displayed entries will be the union of all the given types. If TYPE is not provided or not valid, a list of all valid keywords is printed and dmidecode exits with an error.`)
 	// NB: When adding flags, update resetFlags in dmidecode_test.
@@ -60,17 +61,56 @@ func parseTypeFilter(typeStrings []string) (map[smbios.TableType]bool, error) {
 	return types, nil
 }
 
+func dumpBin(textOut io.Writer, entryData, tableData []byte, fileName string) *dmiDecodeError {
+	// Need to rewrite address to be compatible with dmidecode(8).
+	e32, e64, err := smbios.ParseEntry(entryData)
+	if err != nil {
+		return &dmiDecodeError{code: 1, error: fmt.Errorf("error parsing entry point structure: %s", err)}
+	}
+	var edata []byte
+	switch {
+	case e32 != nil:
+		e32.StructTableAddr = 0x20
+		edata, _ = e32.MarshalBinary()
+	case e64 != nil:
+		e64.StructTableAddr = 0x20
+		edata, _ = e64.MarshalBinary()
+	}
+	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return &dmiDecodeError{code: 1, error: fmt.Errorf("error opening file for writing: %s", err)}
+	}
+	defer f.Close()
+	fmt.Fprintf(textOut, "# Writing %d bytes to %s.\n", len(edata), fileName)
+	if _, err := f.Write(edata); err != nil {
+		return &dmiDecodeError{code: 1, error: fmt.Errorf("error writing entry: %s", err)}
+	}
+	for i := len(edata); i < 0x20; i++ {
+		if _, err := f.Write([]byte{0}); err != nil {
+			return &dmiDecodeError{code: 1, error: fmt.Errorf("error writing entry: %s", err)}
+		}
+	}
+	fmt.Fprintf(textOut, "# Writing %d bytes to %s.\n", len(tableData), fileName)
+	if _, err := f.Write(tableData); err != nil {
+		return &dmiDecodeError{code: 1, error: fmt.Errorf("error writing table data: %s", err)}
+	}
+	return nil
+}
+
 func dmiDecode(textOut io.Writer) *dmiDecodeError {
 	typeFilter, err := parseTypeFilter(*flagType)
 	if err != nil {
 		return &dmiDecodeError{code: 2, error: fmt.Errorf("invalid --type: %s", err)}
 	}
 	fmt.Fprintf(textOut, "# dmidecode-go\n") // TODO: version.
-	entry, data, err := getData(textOut, *flagFromDump, "/sys/firmware/dmi/tables")
+	entryData, tableData, err := getData(textOut, *flagFromDump, "/sys/firmware/dmi/tables")
 	if err != nil {
 		return &dmiDecodeError{code: 1, error: fmt.Errorf("error parsing loading data: %s", err)}
 	}
-	si, err := smbios.ParseInfo(entry, data)
+	if *flagDumpBin != "" {
+		return dumpBin(textOut, entryData, tableData, *flagDumpBin)
+	}
+	si, err := smbios.ParseInfo(entryData, tableData)
 	if err != nil {
 		return &dmiDecodeError{code: 1, error: fmt.Errorf("error parsing data: %s", err)}
 	}
