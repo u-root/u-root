c4a43f4e6354b4adbd25def03a2f9ba926e50bae
Chris Koch <chrisko@google.com>
2019-10-05 14:22:17 -0700

    ulog: klog only on Linux.
    Signed-off-by: Chris Koch <chrisko@google.com>

---
+++ pkg/ulog/klog_other.go
@@ -0,0 +1,10 @@
+// Copyright 2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !linux
+
+package ulog
+
+// KernelLog prints to stderr log on non-Linux systems.
+var KernelLog = Log
