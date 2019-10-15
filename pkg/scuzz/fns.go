6a3b5c6b124243aed82cd9a8352a65e143da6bac
Ronald G. Minnich <rminnich@gmail.com>
2019-10-15 22:18:02 +0000

    scuzz: a package for controlling disk devices
    The scuzz package defines Request and Disk interfaces.
    
    Requests are requests to operate on Disks.
    
    The Disk interface defines functions to create Requests
    and to invoke an operation, given a request.
    
    We provide an implementation that should work for a Linux
    SCSI Generic device. We include a test which should ensure
    the packets are correct. The packet header and data in this
    case are generated from a fork of hdparm maintained at
    github.com/rminnich/hdparm.
    
    We also include a sample hdparm command.
    
    We currently only support one request, namely, Unlock.
    We expect to need more :-)
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ pkg/scuzz/fns.go
@@ -0,0 +1,8 @@
+// Copyright 2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package scuzz
+
+// Debug is an empty function you can replace with, e.g., log.Printf
+var Debug = func(string, ...interface{}) {}
