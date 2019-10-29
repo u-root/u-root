b14fab63a0fb180c4407fd6f2c59f0cc777be48a
J. Lowell Wofford <lowell@lanl.gov>
2019-10-29 09:48:32 -0700

    id refactor; add support for specifying user
    Signed-off-by: J. Lowell Wofford <lowell@lanl.gov>

---
+++ cmds/core/id/groups.go
@@ -0,0 +1,87 @@
+// Copyright 2017-2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Groups manages a group database, typically loaded from /etc/group
+type Groups struct {
+	gidToGroup map[int]string
+	groupToGID map[string]int
+	userToGIDs map[string][]int
+	gidToUsers map[int][]string
+}
+
+// GetGID returns the GID of a group
+func (g *Groups) GetGID(name string) (int, error) {
+	if v, ok := g.groupToGID[name]; ok {
+		return v, nil
+	}
+	return -1, fmt.Errorf("unknown group name: %s", name)
+}
+
+// GetGroup gets the group of a GID
+func (g *Groups) GetGroup(gid int) (string, error) {
+	if v, ok := g.gidToGroup[gid]; ok {
+		return v, nil
+	}
+	return "", fmt.Errorf("unknown gid: %d", gid)
+}
+
+// UserGetGIDs returns a slice of GIDs for a username
+func (g *Groups) UserGetGIDs(username string) []int {
+	if v, ok := g.userToGIDs[username]; ok {
+		return v
+	}
+	return nil
+}
+
+// NewGroups reads the GroupFile for groups.
+// It assumes the format "name:passwd:number:groupList".
+func NewGroups(file string) (g *Groups, e error) {
+	g = &Groups{}
+
+	g.gidToGroup = make(map[int]string)
+	g.groupToGID = make(map[string]int)
+	g.gidToUsers = make(map[int][]string)
+	g.userToGIDs = make(map[string][]int)
+
+	groupFile, err := os.Open(file)
+	if err != nil {
+		return g, err
+	}
+
+	var groupInfo []string
+
+	groupScanner := bufio.NewScanner(groupFile)
+
+	for groupScanner.Scan() {
+		if groupScanner.Text()[0] == '#' { // skip comments
+			continue
+		}
+		groupInfo = strings.Split(groupScanner.Text(), ":")
+		groupNum, err := strconv.Atoi(groupInfo[2])
+		if err != nil {
+			return nil, err
+		}
+
+		g.gidToGroup[groupNum] = groupInfo[0]
+		g.groupToGID[groupInfo[0]] = groupNum
+
+		users := strings.Split(groupInfo[3], ",")
+		for _, u := range users {
+			g.userToGIDs[u] = append(g.userToGIDs[u], groupNum)
+			g.gidToUsers[groupNum] = append(g.gidToUsers[groupNum], u)
+		}
+	}
+
+	return
+}
--- cmds/core/id/id_test.go
+++ cmds/core/id/id_test.go
@@ -6,7 +6,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
+	"sort"
 	"testing"
 
 	"github.com/u-root/u-root/pkg/testutil"
@@ -48,6 +50,217 @@ func TestInvocation(t *testing.T) {
 		if e[logPrefixLength:] != test.out {
 			t.Errorf("id for '%v' failed: got '%s', want '%s'", test.opt, e, test.out)
 		}
+	}
+}
+
+type passwd struct {
+	name string
+	uid  int
+	gid  int
+}
+
+var passwdShould = []passwd{
+	{"root", 0, 0},
+	{"bin", 1, 1},
+	{"daemon", 2, 2},
+	{"lary", 1000, 1000},
+	{"curly", 1001, 1001},
+	{"moe", 1002, 2002},
+}
+
+var passwdShouldnt = []passwd{
+	{"adm", 3, 4},
+}
+
+var passwdFiles = []string{
+	"testdata/passwd-simple.txt",
+	"testdata/passwd-comments.txt",
+}
+
+type group struct {
+	name string
+	gid  int
+}
+
+var groupShould = []group{
+	{"printadmin", 997},
+	{"ssh_keys", 996},
+	{"rpcuser", 29},
+	{"nfsnobody", 65534},
+	{"sshd", 74},
+	{"wheel", 10},
+}
+
+var groupShouldnt = []group{
+	{"bad", 314},
+	{"wrong", 996},
+	{"wheel", 11},
+}
+
+var groupFiles = []string{
+	"testdata/group-simple.txt",
+	"testdata/group-comments.txt",
+}
+
+var groupMembers = map[string][]int{
+	"larry": {10, 74},
+	"curly": {10, 29},
+	"moe":   {10},
+	"joe":   {},
+}
+
+func passwdSame(u *Users, us passwd) error {
+	var s string
+	var d int
+	var err error
+	d, err = u.GetUID(us.name)
+	if err != nil {
+		return fmt.Errorf("failed to GetUID expected user %s: %v", us.name, err)
+	}
+	if d != us.uid {
+		return fmt.Errorf("wrong UID for %s: got %d, expected %d", us.name, d, us.uid)
+	}
+
+	d, err = u.GetGID(us.uid)
+	if err != nil {
+		return fmt.Errorf("failed to GetGID expected uid %d: %v", us.uid, err)
+	}
+	if d != us.gid {
+		return fmt.Errorf("wrong GID for uid %d: got %d, expected %d", us.uid, d, us.gid)
+	}
+
+	s, err = u.GetUser(us.uid)
+	if err != nil {
+		return fmt.Errorf("failed to GetUser expected user %s: %v", us.name, err)
+	}
+	if s != us.name {
+		return fmt.Errorf("wrong username for %d: got %s, expected %s", us.uid, s, us.name)
+	}
+	return nil
+}
+
+func TestUsers(t *testing.T) {
+	t.Run("non-existent passwd file", func(t *testing.T) {
+		f := "testdata/does-not-exist"
+		u, e := NewUsers(f)
+		if e == nil {
+			t.Errorf("NewUser on non-existant file should return an error")
+		}
+		if u == nil {
+			t.Errorf("NewUser should return a valid Users object, even on error")
+		}
+	})
+	t.Run("empty passwd file", func(t *testing.T) {
+		f := "testdata/passwd-empty.txt"
+		u, e := NewUsers(f)
+		if e != nil {
+			t.Errorf("NewUser should not report error for empty passwd file")
+		}
+		if u == nil {
+			t.Errorf("NewUser should return a valid Users object even if passwd file is empty")
+		}
+	})
+	for _, f := range passwdFiles {
+		t.Run(f, func(t *testing.T) {
+			u, e := NewUsers(f)
+			if e != nil {
+				t.Errorf("NewUser should not return an error on valid file")
+			}
+			if u == nil {
+				t.Errorf("NewUser should return a valid Users object on valid file")
+			}
+			for _, us := range passwdShould {
+				if e := passwdSame(u, us); e != nil {
+					t.Errorf("%v", e)
+				}
+			}
+			for _, us := range passwdShouldnt {
+				if e := passwdSame(u, us); e == nil {
+					t.Errorf("user %s matched when it shouldn't", us.name)
+				}
+			}
+		})
+	}
+}
+
+func groupSame(g *Groups, gs group) error {
+	var s string
+	var d int
+	var err error
+
+	d, err = g.GetGID(gs.name)
+	if err != nil {
+		return fmt.Errorf("failed to GetGID expected group %s: %v", gs.name, err)
+	}
+	if d != gs.gid {
+		return fmt.Errorf("wrong GID for %s: got %d, expected %d", gs.name, d, gs.gid)
+	}
+
+	s, err = g.GetGroup(gs.gid)
+	if err != nil {
+		return fmt.Errorf("failed to GetGroup expected group %s: %v", gs.name, err)
+	}
+	if s != gs.name {
+		return fmt.Errorf("wrong groupname for %d: got %s, expected %s", gs.gid, s, gs.name)
+	}
+	return nil
+}
+
+func TestGroups(t *testing.T) {
+	t.Run("non-existent group file", func(t *testing.T) {
+		f := "testdata/does-not-exist"
+		g, e := NewGroups(f)
+		if e == nil {
+			t.Errorf("NewGroups jnon-existant file should return an error")
+		}
+		if g == nil {
+			t.Errorf("NewGroups should return a valid Groups object, even on error")
+		}
+	})
+	t.Run("empty passwd file", func(t *testing.T) {
+		f := "testdata/group-empty.txt"
+		g, e := NewGroups(f)
+		if e != nil {
+			t.Errorf("NewGroups should not report error for empty passwd file")
+		}
+		if g == nil {
+			t.Errorf("NewGroups should return a valid Users object even if passwd file is empty")
+		}
+	})
+	for _, f := range groupFiles {
+		t.Run(f, func(t *testing.T) {
+			g, e := NewGroups(f)
+			if e != nil {
+				t.Errorf("NewGroups should not return an error on valid file")
+			}
+			if g == nil {
+				t.Errorf("NewGroups should return a valid Users object on valid file")
+			}
+			for _, gs := range groupShould {
+				if e := groupSame(g, gs); e != nil {
+					t.Errorf("%v", e)
+				}
+			}
+			for _, gs := range groupShouldnt {
+				if e := groupSame(g, gs); e == nil {
+					t.Errorf("group %s matched when it shouldn't", gs.name)
+				}
+			}
+			for u, is := range groupMembers {
+				js := g.UserGetGIDs(u)
+				if len(js) != len(is) {
+					t.Errorf("unequal gid lists for %s: %v vs %v", u, is, js)
+				} else {
+					sort.Ints(is)
+					sort.Ints(js)
+					for i := range is {
+						if is[i] != js[i] {
+							t.Errorf("unequal gid lists for %s: %v vs %v", u, is, js)
+						}
+					}
+				}
+			}
+		})
 	}
 }
 
