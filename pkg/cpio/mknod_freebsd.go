0291c169cbf886ca47f032cb42eea566f21f8aa7
Daniel Maslowski <info@orangecms.org>
2019-10-31 21:58:22 +0100

    cpio: use uint64 for Mknod on FreeBSD
    see also https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=236131
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

---
+++ pkg/cpio/mknod_freebsd.go
@@ -0,0 +1,13 @@
+// Copyright 2013-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cpio
+
+import (
+	"syscall"
+)
+
+func mknod(path string, mode uint32, dev int) (err error) {
+	return syscall.Mknod(path, mode, uint64(dev))
+}
