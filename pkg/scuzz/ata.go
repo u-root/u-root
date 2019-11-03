ccdd7842fbc3720eb48909ebaf34a7241d684de0
Ronald G. Minnich <rminnich@gmail.com>
2019-11-03 19:58:44 +0000

    SIP: scuzz: add Info struct and support functions
    The Info struct contains information that is drive-independent.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/scuzz/ata.go
+++ pkg/scuzz/ata.go
@@ -4,6 +4,12 @@
 
 package scuzz
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
 const (
 	// golangci's requirements around unused constants
 	// are inconsistent with how one writes user level drivers.
@@ -78,6 +84,10 @@ type (
 	// for all kernels what the rules are.
 	dataBlock [oldSchoolBlockLen]byte
 
+	// Some ATA blocks are best dealt with as blocks of "words".
+	// They are in big-endian order.
+	wordBlock [oldSchoolBlockLen / 2]uint16
+
 	// commandDataBlock defines a command and any associated data.
 	commandDataBlock [ata16Len]byte
 
@@ -85,4 +95,75 @@ type (
 	statusBlock [maxStatusBlockLen]byte
 
 	direction int32
+
+	// ataString is 10 words. Each word contains two bytes of the string,
+	// in BigEndian order, i.e., byte order is 1032547698
+	// Hence, code can not just take a string from the
+	// drive and use it: it must swap the bytes in each word.
+	ataString [10]uint16
 )
+
+func (b dataBlock) toWordBlock() (wordBlock, error) {
+	var w wordBlock
+	err := binary.Read(bytes.NewBuffer(b[:]), binary.BigEndian, &w)
+	return w, err
+}
+
+// mustLBA confirms that we are dealing with an LBA device.
+// This means a post-2003 device. The standard hdparm command deals
+// with all kinds of obsolete stuff; we don't care.
+// The spec does not have a name for the bits or the offsets
+// other than "Obsolete", "Retired", "Must be zero" or "Must be one".
+// We follow the practice of existing code of not naming them either.
+func (w wordBlock) mustLBA() error {
+	var check = []struct {
+		off  int
+		mask uint16
+		bit  uint16
+	}{
+		{0, 0x8000, 0x8000},
+		{49, 0x200, 0x200},
+		{83, 0xc000, 0x4000},
+		{86, 0x400, 0x400},
+	}
+	for _, c := range check {
+		v, m, b := w[c.off], c.mask, c.bit
+		if (v & m) != b {
+			return fmt.Errorf("unsupported and probably non-ATA48 ddevice: word at offset %d: 0x%#x and should be 0x%#x", c.off, c.mask&v, b)
+		}
+	}
+	return nil
+}
+
+// String is a stringer for ataString
+func (a ataString) String() string {
+	var s []byte
+	for i := range a {
+		s = append(s, byte(a[i]), byte(a[i]>>8))
+	}
+	return string(s)
+}
+
+// unpackIdentify unpacks a wordBlock into an Info.
+func unpackIdentify(s statusBlock, w wordBlock) (*Info, error) {
+	// Double check that this is a true LBA48 packet.
+	if err := w.mustLBA(); err != nil {
+		return nil, err
+	}
+	var nsects uint64
+	var info = &Info{}
+	for i := 103; i >= 104; i-- {
+		nsects <<= 16
+		nsects |= uint64(w[i])
+	}
+	info.NumberSectors = nsects
+	// If you look at the Linux hdparm source, you will see it uses
+	// some values this function does not.
+	// Per the standard,
+	// offsets 1, 3, and 6 are obsolete;
+	// 4, 5, 9 are retired.
+	// hdparm presents these as RawCHS, TrkSize, and SecSize.
+	// We ignore them.
+	info.ECCBytes = uint(w[22])
+	return info, nil
+}
