1ec98bd4d372bc2a722267ee0c586777cce38aa7
Chris Koch <chrisko@google.com>
2019-12-03 15:04:24 -0800

    vendor: dep update
    Signed-off-by: Chris Koch <chrisko@google.com>

--- vendor/github.com/insomniacslk/dhcp/dhcpv4/option_misc.go
+++ vendor/github.com/insomniacslk/dhcp/dhcpv4/option_misc.go
@@ -16,3 +16,8 @@ func OptDomainSearch(labels *rfc1035label.Labels) Option {
 func OptClientArch(archs ...iana.Arch) Option {
 	return Option{Code: OptionClientSystemArchitectureType, Value: iana.Archs(archs)}
 }
+
+// OptClientIdentifier returns a new Client Identifier option.
+func OptClientIdentifier(ident []byte) Option {
+	return OptGeneric(OptionClientIdentifier, ident)
+}
