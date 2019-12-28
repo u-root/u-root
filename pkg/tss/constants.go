e695fbee7df4caba120c8ec69efbca6aedda9526
Philipp Deppenwiese <zaolin@das-labor.org>
2019-12-28 18:56:12 +0100

    Initial checkin for tpm library support
    Signed-off-by: Philipp Deppenwiese <zaolin@das-labor.org>

---
+++ pkg/tss/constants.go
@@ -0,0 +1,43 @@
+// Copyright 2020 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package tss provides TPM 1.2/2.0 core functionality and
+// abstraction layer for high-level functions
+
+package tss
+
+// Locality type
+type Locality byte
+
+// TPMVersion is used to configure a preference in
+// which TPM to use, if multiple are available.
+type TPMVersion uint8
+
+// TPMInterface indicates how the client communicates
+// with the TPM.
+type TPMInterface uint8
+
+// Values of locality
+// Note: Localities are summable
+const (
+	LocZero Locality = 1 << iota
+	LocOne
+	LocTwo
+	LocThree
+	LocFour
+)
+
+// TPM versions
+const (
+	TPMVersionAgnostic TPMVersion = iota
+	TPMVersion12
+	TPMVersion20
+)
+
+// TPM interfaces
+const (
+	TPMInterfaceDirect TPMInterface = iota
+	TPMInterfaceKernelManaged
+	TPMInterfaceDaemonManaged
+)
