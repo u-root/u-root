4852434f10d565d5f52c6925ce0632525cbf55a3
Jens Drenhaus <jens.drenhaus@9elements.com>
2019-12-29 19:10:53 +0100

    fix return values
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- pkg/boot/stboot/bootball.go
+++ pkg/boot/stboot/bootball.go
@@ -99,7 +99,7 @@ func BootBallFromConfig(configFile string) (*BootBall, error) {
 	return ball, nil
 }
 
-func (ball *BootBall) init() (err error) {
+func (ball *BootBall) init() error {
 	cert, err := getRootCert(filepath.Join(ball.dir, signaturesDirName, rootCertName))
 	if err != nil {
 		return fmt.Errorf("BootBall: getting configuration faild: %v", err)
@@ -131,22 +131,22 @@ func (ball *BootBall) init() (err error) {
 		}
 	}
 	ball.NumSignatures = x
-	return
+	return nil
 }
 
 // Clean removes the underlaying temporary directory.
-func (ball *BootBall) Clean() (err error) {
-	err = os.RemoveAll(ball.dir)
+func (ball *BootBall) Clean() error {
+	err := os.RemoveAll(ball.dir)
 	if err != nil {
-		return
+		return err
 	}
 	ball.dir = ""
-	return
+	return nil
 }
 
 // Pack archives the all contents of the underlaying temporary
 // directory using zip.
-func (ball *BootBall) Pack() (err error) {
+func (ball *BootBall) Pack() error {
 	if ball.Archive == "" || ball.dir == "" {
 		return errors.New("BootBall.Pacstandak: booball.archive and bootball.dir must be set")
 	}
@@ -154,23 +154,23 @@ func (ball *BootBall) Pack() (err error) {
 }
 
 // Dir returns the temporary directory associated with BootBall.
-func (ball *BootBall) Dir() (dir string) {
+func (ball *BootBall) Dir() string {
 	return ball.dir
 }
 
 // GetBootConfigByIndex returns the Bootconfig at index from the BootBall's configs arrey.
-func (ball *BootBall) GetBootConfigByIndex(index int) (bc *bootconfig.BootConfig, err error) {
-	bc, err = ball.config.getBootConfig(index)
+func (ball *BootBall) GetBootConfigByIndex(index int) (*bootconfig.BootConfig, error) {
+	bc, err := ball.config.getBootConfig(index)
 	if err != nil {
-		return
+		return nil, err
 	}
 	bc.SetFilePathsPrefix(ball.dir)
-	return
+	return bc, nil
 }
 
 // Hash calculates hashes of all boot configurations in BootBall using the
 // BootBall.Signer's hash function
-func (ball *BootBall) Hash() (err error) {
+func (ball *BootBall) Hash() error {
 	ball.hashes = make(map[string][]byte)
 	for i, files := range ball.bootFiles {
 		hash, herr := ball.Signer.hash(files...)
@@ -179,27 +179,27 @@ func (ball *BootBall) Hash() (err error) {
 		}
 		ball.hashes[i] = hash
 	}
-	return
+	return nil
 }
 
 // Sign signes the hashes of all boot configurations in BootBall using the
 // BootBall.Signer's hash function with the provided privKeyFile. The signature
 // is stored along with the provided certFile inside the BootBall.
-func (ball *BootBall) Sign(privKeyFile, certFile string) (err error) {
-	err = validateFiles("", privKeyFile, certFile)
+func (ball *BootBall) Sign(privKeyFile, certFile string) error {
+	err := validateFiles("", privKeyFile, certFile)
 	if err != nil {
-		return
+		return err
 	}
 
 	buf, err := ioutil.ReadFile(certFile)
 	if err != nil {
-		return
+		return err
 	}
 
 	cert, err := parseCertificate(buf)
 	err = validateCertificate(cert, ball.rootCert)
 	if err != nil {
-		return
+		return err
 	}
 
 	log.Printf("Signing with: %s", privKeyFile)
@@ -207,7 +207,7 @@ func (ball *BootBall) Sign(privKeyFile, certFile string) (err error) {
 	if ball.hashes == nil {
 		err = ball.Hash()
 		if err != nil {
-			return
+			return err
 		}
 	}
 
@@ -223,33 +223,33 @@ func (ball *BootBall) Sign(privKeyFile, certFile string) (err error) {
 	}
 
 	if err = writeSignatures(sigs, certFile, ball.dir); err != nil {
-		return
+		return err
 	}
 
 	ball.NumSignatures++
-	return
+	return nil
 }
 
 // VerifyBootconfigs validates the certificates stored together with the
 // signatures of each boot configuration in BootBall and verifies the
 // signatures. A map is returned with the BootConfig's name as key and the
 // according number of valid signatures of this BootConfig.
-func (ball *BootBall) VerifyBootconfigs() (verified map[string]int, err error) {
-	verified = make(map[string]int)
+func (ball *BootBall) VerifyBootconfigs() (map[string]int, error) {
+	verified := make(map[string]int)
 	for i := 0; 1 < ball.NumSignatures; i++ {
 		n, err := ball.VerifyBootconfigByIndex(i)
 		if err != nil {
-			return verified, err
+			return nil, err
 		}
 		verified[ball.config.BootConfigs[i].Name] = n
 	}
-	return
+	return verified, nil
 }
 
 // VerifyBootconfigByIndex validates the certificates stored together with the
 // signatures of BootConfig index at BootBall.Config.BootConfigs[] and verifies
 // the signatures. The number of valid signatures is returned.
-func (ball *BootBall) VerifyBootconfigByIndex(index int) (verified int, err error) {
+func (ball *BootBall) VerifyBootconfigByIndex(index int) (int, error) {
 	bcName := ball.config.BootConfigs[index].Name
 	return ball.VerifyBootconfigByName(bcName)
 }
@@ -257,79 +257,79 @@ func (ball *BootBall) VerifyBootconfigByIndex(index int) (verified int, err erro
 // VerifyBootconfigByName validates the certificates stored together with the
 // signatures of BootConfig name and verifies the signatures. The number of
 // valid signatures is returned.
-func (ball *BootBall) VerifyBootconfigByName(name string) (verified int, err error) {
+func (ball *BootBall) VerifyBootconfigByName(name string) (int, error) {
 	if ball.hashes == nil {
-		err = ball.Hash()
+		err := ball.Hash()
 		if err != nil {
-			return
+			return 0, err
 		}
 	}
 
 	sigs := ball.signatures[name]
-	verified = 0
+	var verified int = 0
 	for _, sig := range sigs {
-		err = validateCertificate(sig.Cert, ball.rootCert)
+		err := validateCertificate(sig.Cert, ball.rootCert)
 		if err != nil {
-			return
+			return verified, err
 		}
 		err = ball.Signer.verify(sig, ball.hashes[name])
 		if err != nil {
-			return
+			return verified, err
 		}
 		verified++
 	}
-	return
+	return verified, nil
 }
 
-func getConfig(dest string) (cfg *Stconfig, err error) {
+func getConfig(dest string) (*Stconfig, error) {
 	cfgBytes, err := ioutil.ReadFile(dest)
 	if err != nil {
-		return
+		return nil, err
 	}
-	cfg, err = stconfigFromBytes(cfgBytes)
+	cfg, err := stconfigFromBytes(cfgBytes)
 	if err != nil {
-		return
+		return nil, err
 	}
 	if !(cfg.IsValid()) {
-		return cfg, errors.New("invalid configuration")
+		return nil, errors.New("invalid configuration")
 	}
-	return
+	return cfg, nil
 }
 
-func getRootCert(dest string) (cert *x509.CertPool, err error) {
+func getRootCert(dest string) (*x509.CertPool, error) {
 	certBytes, err := ioutil.ReadFile(dest)
 	if err != nil {
-		return
+		return nil, err
 	}
-	cert, err = certPool(certBytes)
+	cert, err := certPool(certBytes)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return cert, nil
 }
 
-func getBootFiles(cfg *Stconfig, prefix string) (bootFiles map[string][]string, err error) {
-	bootFiles = make(map[string][]string)
+func getBootFiles(cfg *Stconfig, prefix string) (map[string][]string, error) {
+	bootFiles := make(map[string][]string)
 	for _, bc := range cfg.BootConfigs {
 		files := make([]string, 0)
 		for _, file := range bc.FileNames() {
 			file = filepath.Join(prefix, file)
 			files = append(files, file)
-			if err = validateFiles("", files...); err != nil {
-				return
+			if err := validateFiles("", files...); err != nil {
+				return nil, err
 			}
 		}
 		bootFiles[bc.Name] = files
 	}
-	return
+	return bootFiles, nil
 }
 
-func (ball *BootBall) getSignatures() (err error) {
+func (ball *BootBall) getSignatures() error {
 	ball.signatures = make(map[string][]signature)
 	path := filepath.Join(ball.dir, signaturesDirName)
 
 	sigPool := make([]signature, 0)
-	err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		ext := filepath.Ext(info.Name())
 
 		if !info.IsDir() && (ext == ".signature") {
@@ -366,22 +366,22 @@ func (ball *BootBall) getSignatures() (err error) {
 		return nil
 	})
 	if err != nil {
-		return
+		return err
 	}
-	return
+	return nil
 }
 
-func validateFiles(prefix string, files ...string) (err error) {
+func validateFiles(prefix string, files ...string) error {
 	for _, file := range files {
-		_, err = os.Stat(filepath.Join(prefix, file))
+		_, err := os.Stat(filepath.Join(prefix, file))
 		if err != nil {
-			return
+			return err
 		}
 	}
-	return
+	return nil
 }
 
-func writeSignatures(sigs []signature, certFile, dir string) (err error) {
+func writeSignatures(sigs []signature, certFile, dir string) error {
 	for i, sig := range sigs {
 		d := fmt.Sprintf("%s%d", bootFilesDirName, i)
 		path := filepath.Join(dir, signaturesDirName, d)
@@ -402,23 +402,23 @@ func writeSignatures(sigs []signature, certFile, dir string) (err error) {
 			return cerr
 		}
 	}
-	return
+	return nil
 }
 
-func makeConfigDir(cfg *Stconfig, origDir string) (dir string, err error) {
-	if err = validateFiles(cfg.RootCertPath); err != nil {
-		return
+func makeConfigDir(cfg *Stconfig, origDir string) (string, error) {
+	if err := validateFiles(cfg.RootCertPath); err != nil {
+		return "", err
 	}
 
 	for _, bc := range cfg.BootConfigs {
-		if err = validateFiles(origDir, bc.FileNames()...); err != nil {
-			return
+		if err := validateFiles(origDir, bc.FileNames()...); err != nil {
+			return "", err
 		}
 	}
 
-	dir, err = ioutil.TempDir(os.TempDir(), "bootball")
+	dir, err := ioutil.TempDir(os.TempDir(), "bootball")
 	if err != nil {
-		return
+		return "", err
 	}
 
 	dstPath := filepath.Join(dir, signaturesDirName, rootCertName)
@@ -441,49 +441,49 @@ func makeConfigDir(cfg *Stconfig, origDir string) (dir string, err error) {
 	dstPath = filepath.Join(dir, ConfigName)
 	bytes, err := cfg.bytes()
 	if err != nil {
-		return
+		return "", nil
 	}
 	ioutil.WriteFile(dstPath, bytes, os.ModePerm)
 
-	return
+	return dir, nil
 }
 
-func copyFile(src, dst string) (err error) {
+func copyFile(src, dst string) error {
 	in, err := os.Open(src)
 	if err != nil {
-		return
+		return err
 	}
 	defer in.Close()
 
 	if err = os.MkdirAll(filepath.Dir(dst), os.ModePerm); err != nil {
-		return
+		return err
 	}
 
 	out, err := os.Create(dst)
 	if err != nil {
-		return
+		return err
 	}
 	defer out.Close()
 
 	if _, err = io.Copy(out, in); err != nil {
-		return
+		return err
 	}
 	err = out.Sync()
 
-	return
+	return nil
 }
 
-func toZip(srcDir, dest string) (err error) {
+func toZip(srcDir, dest string) error {
 	info, err := os.Stat(srcDir)
 	if err != nil {
-		return
+		return err
 	}
 	if !(info.IsDir()) {
 		return fmt.Errorf("%s is not a directory", srcDir)
 	}
 	archive, err := os.Create(dest)
 	if err != nil {
-		return
+		return err
 	}
 	defer archive.Close()
 
@@ -532,17 +532,17 @@ func toZip(srcDir, dest string) (err error) {
 		return err
 	})
 
-	return
+	return nil
 }
 
-func fromZip(src, destDir string) (err error) {
+func fromZip(src, destDir string) error {
 	z, err := zip.OpenReader(src)
 	if err != nil {
-		return
+		return err
 	}
 
 	if err = os.MkdirAll(destDir, 0755); err != nil {
-		return
+		return err
 	}
 
 	for _, file := range z.File {
@@ -570,5 +570,5 @@ func fromZip(src, destDir string) (err error) {
 		targetFile.Close()
 	}
 
-	return
+	return nil
 }
--- pkg/boot/stboot/signature.go
+++ pkg/boot/stboot/signature.go
@@ -20,27 +20,33 @@ type signature struct {
 // Signer is used by BootBall to hash, sign and varify the BootConfigs
 // with appropriate algorithms
 type Signer interface {
-	hash(files ...string) (hash []byte, err error)
-	sign(privKey string, data []byte) (sig []byte, err error)
-	verify(sig signature, hash []byte) (err error)
+	hash(files ...string) ([]byte, error)
+	sign(privKey string, data []byte) ([]byte, error)
+	verify(sig signature, hash []byte) error
 }
 
 // DummySigner creates signatures that are always valid.
 type DummySigner struct{}
 
-func (DummySigner) hash(files ...string) (hash []byte, err error) {
-	hash = make([]byte, 8)
-	rand.Read(hash)
-	return
+func (DummySigner) hash(files ...string) ([]byte, error) {
+	hash := make([]byte, 8)
+	_, err := rand.Read(hash)
+	if err != nil {
+		return nil, err
+	}
+	return hash, nil
 }
 
-func (DummySigner) sign(privKey string, data []byte) (sig []byte, err error) {
-	sig = make([]byte, 8)
-	rand.Read(sig)
-	return
+func (DummySigner) sign(privKey string, data []byte) ([]byte, error) {
+	sig := make([]byte, 8)
+	_, err := rand.Read(sig)
+	if err != nil {
+		return nil, err
+	}
+	return sig, nil
 }
 
-func (DummySigner) verify(sig signature, hash []byte) (err error) {
+func (DummySigner) verify(sig signature, hash []byte) error {
 	return nil
 }
 
@@ -48,7 +54,7 @@ func (DummySigner) verify(sig signature, hash []byte) (err error) {
 // x509 certificates.
 type Sha512PssSigner struct{}
 
-func (Sha512PssSigner) hash(files ...string) (hash []byte, err error) {
+func (Sha512PssSigner) hash(files ...string) ([]byte, error) {
 	h := sha512.New()
 	h.Reset()
 
@@ -62,72 +68,70 @@ func (Sha512PssSigner) hash(files ...string) (hash []byte, err error) {
 	return h.Sum(nil), nil
 }
 
-func (Sha512PssSigner) sign(privKey string, data []byte) (sig []byte, err error) {
+func (Sha512PssSigner) sign(privKey string, data []byte) ([]byte, error) {
 	buf, err := ioutil.ReadFile(privKey)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	privPem, _ := pem.Decode(buf)
 	key, err := x509.ParsePKCS1PrivateKey(privPem.Bytes)
 	if err != nil {
-		return
+		return nil, err
 	}
 	if key == nil {
 		err = fmt.Errorf("key is empty")
-		return
+		return nil, err
 	}
 
 	opts := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash}
 
-	sig, err = rsa.SignPSS(rand.Reader, key, crypto.SHA512, data, opts)
+	sig, err := rsa.SignPSS(rand.Reader, key, crypto.SHA512, data, opts)
 	if err != nil {
-		return
+		return nil, err
 	}
 	if sig == nil {
-		err = fmt.Errorf("signature is nil")
-		return
+		return nil, fmt.Errorf("signature is nil")
 	}
-	return
+	return sig, nil
 }
 
-func (Sha512PssSigner) verify(sig signature, hash []byte) (err error) {
+func (Sha512PssSigner) verify(sig signature, hash []byte) error {
 	opts := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash}
-	err = rsa.VerifyPSS(sig.Cert.PublicKey.(*rsa.PublicKey), crypto.SHA512, hash, sig.Bytes, opts)
+	err := rsa.VerifyPSS(sig.Cert.PublicKey.(*rsa.PublicKey), crypto.SHA512, hash, sig.Bytes, opts)
 	if err != nil {
 		return fmt.Errorf("signature verification failed: %v", err)
 	}
-	return
+	return nil
 }
 
 // parseCertificate parses certificate from raw certificate
-func parseCertificate(raw []byte) (cert *x509.Certificate, err error) {
+func parseCertificate(raw []byte) (*x509.Certificate, error) {
 	block, _ := pem.Decode(raw)
 
-	cert, err = x509.ParseCertificate(block.Bytes)
+	cert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return
+	return cert, nil
 }
 
-func certPool(pem []byte) (certPool *x509.CertPool, err error) {
-	certPool = x509.NewCertPool()
+func certPool(pem []byte) (*x509.CertPool, error) {
+	certPool := x509.NewCertPool()
 	ok := certPool.AppendCertsFromPEM(pem)
 	if !ok {
-		err = errors.New("Failed to parse root certificate")
-		return
+		return nil, errors.New("Failed to parse root certificate")
 	}
-	return
+	return certPool, nil
 }
 
-func validateCertificate(cert *x509.Certificate, cerPool *x509.CertPool) (err error) {
+func validateCertificate(cert *x509.Certificate, cerPool *x509.CertPool) error {
 	opts := x509.VerifyOptions{
 		Roots: cerPool,
 	}
-	_, err = cert.Verify(opts)
+	_, err := cert.Verify(opts)
 	if err != nil {
-		return
+		return err
 	}
-	return
+	return nil
 }
