f567da02ca4f9a7717a91fa6f167cba67fc072e5
Jens Drenhaus <jens.drenhaus@9elements.com>
2019-12-29 19:10:53 +0100

    fix return values
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- pkg/boot/stboot/signature_test.go
+++ pkg/boot/stboot/signature_test.go
@@ -10,7 +10,7 @@ var testHash = []byte{184, 125, 17, 19, 148, 144, 119, 239, 215, 18, 102, 79, 23
 
 // not ready yet
 func TestHash(t *testing.T) {
-	signer := sha512PssSigner{}
+	signer := Sha512PssSigner{}
 	hash, err := signer.hash("testdata/testConfigDir/kernels/kernel1.vmlinuz")
 	lenght := len(hash)
 	t.Logf("hash: %v", hash)
@@ -20,7 +20,7 @@ func TestHash(t *testing.T) {
 
 //not ready yet
 func TestSign(t *testing.T) {
-	signer := sha512PssSigner{}
+	signer := Sha512PssSigner{}
 	sig, err := signer.sign("testdata/keys/signing-key-1.key", testHash)
 	t.Logf("signature: %v", sig)
 	require.NoError(t, err)
