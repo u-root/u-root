917ac00435a5d67e5fdb6605d436d50078fd2010
Clément Chigot <clement.chigot@atos.net>
2020-01-09 10:19:15 +0100

    pkg/rand: add AIX support
    This package is sometimes used by other projects, at least
    "github.com/insomniacslk/dhcp/dhcpv4".
    Therefore, it must be ported even if the whole u-root project isn't
    available on AIX.
    
    Signed-off-by: Clément Chigot <clement.chigot@atos.net>

--- pkg/rand/random_urandom.go
+++ pkg/rand/random_urandom.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd nacl netbsd openbsd plan9 solaris linux
+// +build aix darwin dragonfly freebsd nacl netbsd openbsd plan9 solaris linux
 
 // Package rand implements cancelable reads from a cryptographically safe
 // random number source.
