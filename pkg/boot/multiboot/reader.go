9c59c9eb455b2fc260ab4d85c15b3b8b99f34ebe
Chris Koch <chrisko@google.com>
2020-01-28 15:49:50 -0800

    Revert "pkg/boot/multiboot: drop kernelReader"
    This reverts commit eab4ccb61a7c09f46c9630d6c671832c6cdedb25.
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/boot/multiboot/reader.go
+++ pkg/boot/multiboot/reader.go
@@ -5,13 +5,42 @@
 package multiboot
 
 import (
-	"bytes"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"os"
 
 	"github.com/u-root/u-root/pkg/uio"
 )
+
+type kernelReader struct {
+	buf []byte
+	off int
+}
+
+func (kr kernelReader) ReadAt(p []byte, off int64) (n int, err error) {
+	if off < 0 || off > int64(len(kr.buf)) {
+		return 0, fmt.Errorf("bad offset %v", off)
+	}
+	if n = copy(p, kr.buf[off:]); n < len(p) {
+		err = io.EOF
+	}
+	return n, err
+}
+
+func (kr *kernelReader) Read(p []byte) (n int, err error) {
+	if n = copy(p, kr.buf[kr.off:]); n < len(p) {
+		err = io.EOF
+	}
+	kr.off += n
+	return n, err
+}
+
+// implement inMemReaderAt interface
+func (kr kernelReader) Bytes() []byte {
+	return kr.buf
+}
 
 func readGzip(r io.Reader) ([]byte, error) {
 	z, err := gzip.NewReader(r)
@@ -22,12 +51,30 @@ func readGzip(r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(z)
 }
 
+func readFile(name string) ([]byte, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	b, err := readGzip(f)
+	if err == nil {
+		return b, err
+	}
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		return nil, fmt.Errorf("cannot rewind file: %v", err)
+	}
+
+	return ioutil.ReadAll(f)
+}
+
 // TODO: could be tryCompressionFilters, grub support gz,xz and lzop
 // TODO: do we want to keep the filter inside multiboot? This could be the responsibility of the caller...
 func tryGzipFilter(r io.ReaderAt) io.ReaderAt {
 	b, err := readGzip(uio.Reader(r))
 	if err == nil {
-		return bytes.NewReader(b)
+		kernel := kernelReader{buf: b}
+		return kernel
 	}
 	return r
 }
