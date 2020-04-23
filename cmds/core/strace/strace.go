35036e066d9961e7701291e5311e4f7b32754fed
Daniel Maslowski <info@orangecms.org>
2020-04-23 23:28:12 +0200

    cmds/core/strace: disable strace for all four MIPS variants
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- cmds/core/strace/strace.go
+++ cmds/core/strace/strace.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !386,!mipsle
+// +build !386,!mips,!mipsle,!mips64,!mips64le
 
 // strace is a simple multi-process tracer.
 // It starts the comand and lets the strace.Run() do all the work.
