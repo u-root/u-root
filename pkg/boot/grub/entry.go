8b745630123034e332a89b3c75deb1cd05846fcf
Chris Koch <chrisko@google.com>
2020-05-28 19:55:33 -0700

    grub: move gboot/entry into GRUB
    env files are a general grub concept.
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/boot/grub/entry.go
+++ pkg/boot/grub/entry.go
@@ -2,10 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package entry implements config file parsing for GRUB entry files. An entry
-// file is a GRUB environment file consisting of key-value pairs akin to the
-// GRUB commands load_env and save_env.
-package entry
+package grub
 
 import (
 	"bufio"
