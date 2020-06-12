6d4657b98695c8c639b5c89637ddd9353e331c49
Chris Koch <chrisko@google.com>
2020-06-11 18:17:05 -0700

    bare minimum: remove blacklist from the terms we use
    Signed-off-by: Chris Koch <chrisko@google.com>

--- cmds/core/elvish/store/storedefs/storedefs.go
+++ cmds/core/elvish/store/storedefs/storedefs.go
@@ -3,9 +3,6 @@ package storedefs
 
 import "errors"
 
-// NoBlacklist is an empty blacklist, to be used in GetDirs.
-var NoBlacklist = map[string]struct{}{}
-
 // ErrNoMatchingCmd is the error returned when a LastCmd or FirstCmd query
 // completes with no result.
 var ErrNoMatchingCmd = errors.New("no matching command line")
