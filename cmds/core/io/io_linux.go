273317e0dc63fc0e2adb7f16565ea8a83c217325
Ronald G Minnich <rminnich@gmail.com>
2020-08-17 20:14:08 -0700

    Port the io command to plan9
    We get both io and cmos querying commands.
    
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

---
+++ cmds/core/io/io_linux.go
@@ -0,0 +1,25 @@
+// Copyright 2010-2020 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/u-root/u-root/pkg/memio"
+)
+
+func init() {
+	addCmd(readCmds, "rb", &cmd{memio.Read, 64, 8})
+	addCmd(readCmds, "rw", &cmd{memio.Read, 64, 16})
+	addCmd(readCmds, "rl", &cmd{memio.Read, 64, 32})
+	addCmd(readCmds, "rq", &cmd{memio.Read, 64, 64})
+
+	addCmd(writeCmds, "wb", &cmd{memio.Write, 64, 8})
+	addCmd(writeCmds, "ww", &cmd{memio.Write, 64, 16})
+	addCmd(writeCmds, "wl", &cmd{memio.Write, 64, 32})
+	addCmd(writeCmds, "wq", &cmd{memio.Write, 64, 64})
+
+	usageMsg += `io (r{b,w,l,q} address)...
+io (w{b,w,l,q} address value)...
+`
+}
