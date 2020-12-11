dc5c3be2caec7a1ff40e1cc490ce29087b4dbf94
Ryan O'Leary <ryanoleary@google.com>
2020-12-11 11:02:44 -0800

    kexec: Decompress gzip compressed kernels
    For arm64, make simply builds a Image and Image.gz. Not self
    decompression. The bootloader (like u-boot and others) are expected to
    decompress.
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/boot/kexec/kexec_linux_implemented.go
+++ pkg/boot/kexec/kexec_linux_implemented.go
@@ -1,4 +1,4 @@
-// Copyright 2015-2017 the u-root Authors. All rights reserved
+// Copyright 2015-2020 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -7,7 +7,11 @@
 package kexec
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"golang.org/x/sys/unix"
@@ -15,6 +19,10 @@ import (
 
 // FileLoad loads the given kernel as the new kernel with the given ramfs and
 // cmdline.
+//
+// For some architectures (such as arm64), the vmlinuz kernel is not
+// self-decompressing and the expectation is for the bootloader to decompress
+// it. This automatically detects a gzip image and decompresses it.
 //
 // The kexec_file_load(2) syscall is x86-64 and arm64 only.
 func FileLoad(kernel, ramfs *os.File, cmdline string) error {
@@ -26,8 +34,38 @@ func FileLoad(kernel, ramfs *os.File, cmdline string) error {
 		flags |= unix.KEXEC_FILE_NO_INITRAMFS
 	}
 
+	if isGzip(kernel) {
+		g, err := gzip.NewReader(kernel)
+		if err != nil {
+			return fmt.Errorf("could read kernel gzip header: %v", err)
+		}
+		decompressed, err := ioutil.TempFile("", "kernel")
+		if err != nil {
+			return fmt.Errorf("could not create temp file: %v", err)
+		}
+		defer os.Remove(decompressed.Name())
+		if _, err := io.Copy(decompressed, g); err != nil {
+			return fmt.Errorf("could not decompress kernel: %v", err)
+		}
+		if err := g.Close(); err != nil {
+			return fmt.Errorf("could not close gunzip (bad checksum?): %v", err)
+		}
+		if err := decompressed.Sync(); err != nil {
+			return fmt.Errorf("could not sync decompressed kernel: %v", err)
+		}
+		kernel = decompressed
+	}
+
 	if err := unix.KexecFileLoad(int(kernel.Fd()), ramfsfd, cmdline, flags); err != nil {
 		return fmt.Errorf("sys_kexec(%d, %d, %s, %x) = %v", kernel.Fd(), ramfsfd, cmdline, flags, err)
 	}
 	return nil
+}
+
+// isGzip returns true if the file is gzip file format.
+func isGzip(kernel *os.File) bool {
+	gzipMagic := []byte{0x1F, 0x8B}
+	magic := make([]byte, len(gzipMagic))
+	n, err := kernel.ReadAt(magic, 0)
+	return err == nil && n == len(magic) && bytes.Compare(gzipMagic, magic) == 0
 }
