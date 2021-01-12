6cd89619c1804e755265a0ad161ec1079a02ae34
Cheng-Chieh Huang <chengchieh@google.com>
2021-01-12 14:01:30 +0000

    uefiboot: add SerialPortConfig to the payloadConfig
    Signed-off-by: Cheng-Chieh Huang <chengchieh@google.com>

--- cmds/exp/uefiboot/main.go
+++ cmds/exp/uefiboot/main.go
@@ -7,6 +7,7 @@ package main
 import (
 	"flag"
 	"log"
+	"math"
 
 	"github.com/u-root/u-root/pkg/boot"
 	"github.com/u-root/u-root/pkg/boot/uefi"
@@ -15,7 +16,7 @@ import (
 var (
 	debug       = flag.Bool("d", false, "Print debug output")
 	imageBase   = flag.Uint64("i", 0x800000, "Where to load payload image")
-	serialAddr  = flag.Uint("serial_addr", 0x3f8, "Serial port address")
+	serialAddr  = flag.Uint("serial_addr", 0x3f8, "Serial IO port address")
 	serialWidth = flag.Uint("serial_width", 1, "Serial port reg width")
 	serialHertz = flag.Uint("serial_hertz", 1843200, "Serial port input hertz")
 	serialBaud  = flag.Uint("serial_baud", 115200, "Serial port baud rate")
@@ -25,13 +26,15 @@ var v = func(string, ...interface{}) {}
 
 func main() {
 	flag.Parse()
-	v = log.Printf
 	if len(flag.Args()) != 1 {
 		log.Fatal("Usage: uefiboot <payload>")
 	}
 	fv, err := uefi.New(flag.Args()[0])
 	if err != nil {
 		log.Fatal(err)
+	}
+	if *serialAddr > math.MaxUint32 {
+		log.Fatal("Unsupported: serial_addr is greater than uint32.")
 	}
 	fv.ImageBase, fv.SerialConfig = uintptr(*imageBase), uefi.SerialPortConfig{
 		Type:       uefi.SerialPortTypeIO,
--- pkg/boot/uefi/uefi.go
+++ pkg/boot/uefi/uefi.go
@@ -19,11 +19,75 @@ import (
 	"github.com/u-root/u-root/pkg/boot/kexec"
 )
 
+// TODO(chengchieh): Add integration test for uefi package.
+
 const fvEntryImageOffset int64 = 0xA0
 
 var kexecLoad = kexec.Load
 var kexecParseMemoryMap = kexec.ParseMemoryMap
 var getRSDP = acpi.GetRSDP
+
+// Memory type in Linuxboot.h
+// TODO(chengchieh): Merge them with pkg/boot/kexec/memory_linux.go
+const (
+	MemTypeRAM      = 1
+	MemTypeDefault  = 2
+	MemTypeAcpi     = 3
+	MemTypeNVS      = 4
+	MemTypeReserved = 5
+)
+
+type memoryMapEntry struct {
+	Start   uint64
+	End     uint64
+	MemType uint32
+}
+
+var rangeTypeToPayloadMemType = map[kexec.RangeType]uint32{
+	kexec.RangeRAM:      MemTypeRAM,
+	kexec.RangeDefault:  MemTypeDefault,
+	kexec.RangeACPI:     MemTypeAcpi,
+	kexec.RangeNVS:      MemTypeNVS,
+	kexec.RangeReserved: MemTypeReserved,
+}
+
+
+// SerialPortConfig defines debug port configuration
+// This struct will be used to initialize SERIAL_PORT_INFO
+// in payload (UefiPayloadPkg/Include/Guid/SerialPortInfoGuid.h)
+type SerialPortConfig struct {
+	Type        uint32
+	BaseAddr    uint32
+	Baud        uint32
+	RegWidth    uint32
+	InputHertz  uint32
+	UartPciAddr uint32
+}
+
+// Serial port type for Type in UefiPayloadPkg's SERIAL_PORT_INFO
+const (
+	SerialPortTypeIO   = 1
+	SerialPortTypeMMIO = 2
+)
+
+// Current Config Version: 1
+const PayloadConfigVersion = 1
+
+type payloadConfig struct {
+	AcpiBase            uint64
+	AcpiSize            uint64
+	SerialConfig        SerialPortConfig
+	NumMemoryMapEntries uint32
+}
+
+func convertToPayloadMemType(rt kexec.RangeType) uint32 {
+	mt, ok := rangeTypeToPayloadMemType[rt]
+	if !ok {
+		// return reserved if range type is not recognized
+		return MemTypeReserved
+	}
+	return mt
+}
 
 // FvImage is a structure for loading a firmware volume
 type FvImage struct {
@@ -31,7 +95,7 @@ type FvImage struct {
 	mem          kexec.Memory
 	entryAddress uintptr
 	ImageBase    uintptr
-	PciExBase    uint64
+	SerialConfig SerialPortConfig
 }
 
 func checkFvAndGetEntryPoint(name string) (uintptr, error) {
@@ -62,46 +126,8 @@ func New(n string) (*FvImage, error) {
 	return &FvImage{name: n, mem: kexec.Memory{}, entryAddress: entry}, nil
 }
 
-// Memory type in Linuxboot.h
-const (
-	MemTypeRAM      = 1
-	MemTypeDefault  = 2
-	MemTypeAcpi     = 3
-	MemTypeNVS      = 4
-	MemTypeReserved = 5
-)
-
-type payloadConfig struct {
-	AcpiBase            uint64
-	AcpiSize            uint64
-	NumMemoryMapEntries uint32
-}
-
-type memoryMapEntry struct {
-	Start   uint64
-	End     uint64
-	MemType uint32
-}
-
-var rangeTypeToPayloadMemType = map[kexec.RangeType]uint32{
-	kexec.RangeRAM:      MemTypeRAM,
-	kexec.RangeDefault:  MemTypeDefault,
-	kexec.RangeACPI:     MemTypeAcpi,
-	kexec.RangeNVS:      MemTypeNVS,
-	kexec.RangeReserved: MemTypeReserved,
-}
-
-func convertToPayloadMemType(rt kexec.RangeType) uint32 {
-	mt, ok := rangeTypeToPayloadMemType[rt]
-	if !ok {
-		// return reserved if range type is not recognized
-		return MemTypeReserved
-	}
-	return mt
-}
-
 // Reserved 64kb for passing params
-var uefiPayloadConfigSize int = 0x10000
+const uefiPayloadConfigSize = 0x10000
 
 // Load loads fimware volume payload and boot the the payload
 func (fv *FvImage) Load(verbose bool) error {
@@ -131,6 +157,7 @@ func (fv *FvImage) Load(verbose bool) error {
 	pc := payloadConfig{
 		AcpiBase:            uint64(rsdp.RSDPAddr()),
 		AcpiSize:            uint64(rsdp.Len()),
+		SerialConfig:        fv.SerialConfig,
 		NumMemoryMapEntries: uint32(len(mm)),
 	}
 
