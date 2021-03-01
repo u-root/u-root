fef7779fe495f3ba3152dc6c1f95ae17e9e06bec
Cheng-Chieh Huang <chengchieh@google.com>
2021-03-01 07:17:56 +0000

    uefiboot: Add SMBIOS legacy test in QEMU
    Signed-off-by: Cheng-Chieh Huang <chengchieh@google.com>

--- pkg/smbios/smbios_legacy_test.go
+++ pkg/smbios/smbios_legacy_test.go
@@ -6,9 +6,11 @@ package smbios
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/u-root/u-root/pkg/memio"
+	"github.com/u-root/u-root/pkg/testutil"
 )
 
 var tmpBuf = []byte{0, 0, 0, 0, 0, 0}
@@ -26,6 +28,7 @@ func mockMemioRead(base int64, uintn memio.UintN) error {
 }
 
 func TestGetSMBIOSLegacyNotFound(t *testing.T) {
+	defer func(old func(base int64, uintn memio.UintN) error) { memioRead = old }(memioRead)
 	memioRead = mockMemioRead
 
 	_, _, err := GetSMBIOSBaseLegacy()
@@ -52,6 +55,7 @@ func TestGetSMBIOSLegacyMemIoReadError(t *testing.T) {
 
 func TestGetSMBIOSLegacySMBIOS(t *testing.T) {
 	tmpBuf = []byte{0, '_', 'M', 'S', '_', 0, 0, '_', 'S', 'M', '_', 0, 0, 0, 0, 0}
+	defer func(old func(base int64, uintn memio.UintN) error) { memioRead = old }(memioRead)
 	memioRead = mockMemioRead
 	base, size, err := GetSMBIOSBaseLegacy()
 	if err != nil {
@@ -73,6 +77,7 @@ func TestGetSMBIOSLegacySMBIOS(t *testing.T) {
 
 func TestGetSMBIOSLegacySMBIOS3(t *testing.T) {
 	tmpBuf = []byte{0, '_', 'M', 'S', '_', 0, 0, '_', 'S', 'M', '3', '_', 0, 0, 0, 0, 0}
+	defer func(old func(base int64, uintn memio.UintN) error) { memioRead = old }(memioRead)
 	memioRead = mockMemioRead
 	base, size, err := GetSMBIOSBaseLegacy()
 	if err != nil {
@@ -89,5 +94,24 @@ func TestGetSMBIOSLegacySMBIOS3(t *testing.T) {
 
 	if size != wantSize {
 		t.Errorf("GetSMBIOSLegacy() get size 0x%x, want 0x%x", size, wantSize)
+	}
+}
+
+func TestGetSMBIOSLegacyQEMU(t *testing.T) {
+	if runtime.GOARCH != "amd64" {
+		t.Skipf("test not supported on %s", runtime.GOARCH)
+	}
+	testutil.SkipIfNotRoot(t)
+	base, size, err := GetSMBIOSBaseLegacy()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if base == 0 {
+		t.Errorf("GetSMBIOSLegacy() does not get SMBIOS base")
+	}
+
+	if size != smbios2HeaderSize && size != smbios3HeaderSize {
+		t.Errorf("GetSMBIOSLegacy() get size 0x%x, want 0x%x or 0x%x ", size, smbios2HeaderSize, smbios3HeaderSize)
 	}
 }
