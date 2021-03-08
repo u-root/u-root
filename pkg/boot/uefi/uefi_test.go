7babdbe03b9c6236f4e8d23af9053232ab4f3035
Cheng-Chieh Huang <chengchieh@google.com>
2021-03-08 11:07:25 +0000

    uefiboot: add test cases for handling failure load
    Signed-off-by: Cheng-Chieh Huang <chengchieh@google.com>

--- pkg/boot/uefi/uefi_test.go
+++ pkg/boot/uefi/uefi_test.go
@@ -31,39 +31,159 @@ func mockKexecLoad(entry uintptr, segments kexec.Segments, flags uint64) error {
 	return nil
 }
 
+func mockKexecParseMemoryMap() (kexec.MemoryMap, error) {
+	return kexec.MemoryMap{}, nil
+}
+
+func mockGetRSDP() (*acpi.RSDP, error) {
+	return &acpi.RSDP{}, nil
+}
+
+func mockGetSMBIOSBase() (int64, int64, error) {
+	return 100, 200, nil
+}
+
 func TestLoadFvImage(t *testing.T) {
 	fv, err := New("testdata/uefi.fd")
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	defer func(old func() (kexec.MemoryMap, error)) { kexecParseMemoryMap = old }(kexecParseMemoryMap)
+	kexecParseMemoryMap = mockKexecParseMemoryMap
+
 	defer func(old func() (*acpi.RSDP, error)) { getRSDP = old }(getRSDP)
-	getRSDP = func() (*acpi.RSDP, error) {
-		t.Log("mock acpi.GetRSDP()")
-		return &acpi.RSDP{}, nil
-	}
+	getRSDP = mockGetRSDP
+
 	defer func(old func() (int64, int64, error)) { getSMBIOSBase = old }(getSMBIOSBase)
-	getSMBIOSBase = func() (int64, int64, error) {
-		t.Log("mock getSMBIOSBase()")
-		return 100, 200, nil
-	}
+	getSMBIOSBase = mockGetSMBIOSBase
+
 	defer func(old kexecLoadFunc) { kexecLoad = old }(kexecLoad)
 	kexecLoad = mockKexecLoad
-
-	defer func(old func() (kexec.MemoryMap, error)) { kexecParseMemoryMap = old }(kexecParseMemoryMap)
-	kexecParseMemoryMap = func() (kexec.MemoryMap, error) {
-		t.Log("mock kexec.ParseMemMap()")
-		return kexec.MemoryMap{}, nil
-	}
 
 	if err = fv.Load(true); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestLoadFvImageNotFound(t *testing.T) {
+func TestNewNotFound(t *testing.T) {
 	_, err := New("testdata/uefi_NOT_FOUND.fd")
-	if err == nil {
-		t.Fatal("Should not found the payload.")
+	want := "open testdata/uefi_NOT_FOUND.fd: no such file or directory"
+	if err.Error() != want {
+		t.Fatalf("Should be '%s', but get '%v'", want, err)
+	}
+}
+
+func TestNewInvalidPayload(t *testing.T) {
+	_, err := New("testdata/invalid_uefi.fd")
+	want := "Unrecognised COFF file header machine value of 0x76c0."
+	if err.Error() != want {
+		t.Fatalf("Should be '%s', but get '%v'", want, err)
+	}
+}
+
+func TestLoadFvImageNotFound(t *testing.T) {
+	fv := &FVImage{name: "NOT_FOUND"}
+	err := fv.Load(true)
+
+	want := "open NOT_FOUND: no such file or directory"
+
+	if err.Error() != want {
+		t.Fatalf("Should be '%s', but get '%v'", want, err)
+	}
+}
+
+func TestLoadFvImageFailAtParseMemoryMap(t *testing.T) {
+	fv, err := New("testdata/uefi.fd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(old func() (kexec.MemoryMap, error)) { kexecParseMemoryMap = old }(kexecParseMemoryMap)
+	kexecParseMemoryMap = func() (kexec.MemoryMap, error) {
+		return nil, fmt.Errorf("PARSE_MEMORY_MAP_FAILED")
+	}
+
+	err = fv.Load(true)
+
+	want := "PARSE_MEMORY_MAP_FAILED"
+	if err.Error() != want {
+		t.Fatalf("want '%s', get '%v'", want, err)
+	}
+}
+
+func TestLoadFvImageFailAtGetRSDP(t *testing.T) {
+	fv, err := New("testdata/uefi.fd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(old func() (kexec.MemoryMap, error)) { kexecParseMemoryMap = old }(kexecParseMemoryMap)
+	kexecParseMemoryMap = mockKexecParseMemoryMap
+
+	defer func(old func() (*acpi.RSDP, error)) { getRSDP = old }(getRSDP)
+	getRSDP = func() (*acpi.RSDP, error) {
+		return nil, fmt.Errorf("RSDP_NOT_FOUND")
+	}
+
+	err = fv.Load(true)
+
+	want := "RSDP_NOT_FOUND"
+	if err.Error() != want {
+		t.Fatalf("want '%s', get '%v'", want, err)
+	}
+}
+
+func TestLoadFvImageFailAtGetSMBIOS(t *testing.T) {
+	fv, err := New("testdata/uefi.fd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(old func() (kexec.MemoryMap, error)) { kexecParseMemoryMap = old }(kexecParseMemoryMap)
+	kexecParseMemoryMap = mockKexecParseMemoryMap
+
+	defer func(old func() (*acpi.RSDP, error)) { getRSDP = old }(getRSDP)
+	getRSDP = mockGetRSDP
+
+	defer func(old func() (int64, int64, error)) { getSMBIOSBase = old }(getSMBIOSBase)
+	getSMBIOSBase = func() (int64, int64, error) {
+		t.Log("mock getSMBIOSBase()")
+		return 100, 200, fmt.Errorf("SMBIOS_NOT_FOUND")
+	}
+
+	err = fv.Load(true)
+
+	want := "SMBIOS_NOT_FOUND"
+	if err.Error() != want {
+		t.Fatalf("want '%s', get '%v'", want, err)
+	}
+}
+
+func TestLoadFvImageFailAtKexec(t *testing.T) {
+	fv, err := New("testdata/uefi.fd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(old func() (kexec.MemoryMap, error)) { kexecParseMemoryMap = old }(kexecParseMemoryMap)
+	kexecParseMemoryMap = mockKexecParseMemoryMap
+
+	defer func(old func() (*acpi.RSDP, error)) { getRSDP = old }(getRSDP)
+	getRSDP = mockGetRSDP
+
+	defer func(old func() (int64, int64, error)) { getSMBIOSBase = old }(getSMBIOSBase)
+	getSMBIOSBase = mockGetSMBIOSBase
+
+	defer func(old kexecLoadFunc) { kexecLoad = old }(kexecLoad)
+	kexecLoad = func(entry uintptr, segments kexec.Segments, flags uint64) error {
+		return fmt.Errorf("KEXEC_FAILED")
+	}
+
+	err = fv.Load(true)
+
+	want := "kexec.Load() error: KEXEC_FAILED"
+	if err.Error() != want {
+		t.Fatalf("want '%s', get '%v'", want, err)
 	}
 }
