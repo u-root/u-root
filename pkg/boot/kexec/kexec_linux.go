88123d187e31585437d5d19a805db884634b2d8b
Ryan O'Leary <ryanoleary@google.com>
2021-03-11 15:38:00 -0800

    Add daemon for initializing and petting watchdog
    This uses Linux's /dev/watchdog interface.
    
    You can start and interact with the daemon through the watchdogd
    command. Use the watchdog command to interact directly with the device
    driver.
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/boot/kexec/kexec_linux.go
+++ pkg/boot/kexec/kexec_linux.go
@@ -6,11 +6,25 @@ package kexec
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"syscall"
+
+	"github.com/u-root/u-root/pkg/watchdogd"
 )
 
 // Reboot executes a kernel previously loaded with FileInit.
 func Reboot() error {
+	// Optionally disarm the watchdog.
+	if os.Getenv("UROOT_KEXEC_DISARM_WATCHDOG") == "1" {
+		d, err := watchdogd.Find()
+		if err != nil {
+			log.Printf("Error finding watchdog daemon: %v", err)
+		} else if err := d.Disarm(); err != nil {
+			log.Printf("Error disarming watchdog: %v", err)
+		}
+	}
+
 	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_KEXEC); err != nil {
 		return fmt.Errorf("sys_reboot(..., kexec) = %v", err)
 	}
