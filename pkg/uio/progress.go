fd1b0b282b9d9d64781f1b872132c6cb9db2a7cd
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-03-16 16:29:16 +0100

    uio: remove ProgressReader
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>
    Signed-off-by: Balachandra Kumar CS <balchandracs010@gmail.com>

--- pkg/uio/progress.go
+++ pkg/uio/progress.go
@@ -9,33 +9,6 @@ import (
 	"strings"
 )
 
-// ProgressReader implements io.Reader and prints Symbol to W after every
-// Interval bytes passes through R.
-type ProgressReader struct {
-	R io.Reader
-
-	Symbol   string
-	Interval int
-	W        io.Writer
-
-	counter int
-	written bool
-}
-
-// Read implements io.Reader for ProgressReader.
-func (r *ProgressReader) Read(p []byte) (n int, err error) {
-	defer func() {
-		numSymbols := (r.counter%r.Interval + n) / r.Interval
-		r.W.Write([]byte(strings.Repeat(r.Symbol, numSymbols)))
-		r.counter += n
-		r.written = (r.written || numSymbols > 0)
-		if err == io.EOF && r.written {
-			r.W.Write([]byte("\n"))
-		}
-	}()
-	return r.R.Read(p)
-}
-
 // ProgressReadCloser implements io.ReadCloser and prints Symbol to W after every
 // Interval bytes passes through RC.
 type ProgressReadCloser struct {
@@ -45,20 +18,22 @@ type ProgressReadCloser struct {
 	Interval int
 	W        io.Writer
 
-	pr *ProgressReader
+	counter int
+	written bool
 }
 
-// Read implements io.Reader for ProgressReader.
+// Read implements io.Reader for ProgressReadCloser.
 func (rc *ProgressReadCloser) Read(p []byte) (n int, err error) {
-	if rc.pr == nil {
-		rc.pr = &ProgressReader{
-			R:        rc.RC,
-			Symbol:   rc.Symbol,
-			Interval: rc.Interval,
-			W:        rc.W,
+	defer func() {
+		numSymbols := (rc.counter%rc.Interval + n) / rc.Interval
+		rc.W.Write([]byte(strings.Repeat(rc.Symbol, numSymbols)))
+		rc.counter += n
+		rc.written = (rc.written || numSymbols > 0)
+		if err == io.EOF && rc.written {
+			rc.W.Write([]byte("\n"))
 		}
-	}
-	return rc.pr.Read(p)
+	}()
+	return rc.RC.Read(p)
 }
 
 // Read implements io.Closer for ProgressReader.
