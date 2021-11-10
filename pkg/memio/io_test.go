a33519cd4022dc5da57fd0631c9157b47418ad13
Patrik Tesarik <patrik.tesarik@9elements.com>
2021-11-10 16:45:17 +0100

    /pkg/memio: Describe structs for testing
    Move the structs used for testing to a new file: `variables_test.go` to
    enhance readability
    
    Signed-off-by: Patrik Tesarik <patrik.tesarik@9elements.com>

--- pkg/memio/io_test.go
+++ pkg/memio/io_test.go
@@ -13,44 +13,6 @@ import (
 	"testing"
 )
 
-var tests = []struct {
-	name                string
-	addr                int64
-	writeData, readData UintN
-	err                 string
-}{
-	{
-		name:      "uint8",
-		addr:      0x10,
-		writeData: &[]Uint8{0x12}[0],
-		readData:  new(Uint8),
-	},
-	{
-		name:      "uint16",
-		addr:      0x20,
-		writeData: &[]Uint16{0x1234}[0],
-		readData:  new(Uint16),
-	},
-	{
-		name:      "uint32",
-		addr:      0x30,
-		writeData: &[]Uint32{0x12345678}[0],
-		readData:  new(Uint32),
-	},
-	{
-		name:      "uint64",
-		addr:      0x40,
-		writeData: &[]Uint64{0x1234567890abcdef}[0],
-		readData:  new(Uint64),
-	},
-	{
-		name:      "byte slice",
-		addr:      0x50,
-		writeData: &[]ByteSlice{[]byte("Hello")}[0],
-		readData:  &[]ByteSlice{make([]byte, 5)}[0],
-	},
-}
-
 func TestIO(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf(tt.name), func(t *testing.T) {
