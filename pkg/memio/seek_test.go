554de73e5791d0c50f1a2300cc2617b7a76c5757
Patrik Tesarik <patrik.tesarik@9elements.com>
2021-11-10 11:34:55 +0100

    /pkg/memio: Add coverage of seek.go
    pathRead and pathWrite get covered
    
    Signed-off-by: Patrik Tesarik <patrik.tesarik@9elements.com>

---
+++ pkg/memio/seek_test.go
@@ -0,0 +1,44 @@
+// Copyright 2012-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package memio
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestSeek(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf(tt.name), func(t *testing.T) {
+			tmpFile, err := ioutil.TempFile("", "seek_test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(tmpFile.Name())
+
+			memPath = tmpFile.Name()
+			defer func() { memPath = "/dev/mem" }()
+
+			// Write to the file
+			if err := pathWrite(memPath, tt.addr, tt.writeData); err != nil {
+				t.Fatal(err)
+			}
+			if err := pathRead(memPath, tt.addr, tt.readData); err != nil {
+				t.Fatal(err)
+			}
+
+			want := tt.writeData
+			got := tt.readData
+			if !reflect.DeepEqual(want, got) {
+				t.Fatalf("Write(%#016x, %v) = %v; want %v",
+					tt.addr, want, got, want)
+			}
+
+		})
+	}
+}
