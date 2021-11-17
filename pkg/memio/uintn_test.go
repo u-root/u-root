09e361258f1e194ab11996c7bb752904e626c7cf
Patrik Tesarik <patrik.tesarik@9elements.com>
2021-11-17 20:56:28 +0100

    Place table test struct in test.go file
    Signed-off-by: Patrik Tesarik <patrik.tesarik@9elements.com>

--- pkg/memio/uintn_test.go
+++ pkg/memio/uintn_test.go
@@ -10,20 +10,52 @@ import (
 	"testing"
 )
 
-// Slice of strings taken from io_test.go `var tests`
-var wants = []string{
-	"0x12",
-	"0x1234",
-	"0x12345678",
-	"0x1234567890abcdef",
-	"0x48656c6c6f",
+var uintn_tests = []struct {
+	name      string
+	addr      int64
+	writeData UintN
+	want      string
+	err       string
+}{
+	{
+		name:      "uint8",
+		addr:      0x10,
+		writeData: &[]Uint8{0x12}[0],
+		want:      "0x12",
+	},
+	{
+		name:      "uint16",
+		addr:      0x20,
+		writeData: &[]Uint16{0x1234}[0],
+		want:      "0x1234",
+	},
+	{
+		name:      "uint32",
+		addr:      0x30,
+		writeData: &[]Uint32{0x12345678}[0],
+		want:      "0x12345678",
+	},
+	{
+		name:      "uint64",
+		addr:      0x40,
+		writeData: &[]Uint64{0x1234567890abcdef}[0],
+		want:      "0x1234567890abcdef",
+	},
+	{
+		name:      "byte slice",
+		addr:      0x50,
+		writeData: &[]ByteSlice{[]byte("Hello")}[0],
+		want:      "0x48656c6c6f",
+	},
 }
 
+// Slice of strings taken from io_test.go `var tests`
+
 func TestString(t *testing.T) {
-	for i, tt := range tests {
+	for _, tt := range uintn_tests {
 		t.Run(fmt.Sprintf(tt.name), func(t *testing.T) {
 			got := tt.writeData.String()
-			want := wants[i]
+			want := tt.want
 			if !reflect.DeepEqual(want, got) {
 				t.Errorf("Got: %v, want: %v", got, want)
 			}
