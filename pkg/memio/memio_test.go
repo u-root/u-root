1df343c5ff6e9901bfcedf8246c7f4c56c64c940
Patrik Tesarik <patrik.tesarik@9elements.com>
2021-11-17 20:45:43 +0100

    Move variables back into test.go files
    Signed-off-by: Patrik Tesarik <patrik.tesarik@9elements.com>

--- pkg/memio/memio_test.go
+++ pkg/memio/memio_test.go
@@ -14,6 +14,59 @@ import (
 	"testing"
 )
 
+var tests = []struct {
+	name                string
+	addr                int64
+	writeData, readData UintN
+	err                 string
+}{
+	{
+		name:      "uint8",
+		addr:      0x10,
+		writeData: &[]Uint8{0x12}[0],
+		readData:  new(Uint8),
+	},
+	{
+		name:      "uint16",
+		addr:      0x20,
+		writeData: &[]Uint16{0x1234}[0],
+		readData:  new(Uint16),
+	},
+	{
+		name:      "uint32",
+		addr:      0x30,
+		writeData: &[]Uint32{0x12345678}[0],
+		readData:  new(Uint32),
+	},
+	{
+		name:      "uint64",
+		addr:      0x40,
+		writeData: &[]Uint64{0x1234567890abcdef}[0],
+		readData:  new(Uint64),
+	},
+	{
+		name:      "byte slice",
+		addr:      0x50,
+		writeData: &[]ByteSlice{[]byte("Hello")}[0],
+		readData:  &[]ByteSlice{make([]byte, 5)}[0],
+	},
+}
+
+var testsInvalid = []struct {
+	name                string
+	addr                int64
+	writeData, readData UintN
+	err                 string
+	path                string
+}{
+	{
+		name:      "uint8",
+		addr:      0x00,
+		writeData: &[]Uint8{0x12}[0],
+		readData:  new(Uint8),
+	},
+}
+
 // TestIO tests a set of UintN againt the IO operations
 func TestIO(t *testing.T) {
 	for _, tt := range tests {
