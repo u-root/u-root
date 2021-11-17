765451ba34bd78cdae8a5fdc600a202a76ee7e41
Christian Walter <christian.walter@9elements.com>
2021-11-17 18:46:10 +0100

    cmds/core/dd: Refactor code, add tests and push code into pkg/progress
    This commits refactors a majority of the code within cmds/core/dd in
    order to make it testable. Additionally it moves part of the code into
    it's own package (pkg/progress) and adds tests for this package, and the
    cmd itself.
    
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

---
+++ pkg/progress/progress.go
@@ -0,0 +1,97 @@
+// Copyright 2021 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The progress package starts printing the status of a progress every second
+// It takes a pointer to an int64 which holds the amount of
+// bytes copied - and prints it.
+
+package progress
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type progressData struct {
+	mode         string // one of: none, xfer, progress
+	start        time.Time
+	end          time.Time
+	endTimeMutex sync.Mutex
+	variable     *int64 // must be aligned for atomic operations
+	quit         chan struct{}
+}
+
+// Begin - start a progress routine
+//
+// mode describes in which mode it runs, none, progress or xfer
+// variable holds the amount of bytes written
+func Begin(mode string, variable *int64) (ProgressData *progressData) {
+	p := &progressData{
+		mode:         mode,
+		start:        time.Now(),
+		endTimeMutex: sync.Mutex{},
+		variable:     variable,
+	}
+	if p.mode == "progress" {
+		p.print(os.Stderr)
+		// Print progress in a separate goroutine.
+		p.quit = make(chan struct{}, 1)
+		go func() {
+			ticker := time.NewTicker(1 * time.Second)
+			defer ticker.Stop()
+			for {
+				select {
+				case <-ticker.C:
+					p.print(os.Stderr)
+				case <-p.quit:
+					p.endTimeMutex.Lock()
+					p.end = time.Now()
+					p.endTimeMutex.Unlock()
+					return
+				}
+			}
+		}()
+	}
+	return p
+}
+
+// End - Ends the progress and send quit signal to the channel
+func (p *progressData) End() {
+	if p.mode == "progress" {
+		// Properly synchronize goroutine.
+		p.quit <- struct{}{}
+		p.quit <- struct{}{}
+	} else {
+		p.endTimeMutex.Lock()
+		p.end = time.Now()
+		p.endTimeMutex.Unlock()
+	}
+	if p.mode == "progress" || p.mode == "xfer" {
+		// Print grand total.
+		p.print(os.Stderr)
+		fmt.Fprint(os.Stderr, "\n")
+	}
+}
+
+// With "status=progress", this is called from 3 places:
+// - Once at the beginning to appear responsive
+// - Every 1s afterwards
+// - Once at the end so the final value is accurate
+func (p *progressData) print(out io.Writer) {
+	elapse := time.Since(p.start)
+	n := atomic.LoadInt64(p.variable)
+	d := float64(n)
+	const mib = 1024 * 1024
+	const mb = 1000 * 1000
+	// The ANSI escape may be undesirable to some eyes.
+	if p.mode == "progress" {
+		out.Write([]byte("\033[2K\r"))
+	}
+	fmt.Fprintf(out, "%d bytes (%.3f MB, %.3f MiB) copied, %.3f s, %.3f MB/s",
+		n, d/mb, d/mib, elapse.Seconds(), float64(d)/elapse.Seconds()/mb)
+}
