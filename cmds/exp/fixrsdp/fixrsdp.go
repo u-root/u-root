bdd46414da2f3238d666d51015da7c4f139584cb
Christopher Meis <christopher.meis@9elements.com>
2021-11-19 12:50:46 +0100

    fix clean-code
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- cmds/exp/fixrsdp/fixrsdp.go
+++ cmds/exp/fixrsdp/fixrsdp.go
@@ -32,7 +32,7 @@ func main() {
 
 	base := r.RSDPAddr()
 	// Check if ACPI rsdp is already in low memory
-	if base >= 0xe0000 && base+uint64(rLen) < 0xffff0 {
+	if base >= 0xe0000 && base+int64(rLen) < 0xffff0 {
 		log.Printf("RSDP is already in low memory at %#X, no need to fix.", base)
 		return
 	}
