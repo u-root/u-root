2da3b849f715ab824f9a5c7d055b62e08ed9d648
Ryan O'Leary <ryanoleary@google.com>
2021-11-24 13:35:11 -0800

    Fix zipslips for cpio, tar and zip
    Fixes the following CVEs:
    
    * CVE-2020-7669
    * CVE-2020-7666
    * CVE-2020-7665
    
    A zipslip is when a archive contains a path which escapes the extract
    directory. This can be done with absolute paths `/` or with parent
    directories `..`. It is a security problem.
    
    These commands will no longer extract those directories and instead
    print a warning.
    
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/cpio/fs_unix.go
+++ pkg/cpio/fs_unix.go
@@ -10,6 +10,7 @@ package cpio
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"syscall"
@@ -17,6 +18,7 @@ import (
 
 	"github.com/u-root/u-root/pkg/ls"
 	"github.com/u-root/u-root/pkg/uio"
+	"github.com/u-root/u-root/pkg/upath"
 	"golang.org/x/sys/unix"
 )
 
@@ -108,7 +110,13 @@ func CreateFileInRoot(f Record, rootDir string, forcePriv bool) error {
 		return err
 	}
 
-	f.Name = filepath.Clean(filepath.Join(rootDir, f.Name))
+	f.Name, err = upath.SafeFilepathJoin(rootDir, f.Name)
+	if err != nil {
+		// The behavior is to skip files which are unsafe due to
+		// zipslip, but continue extracting everything else.
+		log.Printf("Warning: Skipping file %q due to: %v", f.Name, err)
+		return nil
+	}
 	dir := filepath.Dir(f.Name)
 	// The problem: many cpio archives do not specify the directories and
 	// hence the permissions. They just specify the whole path.  In order
