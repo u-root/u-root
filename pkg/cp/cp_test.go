e4337f09e907b3b117ced747bf4f05b20e2c78aa
Christopher Meis <christopher.meis@9elements.com>
2021-12-09 13:52:26 +0100

    Refactor tests for pkg/cp: * now tests for Copy and CopyTree in table form
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/cp/cp_test.go
+++ pkg/cp/cp_test.go
@@ -5,159 +5,93 @@
 package cp
 
 import (
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io/fs"
 	"os"
-	"path/filepath"
-	"syscall"
 	"testing"
+
+	"golang.org/x/sys/unix"
 )
 
-func copyAndTest(t *testing.T, o Options, src, dst string) {
-	if err := o.Copy(src, dst); err != nil {
-		t.Fatalf("Copy(%q -> %q) = %v, want %v", src, dst, err, nil)
-	}
-	// if err := cmp.IsEqualTree(o, src, dst); err != nil {
-	// 	t.Fatalf("Expected %q and %q to be same, got %v", src, dst, err)
-	// }
-}
-
-func TestSimpleCopy(t *testing.T) {
-	tmpDir := t.TempDir()
-
-	// Copy a directory.
-	origd := filepath.Join(tmpDir, "directory")
-	if err := os.Mkdir(origd, 0o744); err != nil {
-		t.Fatal(err)
-	}
-
-	copyAndTest(t, Default, origd, filepath.Join(tmpDir, "directory-copied"))
-	copyAndTest(t, NoFollowSymlinks, origd, filepath.Join(tmpDir, "directory-copied-2"))
-
-	// Copy a file.
-	origf := filepath.Join(tmpDir, "normal-file")
-	if err := os.WriteFile(origf, []byte("F is for fire that burns down the whole town"), 0o766); err != nil {
-		t.Fatal(err)
-	}
-
-	copyAndTest(t, Default, origf, filepath.Join(tmpDir, "normal-file-copied"))
-	copyAndTest(t, NoFollowSymlinks, origf, filepath.Join(tmpDir, "normal-file-copied-2"))
-
-	// Copy a symlink.
-	origs := filepath.Join(tmpDir, "foobar")
-	// foobar -> normal-file
-	if err := os.Symlink(origf, origs); err != nil {
-		t.Fatal(err)
-	}
-
-	copyAndTest(t, Default, origf, filepath.Join(tmpDir, "foobar-copied"))
-	copyAndTest(t, NoFollowSymlinks, origf, filepath.Join(tmpDir, "foobar-copied-just-symlink"))
-}
-
-func TestCopyTree(t *testing.T) {
-	testfiles := make([]*os.File, 3)
-	tmpDir, err := ioutil.TempDir("", "u-root-pkg-cp-")
+func TestCopy(t *testing.T) {
+	tmpdirDst, err := os.MkdirTemp("", "dst-directory")
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("failed to create tmp directorty: %q", err)
 	}
-	defer os.RemoveAll(tmpDir)
+	defer os.RemoveAll(tmpdirDst)
 
-	// Make src directory.
-	srcd := filepath.Join(tmpDir, "src")
-	if err := os.Mkdir(srcd, 0744); err != nil {
-		t.Fatal(err)
+	tmpdirSrc, err := os.MkdirTemp("", "src-directory")
+	if err != nil {
+		t.Errorf("failed to create tmp src directorty: %q", err)
 	}
+	defer os.RemoveAll(tmpdirSrc)
 
-	if err := os.Chdir(srcd); err != nil {
-		t.Fatal(err)
-	}
-
-	// Make some rnd files
-	for i := 0; i < 3; i++ {
-		testfiles[i], err = os.Create("testfile" + fmt.Sprintf("%d", i))
+	srcfiles := make([]*os.File, 3)
+	dstfiles := make([]*os.File, 3)
+	for iterator := range srcfiles {
+		srcfiles[iterator], err = os.CreateTemp(tmpdirSrc, "file-to-copy"+fmt.Sprintf("%d", iterator))
 		if err != nil {
-			t.Fatal(err)
+			t.Errorf("failed to create temp file: %q", err)
 		}
-	}
-
-	// Make dest directory.
-	dest := filepath.Join(tmpDir, "dest")
-	if err := os.Mkdir(dest, 0744); err != nil {
-		t.Fatal(err)
-	}
-	// Copy the tree
-	if err := CopyTree(srcd, dest); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestCopyFile(t *testing.T) {
-	// Defining files and vars for the test
-	var equalTreeOpts Options
-
-	dirPath := "/tmp/u-root-pkg-cp/"
-	err := os.Mkdir(dirPath, 0700)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dirPath)
-
-	tmpFile1, err := ioutil.TempFile(dirPath, "file1")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer tmpFile1.Close()
-
-	tmpFile2, err := ioutil.TempFile(dirPath, "file2")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer tmpFile2.Close()
-
-	equalTreeOpts.NoFollowSymlinks = true
-	err = os.Symlink(tmpFile1.Name(), filepath.Join(dirPath, "symlink1"))
-	if err != nil {
-		t.Errorf("err while creating a symlink")
-	}
-
-	// Retrieving os.ModeSymlink from symlink
-	srcInfo, err := equalTreeOpts.stat(dirPath + "/symlink1")
-	if err != nil {
-		t.Errorf("err is: %v", err)
-	}
-
-	// Read the symlink
-	err = copyFile(dirPath+"/symlink1", dirPath+"/symlink2", srcInfo)
-	if err != nil {
-		t.Errorf("err is: %v", err)
-	}
-
-	// Error in reading symlink
-	err = copyFile(tmpFile1.Name(), dirPath+"/symlink2", srcInfo)
-	if fmt.Sprintf("%v", err) != "readlink "+tmpFile1.Name()+": invalid argument" {
-		t.Errorf("Test %s: got: (%s), want: (%s)", "error in os.Readlink", err, "readlink "+tmpFile1.Name()+": invalid argument")
-	}
-}
-
-func TestCopyRegularFile(t *testing.T) {
-
-	// Faking os.Open function
-	oopen := open
-	defer func() { open = oopen }()
-	open = func(name string) (*os.File, error) {
-		if name == "srcto" {
-			return nil, fmt.Errorf("error in open src")
-		}
-		f, err := os.OpenFile(name, syscall.O_RDONLY, 0)
+		dstfiles[iterator], err = os.CreateTemp(tmpdirDst, "file-to-copy"+fmt.Sprintf("%d", iterator))
 		if err != nil {
-			return nil, err
+			t.Errorf("failed to create temp file: %q", err)
 		}
-		defer f.Close()
-		return f, nil
 	}
 
-	err := copyRegularFile("srcto", "dst", nil)
-	if fmt.Sprintf("%v", err) != "error in open src" {
-		t.Errorf("Test %s: got: (%s), want: (%s)\n", "error open src", err, "error in open src")
+	for _, tt := range []struct {
+		name    string
+		srcfile string
+		dstfile string
+		opt     Options
+		wantErr error
+	}{
+		{
+			name:    "Success",
+			srcfile: srcfiles[0].Name(),
+			dstfile: dstfiles[0].Name(),
+			opt:     Default,
+		},
+		{
+			name:    "SrcDstDirctoriesSuccess",
+			srcfile: tmpdirSrc,
+			dstfile: tmpdirDst,
+		},
+		{
+			name:    "SrcNotExist",
+			srcfile: "file-does-not-exist",
+			dstfile: dstfiles[0].Name(),
+			wantErr: fs.ErrNotExist,
+		},
+		{
+			name:    "DstIsDirectory",
+			srcfile: srcfiles[0].Name(),
+			dstfile: tmpdirDst,
+			wantErr: unix.EISDIR,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.srcfile, tt.dstfile); !errors.Is(err, tt.wantErr) {
+				t.Errorf("Test %q failed. Want: %q, Got: %q", tt.name, tt.wantErr, err)
+			}
+		})
+
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.opt.Copy(tt.srcfile, tt.dstfile); !errors.Is(err, tt.wantErr) {
+				t.Errorf("Test %q failed. Want: %q, Got: %q", tt.name, tt.wantErr, err)
+			}
+		})
+
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.opt.CopyTree(tt.srcfile, tt.dstfile); !errors.Is(err, tt.wantErr) {
+				t.Errorf("Test %q failed. Want: %q, Got: %q", tt.name, tt.wantErr, err)
+			}
+		})
+		t.Run(tt.name, func(t *testing.T) {
+			if err := CopyTree(tt.srcfile, tt.dstfile); !errors.Is(err, tt.wantErr) {
+				t.Errorf("Test %q failed. Want: %q, Got: %q", tt.name, tt.wantErr, err)
+			}
+		})
 	}
 }
