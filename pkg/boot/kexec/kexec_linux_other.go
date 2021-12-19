e070d7b3441e48d8a5d18fdfcd573067d524efa0
Daniel Maslowski <info@orangecms.org>
2021-12-19 18:24:18 +0100

    pkg/boot/kexec: enable for RISC-V 64
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- pkg/boot/kexec/kexec_linux_other.go
+++ pkg/boot/kexec/kexec_linux_other.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux && !amd64 && !arm64
-// +build linux,!amd64,!arm64
+//go:build linux && !amd64 && !arm64 && !riscv64
+// +build linux,!amd64,!arm64,!riscv64
 
 package kexec
 
