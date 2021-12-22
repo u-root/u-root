92497f5eeee8a629f76c7919b1a937d1146c17e8
Christopher Meis <christopher.meis@9elements.com>
2021-12-22 18:09:32 +0100

    pkg/memio * Add interface implementation for platform independent MemIO read and write * Add unittests in table form for MemIO interface and implementation * Add interface Port as first abstraction for MemIO interface * Adapt linux and plan9 implementation to new interface. Caller can use it without knowledge of the platform * Add tests in table form for linux implementation * Add tests for UintN type * Add syscall abstraction and ReadAt, WriteAt to mmap linux implentation and adapt tests to table form
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/memio/mmap_linux.go
+++ pkg/memio/mmap_linux.go
@@ -11,59 +11,103 @@ import (
 	"unsafe"
 )
 
+var memPath = "/dev/mem"
+
 var pageSize = int64(syscall.Getpagesize())
 
+type syscalls interface {
+	Mmap(int, int64, int, int, int) ([]byte, error)
+	Munmap([]byte) error
+}
+
+type calls struct{}
+
+func (c *calls) Mmap(fd int, page int64, mapSize int, prot int, callid int) ([]byte, error) {
+	return syscall.Mmap(fd, page, mapSize, prot, callid)
+}
+
+func (c *calls) Munmap(mem []byte) error {
+	return syscall.Munmap(mem)
+}
+
+type MMap struct {
+	*os.File
+	syscalls
+}
+
 // mmap aligns the address and maps multiple pages when needed.
-func mmap(f *os.File, addr int64, size int64, prot int) (mem []byte, offset int64, err error) {
-	if addr+size <= addr {
-		return nil, 0, fmt.Errorf("invalid address for size %#x", size)
-	}
+func (m *MMap) mmap(f *os.File, addr int64, size int64, prot int) (mem []byte, offset int64, err error) {
 	page := addr &^ (pageSize - 1)
 	offset = addr - page
 	mapSize := offset + size
-	mem, err = syscall.Mmap(int(f.Fd()), int64(page), int(mapSize), prot, syscall.MAP_SHARED)
+	mem, err = m.Mmap(int(f.Fd()), int64(page), int(mapSize), prot, syscall.MAP_SHARED)
 	return
 }
 
 // Read reads data from physical memory at address addr. On x86 platforms,
 // this uses the seek+read syscalls. On arm platforms, this uses mmap.
-func Read(addr int64, data UintN) error {
-	f, err := os.OpenFile(memPath, os.O_RDONLY, 0)
+func (m *MMap) ReadAt(addr int64, data UintN) error {
+	mem, offset, err := m.mmap(m.File, addr, data.Size(), syscall.PROT_READ)
 	if err != nil {
-		return err
+		return fmt.Errorf("reading %#x/%d: %v", addr, data.Size(), err)
 	}
-	defer f.Close()
-
-	mem, offset, err := mmap(f, addr, data.Size(), syscall.PROT_READ)
-	if err != nil {
-		return fmt.Errorf("Reading %#x/%d: %v", addr, data.Size(), err)
-	}
-	defer syscall.Munmap(mem)
+	defer m.Munmap(mem)
 
 	// MMIO makes this a bit tricky. Reads must be conducted in one load
 	// operation. Review the generated assembly to make sure.
 	if err := data.read(unsafe.Pointer(&mem[offset])); err != nil {
-		return fmt.Errorf("Reading %#x/%d: %v", addr, data.Size(), err)
+		return fmt.Errorf("reading %#x/%d: %v", addr, data.Size(), err)
 	}
 	return nil
 }
 
 // Write writes data to physical memory at address addr. On x86 platforms, this
 // uses the seek+read syscalls. On arm platforms, this uses mmap.
-func Write(addr int64, data UintN) error {
-	f, err := os.OpenFile(memPath, os.O_RDWR, 0)
+func (m *MMap) WriteAt(addr int64, data UintN) error {
+	mem, offset, err := m.mmap(m.File, addr, data.Size(), syscall.PROT_WRITE)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-
-	mem, offset, err := mmap(f, addr, data.Size(), syscall.PROT_WRITE)
-	if err != nil {
-		return err
-	}
-	defer syscall.Munmap(mem)
+	defer m.Munmap(mem)
 
 	// MMIO makes this a bit tricky. Writes must be conducted in one store
 	// operation. Review the generated assembly to make sure.
 	return data.write(unsafe.Pointer(&mem[offset]))
+}
+
+func (m *MMap) Close() error {
+	return m.File.Close()
+}
+
+func NewMMap(path string) (*MMap, error) {
+	f, err := os.OpenFile(path, os.O_RDWR, 0)
+	if err != nil {
+		return nil, err
+	}
+	return &MMap{
+		File:     f,
+		syscalls: &calls{},
+	}, nil
+}
+
+// Read is deprecated. Still here for compatibility.
+// Use NewMMap() and the interface function instead.
+func Read(addr int64, data UintN) error {
+	mmap, err := NewMMap(memPath)
+	if err != nil {
+		return err
+	}
+	defer mmap.Close()
+	return mmap.ReadAt(addr, data)
+}
+
+// Write is deprecated. Still here for compatibility.
+// Use NewMMap() and the interface function instead.
+func Write(addr int64, data UintN) error {
+	mmap, err := NewMMap(memPath)
+	if err != nil {
+		return err
+	}
+	defer mmap.Close()
+	return mmap.WriteAt(addr, data)
 }
