084d1659c7aeb891b5aa756fa80a952ea6315e4a
llogen <christophlange95@googlemail.com>
2022-02-08 08:55:58 +0100

    cmds/core/strings: Add tests. Refactored strings.go and tests in strings_tests.go.
    Signed-off-by: llogen <christophlange95@googlemail.com>

--- cmds/core/strings/strings_test.go
+++ cmds/core/strings/strings_test.go
@@ -7,90 +7,148 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
-
-	"github.com/u-root/u-root/pkg/testutil"
 )
 
-type test struct {
-	name  string
-	flags []string
-	in    string
-	out   string
-}
+func TestStrings(t *testing.T) {
+	tmpdir := t.TempDir()
+	for _, tt := range []struct {
+		name    string
+		files   []string
+		n       int
+		input   string
+		want    string
+		wantErr string
+	}{
+		{
+			name:  "empty",
+			files: []string{},
+			input: "",
+			n:     4,
+			want:  "",
+		},
+		{
+			name:    "n < 1",
+			files:   []string{},
+			input:   "",
+			n:       0,
+			wantErr: fmt.Sprintf("strings: invalid minimum string length %v", 0),
+		},
+		{
+			name:  "string printable",
+			files: []string{},
+			input: "abcdefg",
+			n:     4,
+			want:  "abcdefg\n",
+		},
+		{
+			name:  "test prevent buffer from growing indefinitely",
+			files: []string{},
+			input: "qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty",
+			n:     4,
+			want:  "qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty\n",
+		},
+		{
+			name:  "non ascii char",
+			files: []string{},
+			input: "abcdefg£",
+			n:     4,
+			want:  "abcdefg\n",
+		},
+		{
+			name:  "with file",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "abcdefg",
+			n:     4,
+			want:  "abcdefg\n",
+		},
+		{
+			name:    "with file",
+			files:   []string{"filedoesnotexist"},
+			input:   "abcdefg",
+			n:       4,
+			wantErr: "open filedoesnotexist: no such file or directory",
+		},
+		{
+			name:  "strings are too short",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "\n\na\nab\n\nabc\nabc\xff\n01\n",
+			n:     4,
+			want:  "",
+		},
+		{
+			name:  "string fits perfectly",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "abcd\n",
+			n:     4,
+			want:  "abcd\n",
+		},
+		{
+			name:  "terminating newline",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "abcdefghijklmnopqrstuvwxyz\n",
+			n:     4,
+			want:  "abcdefghijklmnopqrstuvwxyz\n",
+		},
+		{
+			name:  "mix of printable and non-printable sequences",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "\n\na123456\nab\n\nabc\nabcde\xff\n01\n",
+			n:     4,
+			want:  "a123456\nabcde\n",
+		},
+		{
+			name:  "spaces are printable",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: " abcdefghijklm nopqrstuvwxyz ",
+			n:     4,
+			want:  " abcdefghijklm nopqrstuvwxyz \n",
+		},
+		{
+			name:  "shorter value of n",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "\n\na\nab\n\nabc\nabc\xff\n01\n",
+			n:     1,
+			want:  "a\nab\nabc\nabc\n01\n",
+		},
+		{
+			name:  "larger value of n",
+			files: []string{filepath.Join(tmpdir, "file")},
+			input: "\n\na123456\nab\n\nabc\nabcde\xff\n01\n",
+			n:     6,
+			want:  "a123456\n",
+		},
+	} {
+		// Setting the flag n
+		*n = tt.n
 
-var stringsTests = []test{
-	{
-		"empty",
-		[]string{},
-		"", "",
-	},
-	{
-		"sequences are too short",
-		[]string{},
-		"\n\na\nab\n\nabc\nabc\xff\n01\n", "",
-	},
-	{
-		"entire string is too short",
-		[]string{},
-		"abc", "",
-	},
-	{
-		"entire string just fits perfectly",
-		[]string{},
-		"abcd", "abcd\n",
-	},
-	{
-		"entire string is printable",
-		[]string{},
-		"abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz\n",
-	},
-	{
-		"terminating newline",
-		[]string{},
-		"abcdefghijklmnopqrstuvwxyz\n", "abcdefghijklmnopqrstuvwxyz\n",
-	},
-	{
-		"mix of printable and non-printable sequences",
-		[]string{},
-		"\n\na123456\nab\n\nabc\nabcde\xff\n01\n", "a123456\nabcde\n",
-	},
-	{
-		"spaces are printable",
-		[]string{},
-		" abcdefghijklm nopqrstuvwxyz ", " abcdefghijklm nopqrstuvwxyz \n",
-	},
-	{
-		"shorter value of n",
-		[]string{"--n", "1"},
-		"\n\na\nab\n\nabc\nabc\xff\n01\n", "a\nab\nabc\nabc\n01\n",
-	},
-	{
-		"larger value of n",
-		[]string{"--n", "6"},
-		"\n\na123456\nab\n\nabc\nabcde\xff\n01\n", "a123456\n",
-	},
-}
+		// Write input into file than seek to the beginning and truncate the file afterwards
+		// Create file for input data
+		file, err := os.Create(filepath.Join(tmpdir, "file"))
+		if err != nil {
+			t.Errorf("failed to create tmp file: %v", err)
+		}
+		_, err = file.WriteString(tt.input)
+		if err != nil {
+			t.Errorf("failed to write to file: %v", err)
+		}
 
-// strings < in > out
-func TestSortWithPipes(t *testing.T) {
-	// Table-driven testing
-	for _, tt := range stringsTests {
-		t.Run(fmt.Sprintf("%v", tt.name), func(t *testing.T) {
-			cmd := testutil.Command(t, tt.flags...)
-			cmd.Stdin = bytes.NewReader([]byte(tt.in))
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				t.Errorf("strings(%#v): %v", tt.in, err)
-			}
-			if string(out) != tt.out {
-				t.Errorf("strings(%#v) = %#v; want %#v", tt.in,
-					string(out), tt.out)
+		// Run tests
+		t.Run(tt.name, func(t *testing.T) {
+			bufIn := &bytes.Buffer{}
+			bufIn.WriteString(tt.input)
+			bufOut := &bytes.Buffer{}
+			if got := strings(bufOut, bufIn, tt.files...); got != nil {
+				if got.Error() != tt.wantErr {
+					t.Errorf("strings() = %q, want: %q", got.Error(), tt.wantErr)
+				}
+			} else {
+				if bufOut.String() != tt.want {
+					t.Errorf("strings() = %q, want: %q", bufOut.String(), tt.want)
+				}
 			}
 		})
 	}
-}
-
-func TestMain(m *testing.M) {
-	testutil.Run(m, main)
 }
