2d17df9e8fa935793b26fa8c6af16261ef68bead
Chris Koch <chrisko@google.com>
2022-02-20 15:49:27 -0800

    kexec PurgLoad: clarify docs + errors
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/boot/kexec/purg_load.go
+++ pkg/boot/kexec/purg_load.go
@@ -14,18 +14,21 @@ import (
 	"github.com/u-root/u-root/pkg/boot/align"
 )
 
-// PurgeLoad loads an elf file at the desired address.
+// PurgeLoad loads an ELF (which must be one of the purgatories from the
+// pkg/boot/linux package) into kmem, instructing it to jump to entry with RSI
+// set to rsi.
 func PurgeLoad(kmem *Memory, elfBuf []byte, start, param uintptr) (uintptr, error) {
 	elfFile, err := elf.NewFile(bytes.NewReader(elfBuf))
 	if err != nil {
-		return 0, fmt.Errorf("parse elf file from elf buffer: %v", err)
+		return 0, fmt.Errorf("parse purgatory ELF file from ELF buffer: %v", err)
 	}
 
 	log.Printf("Elf file: %#v, %d Progs", elfFile, len(elfFile.Progs))
 	if len(elfFile.Progs) != 1 {
-		return 0, fmt.Errorf("parse elf file: can only handle one Prog, not %d", len(elfFile.Progs))
+		return 0, fmt.Errorf("parse purgatory ELF file: can only handle one Prog, not %d", len(elfFile.Progs))
 	}
 	p := elfFile.Progs[0]
+
 	// the package really wants things page-sized, and rather than
 	// deal with all the bugs that arise from that, just keep it happy.
 	p.Memsz = uint64(align.AlignUpPageSize(uint(p.Memsz)))
@@ -33,17 +36,20 @@ func PurgeLoad(kmem *Memory, elfBuf []byte, start, param uintptr) (uintptr, erro
 	if _, err := p.ReadAt(b[:p.Filesz], 0); err != nil {
 		return 0, err
 	}
-	entry := elfFile.Entry
+	entry := uintptr(elfFile.Entry)
 	Debug("Start is %#x, param is %#x", start, param)
+
 	binary.LittleEndian.PutUint64(b[8:], uint64(start))
 	binary.LittleEndian.PutUint64(b[16:], uint64(param))
+
+	// TODO: Purgatories would be easier to handle if they were position-independent.
 	min := uintptr(p.Vaddr)
 	max := uintptr(p.Vaddr + uint64(len(b)))
 
 	phyRange, err := kmem.ReservePhys(uint(len(b)), RangeFromInterval(min, max))
 	if err != nil {
-		return uintptr(entry), fmt.Errorf("reserve phys ram of size %d between range(%d, %d): %v", len(b), min, max, err)
+		return 0, fmt.Errorf("purgatory: reserve phys ram of size %d between range(%d, %d): %v", len(b), min, max, err)
 	}
 	kmem.Segments.Insert(NewSegment(b, phyRange))
-	return uintptr(entry), nil
+	return entry, nil
 }
