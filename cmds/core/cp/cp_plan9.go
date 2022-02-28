b0c6d08d940a35fc29572ed04b7e9e39b8fe1019
Ronald G. Minnich <rminnich@gmail.com>
2022-02-28 15:37:43 -0800

    cmds/core/cp: make it buildable on Plan 9
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/core/cp/cp_plan9.go
@@ -0,0 +1,9 @@
+// Copyright 2022 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+var eNotDir = fmt.Errorf("not a directory")
