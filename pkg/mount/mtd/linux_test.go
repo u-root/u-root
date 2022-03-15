b5d74720e5822591b020d26f77e927f36288fd7d
Christopher Meis <christopher.meis@9elements.com>
2022-03-15 10:36:22 +0100

    pkg/mount/mtd: * Add tests for Dev struct of Flasher interface
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/mount/mtd/linux_test.go
+++ pkg/mount/mtd/linux_test.go
@@ -5,21 +5,42 @@
 package mtd
 
 import (
+	"bytes"
 	"os"
 	"testing"
 )
 
+var (
+	testString = "This is just a test string"
+)
+
 func TestOpen(t *testing.T) {
-	// If there's no such device then don't bother with the
-	// test.
 	if _, err := os.Stat(DevName); err != nil {
-		t.Skip("No device to test")
+		tmpDir := t.TempDir()
+		testmtd, err := os.CreateTemp(tmpDir, "testmtd")
+		if err != nil {
+			t.Errorf(`os.Create(tmpDir, "testmtd")=file, %q, want file, nil`, err)
+		}
+		DevName = testmtd.Name()
 	}
+
 	m, err := NewDev(DevName)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := m.Close(); err != nil {
-		t.Fatal(err)
+	defer m.Close()
+
+	if _, err := m.QueueWrite([]byte(testString), 0); err != nil {
+		t.Errorf("m.QueueWrite([]byte(testString), 0)=-,%q, want _,nil", err)
+	}
+	buf := make([]byte, 26)
+	if _, err := m.ReadAt(buf, 0); err != nil {
+		t.Errorf("m.ReadAt([]byte(), 0)=-,%q, want _,nil", err)
+	}
+	if !bytes.Equal(buf, []byte(testString)) {
+		t.Errorf("bytes.Equal(buf, []byte(testString))=false, want true")
+	}
+	if m.Name() != DevName {
+		t.Errorf("want %s == %s, want m.DevName() == testmtd.Name()", m.Name(), DevName)
 	}
 }
