29cc1d0f55c66bfa1a11fbb385483ae135ee4a29
Patrick Colp <patrick.colp@oracle.com>
2022-06-09 14:44:21 -0700

    securelaunch: Use a public key and signature to verify the policy file
    Currently, the policy file provided is trusted implicitly. However, an
    attacker could simply swap the policy file out with another and it will
    be processed.
    
    The first step in addressing this issue is to verify the policy file
    using a public key and signature. Currently, the key and signature are
    both trusted and the attacker could simply switch out all the files.
    A future commit will use a hash of the public key file, stored in the
    TPM, to verify that the public key provided is the expected one.
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>

--- pkg/securelaunch/policy/vm_test.go
+++ pkg/securelaunch/policy/vm_test.go
@@ -5,6 +5,7 @@
 package policy
 
 import (
+	"encoding/hex"
 	"testing"
 	"time"
 
@@ -73,6 +74,41 @@ const policyStr = `{
 }
 `
 
+const pubkeyStr = `
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmsvG6Bj+cylwEmrUrqKj
+W7lvovDDjJxizSrFTizd9gid/V9AT8mbwwoJJp2S2WVdGHPn3hDuFlyQvKTkqQlE
+6l1+ALEiAxjiGrKz6c+x21X1thHv01w9/NX/f7K3B1QNnj972k96z6PW0jjafxYe
+oc+ylGEqh4GxOlYbcdELdXi261+n+I0CsDJEzLVJnbs7YGW0dPN4UlGIAoVe4O4x
+Taz74TBJEODD6GrNLpZeDD2ke0KwZSjMF2lDBX40Oyj/yn10dYUqKvfODNvWYIQg
+fRCANXtNC3u89wU0HamJj8agITLOqIxp2UOaBj0qF4lAN4PP+pF6+6r0bngmfy0w
+3hnEFVplySGp/S94071hR7zAMd4ZSIACcUQjFvt5BqmHtXgDRviJc4lh8niZ7cg0
+ID8PgLjn+wLL5EZKsztWsk4030TeGtZ82eMiczqpsj7/5SWsxRBUraEi5aD6CREG
+LIRbeVynOqC8KPXWLWDUvZSgwWUOYykZHYY9qCU4UgWNAx/uOfl4ZHEVK4LtrNI0
++byI7V27o9f5uhz5XJiAjObTAyh31xP2p4Xu6wrCQy1E4i1l9gTsCl6rQAPY0K+A
+U8CAmAOKf2E5jEe8itnnsKu6Ii46ndLwJrBGgjsEYRNVln04owEiTsPrUCTPBS3O
+RaNetBXso+Hgdg8M25WgCI8CAwEAAQ==
+-----END PUBLIC KEY-----
+`
+
+const signatureStr = "" +
+	"5e344a9ee66b03445d83995f83bace0227d303082e824ff7137d9362e636406a" +
+	"ebdfa000b737dd9374d51278753356585aeedd2d8a2c884f2dc8036710f4a24c" +
+	"050fd129d2eb1b95ffb4c39cd2de58ad4b0c55d6480eea54d9a0288ba65bdef5" +
+	"7e5398dcf2b2510dcde1fb3bb1a7e0c17e00683a0dbfd53ad4170b2ac24741b7" +
+	"dfff09ab7b6a960e7f6937ff85b6289f9a1e83a1abeb653ae0aa2682809b6242" +
+	"2adb0130cad9ba9db54e0af7be0b5b008dcb4850e049a7b3b238f8feb5bbc91e" +
+	"97c55e3becaf7a1fec21aca19a1bb6d283c8c7848686746344ed1481a57154d2" +
+	"56c20efdfe0121097734222d02f0529372ab7ab296ec0b244e830c5540270e7f" +
+	"4fa729ec35328f63f83b3e6494187c254372261970764fb99438976d904bcffb" +
+	"1bb9e0428cc03e50595f07273fc387a4df1611275bc82fb156d9bf0ce8bbd0b7" +
+	"f6e008835c9f701e25afd6a531e7c8ab354c3ff9672a5354306091f919fa46bf" +
+	"8d3ca668f837ccf7a3adc6841c0c416ecafd299de3c5e03cff28daad9efd5c2a" +
+	"2de4526869a209fa6e5703ea6b63d40cb534e1f192458978c475dd155d919fe8" +
+	"ff16685d488051f3618b4a3112154a12206e321299b6e0a6a4ddff623a2f4f08" +
+	"4d254392e9285aa245242505bb2d94e6d6fd4ada8d12a6ffd3440227e17771e6" +
+	"68404e53b87a8a74314f70a8bc710c83459932357d068ad62cd5feeacae6f5c1"
+
 // VM setup:
 //
 //  /dev/sda is ./testdata/mbrdisk
@@ -107,7 +143,7 @@ func TestParse(t *testing.T) {
 		t.Fatalf(`WriteFile(policyStr.bytes, tempFile) = %v, not nil`, err)
 	}
 
-	if err := Load(policyFile); err != nil {
+	if err := Load(policyFile, "", ""); err != nil {
 		t.Fatalf("Load() = %v, not nil", err)
 	}
 
@@ -117,5 +153,37 @@ func TestParse(t *testing.T) {
 	}
 	if policy == nil {
 		t.Fatalf("no policy file returned")
+	}
+}
+
+func TestVerify(t *testing.T) {
+	guest.SkipIfNotInVM(t)
+
+	policyFile := "sda1:" + "/securelaunch.policy"
+	pubkeyFile := "sda1:" + "/securelaunch.pubkey"
+	signatureFile := "sda1:" + "/securelaunch.sig"
+
+	if err := slaunch.WriteFile([]byte(policyStr), policyFile); err != nil {
+		t.Fatalf(`WriteFile(policyStr.bytes, policyFile) = %v, not nil`, err)
+	}
+
+	if err := slaunch.WriteFile([]byte(pubkeyStr), pubkeyFile); err != nil {
+		t.Fatalf(`WriteFile(pubkeyStr.bytes, pubkeyFile) = %v, not nil`, err)
+	}
+
+	signatureBytes, err := hex.DecodeString(signatureStr)
+	if err != nil {
+		t.Fatalf(`hex.DecodeString(signatureStr) = %v, not nil`, err)
+	}
+	if err := slaunch.WriteFile(signatureBytes, signatureFile); err != nil {
+		t.Fatalf(`WriteFile(signatureBytes, signatureFile) = %v, not nil`, err)
+	}
+
+	if err := Load(policyFile, pubkeyFile, signatureFile); err != nil {
+		t.Fatalf("Load() = %v, not nil", err)
+	}
+
+	if err := Verify(); err != nil {
+		t.Fatalf("Verify() = %v, not nil", err)
 	}
 }
