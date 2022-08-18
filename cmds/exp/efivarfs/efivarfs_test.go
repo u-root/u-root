d2078b253852684b0f0edd3ef59cb644b298461d
Ronald G. Minnich <rminnich@gmail.com>
2022-08-18 15:31:40 +0000

    xpkg/efivar, cmds/exp/efivarfs: fix to run local and use errors package
    Make the errors wrap standard error values from os so the errors package can be used.
    
    Use errors.Is in tests so we don't have to use string compares.
    
    Skip any tests that need root when we are not root.
    
    With these changes, the tests now run locally.
    
    Other fixes:
    
    EFIVar is now an interface, with one implementation in the package and another
    in the command itself, for testing.
    
    Names were being parsed in a way that let users trivially panic the code, this is fixed.
    
    The run() function spewed all over test output; it is not called with an io.Writer as the
    first argument, and tests can call it with io.Discard.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/exp/efivarfs/efivarfs_test.go
+++ cmds/exp/efivarfs/efivarfs_test.go
@@ -5,71 +5,133 @@
 package main
 
 import (
+	"errors"
+	"io"
 	"os"
 	"path/filepath"
-	"strings"
+	"syscall"
 	"testing"
+
+	"github.com/u-root/u-root/pkg/efivarfs"
 )
 
+type failingos struct {
+	err error
+}
+
+func (f *failingos) Get(desc efivarfs.VariableDescriptor) (efivarfs.VariableAttributes, []byte, error) {
+	return efivarfs.VariableAttributes(0), make([]byte, 32), f.err
+}
+
+func (f *failingos) Set(desc efivarfs.VariableDescriptor, attrs efivarfs.VariableAttributes, data []byte) error {
+	return f.err
+}
+
+func (f *failingos) Remove(desc efivarfs.VariableDescriptor) error {
+	return f.err
+}
+
+func (f *failingos) List() ([]efivarfs.VariableDescriptor, error) {
+	return make([]efivarfs.VariableDescriptor, 3), f.err
+}
+
+var _ efivarfs.EFIVar = &failingos{}
+
+var (
+	badfs = &failingos{err: os.ErrNotExist}
+	nofs  = &failingos{err: efivarfs.ErrNoFS}
+	iofs  = &failingos{err: syscall.EIO}
+	okfs  = &failingos{err: nil}
+)
+
+// We should not test the actual /sys varfs itself. That is done in the package.
+// So it suffices to test the login in run() with a faked up EFIVarFS that points to /tmp.
 func TestRun(t *testing.T) {
 	for _, tt := range []struct {
-		name    string
-		setup   func(path string, t *testing.T) string
-		list    bool
-		read    string
-		delete  string
-		write   string
-		content string
-		wantErr string
+		name     string
+		e        efivarfs.EFIVar
+		setup    func(path string, t *testing.T) string
+		list     bool
+		read     string
+		delete   string
+		write    string
+		wantErr  error
+		needRoot bool
 	}{
 		{
 			name: "list no efivarfs",
+			e:    nofs,
 			setup: func(path string, t *testing.T) string {
 				t.Helper()
 				return ""
 			},
 			list:    true,
-			wantErr: "no efivarfs",
+			wantErr: efivarfs.ErrNoFS,
 		},
 		{
 			name: "read no efivarfs",
+			e:    badfs,
 			setup: func(path string, t *testing.T) string {
 				t.Helper()
 				return ""
 			},
 			read:    "TestVar-bc54d3fb-ed45-462d-9df8-b9f736228350",
-			wantErr: "no efivarfs",
+			wantErr: os.ErrNotExist,
+		},
+		{
+			name: "read bad variable",
+			e:    badfs,
+			setup: func(path string, t *testing.T) string {
+				t.Helper()
+				return ""
+			},
+			read:    "TestVar",
+			wantErr: os.ErrInvalid,
+		},
+		{
+			name: "read good variable",
+			e:    okfs,
+			setup: func(path string, t *testing.T) string {
+				t.Helper()
+				return ""
+			},
+			read:    " WriteOnceStatus-4b3082a3-80c6-4d7e-9cd0-583917265df1",
+			wantErr: nil,
 		},
 		{
 			name: "delete no efivarfs",
+			e:    badfs,
 			setup: func(path string, t *testing.T) string {
 				t.Helper()
 				return ""
 			},
 			delete:  "TestVar-bc54d3fb-ed45-462d-9df8-b9f736228350",
-			wantErr: "no efivarfs",
+			wantErr: os.ErrNotExist,
 		},
 		{
 			name: "write malformed var",
+			e:    badfs,
 			setup: func(path string, t *testing.T) string {
 				t.Helper()
 				return ""
 			},
 			write:   "TestVar-bc54d3fb-ed45-462d-9df8-b9f736228350000",
-			wantErr: "var name malformed",
+			wantErr: os.ErrInvalid,
 		},
 		{
 			name: "write no content",
+			e:    badfs,
 			setup: func(path string, t *testing.T) string {
 				t.Helper()
-				return ""
+				// oh fun this is what actually sets content.
+				return "/bogus"
 			},
 			write:   "TestVar-bc54d3fb-ed45-462d-9df8-b9f736228350",
-			content: "/bogus",
-			wantErr: "failed to read file",
+			wantErr: os.ErrNotExist,
 		},
 		{
 			name:  "write no guid no efivarfs",
+			e:     iofs,
 			write: "TestVar",
 			setup: func(path string, t *testing.T) string {
 				t.Helper()
@@ -83,16 +145,58 @@ func TestRun(t *testing.T) {
 				}
 				return s
 			},
-			wantErr: "no efivarfs",
+			wantErr: syscall.EIO,
+		},
+		{
+			name:  "write good variable bad content",
+			e:     okfs,
+			write: " WriteOnceStatus-4b3082a3-80c6-4d7e-9cd0-583917265df1",
+			setup: func(path string, t *testing.T) string {
+				t.Helper()
+				return filepath.Join(path, "content")
+			},
+			wantErr: os.ErrNotExist,
+		},
+		{
+			name:  "write good variable succeeds",
+			e:     okfs,
+			write: " WriteOnceStatus-4b3082a3-80c6-4d7e-9cd0-583917265df1",
+			setup: func(path string, t *testing.T) string {
+				t.Helper()
+				f, err := os.Create(filepath.Join(path, "content"))
+				if err != nil {
+					t.Errorf("Failed to create file: %v", err)
+				}
+				s := f.Name()
+				if err := f.Close(); err != nil {
+					t.Errorf("Failed to close file: %v", err)
+				}
+				return s
+			},
+			wantErr: nil,
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.content = tt.setup(t.TempDir(), t)
-			if err := run(tt.list, tt.read, tt.delete, tt.write, tt.content); err != nil {
-				if !strings.Contains(err.Error(), tt.wantErr) {
-					t.Errorf("Want: %q, Got: %v", tt.wantErr, err)
+			if err := run(io.Discard, tt.e, tt.list, tt.read, tt.delete, tt.write, tt.setup(t.TempDir(), t)); err != nil {
+				if !errors.Is(err, tt.wantErr) {
+					t.Errorf("Got: %q, Want: %v", err, tt.wantErr)
 				}
 			}
 		})
+	}
+}
+
+func TestBadRunPath(t *testing.T) {
+	if err := runpath(os.Stdout, "/tmp", false, "", "", "", ""); !errors.Is(err, efivarfs.ErrNoFS) {
+		t.Errorf(`runpath(os.Stdout, "/tmp", false, "", "", "", "", ""): %v != %v`, err, efivarfs.ErrNoFS)
+	}
+}
+func TestGoodRunPath(t *testing.T) {
+	if _, err := os.Stat(efivarfs.DefaultVarFS); err != nil {
+		t.Skipf("%q: %v, skipping test", efivarfs.DefaultVarFS, err)
+	}
+
+	if err := runpath(os.Stdout, efivarfs.DefaultVarFS, false, "", "", "", ""); err != nil {
+		t.Errorf(`runpath(os.Stdout, %q, false, "", "", "", "", ""): %v != %v`, efivarfs.DefaultVarFS, err, efivarfs.ErrNoFS)
 	}
 }
