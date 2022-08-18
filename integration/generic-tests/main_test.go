7ad9485128e914dca3128e5fd3c95c1def469729
Ronald G. Minnich <rminnich@gmail.com>
2022-08-18 19:07:22 +0000

    testing: make it work locally
    Until now, go test ./... from top level always failed locally.
    
    These changes:
    o provide an easy script to get a kernel and qemu to use
    o fix a longstanding problem in mount integration tests
      that caused qemu to fail with locking problems
    o make the error from the integration tests more useful.
    
    With this change, the user needs to:
    export UROOT_SOURCE=`pwd`
    export UROOT_QEMU_COVERPROFILE=1
    
    possibly,
    . integration/GET_KERNEL_QEMU
    
    and to test ./... works.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- integration/generic-tests/main_test.go
+++ integration/generic-tests/main_test.go
@@ -12,10 +12,10 @@ import (
 
 func TestMain(m *testing.M) {
 	if len(os.Getenv("UROOT_KERNEL")) == 0 {
-		log.Fatalf("Failed to run tests: no kernel provided")
+		log.Fatalf("Failed to run tests: no kernel provide: source integration/GET_KERNEL_QEMU to get a kernel")
 	}
 	if len(os.Getenv("UROOT_QEMU")) == 0 {
-		log.Fatalf("Failed to run tests: no QEMU binary provided")
+		log.Fatalf("Failed to run tests: no QEMU binary provided: source integration/GET_KERNEL_QEMU to get a qemu")
 	}
 
 	log.Printf("Starting generic tests...")
