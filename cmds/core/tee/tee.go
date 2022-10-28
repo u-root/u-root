d184a2994d985ce08dde45cb4f99ccdc7e5a9f1b
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2022-10-28 21:07:30 +0300

    cmds/core/tee add test coverage
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/tee/tee.go
+++ cmds/core/tee/tee.go
@@ -25,19 +25,13 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-const name = "tee"
-
 var (
 	cat    = flag.BoolP("append", "a", false, "append the output to the files rather than rewriting them")
 	ignore = flag.BoolP("ignore-interrupts", "i", false, "ignore the SIGINT signal")
 )
 
-// handeFlags parses all the flags and sets variables accordingly
-func handleFlags() int {
-	flag.Parse()
-
+func run(stdin io.Reader, stdout io.Writer, stderr io.Writer, args []string) error {
 	oflags := os.O_WRONLY | os.O_CREATE
-
 	if *cat {
 		oflags |= os.O_APPEND
 	}
@@ -46,32 +40,35 @@ func handleFlags() int {
 		signal.Ignore(os.Interrupt)
 	}
 
-	return oflags
-}
-
-func main() {
-	oflags := handleFlags()
-
-	files := make([]*os.File, 0, flag.NArg())
-	writers := make([]io.Writer, 0, flag.NArg()+1)
-	for _, fname := range flag.Args() {
+	files := make([]*os.File, 0, len(args))
+	writers := make([]io.Writer, 0, len(args)+1)
+	for _, fname := range args {
 		f, err := os.OpenFile(fname, oflags, 0o666)
 		if err != nil {
-			log.Fatalf("%s: error opening %s: %v", name, fname, err)
+			return fmt.Errorf("error opening %s: %v", fname, err)
 		}
 		files = append(files, f)
 		writers = append(writers, f)
 	}
-	writers = append(writers, os.Stdout)
+	writers = append(writers, stdout)
 
 	mw := io.MultiWriter(writers...)
-	if _, err := io.Copy(mw, os.Stdin); err != nil {
-		log.Fatalf("%s: error: %v", name, err)
+	if _, err := io.Copy(mw, stdin); err != nil {
+		return fmt.Errorf("error: %v", err)
 	}
 
 	for _, f := range files {
 		if err := f.Close(); err != nil {
-			fmt.Fprintf(os.Stderr, "%s: error closing file %q: %v\n", name, f.Name(), err)
+			fmt.Fprintf(stderr, "tee: error closing file %q: %v\n", f.Name(), err)
 		}
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if err := run(os.Stdin, os.Stdout, os.Stderr, flag.Args()); err != nil {
+		log.Fatalf("tee: %v", err)
 	}
 }
