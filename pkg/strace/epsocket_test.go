a003fd0e9e65680bd06ba0e22cfb71c71ac304c4
Ronald G. Minnich <rminnich@google.com>
2023-02-09 17:12:38 +0000

    pkg/strace: add more epsocket coverage
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

--- pkg/strace/epsocket_test.go
+++ pkg/strace/epsocket_test.go
@@ -24,8 +24,11 @@ func TestGetAddress(t *testing.T) {
 		{name: "too short", addr: []byte{unix.AF_UNIX, 0, 0}, f: nil, err: unix.EINVAL},
 		{name: "bad family", addr: []byte{0, 2, 'h', 'i', 0}, f: nil, err: unix.ENOTSUP},
 		{name: "unix", addr: []byte{unix.AF_UNIX, 0, 'h', 'i', 0}, f: &FullAddress{Addr: "hi"}, err: nil},
+		{name: "unix ENAMETOOLONG", addr: (&[unix.PathMax * 2]byte{unix.AF_UNIX, 0, 'h', 'i', 0})[:], f: nil, err: unix.ENAMETOOLONG},
 		{name: "IP4", addr: []byte{unix.AF_INET, 0, 13, 14, 1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0}, f: &FullAddress{Addr: "1.2.3.4", Port: 3342}, err: nil},
+		{name: "IP4short", addr: []byte{unix.AF_INET, 0, 13}, f: nil, err: unix.EFAULT},
 		{name: "IP6", addr: []byte{unix.AF_INET6, 0, 13, 14, 0xde, 0xad, 0xbe, 0xef, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0xa, 0xb, 0xc, 0xd}, f: &FullAddress{Addr: "1:203:405:607:809:a0b:c0d:e0f", Port: 3342}, err: nil},
+		{name: "IP6short", addr: []byte{unix.AF_INET6, 0, 13, 14}, f: nil, err: unix.EFAULT},
 	} {
 		f, err := GetAddress(tt.addr)
 		if !errors.Is(err, tt.err) {
@@ -35,6 +38,20 @@ func TestGetAddress(t *testing.T) {
 		if !reflect.DeepEqual(f, tt.f) {
 			t.Errorf("%s:got FullAddress %s, want %s", tt.name, f.String(), tt.f.String())
 		}
+	}
+
+}
+
+func TestFullAddressStringer(t *testing.T) {
+	var a *FullAddress
+	s := a.String()
+	if s != ":" {
+		t.Errorf("nil String: got %q, want %q", s, ":")
+	}
+	a = &FullAddress{Addr: "unix", Port: 0xdead}
+	s = a.String()
+	if s != "unix:0xdead" {
+		t.Errorf("String: got %q, want %q", s, "unix:0xdead")
 	}
 
 }
