82e56fef3110690678f759b3e678be19161fbc58
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2023-07-24 22:26:31 +0300

    cmds/core/netcat: remove globals
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/netcat/netcat.go
+++ cmds/core/netcat/netcat.go
@@ -1,4 +1,4 @@
-// Copyright 2012-2017 the u-root Authors. All rights reserved
+// Copyright 2012-2023 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -18,57 +18,105 @@ import (
 
 const usage = "netcat [go-style network address]"
 
-var (
-	netType = flag.String("net", "tcp", "What net type to use, e.g. tcp, unix, etc.")
-	listen  = flag.Bool("l", false, "Listen for connections.")
-	verbose = flag.Bool("v", false, "Verbose output.")
-)
+var errMissingHostnameOrPort = fmt.Errorf("missing hostname or port")
+
+type params struct {
+	network string
+	listen  bool
+	verbose bool
+	host    string
+	port    string
+}
+
+func parseParms() (params, error) {
+	netType := flag.String("net", "tcp", "What net type to use, e.g. tcp, unix, etc.")
+	listen := flag.Bool("l", false, "Listen for connections.")
+	verbose := flag.Bool("v", false, "Verbose output.")
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		return params{}, errMissingHostnameOrPort
+	}
+
+	return params{
+		network: *netType,
+		listen:  *listen,
+		verbose: *verbose,
+		host:    flag.Args()[0],
+		port:    flag.Args()[1],
+	}, nil
+}
+
+type cmd struct {
+	stdin  io.Reader
+	stdout io.Writer
+	stderr io.Writer
+	params
+}
+
+func command(stdin io.Reader, stdout io.Writer, stderr io.Writer, p params) *cmd {
+	return &cmd{
+		stdin:  stdin,
+		stdout: stdout,
+		stderr: stderr,
+		params: p,
+	}
+}
 
 func init() {
 	flag.Usage = util.Usage(flag.Usage, usage)
 }
 
 func main() {
-	var c net.Conn
-	var err error
-	if flag.Parse(); len(flag.Args()) != 1 {
+	p, err := parseParms()
+	if err != nil {
 		flag.Usage()
 		os.Exit(1)
 	}
+	if err := command(os.Stdin, os.Stdout, os.Stderr, p).run(); err != nil {
+		log.Fatalf("nc: %v", err)
+	}
+}
 
-	addr := flag.Args()[0]
+func (c *cmd) run() error {
+	var conn net.Conn
+	var err error
 
-	if *listen {
-		ln, err := net.Listen(*netType, addr)
+	addr := net.JoinHostPort(c.host, c.port)
+
+	if c.listen {
+		ln, err := net.Listen(c.network, addr)
 		if err != nil {
-			log.Fatalln(err)
+			return err
 		}
-		if *verbose {
-			fmt.Fprintln(os.Stderr, "Listening on", ln.Addr())
+		if c.verbose {
+			fmt.Fprintln(c.stderr, "Listening on", ln.Addr())
 		}
 
-		c, err = ln.Accept()
+		conn, err = ln.Accept()
 		if err != nil {
-			log.Fatalln(err)
+			return err
 		}
 	} else {
-		if c, err = net.Dial(*netType, addr); err != nil {
-			log.Fatalln(err)
+		if conn, err = net.Dial(c.network, addr); err != nil {
+			return err
 		}
 	}
-	if *verbose {
-		fmt.Fprintln(os.Stderr, "Connected to", c.RemoteAddr())
+	if c.verbose {
+		fmt.Fprintln(c.stderr, "Connected to", conn.RemoteAddr())
 	}
 
 	go func() {
-		if _, err := io.Copy(c, os.Stdin); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		if _, err := io.Copy(conn, c.stdin); err != nil {
+			fmt.Fprintln(c.stderr, err)
 		}
 	}()
-	if _, err = io.Copy(os.Stdout, c); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	if _, err = io.Copy(c.stdout, conn); err != nil {
+		fmt.Fprintln(c.stderr, err)
 	}
-	if *verbose {
-		fmt.Fprintln(os.Stderr, "Disconnected")
+	if c.verbose {
+		fmt.Fprintln(c.stderr, "Disconnected")
 	}
+
+	return nil
 }
