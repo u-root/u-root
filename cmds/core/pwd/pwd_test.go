e31697f100e2a7f7f94a4c5e19211589629cbda2
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2023-08-20 19:28:40 +0300

    cmds/core/pwd: add test for no follow
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/pwd/pwd_test.go
+++ cmds/core/pwd/pwd_test.go
@@ -11,23 +11,50 @@ import (
 )
 
 func TestPWD(t *testing.T) {
-	dir := t.TempDir()
-	err := os.Chdir(dir)
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		name     string
+		physical bool
+	}{
+		{
+			name:     "follow-symlinks",
+			physical: true,
+		},
+		{
+			name:     "no-follow-symlinks",
+			physical: false,
+		},
 	}
 
-	rs, err := filepath.EvalSymlinks(dir)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir := t.TempDir()
+			err := os.Chdir(dir)
+			if err != nil {
+				t.Fatalf("os.Chdir(%q): got %v, want nil", dir, err)
+			}
 
-	r, err := pwd(true)
-	if err != nil {
-		t.Fatal(err)
-	}
+			// required on macOS, where tempDir returns path with symlink
+			// and PWD is defined on bash and zsh
+			err = os.Setenv("PWD", dir)
+			if err != nil {
+				t.Fatalf("os.Setenv(%q): got %v, want nil", dir, err)
+			}
 
-	if r != rs {
-		t.Errorf("expected: %q, got %q", rs, r)
+			expected := dir
+			if tt.physical {
+				expected, err = filepath.EvalSymlinks(dir)
+				if err != nil {
+					t.Fatalf("filepath.EvalSymlinks(%q): got %v, want nil", dir, err)
+				}
+			}
+
+			res, err := pwd(tt.physical)
+			if err != nil {
+				t.Fatalf("pwd(%t): error got %v, want nil", tt.physical, err)
+			}
+			if res != expected {
+				t.Errorf("pwd(%t): got %q, want %q", tt.physical, res, expected)
+			}
+		})
 	}
 }
