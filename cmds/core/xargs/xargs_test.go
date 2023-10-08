65982a7b30f7c2b7446c009bb6ec06ad5a7814b6
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2023-10-08 11:27:15 +0300

    cmds/core/xargs: minimal xargs, just passing args
    Parsing is simplified by strings.Fields, so no quotes support.
    
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

---
+++ cmds/core/xargs/xargs_test.go
@@ -0,0 +1,34 @@
+// Copyright 2013-2023 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"errors"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestCommandNotFound(t *testing.T) {
+	stdin := strings.NewReader("hello world")
+	err := run(stdin, nil, nil, []string{"commandnotfound", "arg1"})
+	if !errors.Is(err, exec.ErrNotFound) {
+		t.Fatalf("expected %v, got %v", exec.ErrNotFound, err)
+	}
+}
+
+func TestEcho(t *testing.T) {
+	stdin := strings.NewReader("hello world")
+	stdout := &bytes.Buffer{}
+	err := run(stdin, stdout, nil, nil)
+	if err != nil {
+		t.Fatalf("expected nil, got %v", err)
+	}
+
+	if stdout.String() != "hello world\n" {
+		t.Fatalf("expected 'hello world', got %q", stdout.String())
+	}
+}
