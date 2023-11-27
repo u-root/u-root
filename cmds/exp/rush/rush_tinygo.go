490741616326012eaa93bdba7f50376e42c7cf42
Ronald G. Minnich <rminnich@gmail.com>
2023-11-26 19:39:31 -0800

    tinygobb: update and make it work again
    reduce the number of functions tinygobb updates
    to just runone.
    
    Add a placeholder cmds/exp/runone_tinygo.go,
    so that gobusybox will not get confused.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/exp/rush/rush_tinygo.go
@@ -0,0 +1,15 @@
+// Copyright 2023 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file exists to make gobusybox and u-root happy.
+// It is replaced by the tinygobb tool.
+
+//go:build tinygo
+// +build tinygo
+
+package main
+
+func runone(c *Command) error {
+	return nil
+}
