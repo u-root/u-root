2ccf654ec478ac913292a2fb1fbe8ebe7fd23720
Chris Koch <chrisko@google.com>
2023-12-17 22:58:04 -0800

    Convert pxeboot to new vmtest
    Signed-off-by: Chris Koch <chrisko@google.com>

--- integration/generic-tests/pxeboot_test.go
+++ integration/generic-tests/pxeboot_test.go
@@ -11,90 +11,93 @@ import (
 	"testing"
 	"time"
 
-	"github.com/u-root/u-root/pkg/qemu"
+	"github.com/hugelgupf/vmtest"
+	"github.com/hugelgupf/vmtest/qemu"
+	"github.com/hugelgupf/vmtest/qemu/network"
 	"github.com/u-root/u-root/pkg/testutil"
 	"github.com/u-root/u-root/pkg/uroot"
-	"github.com/u-root/u-root/pkg/vmtest"
 )
 
 // TestPxeboot runs a server and client to test pxebooting a node.
 func TestPxeboot4(t *testing.T) {
 	// TODO: support arm
-	if vmtest.TestArch() != "amd64" && vmtest.TestArch() != "arm64" {
-		t.Skipf("test not supported on %s", vmtest.TestArch())
-	}
+	vmtest.SkipIfNotArch(t, qemu.ArchAMD64, qemu.ArchArm64)
 
-	network := qemu.NewNetwork()
-	dhcpServer, scleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		Name: "TestPxeboot_Server",
-		BuildOpts: uroot.Opts{
+	serverCmds := []string{
+		"ip addr add 192.168.0.1/24 dev eth0",
+		"ip link set eth0 up",
+		"ip route add 0.0.0.0/0 dev eth0",
+		"ls -l /pxeroot",
+		"pxeserver -tftp-dir=/pxeroot",
+	}
+	net := network.NewInterVM()
+	serverVM := vmtest.StartVMAndRunCmds(t, serverCmds,
+		vmtest.WithName("TestPxeboot_Server"),
+		vmtest.WithMergedInitramfs(uroot.Opts{
+			Commands: uroot.BusyBoxCmds(
+				"github.com/u-root/u-root/cmds/core/ip",
+				"github.com/u-root/u-root/cmds/core/ls",
+				"github.com/u-root/u-root/cmds/exp/pxeserver",
+			),
 			ExtraFiles: []string{
 				"./testdata/pxe:pxeroot",
 			},
-		},
-		TestCmds: []string{
-			"ip addr add 192.168.0.1/24 dev eth0",
-			"ip link set eth0 up",
-			"ip route add 0.0.0.0/0 dev eth0",
-			"ls -l /pxeroot",
-			"pxeserver -tftp-dir=/pxeroot",
-		},
-		QEMUOpts: qemu.Options{
-			SerialOutput: vmtest.TestLineWriter(t, "server"),
-			Timeout:      30 * time.Second,
-			Devices: []qemu.Device{
-				network.NewVM(),
-			},
-		},
-	})
-	defer scleanup()
+		}),
+		vmtest.WithQEMUFn(
+			qemu.WithVMTimeout(time.Minute),
+			net.NewVM(),
+		),
+	)
 
-	dhcpClient, ccleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		Name: "TestPxeboot_Client",
-		BuildOpts: uroot.Opts{
+	testCmds := []string{
+		"pxeboot --no-exec -v",
+		// Sleep so serial console output gets flushed. The expect library is racy.
+		"sleep 5",
+		"shutdown -h",
+	}
+	clientVM := vmtest.StartVMAndRunCmds(t, testCmds,
+		vmtest.WithName("TestPxeboot_Client"),
+		vmtest.WithMergedInitramfs(uroot.Opts{
 			Commands: uroot.BusyBoxCmds(
-				"github.com/u-root/u-root/cmds/core/init",
-				"github.com/u-root/u-root/cmds/core/elvish",
 				"github.com/u-root/u-root/cmds/core/ip",
 				"github.com/u-root/u-root/cmds/core/shutdown",
 				"github.com/u-root/u-root/cmds/core/sleep",
 				"github.com/u-root/u-root/cmds/boot/pxeboot",
 			),
-		},
-		TestCmds: []string{
-			"pxeboot --no-exec -v",
-			// Sleep so serial console output gets flushed. The expect library is racy.
-			"sleep 5",
-			"shutdown -h",
-		},
-		QEMUOpts: qemu.Options{
-			SerialOutput: vmtest.TestLineWriter(t, "client"),
-			Timeout:      30 * time.Second,
-			Devices: []qemu.Device{
-				network.NewVM(),
-			},
-		},
-	})
-	defer ccleanup()
+		}),
+		vmtest.WithQEMUFn(
+			qemu.WithVMTimeout(time.Minute),
+			net.NewVM(),
+		),
+	)
 
-	if err := dhcpServer.Expect("starting file server"); err != nil {
+	if _, err := serverVM.Console.ExpectString("starting file server"); err != nil {
 		t.Errorf("%s File server: %v", testutil.NowLog(), err)
 	}
-	if err := dhcpClient.Expect("Got DHCPv4 lease on eth0:"); err != nil {
+	if _, err := clientVM.Console.ExpectString("Got DHCPv4 lease on eth0:"); err != nil {
 		t.Errorf("%s Lease %v:", testutil.NowLog(), err)
 	}
-	if err := dhcpClient.Expect("Boot URI: tftp://192.168.0.1/pxelinux.0"); err != nil {
+	if _, err := clientVM.Console.ExpectString("Boot URI: tftp://192.168.0.1/pxelinux.0"); err != nil {
 		t.Errorf("%s Boot: %v", testutil.NowLog(), err)
 	}
 
 	// Boot menu should show the label from the pxelinux file.
-	if err := dhcpClient.Expect("01. some-random-kernel"); err != nil {
+	if _, err := clientVM.Console.ExpectString("01. some-random-kernel"); err != nil {
 		t.Errorf("%s Boot Menu: %v", testutil.NowLog(), err)
 	}
-	if err := dhcpClient.Expect("Attempting to boot"); err != nil {
+	if _, err := clientVM.Console.ExpectString("Attempting to boot"); err != nil {
 		t.Errorf("%s Boot Menu: %v", testutil.NowLog(), err)
 	}
-	if err := dhcpClient.Expect("Kernel: tftp://192.168.0.1/kernel"); err != nil {
+	if _, err := clientVM.Console.ExpectString("Kernel: tftp://192.168.0.1/kernel"); err != nil {
 		t.Errorf("%s parsed kernel: %v", testutil.NowLog(), err)
+	}
+
+	if err := serverVM.Kill(); err != nil {
+		t.Error(err)
+	}
+	serverVM.Wait()
+
+	if err := clientVM.Wait(); err != nil {
+		t.Errorf("Client VM Wait: %v", err)
 	}
 }
