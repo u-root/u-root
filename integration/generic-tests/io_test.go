d04ba4acfdc958d4adefb99dc01041def6880533
Chris Koch <chrisko@google.com>
2023-12-17 21:49:48 -0800

    Convert io test to new vmtest
    Signed-off-by: Chris Koch <chrisko@google.com>

--- integration/generic-tests/io_test.go
+++ integration/generic-tests/io_test.go
@@ -10,52 +10,60 @@ package integration
 import (
 	"fmt"
 	"testing"
+	"time"
 
-	"github.com/u-root/u-root/pkg/vmtest"
+	"github.com/hugelgupf/vmtest"
+	"github.com/hugelgupf/vmtest/qemu"
+	"github.com/u-root/u-root/pkg/uroot"
 )
 
 // TestIO tests the string "UART TEST" is written to the serial port on 0x3f8.
 func TestIO(t *testing.T) {
-	// TODO: support arm
-	if vmtest.TestArch() != "amd64" {
-		t.Skipf("test not supported on %s", vmtest.TestArch())
-	}
+	vmtest.SkipIfNotArch(t, qemu.ArchAMD64)
 
 	testCmds := []string{}
 	for _, b := range []byte("UART TEST\r\n") {
 		testCmds = append(testCmds, fmt.Sprintf("io outb 0x3f8 %d", b))
 	}
 
-	// Create the CPIO and start QEMU.
-	q, cleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		TestCmds: testCmds,
-	})
-	defer cleanup()
+	vm := vmtest.StartVMAndRunCmds(t, testCmds,
+		vmtest.WithMergedInitramfs(uroot.Opts{Commands: uroot.BusyBoxCmds(
+			"github.com/u-root/u-root/cmds/core/io",
+		)}),
+		vmtest.WithQEMUFn(qemu.WithVMTimeout(30*time.Second)),
+	)
 
-	if err := q.Expect("UART TEST"); err != nil {
-		t.Fatal(`expected "UART TEST", got error: `, err)
+	if _, err := vm.Console.ExpectString("UART TEST"); err != nil {
+		t.Error(`expected "UART TEST", got error: `, err)
+	}
+	if err := vm.Wait(); err != nil {
+		t.Errorf("Wait: %v", err)
 	}
 }
 
 // TestCMOS runs a series of cmos read and write commands and then checks if the changes to CMOS are reflected.
 func TestCMOS(t *testing.T) {
-	// TODO: support arm
-	if vmtest.TestArch() != "amd64" {
-		t.Skipf("test not supported on %s", vmtest.TestArch())
-	}
-	q, cleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		Name: "ShellScript",
-		TestCmds: []string{
-			"io cw 14 1 cr 14 cw 14 0 cr 14",
-			"shutdown -h",
-		},
-	})
-	defer cleanup()
+	vmtest.SkipIfNotArch(t, qemu.ArchAMD64)
 
-	if err := q.Expect("0x01"); err != nil {
-		t.Fatal(`expected "0x01", got error: `, err)
+	testCmds := []string{
+		"io cw 14 1 cr 14 cw 14 0 cr 14",
+		"shutdown -h",
 	}
-	if err := q.Expect("0x00"); err != nil {
-		t.Fatal(`expected "0x00", got error: `, err)
+	vm := vmtest.StartVMAndRunCmds(t, testCmds,
+		vmtest.WithMergedInitramfs(uroot.Opts{Commands: uroot.BusyBoxCmds(
+			"github.com/u-root/u-root/cmds/core/io",
+			"github.com/u-root/u-root/cmds/core/shutdown",
+		)}),
+		vmtest.WithQEMUFn(qemu.WithVMTimeout(30*time.Second)),
+	)
+
+	if _, err := vm.Console.ExpectString("0x01"); err != nil {
+		t.Error(`expected "0x01", got error: `, err)
+	}
+	if _, err := vm.Console.ExpectString("0x00"); err != nil {
+		t.Error(`expected "0x00", got error: `, err)
+	}
+	if err := vm.Wait(); err != nil {
+		t.Errorf("Wait: %v", err)
 	}
 }
