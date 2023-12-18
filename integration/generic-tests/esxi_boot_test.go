5c5784edb692860c95bd070f15b4b1f3e35280d7
Chris Koch <chrisko@google.com>
2023-12-17 21:43:41 -0800

    Convert ESXi boot test
    Signed-off-by: Chris Koch <chrisko@google.com>

--- integration/generic-tests/esxi_boot_test.go
+++ integration/generic-tests/esxi_boot_test.go
@@ -13,9 +13,10 @@ import (
 	"testing"
 	"time"
 
-	expect "github.com/google/goexpect"
-	"github.com/u-root/u-root/pkg/qemu"
-	"github.com/u-root/u-root/pkg/vmtest"
+	"github.com/Netflix/go-expect"
+	"github.com/hugelgupf/vmtest"
+	"github.com/hugelgupf/vmtest/qemu"
+	"github.com/u-root/u-root/pkg/uroot"
 )
 
 func TestESXi(t *testing.T) {
@@ -24,48 +25,52 @@ func TestESXi(t *testing.T) {
 		t.Skip("ESXi disk image is not present. Please set UROOT_ESXI_IMAGE= to an installed ESXi disk image.")
 	}
 
-	q, cleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		TestCmds: []string{
-			`esxiboot -d="/dev/sda" --append="vmkBootVerbose=TRUE vmbLog=TRUE debugLogToSerial=1 logPort=com1"`,
-		},
-		QEMUOpts: qemu.Options{
-			Devices: []qemu.Device{
-				qemu.IDEBlockDevice{File: img},
-				// If at some point we get virtio-net working
-				// again in ESXi, you may need to set num CPUs
-				// to 4.
-				qemu.ArbitraryArgs{"-smp", "2"},
-
-				// QEMU is not good enough to do ESXi without KVM. You'll get kernel panic.
-				qemu.ArbitraryArgs{"-enable-kvm"},
-
-				// IvyBridge is the lowest-common-denominator.
-				// ESXi 7.0 drops support for SandyBridge
-				// afaict.
-				qemu.ArbitraryArgs{"-cpu", "IvyBridge"},
-
-				// Min ESXi requirement is 4G of memory, but in
-				// ESXi 7.0 some plugins fail to load at 4G
-				// under memory pressure, and we never get to
-				// "Boot Successful"
-				qemu.ArbitraryArgs{"-m", "8192"},
-			},
-		},
-	})
-	defer cleanup()
-
-	if out, err := q.ExpectBatch([]expect.Batcher{
-		// Last Linux print.
-		&expect.BExp{R: "kexec_core: Starting new kernel"},
-		// First b.b00 ESXi first-stage kernel print
-		&expect.BExp{R: "Serial port initialized..."},
-		// ~thirdish k.b00 ESXi second-stage kernel print
-		&expect.BExp{R: "Parsing command line boot options"},
-		// When we can be confident it's done.
-		&expect.BExp{R: "Boot Successful"},
-	}, 2*time.Minute); err != nil {
-		t.Fatalf("VM output did not match expectations: %v", out)
+	testCmds := []string{
+		`esxiboot -d="/dev/sda" --append="vmkBootVerbose=TRUE vmbLog=TRUE debugLogToSerial=1 logPort=com1"`,
 	}
+	vm := vmtest.StartVMAndRunCmds(t, testCmds,
+		vmtest.WithMergedInitramfs(uroot.Opts{Commands: uroot.BusyBoxCmds(
+			"github.com/u-root/u-root/cmds/exp/esxiboot",
+		)}),
+		vmtest.WithQEMUFn(
+			qemu.WithVMTimeout(2*time.Minute),
+			qemu.IDEBlockDevice(img),
+			// If at some point we get virtio-net working
+			// again in ESXi, you may need to set num CPUs
+			// to 4.
+			qemu.ArbitraryArgs("-smp", "2"),
+
+			// QEMU is not good enough to do ESXi without KVM. You'll get kernel panic.
+			qemu.ArbitraryArgs("-enable-kvm"),
+
+			// IvyBridge is the lowest-common-denominator.
+			// ESXi 7.0 drops support for SandyBridge
+			// afaict.
+			qemu.ArbitraryArgs("-cpu", "IvyBridge"),
+
+			// Min ESXi requirement is 4G of memory, but in
+			// ESXi 7.0 some plugins fail to load at 4G
+			// under memory pressure, and we never get to
+			// "Boot Successful"
+			qemu.ArbitraryArgs("-m", "8192"),
+		),
+	)
+
+	if _, err := vm.Console.Expect(expect.All(
+		// First b.b00 ESXi first-stage kernel print
+		expect.String("Serial port initialized..."),
+		// ~thirdish k.b00 ESXi second-stage kernel print
+		expect.String("Parsing command line boot options"),
+		// When we can be confident it's done.
+		expect.String("Boot Successful"),
+	)); err != nil {
+		t.Errorf("VM output did not match expectations: %v", err)
+	}
+
+	if err := vm.Kill(); err != nil {
+		t.Errorf("Wait: %v", err)
+	}
+	_ = vm.Wait()
 }
 
 func TestESXiNVMe(t *testing.T) {
@@ -74,48 +79,51 @@ func TestESXiNVMe(t *testing.T) {
 		t.Skip("ESXi disk image is not present. Please set UROOT_ESXI_IMAGE= to an installed ESXi disk image.")
 	}
 
-	q, cleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		TestCmds: []string{
-			`esxiboot -d="/dev/nvme0n1" --append="vmkBootVerbose=TRUE vmbLog=TRUE debugLogToSerial=1 logPort=com1"`,
-		},
-		QEMUOpts: qemu.Options{
-			Devices: []qemu.Device{
-				qemu.ArbitraryArgs{"-device", "nvme,drive=NVME1,serial=nvme-1"},
-				qemu.ArbitraryArgs{"-drive", fmt.Sprintf("file=%s,if=none,id=NVME1", img)},
-
-				// If at some point we get virtio-net working
-				// again in ESXi, you may need to set num CPUs
-				// to 4.
-				qemu.ArbitraryArgs{"-smp", "2"},
-
-				// QEMU is not good enough to do ESXi without KVM. You'll get kernel panic.
-				qemu.ArbitraryArgs{"-enable-kvm"},
-
-				// IvyBridge is the lowest-common-denominator.
-				// ESXi 7.0 drops support for SandyBridge
-				// afaict.
-				qemu.ArbitraryArgs{"-cpu", "IvyBridge"},
-
-				// Min ESXi requirement is 4G of memory, but in
-				// ESXi 7.0 some plugins fail to load at 4G
-				// under memory pressure, and we never get to
-				// "Boot Successful"
-				qemu.ArbitraryArgs{"-m", "8192"},
-			},
-		},
-	})
-	defer cleanup()
-
-	if out, err := q.ExpectBatch([]expect.Batcher{
-		// Last Linux print.
-		&expect.BExp{R: "kexec_core: Starting new kernel"},
-		// First b.b00 ESXi first-stage kernel print
-		&expect.BExp{R: "Serial port initialized..."},
-		// ~thirdish k.b00 ESXi second-stage kernel print
-		&expect.BExp{R: "Parsing command line boot options"},
-		// When we can be confident it's done.
-		&expect.BExp{R: "Boot Successful"},
-	}, 2*time.Minute); err != nil {
-		t.Fatalf("VM output did not match expectations: %v", out)
+	testCmds := []string{
+		`esxiboot -d="/dev/nvme0n1" --append="vmkBootVerbose=TRUE vmbLog=TRUE debugLogToSerial=1 logPort=com1"`,
 	}
+	vm := vmtest.StartVMAndRunCmds(t, testCmds,
+		vmtest.WithMergedInitramfs(uroot.Opts{Commands: uroot.BusyBoxCmds(
+			"github.com/u-root/u-root/cmds/exp/esxiboot",
+		)}),
+		vmtest.WithQEMUFn(
+			qemu.WithVMTimeout(2*time.Minute),
+			qemu.ArbitraryArgs("-device", "nvme,drive=NVME1,serial=nvme-1"),
+			qemu.ArbitraryArgs("-drive", fmt.Sprintf("file=%s,if=none,id=NVME1", img)),
+			// If at some point we get virtio-net working
+			// again in ESXi, you may need to set num CPUs
+			// to 4.
+			qemu.ArbitraryArgs("-smp", "2"),
+
+			// QEMU is not good enough to do ESXi without KVM. You'll get kernel panic.
+			qemu.ArbitraryArgs("-enable-kvm"),
+
+			// IvyBridge is the lowest-common-denominator.
+			// ESXi 7.0 drops support for SandyBridge
+			// afaict.
+			qemu.ArbitraryArgs("-cpu", "IvyBridge"),
+
+			// Min ESXi requirement is 4G of memory, but in
+			// ESXi 7.0 some plugins fail to load at 4G
+			// under memory pressure, and we never get to
+			// "Boot Successful"
+			qemu.ArbitraryArgs("-m", "8192"),
+		),
+	)
+
+	if _, err := vm.Console.Expect(expect.All(
+		// First b.b00 ESXi first-stage kernel print
+		expect.String("Serial port initialized..."),
+		// ~thirdish k.b00 ESXi second-stage kernel print
+		expect.String("Parsing command line boot options"),
+		// When we can be confident it's done.
+		expect.String("Boot Successful"),
+	)); err != nil {
+		t.Errorf("VM output did not match expectations: %v", err)
+	}
+
+	if err := vm.Kill(); err != nil {
+		t.Errorf("Wait: %v", err)
+	}
+	_ = vm.Wait()
 }
