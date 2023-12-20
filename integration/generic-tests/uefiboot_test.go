6dc1044a6279dd09604811ddcfbfeff4edfb4b64
Chris Koch <chrisko@google.com>
2023-12-19 20:15:14 -0800

    Convert uefiboot test to vmtest
    Signed-off-by: Chris Koch <chrisko@google.com>

--- integration/generic-tests/uefiboot_test.go
+++ integration/generic-tests/uefiboot_test.go
@@ -14,16 +14,15 @@ import (
 	"testing"
 	"time"
 
-	expect "github.com/google/goexpect"
-	"github.com/u-root/u-root/pkg/qemu"
-	"github.com/u-root/u-root/pkg/vmtest"
+	"github.com/Netflix/go-expect"
+	"github.com/hugelgupf/vmtest"
+	"github.com/hugelgupf/vmtest/qemu"
+	"github.com/u-root/u-root/pkg/uroot"
 )
 
 // TestUefiboot tests uefiboot commmands to boot to uefishell.
 func TestUefiBoot(t *testing.T) {
-	if vmtest.TestArch() != "amd64" {
-		t.Skipf("test not supported on %s", vmtest.TestArch())
-	}
+	vmtest.SkipIfNotArch(t, qemu.ArchAMD64)
 
 	payload := "UEFIPAYLOAD.fd"
 	src := fmt.Sprintf("/home/circleci/%v", payload)
@@ -35,27 +34,35 @@ func TestUefiBoot(t *testing.T) {
 		t.Skipf("UEFI payload image is not found: %s\n Usage: uefiboot <payload>", src)
 	}
 
-	// Create the CPIO and start QEMU.
-	q, cleanup := vmtest.QEMUTest(t, &vmtest.Options{
-		TestCmds: []string{"uefiboot /dev/sda"},
-		QEMUOpts: qemu.Options{
-			Devices: []qemu.Device{
-				qemu.IDEBlockDevice{File: src},
-				qemu.ArbitraryArgs{"-machine", "q35"},
-				qemu.ArbitraryArgs{"-m", "4096"},
-			},
-		},
-	})
-	defer cleanup()
+	testCmds := []string{
+		"uefiboot /dev/sda",
+	}
+	vm := vmtest.StartVMAndRunCmds(t, testCmds,
+		vmtest.WithMergedInitramfs(uroot.Opts{Commands: uroot.BusyBoxCmds(
+			"github.com/u-root/u-root/cmds/exp/uefiboot",
+		)}),
+		vmtest.WithQEMUFn(
+			qemu.WithVMTimeout(2*time.Minute),
+			qemu.IDEBlockDevice(src),
+			qemu.ArbitraryArgs("-machine", "q35"),
+			qemu.ArbitraryArgs("-m", "4096"),
+		),
+	)
 
 	// Edk2 debug mode will print PROGRESS CODE. We will want to make sure
 	// payload is booting to uefi shell correctly.
-	if out, err := q.ExpectBatch([]expect.Batcher{
+	if _, err := vm.Console.Expect(expect.All(
 		// Finish booting.
-		&expect.BExp{R: "PROGRESS CODE: V02070003"},
+		expect.String("PROGRESS CODE: V02070003"),
 		// Last code before booting to UEFI Shell
-		&expect.BExp{R: "PROGRESS CODE: V03058001"},
-	}, 50*time.Second); err != nil {
-		t.Fatalf("VM output did not match expectations: %v", out)
+		expect.String("PROGRESS CODE: V03058001"),
+	)); err != nil {
+		t.Errorf("VM output did not match expectations: %v", err)
 	}
+
+	if err := vm.Kill(); err != nil {
+		t.Errorf("Wait: %v", err)
+	}
+	_ = vm.Wait()
+
 }
