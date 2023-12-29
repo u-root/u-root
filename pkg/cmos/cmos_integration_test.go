a06f8c27d05a0964a4f91a7b1da647b90452b367
Chris Koch <chrisko@google.com>
2023-12-28 17:46:32 -0800

    Convert cmos test to new vmtest
    Signed-off-by: Chris Koch <chrisko@google.com>

--- pkg/cmos/cmos_integration_test.go
+++ pkg/cmos/cmos_integration_test.go
@@ -6,20 +6,16 @@ package cmos
 
 import (
 	"testing"
+	"time"
 
-	"github.com/u-root/u-root/pkg/qemu"
-	"github.com/u-root/u-root/pkg/vmtest"
+	"github.com/hugelgupf/vmtest"
+	"github.com/hugelgupf/vmtest/qemu"
 )
 
-// Copyright 2021 the u-root Authors. All rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 func TestIntegration(t *testing.T) {
-	o := &vmtest.Options{
-		QEMUOpts: qemu.Options{
-			Devices: []qemu.Device{},
-		},
-	}
-	vmtest.GolangTest(t, []string{"github.com/u-root/u-root/pkg/cmos"}, o)
+	vmtest.SkipIfNotArch(t, qemu.ArchAMD64)
+
+	vmtest.RunGoTestsInVM(t, []string{"github.com/u-root/u-root/pkg/cmos"},
+		vmtest.WithVMOpt(vmtest.WithQEMUFn(qemu.WithVMTimeout(time.Minute))),
+	)
 }
