cdb739e79581db4e2c833b493bfb84b5c9f8efae
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2024-01-04 19:55:13 +0200

    WIP ping icmp
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/ping/ping.go
+++ cmds/core/ping/ping.go
@@ -21,9 +21,10 @@
 package main
 
 import (
-	"encoding/binary"
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"math"
 	"net"
@@ -31,158 +32,142 @@ import (
 	"time"
 
 	"github.com/u-root/u-root/pkg/uroot/util"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 )
 
 const usage = "ping [-V] [-6] [-c count] [-i interval] [-s packetsize] [-w deadline] [-a audible] destination"
 
-var (
-	net6       = flag.Bool("6", false, "use ipv4 (means ip4:icmp) or 6 (ip6:ipv6-icmp)")
-	packetSize = flag.Int("s", 64, "Data size")
-	iter       = flag.Uint64("c", math.MaxUint64, "# iterations")
-	intv       = flag.Int("i", 1000, "interval in milliseconds")
-	wtf        = flag.Int("w", 100, "wait time in milliseconds")
-	audible    = flag.Bool("a", false, "Audible rings a bell when a packet is received")
-)
-
-const (
-	ICMP_TYPE_ECHO_REQUEST             = 8
-	ICMP_TYPE_ECHO_REPLY               = 0
-	ICMP_ECHO_REPLY_HEADER_IPV4_OFFSET = 20
-)
-
-const (
-	ICMP6_TYPE_ECHO_REQUEST             = 128
-	ICMP6_TYPE_ECHO_REPLY               = 129
-	ICMP6_ECHO_REPLY_HEADER_IPV6_OFFSET = 40
-)
-
-type Ping struct {
-	dial func(string, string) (net.Conn, error)
+type params struct {
+	packetSize int
+	intv       int
+	wtf        int
+	iter       uint64
+	host       string
+	net6       bool
+	audible    bool
 }
 
-func New() *Ping {
-	return &Ping{
-		dial: net.Dial,
-	}
+type cmd struct {
+	stdout io.Writer
+	conn   net.PacketConn
+	params
 }
 
-func cksum(bs []byte) uint16 {
-	sum := uint32(0)
-
-	for k := 0; k < len(bs)/2; k++ {
-		sum += uint32(bs[k*2]) << 8
-		sum += uint32(bs[k*2+1])
+func command(stdin io.Writer, p params) (*cmd, error) {
+	netname, address := "ip4:icmp", "0.0.0.0"
+	if p.net6 {
+		netname, address = "ip6:ipv6-icmp", "::1"
 	}
-	if len(bs)%2 != 0 {
-		sum += uint32(bs[len(bs)-1]) << 8
-	}
-	sum = (sum >> 16) + (sum & 0xffff)
-	sum = (sum >> 16) + (sum & 0xffff)
-	if sum == 0xffff {
-		sum = 0
-	}
-
-	return ^uint16(sum)
-}
-
-func (p *Ping) ping1(net6 bool, host string, i uint64, waitFor time.Duration) (string, error) {
-	netname := "ip4:icmp"
-	// todo: just figure out if it's an ip6 address and go from there.
-	if net6 {
-		netname = "ip6:ipv6-icmp"
-	}
-	c, err := p.dial(netname, host)
+	conn, err := icmp.ListenPacket(netname, address)
 	if err != nil {
-		return "", fmt.Errorf("net.Dial(%v %v) failed: %v", netname, host, err)
-	}
-	defer c.Close()
-
-	if net6 {
-		ipc := c.(*net.IPConn)
-		if err := setupICMPv6Socket(ipc); err != nil {
-			return "", fmt.Errorf("failed to set up the ICMPv6 connection: %w", err)
-		}
+		return nil, fmt.Errorf("can't setup %s socket on %s: %v", netname, address, err)
 	}
 
-	// Send ICMP Echo Request
-	c.SetDeadline(time.Now().Add(waitFor))
-	msg := make([]byte, *packetSize)
-	if net6 {
-		msg[0] = ICMP6_TYPE_ECHO_REQUEST
-	} else {
-		msg[0] = ICMP_TYPE_ECHO_REQUEST
-	}
-	msg[1] = 0
-	binary.BigEndian.PutUint16(msg[6:], uint16(i))
-	binary.BigEndian.PutUint16(msg[4:], uint16(i>>16))
-	binary.BigEndian.PutUint16(msg[2:], cksum(msg))
-	if _, err := c.Write(msg[:]); err != nil {
-		return "", fmt.Errorf("write failed: %v", err)
-	}
-
-	// Get ICMP Echo Reply
-	c.SetDeadline(time.Now().Add(waitFor))
-	rmsg := make([]byte, *packetSize+256)
-	before := time.Now()
-	amt, err := c.Read(rmsg[:])
-	if err != nil {
-		return "", fmt.Errorf("read failed: %v", err)
-	}
-	latency := time.Since(before)
-	if !net6 {
-		rmsg = rmsg[ICMP_ECHO_REPLY_HEADER_IPV4_OFFSET:]
-	}
-	if net6 {
-		if rmsg[0] != ICMP6_TYPE_ECHO_REPLY {
-			return "", fmt.Errorf("bad ICMPv6 echo reply type, got %d, want %d", rmsg[0], ICMP6_TYPE_ECHO_REPLY)
-		}
-	} else {
-		if rmsg[0] != ICMP_TYPE_ECHO_REPLY {
-			return "", fmt.Errorf("bad ICMP echo reply type, got %d, want %d", rmsg[0], ICMP_TYPE_ECHO_REPLY)
-		}
-	}
-	cks := binary.BigEndian.Uint16(rmsg[2:])
-	binary.BigEndian.PutUint16(rmsg[2:], 0)
-	// only validate the checksum for IPv4. For IPv6 this *should* be done by the
-	// TCP stack (and do we need to validate the checksum anyway?)
-	if !net6 && cks != cksum(rmsg) {
-		return "", fmt.Errorf("bad ICMP checksum: %v (expected %v)", cks, cksum(rmsg))
-	}
-	id := binary.BigEndian.Uint16(rmsg[4:])
-	seq := binary.BigEndian.Uint16(rmsg[6:])
-	rseq := uint64(id)<<16 + uint64(seq)
-	if rseq != i {
-		return "", fmt.Errorf("wrong sequence number %v (expected %v)", rseq, i)
-	}
-
-	return fmt.Sprintf("%d bytes from %v: icmp_seq=%v, time=%v", amt, host, i, latency), nil
+	return &cmd{stdin, conn, p}, nil
 }
 
-func ping(host string) error {
-	if *packetSize < 8 {
-		return fmt.Errorf("packet size too small (must be >= 8): %v", *packetSize)
+func (c *cmd) run() error {
+	defer c.conn.Close()
+	if c.packetSize < 8 {
+		return fmt.Errorf("packet size too small (must be >= 8): %v", c.packetSize)
 	}
 
-	interval := time.Duration(*intv)
-	p := New()
-	// ping needs to run forever if count is not specified, so default value is MaxUint64
-	waitFor := time.Duration(*wtf) * time.Millisecond
-	for i := uint64(0); i < *iter; i++ {
-		msg, err := p.ping1(*net6, host, i+1, waitFor)
+	network := "ip4"
+	if c.net6 {
+		network = "ip6"
+	}
+
+	addr, err := net.ResolveIPAddr(network, c.host)
+	if err != nil {
+		return fmt.Errorf("failed to resolve address: %v", err)
+	}
+
+	interval := time.Duration(c.intv)
+	waitFor := time.Duration(c.wtf) * time.Millisecond
+	for i := uint64(0); i < c.iter; i++ {
+		msg, err := c.ping(addr, i+1, waitFor)
 		if err != nil {
 			return fmt.Errorf("ping failed: %v", err)
 		}
-		if *audible {
+		if c.audible {
 			msg = "\a" + msg
 		}
-		log.Print(msg)
+		fmt.Fprintf(c.stdout, "%s\n", msg)
 		time.Sleep(time.Millisecond * interval)
 	}
 
 	return nil
 }
 
+func (c *cmd) ping(addr *net.IPAddr, i uint64, waitFor time.Duration) (string, error) {
+	c.conn.SetDeadline(time.Now().Add(waitFor))
+
+	var echoRequestType icmp.Type = ipv4.ICMPTypeEcho
+	if c.net6 {
+		echoRequestType = ipv6.ICMPTypeEchoRequest
+	}
+
+	wm := icmp.Message{Type: echoRequestType, Code: 0, Body: &icmp.Echo{
+		ID:   os.Getpid() & 0xffff,
+		Seq:  int(i),
+		Data: bytes.Repeat([]byte{1}, c.packetSize)},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		return "", fmt.Errorf("icmp.Message.Marshal failed: %v", err)
+	}
+
+	startTime := time.Now()
+	_, err = c.conn.WriteTo(wb, addr)
+	if err != nil {
+		return "", fmt.Errorf("conn.Write failed: %v", err)
+	}
+
+	rb := make([]byte, 1500)
+	n, _, err := c.conn.ReadFrom(rb)
+	if err != nil {
+		return "", fmt.Errorf("conn.Read failed: %v", err)
+	}
+
+	latency := time.Since(startTime)
+
+	var echoReplyType icmp.Type = ipv4.ICMPTypeEchoReply
+	if c.net6 {
+		echoReplyType = ipv6.ICMPTypeEchoReply
+	}
+
+	msg, err := icmp.ParseMessage(echoReplyType.Protocol(), rb[:n])
+	if err != nil {
+		return "", fmt.Errorf("icmp.ParseMessage failed: %v", err)
+	}
+
+	echoReply, ok := msg.Body.(*icmp.Echo)
+	if !ok {
+		return "", fmt.Errorf("got %+v; want echo reply", msg)
+	}
+
+	if echoReply.ID != os.Getpid()&0xffff {
+		return "", fmt.Errorf("got id %v; want %v", echoReply.ID, os.Getpid()&0xffff)
+	}
+	if echoReply.Seq != int(i) {
+		return "", fmt.Errorf("got seq %v; want %v", echoReply.Seq, i)
+	}
+
+	return fmt.Sprintf("%d bytes from %v: icmp_seq=%v time=%v", n, c.host, i, latency), nil
+}
+
 func main() {
+	var (
+		net6       = flag.Bool("6", false, "use ipv4 (means ip4:icmp) or 6 (ip6:ipv6-icmp)")
+		packetSize = flag.Int("s", 56, "Data size")
+		iter       = flag.Uint64("c", math.MaxUint64, "# iterations")
+		intv       = flag.Int("i", 1000, "interval in milliseconds")
+		wtf        = flag.Int("w", 100, "wait time in milliseconds")
+		audible    = flag.Bool("a", false, "Audible rings a bell when a packet is received")
+	)
+
 	flag.Usage = util.Usage(flag.Usage, usage)
 	flag.Parse()
 	// options without parameters (right now just: -hV)
@@ -191,7 +176,11 @@ func main() {
 		os.Exit(1)
 	}
 	host := flag.Args()[0]
-	if err := ping(host); err != nil {
+	cmd, err := command(os.Stdout, params{*packetSize, *intv, *wtf, *iter, host, *net6, *audible})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.run(); err != nil {
 		log.Fatal(err)
 	}
 }
