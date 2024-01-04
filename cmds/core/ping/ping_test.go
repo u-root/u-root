25faeb4566c826ea11bc725f291af8f46d893cc5
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2024-01-04 19:55:13 +0200

    WIP ping icmp
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/ping/ping_test.go
+++ cmds/core/ping/ping_test.go
@@ -5,222 +5,199 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"net"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/hugelgupf/vmtest/guest"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
-type myConn struct {
-	net.IPConn
-	testRun string
+type testConn struct {
+	lastMessage []byte
 }
 
-func (M *myConn) Read(b []byte) (int, error) {
-	if M.testRun == "error in read" {
-		return 0, fmt.Errorf("err")
+func (tc *testConn) ReadFrom(b []byte) (int, net.Addr, error) {
+	m, err := icmp.ParseMessage(ipv4.ICMPTypeEcho.Protocol(), tc.lastMessage)
+	if err != nil {
+		return 0, nil, err
 	}
-	if M.testRun == "rmsg[0] != ICMP_TYPE_ECHO_REPLY" {
-		b[20] = 0xff
+
+	body := m.Body.(*icmp.Echo)
+
+	respone := icmp.Message{
+		Type: ipv4.ICMPTypeEchoReply,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID:   body.ID,
+			Seq:  body.Seq,
+			Data: body.Data,
+		},
 	}
-	if M.testRun == "!net6 && cks != cksum(rmsg)" {
-		return 0, nil
+
+	resp, err := respone.Marshal(nil)
+	if err != nil {
+		return 0, nil, err
 	}
-	b[22] = 0xff
-	b[23] = 0xff
-	return 0, nil
+
+	n := copy(b, resp)
+	return n, nil, nil
 }
 
-func (M *myConn) Write(b []byte) (int, error) {
-	if M.testRun == "error in write" {
-		return 0, fmt.Errorf("err")
-	}
-	return 0, nil
+func (tc *testConn) WriteTo(b []byte, addr net.Addr) (int, error) {
+	tc.lastMessage = b
+	return len(b), nil
 }
 
-// Test cksum
-func TestCkSum(t *testing.T) {
-	for _, tt := range []struct {
-		name  string
-		input []byte
-		want  uint16
-	}{
-		{
-			name:  "ultimate test, triggers the sum == 0xffff and len(bs)%2 !=0",
-			input: []byte{0xff, 0xff, 0x00},
-			want:  65535,
-		},
-		{
-			name:  "another input",
-			input: []byte{0xfe, 0xfe, 0xfe, 0xfe},
-			want:  514,
-		},
-		{
-			name:  "empty input",
-			input: []byte{},
-			want:  65535,
-		},
-	} {
-		if got := cksum(tt.input); got != tt.want {
-			t.Errorf("cksum() = '%d', want: '%d'", got, tt.want)
+func (tc *testConn) Close() error {
+	return nil
+}
+
+func (tc *testConn) LocalAddr() net.Addr {
+	return nil
+}
+
+func (tc *testConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (tc *testConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (tc *testConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+type pingOutputLine struct {
+	addr    string
+	size    int
+	seq     int
+	audible bool
+}
+
+func parsePingLines(t *testing.T, output []byte) []pingOutputLine {
+	t.Helper()
+	var lines []pingOutputLine
+
+	for _, line := range bytes.Split(output, []byte("\n")) {
+		if len(line) == 0 {
+			continue
 		}
-	}
 
-}
-
-// Test Ping1
-func TestPing1(t *testing.T) {
-	for _, tt := range []struct {
-		name    string
-		p       Ping
-		net6    bool
-		host    string
-		i       uint64
-		waitFor time.Duration
-		want    error
-	}{
-		{
-			name: "ping1 without error",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return &myConn{testRun: "ping1 without error"}, nil
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       0,
-			waitFor: time.Minute,
-			want:    fmt.Errorf(""),
-		},
-		{
-			name: "error in dial",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return nil, fmt.Errorf("err")
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       1,
-			waitFor: time.Minute,
-			want:    fmt.Errorf("net.Dial(%v %v) failed: %v", "ip4:icmp", "test.com", fmt.Errorf("err")),
-		},
-		{
-			name: "error in write",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return &myConn{testRun: "error in write"}, nil
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       0,
-			waitFor: time.Minute,
-			want:    fmt.Errorf("write failed: err"),
-		},
-		{
-			name: "error in read",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return &myConn{testRun: "error in read"}, nil
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       0,
-			waitFor: time.Minute,
-			want:    fmt.Errorf("read failed: err"),
-		},
-		{
-			name: "rmsg[0] != ICMP_TYPE_ECHO_REPLY",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return &myConn{testRun: "rmsg[0] != ICMP_TYPE_ECHO_REPLY"}, nil
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       0,
-			waitFor: time.Minute,
-			want:    fmt.Errorf("bad ICMP echo reply type, got %d, want %d", 0xff, ICMP_TYPE_ECHO_REPLY),
-		},
-		{
-			name: "!net6 && cks != cksum(rmsg)",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return &myConn{testRun: "!net6 && cks != cksum(rmsg)"}, nil
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       0,
-			waitFor: time.Minute,
-			want:    fmt.Errorf("bad ICMP checksum: %v (expected %v)", 0, 65535),
-		},
-		{
-			name: "rseq != i",
-			p: Ping{
-				dial: func(s1, s2 string) (net.Conn, error) {
-					return &myConn{testRun: "rseq != i"}, nil
-				},
-			},
-			net6:    false,
-			host:    "test.com",
-			i:       1,
-			waitFor: time.Minute,
-			want:    fmt.Errorf("wrong sequence number %v (expected %v)", 0, 1),
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			if _, got := tt.p.ping1(tt.net6, tt.host, tt.i, tt.waitFor); got != nil {
-				if got.Error() != tt.want.Error() {
-					t.Errorf("ping1() = '%s', want: '%s'", got, tt.want)
-				}
+		var pl pingOutputLine
+		sp := strings.Split(string(line), " ")
+		if sp[0][0] == '\a' {
+			pl.audible = true
+			size, err := strconv.Atoi(sp[0][1:])
+			if err != nil {
+				t.Fatalf("size atoi failed: %v", err)
 			}
-		})
+			pl.size = size
+		} else {
+			size, err := strconv.Atoi(sp[0])
+			if err != nil {
+				t.Fatalf("size atoi failed: %v", err)
+			}
+			pl.size = size
+		}
+
+		pl.addr = sp[3][:len(sp[3])-1]
+		sp = strings.Split(sp[4], "=")
+		sqn, err := strconv.Atoi(sp[1])
+		if err != nil {
+			t.Fatalf("address atoi failed: %v", err)
+		}
+		pl.seq = sqn
+
+		lines = append(lines, pl)
 	}
+
+	return lines
 }
 
-// Test refactored ping()
 func TestPing(t *testing.T) {
-	for _, tt := range []struct {
-		name       string
-		packetSize int
-		audible    bool
-		host       string
-		waitFor    time.Duration
-		want       error
-	}{
-		{
-			name:       "packetSize < 8",
-			packetSize: 7,
-			host:       "test.com",
+	var paConn net.PacketConn = &testConn{}
+	paConn.Close()
+
+	stdout := &bytes.Buffer{}
+	cmd := &cmd{
+		stdout: stdout,
+		conn:   paConn,
+		params: params{
+			host:       "1.1.1.1",
+			packetSize: 56,
+			intv:       1000,
+			wtf:        100,
+			iter:       1,
+			net6:       false,
 			audible:    true,
-			waitFor:    time.Minute,
-			want:       fmt.Errorf("packet size too small (must be >= 8): %v", 7),
 		},
-		{
-			name:       "ping with error",
-			packetSize: 8,
-			host:       "",
-			audible:    true,
-			waitFor:    time.Minute,
-			want:       fmt.Errorf("ping failed: net.Dial(ip4:icmp ) failed: dial ip4:icmp: missing address"),
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			*packetSize = tt.packetSize
-			*audible = tt.audible
-			if got := ping(tt.host); got != nil {
-				if got.Error() != tt.want.Error() {
-					t.Errorf("ping() = '%s', want: '%s'", got, tt.want)
-				}
-			}
-		})
+	}
+
+	err := cmd.run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	lines := parsePingLines(t, stdout.Bytes())
+	if len(lines) != 1 {
+		t.Errorf("expected 1 line, got %d", len(lines))
+	}
+
+	if lines[0].size != 64 {
+		t.Errorf("expected size 64 (56 + header), got %d", lines[0].size)
+	}
+	if !lines[0].audible {
+		t.Errorf("expected audible, got %v", lines[0].audible)
+	}
+	if lines[0].seq != 1 {
+		t.Errorf("expected seq 1, got %d", lines[0].seq)
+	}
+	if lines[0].addr != "1.1.1.1" {
+		t.Errorf("expected addr, got %s", lines[0].addr)
 	}
 }
 
-// This test gets the coverage higher and does not test any functionality.
-func TestNew(t *testing.T) {
-	_ = New()
+func TestRawPing(t *testing.T) {
+	guest.SkipIfNotInVM(t)
+	stdout := &bytes.Buffer{}
+	cmd, err := command(stdout, params{
+		packetSize: 56,
+		host:       "127.0.0.1",
+		intv:       1000,
+		wtf:        100,
+		iter:       1,
+	})
+	if err != nil {
+		t.Errorf("command() failed: %v", err)
+	}
+
+	err = cmd.run()
+	if err != nil {
+		t.Errorf("run() failed: %v", err)
+	}
+
+	lines := parsePingLines(t, stdout.Bytes())
+
+	if len(lines) != 1 {
+		t.Errorf("expected 1 line, got %d", len(lines))
+	}
+
+	if lines[0].size != 64 {
+		t.Errorf("expected size 64 (56 + header), got %d", lines[0].size)
+	}
+	if lines[0].audible {
+		t.Errorf("expected no audible, got %v", lines[0].audible)
+	}
+	if lines[0].seq != 1 {
+		t.Errorf("expected seq 1, got %d", lines[0].seq)
+	}
+	if lines[0].addr != "127.0.0.1" {
+		t.Errorf("expected addr, got %s", lines[0].addr)
+	}
 }
