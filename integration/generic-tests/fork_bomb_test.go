5704e54f263d09f3540db98d16045f481e5b4e12
Chris Koch <chrisko@google.com>
2024-03-02 20:08:14 +0000

    Fork bomb regression test
    Signed-off-by: Chris Koch <chrisko@google.com>

---
+++ integration/generic-tests/fork_bomb_test.go
@@ -0,0 +1,54 @@
+// Copyright 2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !race
+// +build !race
+
+package integration
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/hugelgupf/vmtest/qemu"
+	"github.com/hugelgupf/vmtest/qemu/quimage"
+	"github.com/u-root/mkuimage/uimage"
+)
+
+// Regression test for #2938.
+func TestGoshRegression2938(t *testing.T) {
+	qemu.SkipIfNotArch(t, qemu.ArchAMD64, qemu.ArchArm64)
+
+	script := `#!/bin/sh
+		echo "bar"
+	`
+	scriptPath := filepath.Join(t.TempDir(), "script.sh")
+	_ = os.WriteFile(scriptPath, []byte(script), 0o777)
+
+	vm := qemu.StartT(t, "vm", qemu.ArchUseEnvv,
+		quimage.WithUimageT(t,
+			uimage.WithInit("init"),
+			uimage.WithUinit("/script.sh"),
+			uimage.WithShell("gosh"),
+			uimage.WithBusyboxCommands(
+				"github.com/u-root/u-root/cmds/core/init",
+				"github.com/u-root/u-root/cmds/core/sync",
+				"github.com/u-root/u-root/cmds/core/echo",
+			),
+			uimage.WithBinaryCommands(
+				"github.com/u-root/u-root/cmds/core/gosh",
+			),
+			uimage.WithFiles(
+				fmt.Sprintf("%s:script.sh", scriptPath),
+			),
+		),
+		qemu.WithVMTimeout(time.Minute),
+		qemu.ArbitraryArgs("-m", "512"),
+	)
+
+	_ = vm.Wait()
+}
