014fe717f1f4fbf4a0ec09324a0c493f4a980889
Christopher Meis <christopher.meis@9elements.com>
2024-05-17 10:17:42 +0200

    * Add processing of output format flags * Add structure and member-functions to handle the different flag mechanism
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- cmds/core/netstat/netstat.go
+++ cmds/core/netstat/netstat.go
@@ -15,12 +15,45 @@ import (
 var (
 	interfacesFlag = flag.BoolP("interfaces", "i", false, "display interface table")
 	ifFlag         = flag.StringP("interface", "I", "", "Display interface table for interface <if>")
-
+	// Format flags
+	wideFlag      = flag.BoolP("wide", "W", false, "don't truncate IP addresses")
+	numericFlag   = flag.BoolP("numeric", "n", false, "don't resolve names")
+	numHostFlag   = flag.Bool("numeric-hosts", false, "don't resolve host names")
+	numPortsFlag  = flag.Bool("numeric-ports", false, "don't resolve port names")
+	numUsersFlag  = flag.Bool("numeric-users", false, "don't resolve user names")
+	symbolicFlag  = flag.BoolP("symbolic", "N", false, "resolve hardware names")
+	extendFlag    = flag.BoolP("extend", "e", false, "display other/more information")
+	programsFlag  = flag.BoolP("programs", "p", false, "display PID/Program name for sockets")
+	timersFlag    = flag.BoolP("timers", "o", false, "display timers")
 	continFlag    = flag.BoolP("continuous", "c", false, "continuous listing")
 )
 
 func evalFlags() error {
 	flag.Parse()
+	// Evaluate numeric flags
+	if *numericFlag {
+		*numHostFlag = true
+		*numPortsFlag = true
+		*numUsersFlag = true
+	}
+
+	// Set up format flags for route listing and socket listing
+	outflags := netstat.FmtFlags{
+		Extend:    *extendFlag,
+		Wide:      *wideFlag,
+		NumHosts:  *numHostFlag,
+		NumPorts:  *numPortsFlag,
+		NumUsers:  *numUsersFlag,
+		ProgNames: *programsFlag,
+		Timer:     *timersFlag,
+		Symbolic:  *symbolicFlag,
+	}
+
+	// Set up output generator for route and socket listing
+	outfmts, err := netstat.NewOutput(outflags)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if *interfacesFlag {
 		if err := netstat.PrintInterfaceTable(*ifFlag, *continFlag); err != nil {
