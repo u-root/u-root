afb8ef252ea14d71db2a3ddde11b9e58253aa2a6
Christopher Meis <christopher.meis@9elements.com>
2024-06-03 06:23:17 +0200

    Add cmds/core/tftp
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ cmds/core/tftp/client.go
@@ -0,0 +1,73 @@
+// Copyright 2012-2024 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"io"
+
+	"pack.ag/tftp"
+)
+
+type Response interface {
+	Read([]byte) (int, error)
+	Size() (int64, error)
+}
+
+type ClientIf interface {
+	Put(string, io.Reader, int64) error
+	Get(string) (Response, error)
+}
+
+type ClientMock struct{}
+
+type DummyResp struct{}
+
+func (d *DummyResp) Size() (int64, error) {
+	return 0, nil
+}
+
+func (d *DummyResp) Read(b []byte) (int, error) {
+	return 0, nil
+}
+
+func (c *ClientMock) Get(url string) (Response, error) {
+	return &DummyResp{}, nil
+}
+
+func (c *ClientMock) Put(url string, r io.Reader, size int64) error {
+	return nil
+}
+
+type Client struct {
+	*tftp.Client
+}
+
+type RealResponse struct {
+	*tftp.Response
+}
+
+func (r *RealResponse) Read(b []byte) (int, error) {
+	return r.Response.Read(b)
+}
+
+func (r *RealResponse) Size() (int64, error) {
+	return r.Response.Size()
+}
+
+func NewClient(ccfg *clientCfg) (*Client, error) {
+	c, err := tftp.NewClient(tftp.ClientMode(ccfg.mode), ccfg.rexmt, ccfg.timeout)
+	return &Client{
+		Client: c,
+	}, err
+}
+
+func (c *Client) Get(url string) (Response, error) {
+	r, err := c.Client.Get(url)
+	return &RealResponse{r}, err
+}
+
+func (c *Client) Put(url string, r io.Reader, size int64) error {
+	return c.Client.Put(url, r, size)
+}
