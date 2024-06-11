8fdc246005cef53592cb2cbca419eddabe5dd376
Jens Drenhaus <jens.drenhaus@9elements.com>
2024-06-11 18:19:19 +0200

    cmds/core/shasum: use flag package
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- cmds/core/shasum/shasum.go
+++ cmds/core/shasum/shasum.go
@@ -8,25 +8,17 @@ import (
 	"bufio"
 	"crypto/sha1"
 	"crypto/sha256"
+	"flag"
 	"fmt"
 	"hash"
 	"io"
 	"log"
 	"os"
-
-	"github.com/spf13/pflag"
 )
 
-var (
-	algorithm = pflag.IntP("algorithm", "a", 1, "SHA algorithm, valid args are 1 and 256")
-	help      = pflag.BoolP("help", "h", false, "Show this help and exit")
-)
+var algorithm int
+
 var usage = "Usage:\nshasum -a <algorithm> <File Name>"
-
-func helpPrinter() {
-	fmt.Println(usage)
-	pflag.PrintDefaults()
-}
 
 // shaPrinter prints sha1/sha256 of given data. The
 // value of algorithm is expected to be 1 for SHA1
@@ -48,15 +40,11 @@ func shaGenerator(w io.Writer, r io.Reader, algo int) ([]byte, error) {
 }
 
 func shasum(w io.Writer, r io.Reader, args ...string) error {
-	if *help {
-		helpPrinter()
-		return nil
-	}
 	var hashbytes []byte
 	var err error
 	if len(args) == 0 {
 		buf := bufio.NewReader(r)
-		if hashbytes, err = shaGenerator(w, buf, *algorithm); err != nil {
+		if hashbytes, err = shaGenerator(w, buf, algorithm); err != nil {
 			return err
 		}
 		fmt.Fprintf(w, "%x -\n", hashbytes)
@@ -68,7 +56,7 @@ func shasum(w io.Writer, r io.Reader, args ...string) error {
 			return err
 		}
 		defer file.Close()
-		if hashbytes, err = shaGenerator(w, file, *algorithm); err != nil {
+		if hashbytes, err = shaGenerator(w, file, algorithm); err != nil {
 			return err
 		}
 		fmt.Fprintf(w, "%x %s\n", hashbytes, arg)
@@ -77,8 +65,16 @@ func shasum(w io.Writer, r io.Reader, args ...string) error {
 }
 
 func main() {
-	pflag.Parse()
-	if err := shasum(os.Stdout, os.Stdin, pflag.Args()...); err != nil {
+	flag.IntVar(&algorithm, "algorithm", 1, "SHA algorithm, valid args are 1 and 256")
+	flag.IntVar(&algorithm, "a", 1, "SHA algorithm, valid args are 1 and 256 (shorthand)")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), usage+"\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	if err := shasum(os.Stdout, os.Stdin, flag.Args()...); err != nil {
 		log.Fatal(err)
 	}
 }
