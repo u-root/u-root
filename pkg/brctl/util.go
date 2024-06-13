fb2d65e1f7688b69311fd67cbe82be50a1039688
Christopher Meis <christopher.meis@9elements.com>
2024-06-13 13:39:20 +0200

    Remove unused code in util.go
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/brctl/util.go
+++ pkg/brctl/util.go
@@ -9,9 +9,7 @@ package brctl
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -22,12 +20,6 @@ import (
 )
 
 var errno0 = syscall.Errno(0)
-
-// Helper for issuing raw ioctl wi//go:build !plan9
-type ifreqptr struct {
-	Ifrn [16]byte
-	ptr  unsafe.Pointer
-}
 
 // BridgeInfo contains information about a bridge
 // This information is not exhaustive, only the most important fields are included
@@ -47,25 +39,11 @@ func sysconfhz() (int, error) {
 	return int(clktck), nil
 }
 
-func getIfreqOption(ifreq *unix.Ifreq, ptr unsafe.Pointer) ifreqptr {
-	i := ifreqptr{ptr: ptr}
-	copy(i.Ifrn[:], ifreq.Name())
-	return i
-}
-
 // ioctl helpers
 // TODO: maybe use ifreq.withData for this?
 func executeIoctlStr(fd int, req uint, raw string) (int, error) {
 	local_bytes := append([]byte(raw), 0)
 	_, _, errno := syscall.Syscall(unix.SYS_IOCTL, uintptr(fd), uintptr(req), uintptr(unsafe.Pointer(&local_bytes[0])))
-	if errno != 0 {
-		return 0, fmt.Errorf("syscall.Syscall: %w", errno)
-	}
-	return 0, nil
-}
-
-func ioctl(fd int, req uint, addr uintptr) (int, error) {
-	_, _, errno := syscall.Syscall(unix.SYS_IOCTL, uintptr(fd), uintptr(req), addr)
 	if errno != 0 {
 		return 0, fmt.Errorf("syscall.Syscall: %w", errno)
 	}
@@ -114,27 +92,6 @@ func getBridgeValue(bridge string, name string) (string, error) {
 		return "", err
 	}
 	return strings.TrimSuffix(string(out), "\n"), nil
-}
-
-// Set the value of a port in a bridge
-//
-//	SYSFS_CLASS_NET "%s/brport/%s", ifname, name
-func setBridgePort(bridge string, iface string, name string, value uint64, _ uint64) error {
-	err := os.WriteFile(BRCTL_SYS_NET+iface+"/brport/"+bridge+"/"+name, []byte(strconv.FormatUint(value, 10)), 0)
-	if err != nil {
-		log.Printf("br_set_port: %v", err)
-		return nil
-	}
-	return nil
-}
-
-// Get the value of a port in a bridge
-func getBridgePort(bridge string, iface string, name string) (string, error) {
-	out, err := os.ReadFile(BRCTL_SYS_NET + iface + "/brport/" + bridge + "/" + name)
-	if err != nil {
-		return "", err
-	}
-	return string(out), nil
 }
 
 func setPortBrportValue(port string, name string, value []byte) error {
