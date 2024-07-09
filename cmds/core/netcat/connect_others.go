f4c5ef344d7d483424afc4901c6f0baaa6267f1f
Fabian Wienand <fabian.wienand@9elements.com>
2024-07-09 11:10:27 +0200

    cmds/core/netcat: separate plan9 impl. more granular
    Signed-off-by: Fabian Wienand <fabian.wienand@9elements.com>

--- cmds/core/netcat/connect_others.go
+++ cmds/core/netcat/connect_others.go
@@ -2,13 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build linux || windows
+
 package main
 
 import (
 	"fmt"
 	"net"
+
+	"github.com/ishidawataru/sctp"
 )
 
 func connectToSCTPSocket(network, address string) (net.Conn, error) {
-	return nil, fmt.Errorf("sctp is not supported on Plan 9")
+	sctpAddr, err := sctp.ResolveSCTPAddr(network, address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve SCTP address: %w", err)
+	}
+
+	return sctp.DialSCTP(network, nil, sctpAddr)
 }
--- cmds/core/netcat/listen_others.go
+++ cmds/core/netcat/listen_others.go
@@ -1,13 +1,23 @@
 // Copyright 2024 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+//go:build linux || windows
+
 package main
 
 import (
 	"fmt"
 	"net"
+
+	"github.com/ishidawataru/sctp"
 )
 
 func listenToSCTPSocket(network, address string) (net.Listener, error) {
-	return nil, fmt.Errorf("sctp is not supported on Plan 9")
+	sctpAddr, err := sctp.ResolveSCTPAddr(network, address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve SCTP address: %w", err)
+	}
+
+	return sctp.ListenSCTP(network, sctpAddr)
 }
