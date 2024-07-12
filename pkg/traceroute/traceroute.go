fcfead2913beeceb14ba0e515aabfd137bf91d1d
Christopher Meis <christopher.meis@9elements.com>
2024-07-12 10:45:39 +0200

    Add icmp tracing
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/traceroute/traceroute.go
+++ pkg/traceroute/traceroute.go
@@ -48,11 +48,20 @@ func RunTraceroute(host string, prot string, debug bool) error {
 		go mod.SendTracesUDP4()
 	case "tcp4":
 		go mod.SendTracesTCP4()
+	case "icmp4":
+		go mod.SendTracesICMP4()
+
 	}
 
 	printMap := runTransmission(cc)
 
 	destTTL := findDestinationTTL(printMap, dAddr)
+	fmt.Printf("traceroute to %s (%s), %d hops max, %d byte packets\n",
+		host,
+		net.IPv4(dAddr[0], dAddr[1], dAddr[2], dAddr[3]).String(),
+		mod.MaxHops,
+		60)
+
 	for i := 1; i <= destTTL; i++ {
 		pbs := getProbesByTLL(printMap, i)
 		if len(pbs) == 0 {
@@ -84,8 +93,9 @@ func runTransmission(cc coms) map[int]*Probe {
 					sendProbes[i].done = true
 					// Add to map
 					printMap[int(sp.id)] = sendProbes[i]
+
 					if p.saddr == net.IPv4(sp.dest[0], sp.dest[1], sp.dest[2], sp.dest[3]).String() {
-						fmt.Println(p.saddr)
+						//fmt.Println(p.saddr)
 						return printMap
 					}
 				}
