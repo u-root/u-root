6421a7ee753382189d2ea4f172d67ea2cf10a257
Fabian Wienand <fabian.wienand@9elements.com>
2024-07-17 11:47:39 +0200

    cmds/core/ip: vrf subcommand
    Signed-off-by: Fabian Wienand <fabian.wienand@9elements.com>

--- cmds/core/ip/ip_linux.go
+++ cmds/core/ip/ip_linux.go
@@ -73,7 +73,7 @@ func run(out io.Writer) error {
 	} else if inet4 {
 		family = netlink.FAMILY_V4
 	}
-	whatIWant = []string{"address", "route", "link", "monitor", "neigh", "tunnel", "tuntap", "tap", "tcp_metrics", "tcpmetrics", "xfrm"}
+	whatIWant = []string{"address", "route", "link", "monitor", "neigh", "tunnel", "tuntap", "tap", "tcp_metrics", "tcpmetrics", "vrf", "xfrm"}
 	cursor = 0
 
 	defer func() {
@@ -115,6 +115,8 @@ func run(out io.Writer) error {
 		err = tuntap(out)
 	case "tcpmetrics", "tcp_metrics":
 		err = tcpMetrics(out)
+	case "vrf":
+		err = vrf(out)
 	case "xfrm":
 		err = xfrm(out)
 	default:
