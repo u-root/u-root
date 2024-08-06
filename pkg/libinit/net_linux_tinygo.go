317360995268506c6c5da8147fde00321244a586
leongross <leon.gross@9elements.com>
2024-08-06 14:11:11 +0200

    tinygo: enable cmd/core/init
    Add conditional compilation to exclude any use of network packages for
    now. Further, `klauspost/zstd` is used, so to build this cmdlet with
    tinygo requires to set the build tag `noasm` as well:
    
    ```
    tinygo build -tags noasm .
    ```
    
    Signed-off-by: leongross <leon.gross@9elements.com>

---
+++ pkg/libinit/net_linux_tinygo.go
@@ -0,0 +1,21 @@
+// Copyright 2014-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build linux && tinygo
+// +build linux,tinygo
+
+package libinit
+
+import (
+	"github.com/u-root/u-root/pkg/ulog"
+)
+
+// Stub for notifying the user that network functionality is not supported on linux platforms
+func linuxNetInit() {
+	ulog.KernelLog.Printf("tinygo builds currently do not support network functionality on linux platforms\n")
+}
+
+func init() {
+	osNetInit = linuxNetInit
+}
