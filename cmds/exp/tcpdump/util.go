51de514703b5e79eb155b40e80423a4510c6fbae
Fabian Wienand <fabian.wienand@9elements.com>
2024-08-12 08:31:19 +0200

    cmds/exp/tcpdump: autogenerate well-known-ports map
    Signed-off-by: Fabian Wienand <fabian.wienand@9elements.com>

--- cmds/exp/tcpdump/util.go
+++ cmds/exp/tcpdump/util.go
@@ -11,27 +11,7 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-var wellKnownPortsMap = map[string]string{
-	"20":  "ftp-data",
-	"21":  "ftp",
-	"22":  "ssh-scp",
-	"23":  "telnet",
-	"25":  "smtp",
-	"53":  "domain",
-	"80":  "http",
-	"88":  "kerberos",
-	"110": "pop3",
-	"119": "nntp",
-	"123": "ntp",
-	"143": "imap",
-	"443": "https",
-	"465": "smtps",
-	"563": "nntps",
-	"989": "ftps-data",
-	"990": "ftps",
-	"993": "imaps",
-	"995": "pop3s",
-}
+//go:generate go run gen.go
 
 // wellKnownPorts returns the well-known name of the port or the port number itself.
 func (cmd cmd) wellKnownPorts(port string) string {
