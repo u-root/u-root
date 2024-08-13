27d27adb747c52d4d1c55eb24af1da3994cdbce7
Roger Standridge <9526806+archie2x@users.noreply.github.com>
2024-08-13 09:45:06 -0700

    tinygo #2979 remove tinygo build constraints
    Signed-off-by: Roger Standridge <9526806+archie2x@users.noreply.github.com>

--- cmds/core/io/asmports.go
+++ cmds/core/io/asmports.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !tinygo && ((linux && amd64) || (linux && 386))
+//go:build (linux && amd64) || (linux && 386)
 
 package main
 
