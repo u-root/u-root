1c579b35c0ccce7e11a296940475c14f0b48e348
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2024-08-16 07:13:59 +0300

    cmds/core/du: add test for block counting
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/du/du.go
+++ cmds/core/du/du.go
@@ -1,6 +1,9 @@
 // Copyright 2024 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+//go:build !plan9
+
 package main
 
 import (
---
+++ cmds/core/du/du_test.go
@@ -0,0 +1,62 @@
+// Copyright 2024 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestDU(t *testing.T) {
+	t.Run("8K file", func(t *testing.T) {
+		dir := t.TempDir()
+		f, err := os.CreateTemp(dir, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		f.Write(make([]byte, 8096))
+
+		blocks, err := du(f.Name())
+		if err != nil {
+			t.Fatalf("expected nil got %v", err)
+		}
+
+		if blocks != 16 {
+			t.Errorf("expected 16 blocks, got %d", blocks)
+		}
+	})
+	t.Run("empty file", func(t *testing.T) {
+		dir := t.TempDir()
+		f, err := os.CreateTemp(dir, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		blocks, err := du(f.Name())
+		if err != nil {
+			t.Fatalf("expected nil got %v", err)
+		}
+
+		if blocks != 0 {
+			t.Errorf("expected 0 blocks, got %d", blocks)
+		}
+	})
+	t.Run("one bit file", func(t *testing.T) {
+		dir := t.TempDir()
+		f, err := os.CreateTemp(dir, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		f.Write(make([]byte, 1))
+
+		blocks, err := du(f.Name())
+		if err != nil {
+			t.Fatalf("expected nil got %v", err)
+		}
+
+		if blocks != 8 {
+			t.Errorf("expected 8 blocks, got %d", blocks)
+		}
+	})
+}
