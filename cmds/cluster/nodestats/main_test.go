d4e4dbe12fa5a829dc85769a5c9a4aa330ff40e3
Ron Minnich <rminnich@google.com>
2024-08-21 15:20:10 +0000

    add cluster/nodestat command
    Takes no arguments, dumps useful information.
    
    Signed-off-by: Ron Minnich <rminnich@google.com>

---
+++ cmds/cluster/nodestats/main_test.go
@@ -0,0 +1,23 @@
+// Copyright 2024 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNodeStat(t *testing.T) {
+	var b bytes.Buffer
+	if err := run(&b, []string{"nodestat"}); err != nil {
+		t.Fatal(err)
+	}
+	if b.Len() == 0 {
+		t.Fatal("output from run: got 0 bytes, expected at least 1")
+	}
+	if err := run(&b, []string{"nodestat", "bad arg"}); err == nil {
+		t.Fatalf("run with one arg: got nil, expect err")
+	}
+}
