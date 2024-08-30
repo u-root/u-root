435893371fc42121ff6bf5df98e0780cdc265717
Raysmond <raysmond.lei@newfw.com>
2024-08-30 04:08:30 +0000

    pkg/boot/universalpayload: code style changes
    Change-Id: I59415c8cea8cbf595d5ea697d5014be75a1c2cff
    Signed-off-by: Raysmond <raysmond.lei@newfw.com>

--- pkg/boot/universalpayload/handoffblock.go
+++ pkg/boot/universalpayload/handoffblock.go
@@ -12,151 +12,157 @@ import (
 	"github.com/u-root/u-root/pkg/boot/kexec"
 )
 
-// EFI HOB Handoff Info Table
-const EFIHOBHandoffTableVersion = 0x0009
-const EFIHOBAlignment = 0x100000
+// EFIHOBGenericHeader types
+type EFIHOBType uint16
+type EFIHOBLength uint16
+type EFIHOBReserved uint32
 
-// EFI HOB Types
-const (
-	EfiHobTypeHandoff            = 0x0001
-	EfiHobTypeMemoryAllocation   = 0x0002
-	EfiHobTypeResourceDescriptor = 0x0003
-	EfiHobTypeGUIDExtension      = 0x0004
-	EfiHobTypeFv                 = 0x0005
-	EfiHobTypeCPU                = 0x0006
-	EfiHobTypeMemoryPool         = 0x0007
-	EfiHobTypeFv2                = 0x0009
-	EfiHobTypeLoadPeimUnused     = 0x000A
-	EfiHobTypeUefiCapsule        = 0x000B
-	EfiHobTypeFv3                = 0x000C
-	EfiHobTypeUnused             = 0xFFFE
-	EfiHobTypeEndOfHobList       = 0xFFFF
-)
-
-// EFI Resource Types
-const (
-	EfiResourceSystemMemory       = 0x00000000
-	EfiResourceMemoryMappedIO     = 0x00000001
-	EfiResourceIO                 = 0x00000002
-	EfiResourceFirmwareDevice     = 0x00000003
-	EfiResourceMemoryMappedIOPort = 0x00000004
-	EfiResourceMemoryReserved     = 0x00000005
-	EfiResourceIOReserved         = 0x00000006
-	EfiResourceMemoryUnaccepted   = 0x00000007
-	EfiResourceMaxMemoryType      = 0x00000008
-	EfiResourceUnimplemeted       = 0x00000008
-)
-
-// EFI Resource Attributes
-const (
-	EfiResourceAttributePresent               = 0x00000001
-	EfiResourceAttributeInitialized           = 0x00000002
-	EfiResourceAttributeTested                = 0x00000004
-	EfiResourceAttributeReadProtected         = 0x00000080
-	EfiResourceAttributeWriteProtected        = 0x00000100
-	EfiResourceAttributeExecutionProtected    = 0x00000200
-	EfiResourceAttributePersistent            = 0x00800000
-	EfiResourceAttributeSingleBitECC          = 0x00000008
-	EfiResourceAttributeMultipleBitECC        = 0x00000010
-	EfiResourceAttributeECCReserved1          = 0x00000020
-	EfiResourceAttributeECCReserved2          = 0x00000040
-	EfiResourceAttributeUncacheable           = 0x00000400
-	EfiResourceAttributeWriteCombineable      = 0x00000800
-	EfiResourceAttributeWriteThroughCacheable = 0x00001000
-	EfiResourceAttributeWriteBackCacheable    = 0x00002000
-	EfiResourceAttribute16BitIO               = 0x00004000
-	EfiResourceAttribute32BitIO               = 0x00008000
-	EfiResourceAttribute64BitIO               = 0x00010000
-	EfiResourceAttributeUncachedExported      = 0x00020000
-	EfiResourceAttributeReadProtectable       = 0x00100000
-	EfiResourceAttributeWriteProtectable      = 0x00200000
-	EfiResourceAttributeExecutionProtectable  = 0x00400000
-	EfiResourceAttributePersistable           = 0x01000000
-	EfiResourceAttributeReadOnlyProtected     = 0x00040000
-	EfiResourceAttributeReadOnlyProtectable   = 0x00080000
-	EfiResourceAttributeEncrypted             = 0x04000000
-	EfiResourceAttributeSpecialPurpose        = 0x08000000
-	EfiResourceAttributeMoreReliable          = 0x02000000
-)
-
-// HandoffHob values
-const (
-	EfiHobHandoffInfoVersion          = 0x09
-	EfiHobHandoffInfoBootMode         = 0x0
-	EfiHobHandoffInfoEfiMemoryTop     = 0x4E00000
-	EfiHobHandoffInfoEfiMemoryBottom  = 0x800000
-	EfiHobHandoffInfoFreeEfiMemoryTop = 0x4E00000
-	EfiHobHandoffInfoFreeMemoryBottom = 0xE00000
-)
-
-// According to Intel SDM Volume 1 Chapter 19.3 "I/O ADDRESS SPACE":
-// The I/O address space consists of 2^16 (64K) individually addressable
-// 8-bit I/O ports, numbered 0 through FFFFH.
-// Set the default IO Address size to 16
-const (
-	DefaultIOAddressSize = 16
-)
-
-// Type Aliases
-type EfiBootMode uint32
-type EfiResourceType uint32
-type EfiPhysicalAddress uint64
-type EfiResourceAttributeType uint32
-
-// Efi HOB Generic Header
-type EfiHobGenericHeader struct {
-	HobType   uint16
-	HobLength uint16
-	Reserved  uint32
+// EFIHOBGenericHeader describes the format and size of the data inside the HOB
+type EFIHOBGenericHeader struct {
+	HOBType   EFIHOBType
+	HOBLength EFIHOBLength
+	Reserved  EFIHOBReserved
 }
 
-type EfiHobHandoffInfoTable struct {
-	Header              EfiHobGenericHeader
-	Version             uint32
-	BootMode            EfiBootMode
-	EfiMemoryTop        EfiPhysicalAddress
-	EfiMemoryBottom     EfiPhysicalAddress
-	EfiFreeMemoryTop    EfiPhysicalAddress
-	EfiFreeMemoryBottom EfiPhysicalAddress
-	EfiEndOfHobList     EfiPhysicalAddress
+// EFIHOBType values
+const (
+	EFIHOBTypeHandoff            EFIHOBType = 0x0001
+	EFIHOBTypeMemoryAllocation   EFIHOBType = 0x0002
+	EFIHOBTypeResourceDescriptor EFIHOBType = 0x0003
+	EFIHOBTypeGUIDExtension      EFIHOBType = 0x0004
+	EFIHOBTypeFv                 EFIHOBType = 0x0005
+	EFIHOBTypeCPU                EFIHOBType = 0x0006
+	EFIHOBTypeMemoryPool         EFIHOBType = 0x0007
+	EFIHOBTypeFv2                EFIHOBType = 0x0009
+	EFIHOBTypeLoadPeimUnused     EFIHOBType = 0x000A
+	EFIHOBTypeUEFICapsule        EFIHOBType = 0x000B
+	EFIHOBTypeFv3                EFIHOBType = 0x000C
+	EFIHOBTypeUnused             EFIHOBType = 0xFFFE
+	EFIHOBTypeEndOfHOBList       EFIHOBType = 0xFFFF
+)
+
+type EFIResourceType uint32
+
+// EFIResourceType values
+const (
+	EFIResourceSystemMemory       EFIResourceType = 0x00000000
+	EFIResourceMemoryMappedIO     EFIResourceType = 0x00000001
+	EFIResourceIO                 EFIResourceType = 0x00000002
+	EFIResourceEFIFirmwareDevice  EFIResourceType = 0x00000003
+	EFIResourceMemoryMappedIOPort EFIResourceType = 0x00000004
+	EFIResourceMemoryReserved     EFIResourceType = 0x00000005
+	EFIResourceIOReserved         EFIResourceType = 0x00000006
+	EFIResourceMemoryUnaccepted   EFIResourceType = 0x00000007
+	EFIResourceMaxMemoryType      EFIResourceType = 0x00000008
+	EFIResourceUnimplemented      EFIResourceType = 0x00000008
+)
+
+type EFIResourceAttributeType uint32
+
+// EFIResourceAttributeType values
+const (
+	EFIResourceAttributePresent               EFIResourceAttributeType = 0x00000001
+	EFIResourceAttributeInitialized           EFIResourceAttributeType = 0x00000002
+	EFIResourceAttributeTested                EFIResourceAttributeType = 0x00000004
+	EFIResourceAttributeReadProtected         EFIResourceAttributeType = 0x00000080
+	EFIResourceAttributeWriteProtected        EFIResourceAttributeType = 0x00000100
+	EFIResourceAttributeExecutionProtected    EFIResourceAttributeType = 0x00000200
+	EFIResourceAttributePersistent            EFIResourceAttributeType = 0x00800000
+	EFIResourceAttributeSingleBitECC          EFIResourceAttributeType = 0x00000008
+	EFIResourceAttributeMultipleBitECC        EFIResourceAttributeType = 0x00000010
+	EFIResourceAttributeECCReserved1          EFIResourceAttributeType = 0x00000020
+	EFIResourceAttributeECCReserved2          EFIResourceAttributeType = 0x00000040
+	EFIResourceAttributeUncacheable           EFIResourceAttributeType = 0x00000400
+	EFIResourceAttributeWriteCombineable      EFIResourceAttributeType = 0x00000800
+	EFIResourceAttributeWriteThroughCacheable EFIResourceAttributeType = 0x00001000
+	EFIResourceAttributeWriteBackCacheable    EFIResourceAttributeType = 0x00002000
+	EFIResourceAttribute16BitIO               EFIResourceAttributeType = 0x00004000
+	EFIResourceAttribute32BitIO               EFIResourceAttributeType = 0x00008000
+	EFIResourceAttribute64BitIO               EFIResourceAttributeType = 0x00010000
+	EFIResourceAttributeUncachedExported      EFIResourceAttributeType = 0x00020000
+	EFIResourceAttributeReadProtectable       EFIResourceAttributeType = 0x00100000
+	EFIResourceAttributeWriteProtectable      EFIResourceAttributeType = 0x00200000
+	EFIResourceAttributeExecutionProtectable  EFIResourceAttributeType = 0x00400000
+	EFIResourceAttributePersistable           EFIResourceAttributeType = 0x01000000
+	EFIResourceAttributeReadOnlyProtected     EFIResourceAttributeType = 0x00040000
+	EFIResourceAttributeReadOnlyProtectable   EFIResourceAttributeType = 0x00080000
+	EFIResourceAttributeEncrypted             EFIResourceAttributeType = 0x04000000
+	EFIResourceAttributeSpecialPurpose        EFIResourceAttributeType = 0x08000000
+	EFIResourceAttributeMoreReliable          EFIResourceAttributeType = 0x02000000
+)
+
+type EFIPhysicalAddress uint64
+type EFIHOBHandOffBootModeType uint32
+type EFIHOBHandoffInfoVersionType uint32
+
+// EFIHOBHandoffInfoTable values
+const (
+	EFIHOBHandoffInfoVersion          EFIHOBHandoffInfoVersionType = 0x09
+	EFIHOBHandoffInfoBootMode         EFIHOBHandOffBootModeType    = 0x0
+	EFIHOBHandoffInfoEFIMemoryTop     EFIPhysicalAddress           = 0x4E00000
+	EFIHOBHandoffInfoEFIMemoryBottom  EFIPhysicalAddress           = 0x800000
+	EFIHOBHandoffInfoFreeEFIMemoryTop EFIPhysicalAddress           = 0x4E00000
+	EFIHOBHandoffInfoFreeMemoryBottom EFIPhysicalAddress           = 0xE00000
+)
+
+// EFIHOBHandoffInfoTable appears in the first in HOB list, which contains some general information
+type EFIHOBHandoffInfoTable struct {
+	Header           EFIHOBGenericHeader
+	Version          EFIHOBHandoffInfoVersionType
+	BootMode         EFIHOBHandOffBootModeType
+	MemoryTop        EFIPhysicalAddress
+	MemoryBottom     EFIPhysicalAddress
+	FreeMemoryTop    EFIPhysicalAddress
+	FreeMemoryBottom EFIPhysicalAddress
+	EndOfHOBList     EFIPhysicalAddress
 }
 
-// Efi HOB Resource Descriptor
-type EfiHobResourceDescriptor struct {
-	Header            EfiHobGenericHeader
-	Owner             [16]byte // EfiGUID (assuming a byte array for simplicity)
-	ResourceType      EfiResourceType
-	ResourceAttribute EfiResourceAttributeType
-	PhysicalStart     EfiPhysicalAddress
+// EFIHOBResourceDescriptor describes all resource properties found on the processor host bus
+type EFIHOBResourceDescriptor struct {
+	Header            EFIHOBGenericHeader
+	Owner             guid.UUID // EFI GUID (assuming a byte array for simplicity)
+	ResourceType      EFIResourceType
+	ResourceAttribute EFIResourceAttributeType
+	PhysicalStart     EFIPhysicalAddress
 	ResourceLength    uint64
 }
 
-// Efi HOB Firmware Volume
-type EfiHobFirmwareVolume struct {
-	Header      EfiHobGenericHeader
-	BaseAddress EfiPhysicalAddress
+// EFIHOBFirmwareVolume describes the location of firmware locations
+type EFIHOBFirmwareVolume struct {
+	Header      EFIHOBGenericHeader
+	BaseAddress EFIPhysicalAddress
 	Length      uint64
 }
 
-// Efi HOB Firmware Volume
-type EfiHobGUIDType struct {
-	Header EfiHobGenericHeader
+// EFIHOBGUIDType describes the extension information which allows
+// writing executable content in the HOB producer phase.
+type EFIHOBGUIDType struct {
+	Header EFIHOBGenericHeader
 	Name   guid.UUID
 }
 
-// Efi HOB CPU
-type EfiHobCPU struct {
-	Header            EfiHobGenericHeader
+// EFIHOBCPU describes CPU information
+type EFIHOBCPU struct {
+	Header            EFIHOBGenericHeader
 	SizeOfMemorySpace uint8
 	SizeOfIOSpace     uint8
 	Reserved          [6]byte
 }
 
-type EfiMemoryMapHob []EfiHobResourceDescriptor
+// Common Constants
+// According to Intel SDM Volume 1 Chapter 19.3 "I/O ADDRESS SPACE":
+// The I/O address space consists of 2^16 (64K) individually addressable
+// 8-bit I/O ports, numbered 0 through FFFFH.
+// Set the default IO Address size to 16
+const (
+	DefaultIOAddressSize uint8 = 16
+)
 
-// Translate System Map with "System RAM" type to Resource code Hobs.
-func hobFromMemMap(memMap kexec.MemoryMap) (EfiMemoryMapHob, uint64) {
-	var memMapHob EfiMemoryMapHob
+type EFIMemoryMapHOB []EFIHOBResourceDescriptor
+
+// Translate System Map with "System RAM" type to Resource code HOBs.
+func hobFromMemMap(memMap kexec.MemoryMap) (EFIMemoryMapHOB, uint64) {
+	var memMapHOB EFIMemoryMapHOB
 	var length uint64
 
 	for _, entry := range memMap {
@@ -164,69 +170,69 @@ func hobFromMemMap(memMap kexec.MemoryMap) (EfiMemoryMapHob, uint64) {
 		memType := strings.TrimSpace(string(entry.Type))
 
 		if memType == kexec.RangeRAM.String() {
-			memMapHob = append(memMapHob, EfiHobResourceDescriptor{
-				Header: EfiHobGenericHeader{
-					HobType:   EfiHobTypeResourceDescriptor,
-					HobLength: uint16(unsafe.Sizeof(EfiHobResourceDescriptor{})),
+			memMapHOB = append(memMapHOB, EFIHOBResourceDescriptor{
+				Header: EFIHOBGenericHeader{
+					HOBType:   EFIHOBTypeResourceDescriptor,
+					HOBLength: EFIHOBLength(unsafe.Sizeof(EFIHOBResourceDescriptor{})),
 				},
-				ResourceType: EfiResourceSystemMemory,
-				ResourceAttribute: EfiResourceAttributePresent |
-					EfiResourceAttributeInitialized |
-					EfiResourceAttributeTested |
-					EfiResourceAttributeUncacheable |
-					EfiResourceAttributeWriteCombineable |
-					EfiResourceAttributeWriteThroughCacheable |
-					EfiResourceAttributeWriteBackCacheable,
-				PhysicalStart:  EfiPhysicalAddress(entry.Start),
+				ResourceType: EFIResourceSystemMemory,
+				ResourceAttribute: EFIResourceAttributePresent |
+					EFIResourceAttributeInitialized |
+					EFIResourceAttributeTested |
+					EFIResourceAttributeUncacheable |
+					EFIResourceAttributeWriteCombineable |
+					EFIResourceAttributeWriteThroughCacheable |
+					EFIResourceAttributeWriteBackCacheable,
+				PhysicalStart:  EFIPhysicalAddress(entry.Start),
 				ResourceLength: uint64(entry.Size),
 			})
-			length += uint64(unsafe.Sizeof(EfiHobResourceDescriptor{}))
+			length += uint64(unsafe.Sizeof(EFIHOBResourceDescriptor{}))
 		}
 	}
 
-	return memMapHob, length
+	return memMapHOB, length
 }
 
-func hobCreateEndHob() EfiHobGenericHeader {
-	return EfiHobGenericHeader{
-		HobType:   EfiHobTypeEndOfHobList,
-		HobLength: uint16(unsafe.Sizeof(EfiHobGenericHeader{})),
+func hobCreateEndHOB() EFIHOBGenericHeader {
+	return EFIHOBGenericHeader{
+		HOBType:   EFIHOBTypeEndOfHOBList,
+		HOBLength: EFIHOBLength(unsafe.Sizeof(EFIHOBGenericHeader{})),
 		Reserved:  0,
 	}
 }
 
-// Create handoff information Hob with specified length.
-// Handoff info Hob should be created after all Hobs in Hob list
-// have been created, since length of Hob list should be provided
+// Create handoff information HOB with specified length.
+// Handoff info HOB should be created after all HOBs in HOB list
+// have been created, since length of HOB list should be provided
 // as input parameter.
-func hobCreateEfiHobHandoffInfoTable(length uint64) EfiHobHandoffInfoTable {
-	length += uint64(unsafe.Sizeof(EfiHobHandoffInfoTable{}))
+func hobCreateEFIHOBHandoffInfoTable(length uint64) EFIHOBHandoffInfoTable {
+	length += uint64(unsafe.Sizeof(EFIHOBHandoffInfoTable{}))
 
-	return EfiHobHandoffInfoTable{
-		Header: EfiHobGenericHeader{
-			HobType:   EfiHobTypeHandoff,
-			HobLength: uint16(unsafe.Sizeof(EfiHobHandoffInfoTable{})),
+	return EFIHOBHandoffInfoTable{
+		Header: EFIHOBGenericHeader{
+			HOBType:   EFIHOBTypeHandoff,
+			HOBLength: EFIHOBLength(unsafe.Sizeof(EFIHOBHandoffInfoTable{})),
 		},
-		Version:             EfiHobHandoffInfoVersion,
-		BootMode:            EfiHobHandoffInfoBootMode,
-		EfiMemoryTop:        EfiHobHandoffInfoEfiMemoryTop,
-		EfiMemoryBottom:     EfiHobHandoffInfoEfiMemoryBottom,
-		EfiFreeMemoryTop:    EfiHobHandoffInfoFreeEfiMemoryTop,
-		EfiFreeMemoryBottom: EfiPhysicalAddress(EfiHobHandoffInfoFreeMemoryBottom + length + 8),
-		EfiEndOfHobList:     EfiPhysicalAddress(EfiHobHandoffInfoFreeMemoryBottom + length),
+		Version:          EFIHOBHandoffInfoVersion,
+		BootMode:         EFIHOBHandoffInfoBootMode,
+		MemoryTop:        EFIHOBHandoffInfoEFIMemoryTop,
+		MemoryBottom:     EFIHOBHandoffInfoEFIMemoryBottom,
+		FreeMemoryTop:    EFIHOBHandoffInfoFreeEFIMemoryTop,
+		FreeMemoryBottom: EFIHOBHandoffInfoFreeMemoryBottom + EFIPhysicalAddress(length+8),
+		EndOfHOBList:     EFIHOBHandoffInfoFreeMemoryBottom + EFIPhysicalAddress(length),
 	}
 }
 
-func hobCreateEfiHobCPU() (*EfiHobCPU, error) {
+func hobCreateEFIHOBCPU() (*EFIHOBCPU, error) {
 	phyAddrSize, err := getPhysicalAddressSizes()
 	if err != nil {
 		return nil, err
 	}
 
-	return &EfiHobCPU{
-		Header: EfiHobGenericHeader{
-			HobType:   EfiHobTypeCPU,
-			HobLength: uint16(unsafe.Sizeof(EfiHobCPU{})),
+	return &EFIHOBCPU{
+		Header: EFIHOBGenericHeader{
+			HOBType:   EFIHOBTypeCPU,
+			HOBLength: EFIHOBLength(unsafe.Sizeof(EFIHOBCPU{})),
 		},
 		SizeOfMemorySpace: phyAddrSize,
 		SizeOfIOSpace:     DefaultIOAddressSize,
