a3896f6bbf905fff5aa1a8b0210ad7253102e145
leongross <leon.gross@9elements.com>
2024-09-20 18:22:21 +0200

    modules: udpdate go-tpm to incorporate tingygo issues
    To enable the build of more u-root cmdlets @chrisfenner
    made a patch to fix the build issues. This PR updates
    the go-tpm module to commit d96ccf71568571dba925660724354682fe8e5fa2
    
    Signed-off-by: leongross <leon.gross@9elements.com>

--- vendor/github.com/google/go-tpm/tpm2/transport/open_other.go
+++ vendor/github.com/google/go-tpm/tpm2/transport/open_other.go
@@ -6,17 +6,11 @@ import (
 	legacy "github.com/google/go-tpm/legacy/tpm2"
 )
 
-// Wrap the legacy OpenTPM function so callers don't have to import both the
-// legacy and the new TPM 2.0 API.
-// TODO: When we delete the legacy API, we can make this the only copy of
-// OpenTPM.
-
-// OpenTPM opens a channel to the TPM at the given path. If the file is a
-// device, then it treats it like a normal TPM device, and if the file is a
-// Unix domain socket, then it opens a connection to the socket.
+// OpenTPM opens the TPM at the given path. If no path is provided, it will
+// attempt to use reasonable defaults.
 //
-// This function may also be invoked with no paths, as tpm2.OpenTPM(). In this
-// case, the default paths on Linux (/dev/tpmrm0 then /dev/tpm0), will be used.
+// Deprecated: Please use the individual transport packages (e.g.,
+// go-tpm/tpm2/transport/linuxtpm).
 func OpenTPM(path ...string) (TPMCloser, error) {
 	rwc, err := legacy.OpenTPM(path...)
 	if err != nil {
