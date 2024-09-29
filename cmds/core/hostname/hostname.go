efa5c6e057b156e17d25b6b71c45afb6900c3c6a
Ronald G. Minnich <rminnich@gmail.com>
2024-09-29 03:37:57 +0000

    tag hostname command for plan9 || linux
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/core/hostname/hostname.go
+++ cmds/core/hostname/hostname.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build plan9 || linux
+
 // hostname prints or changes the system's hostname.
 //
 // Synopsis:
