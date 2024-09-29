b74afae5ba93f5e0d4ffc19ae03923f464244c1f
Ronald G. Minnich <rminnich@gmail.com>
2024-09-29 09:22:36 +0000

    cmds/core/mount: mount.go -> mount_linux.go
    mounting on unix-like systems is no longer generic.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/core/mount/mount_linux.go
+++ cmds/core/mount/mount_linux.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !plan9 && !freebsd
-
 // mount mounts a filesystem at the specified path.
 //
 // Synopsis:
