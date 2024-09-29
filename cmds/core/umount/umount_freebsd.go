4ef1f4136927e3372967697a1edeac610d9387c6
Ronald G. Minnich <rminnich@gmail.com>
2024-09-29 11:00:01 +0000

    cmds/core/umount: fix for freebsd
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/core/umount/umount_freebsd.go
@@ -0,0 +1,26 @@
+// Copyright 2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"errors"
+	"flag"
+
+	"github.com/u-root/u-root/pkg/mount"
+)
+
+var (
+	force = flag.Bool("f", false, "Force unmount")
+)
+
+func umount() error {
+	flag.Parse()
+	a := flag.Args()
+	if len(a) != 1 {
+		return errors.New("usage: umount [-f] path")
+	}
+	path := a[0]
+	return mount.Unmount(path, *force, false)
+}
