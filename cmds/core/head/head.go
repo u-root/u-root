8b8e83eda9cb3a8a9cb83434fa7a55a2f50879da
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2024-10-02 09:21:15 +0300

    cmds/core/head: add stdin support
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/head/head.go
+++ cmds/core/head/head.go
@@ -17,7 +17,7 @@ import (
 
 var errCombine = fmt.Errorf("can't combine line and byte counts")
 
-func run(stdout io.Writer, stderr io.Writer, bytes, count int, files ...string) error {
+func run(stdin io.Reader, stdout io.Writer, stderr io.Writer, bytes, count int, files ...string) error {
 	if bytes > 0 && count > 0 {
 		return errCombine
 	}
@@ -36,39 +36,57 @@ func run(stdout io.Writer, stderr io.Writer, bytes, count int, files ...string) 
 	var newLineHeader bool
 	var errs error
 
+	var handle = func(r io.Reader, name string) error {
+		if len(files) > 1 {
+			if newLineHeader {
+				fmt.Fprintf(stdout, "\n==> %s <==\n", filepath.Base(name))
+			} else {
+				fmt.Fprintf(stdout, "==> %s <==\n", filepath.Base(name))
+				newLineHeader = true
+			}
+		}
+		if printBytes {
+			n, err := io.ReadFull(r, buffer)
+			if err == io.ErrUnexpectedEOF {
+				// ignore if user request more bytes than file has
+			} else if err != nil {
+				errs = errors.Join(errs, fmt.Errorf("head: %w", err))
+			}
+			stdout.Write(buffer[:n])
+
+			// cleanup stdin
+			if len(files) == 0 {
+				_, _ = bufio.NewReader(stdin).ReadString('\n')
+			}
+		} else {
+			var c int
+			scanner := bufio.NewScanner(r)
+			for scanner.Scan() {
+				fmt.Fprintln(stdout, scanner.Text())
+				c++
+				if c == count {
+					break
+				}
+			}
+		}
+
+		return nil
+	}
+
+	// handle stdin
+	if len(files) == 0 {
+		return handle(stdin, "")
+	}
+
 	for _, file := range files {
 		f, err := os.Open(file)
 		if err != nil {
 			errs = errors.Join(errs, fmt.Errorf("head: %w", err))
 			continue
 		}
-		if len(files) > 1 {
-			if newLineHeader {
-				fmt.Fprintf(stdout, "\n==> %s <==\n", filepath.Base(f.Name()))
-			} else {
-				fmt.Fprintf(stdout, "==> %s <==\n", filepath.Base(f.Name()))
-				newLineHeader = true
-			}
-		}
-		if printBytes {
-			n, err := io.ReadFull(f, buffer)
-			if err == io.ErrUnexpectedEOF {
-				// ignore if user request more bytes than file has
-			} else if err != nil {
-				errs = errors.Join(errs, fmt.Errorf("head: %w", err))
-				continue
-			}
-			stdout.Write(buffer[:n])
-		} else {
-			var c int
-			scanner := bufio.NewScanner(f)
-			for scanner.Scan() {
-				if c == count {
-					break
-				}
-				fmt.Fprintln(stdout, scanner.Text())
-				c++
-			}
+		err = handle(f, f.Name())
+		if err != nil {
+			errs = errors.Join(errs, err)
 		}
 	}
 
@@ -83,7 +101,7 @@ func main() {
 	var n = flag.Int("n", 0, "Print count lines of each of the specified files")
 
 	flag.Parse()
-	if err := run(os.Stdin, os.Stderr, *c, *n, flag.Args()...); err != nil {
+	if err := run(os.Stdin, os.Stdout, os.Stderr, *c, *n, flag.Args()...); err != nil {
 		log.Fatalf("head: %v", err)
 	}
 }
