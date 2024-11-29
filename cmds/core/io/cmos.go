7f12f717a42431785ca231638f9063f528338ac9
Ronald G Minnich <rminnich@gmail.com>
2024-11-28 16:52:45 -0800

    Windows support including CI
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- cmds/core/io/cmos.go
+++ cmds/core/io/cmos.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (!tinygo || tinygo.enable) && (amd64 || 386) && !freebsd
+//go:build (!tinygo || tinygo.enable) && (amd64 || 386) && !freebsd && !windows
 
 package main
 
