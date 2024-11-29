4266e692de0565b9d197cb9adf158f003114023f
Ronald G Minnich <rminnich@gmail.com>
2024-11-28 16:07:35 -0800

    Fix date for windows
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- cmds/core/date/date_windows.go
+++ cmds/core/date/date_windows.go
@@ -2,21 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !plan9 && !windows
-
 package main
 
 import (
-	"log"
-	"syscall"
+	"os"
 	"time"
 )
 
 func setDate(d string, z *time.Location, clocksource Clock) error {
-	t, err := getTime(z, d, clocksource)
-	if err != nil {
-		log.Fatalf("%v: %v", d, err)
-	}
-	tv := syscall.NsecToTimeval(t.UnixNano())
-	return syscall.Settimeofday(&tv)
+	return os.ErrPermission
 }
