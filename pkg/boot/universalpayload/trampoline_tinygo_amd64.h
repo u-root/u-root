b88827ca36a4c4bb1d3bdde18aa736e5253d7b9a
leongross <leon.gross@9elements.com>
2024-12-06 13:22:28 +0100

    universalpayload: enable build for tinygo compiler
    Signed-off-by: leongross <leon.gross@9elements.com>

---
+++ pkg/boot/universalpayload/trampoline_tinygo_amd64.h
@@ -0,0 +1,55 @@
+// Copyright 2024 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <stdint.h>
+
+// globals; initial values taken from pkg/boot/universalpayload/trampoline_amd64.go
+long stack_top = 0xdeadbeef;
+long hob_addr = 0xdeadbeef;
+long entry_point = 0xdeadbeef;
+
+uintptr_t addrOfStartU()
+{
+    extern void trampoline_startU();
+    return (uintptr_t)trampoline_startU;
+}
+
+uintptr_t addrOfStackTopU()
+{
+    extern long stack_top;
+    return (uintptr_t)&stack_top;
+}
+
+uintptr_t addrOfHobAddrU()
+{
+    extern long hob_addr;
+    return (uintptr_t)&hob_addr;
+}
+
+void trampoline_startU()
+{
+    extern long stack_top;
+    extern long hob_addr;
+    extern long entry_point;
+
+    asm volatile(
+        // Load stack_top address into rax
+        "leaq stack_top(%%rip), %%rax\n"
+        "movq (%%rax), %%rax\n"
+        "movq %%rax, %%rsp\n"
+
+        // Load hob_addr into rax and move to rcx
+        "leaq hob_addr(%%rip), %%rax\n"
+        "movq (%%rax), %%rax\n"
+        "movq %%rax, %%rcx\n"
+
+        // Load entry_point into AX and jump
+        "leaq entry_point(%%rip), %%rax\n"
+        "movq (%%rax), %%rax\n"
+        "jmp *%%rax\n"
+        :
+        : "m"(stack_top), "m"(hob_addr), "m"(entry_point)
+        : "rax", "rcx" // Clobbered registers
+    );
+}
