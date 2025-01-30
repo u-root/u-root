940c5c7e2396c4fe9db7e1828e0b64c146a62154
Jens Drenhaus <jens.drenhaus@9elements.com>
2025-01-30 16:07:17 +0100

    cmds/core/ip: refactor subcommand show link/addr
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- cmds/core/ip/ops_linux_test.go
+++ cmds/core/ip/ops_linux_test.go
@@ -14,78 +14,18 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-func TestShowLinkAddresses(t *testing.T) {
+func TestPrintLinkJSON(t *testing.T) {
 	tests := []struct {
 		name     string
-		addrs    []netlink.Addr
+		links    []linkData
+		opts     flags
 		expected string
 	}{
 		{
-			name: "IPv4 address",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{
-						IP:   net.IPv4(192, 168, 1, 1),
-						Mask: net.CIDRMask(24, 32),
-					},
-					Broadcast:   net.IPv4(192, 168, 1, 255),
-					Scope:       int(netlink.SCOPE_HOST),
-					Label:       "eth0",
-					PreferedLft: 0,
-					ValidLft:    0,
-				},
-			},
-			expected: "    inet 192.168.1.1 brd 192.168.1.255 scope host eth0\n       valid_lft 0sec preferred_lft 0sec\n",
-		},
-		{
-			name: "IPv6 address",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{
-						IP:   net.ParseIP("2001:db8::1"),
-						Mask: net.CIDRMask(64, 128),
-					},
-					Broadcast:   nil,
-					Scope:       int(netlink.SCOPE_HOST),
-					Label:       "eth0",
-					PreferedLft: 3600,
-					ValidLft:    7200,
-				},
-			},
-			expected: "    inet6 2001:db8::1 scope host eth0\n       valid_lft 7200sec preferred_lft 3600sec\n",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			var out bytes.Buffer
-			cmd := cmd{Out: &out}
-
-			err := cmd.showLinkAddresses(tt.addrs)
-			if err != nil {
-				t.Fatalf("showLinkAddresses() error = %v", err)
-			}
-
-			if c := cmp.Diff(out.String(), tt.expected); c != "" {
-				t.Errorf("showLinkAddresses() = diff: %v", c)
-			}
-		})
-	}
-}
-
-func TestPrintLinkJSON(t *testing.T) {
-	tests := []struct {
-		name      string
-		links     []netlink.Link
-		addresses [][]netlink.Addr
-		opts      flags
-		expected  string
-	}{
-		{
 			name: "Single link with IPv4 address",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -95,20 +35,19 @@ func TestPrintLinkJSON(t *testing.T) {
 						Group:        0,
 						TxQLen:       1000,
 					},
-				},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.IPv4(192, 168, 1, 1),
-							Mask: net.CIDRMask(24, 32),
+					typeName: "device",
+					addresses: []netlink.Addr{
+						{
+							IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 1),
+								Mask: net.CIDRMask(24, 32),
+							},
+							Broadcast:   net.IPv4(192, 168, 1, 255),
+							Scope:       int(netlink.SCOPE_HOST),
+							Label:       "eth0",
+							PreferedLft: 0,
+							ValidLft:    0,
 						},
-						Broadcast:   net.IPv4(192, 168, 1, 255),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
 					},
 				},
 			},
@@ -143,9 +82,9 @@ func TestPrintLinkJSON(t *testing.T) {
 		},
 		{
 			name: "Single link with IPv6 address",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -155,20 +94,19 @@ func TestPrintLinkJSON(t *testing.T) {
 						Group:        0,
 						TxQLen:       1000,
 					},
-				},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.ParseIP("2001:db8::1"),
-							Mask: net.CIDRMask(24, 32),
+					typeName: "device",
+					addresses: []netlink.Addr{
+						{
+							IPNet: &net.IPNet{
+								IP:   net.ParseIP("2001:db8::1"),
+								Mask: net.CIDRMask(64, 128),
+							},
+							Broadcast:   nil,
+							Scope:       int(netlink.SCOPE_HOST),
+							Label:       "eth0",
+							PreferedLft: 3600,
+							ValidLft:    7200,
 						},
-						Broadcast:   net.ParseIP("2001:db8::2"),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
 					},
 				},
 			},
@@ -190,12 +128,11 @@ func TestPrintLinkJSON(t *testing.T) {
             {
                 "ip": "inet6",
                 "local": "2001:db8::1",
-                "prefixlen": "ffffff00",
-                "broadcast": "2001:db8::2",
+                "prefixlen": "ffffffffffffffff0000000000000000",
                 "scope": "host",
                 "label": "eth0",
-                "valid_life_time": "0sec",
-                "preferred_life_time": "0sec"
+                "valid_life_time": "7200sec",
+                "preferred_life_time": "3600sec"
             }
         ]
     }
@@ -211,7 +148,7 @@ func TestPrintLinkJSON(t *testing.T) {
 				Opts: tt.opts,
 			}
 
-			err := cmd.printLinkJSON(tt.links, tt.addresses)
+			err := cmd.printLinkJSON(tt.links)
 			if err != nil {
 				t.Fatalf("printLinkJSON() error = %v", err)
 			}
@@ -225,18 +162,16 @@ func TestPrintLinkJSON(t *testing.T) {
 
 func TestShowLinks(t *testing.T) {
 	tests := []struct {
-		name      string
-		links     []netlink.Link
-		addresses [][]netlink.Addr
-		filter    []string
-		opts      flags
-		expected  string
+		name     string
+		links    []linkData
+		opts     flags
+		expected string
 	}{
 		{
 			name: "Single link with IPv4 address JSON",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -245,21 +180,30 @@ func TestShowLinks(t *testing.T) {
 						MTU:          1500,
 						Group:        0,
 						TxQLen:       1000,
-					},
-				},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.IPv4(192, 168, 1, 1),
-							Mask: net.CIDRMask(24, 32),
+						Statistics: &netlink.LinkStatistics{
+							RxPackets: 100,
+							TxPackets: 200,
+							RxBytes:   1000,
+							TxBytes:   2000,
+							RxErrors:  10,
+							TxErrors:  20,
+							RxDropped: 1,
+							TxDropped: 2,
 						},
-						Broadcast:   net.IPv4(192, 168, 1, 255),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
+					},
+					typeName: "device",
+					addresses: []netlink.Addr{
+						{
+							IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 1),
+								Mask: net.CIDRMask(24, 32),
+							},
+							Broadcast:   net.IPv4(192, 168, 1, 255),
+							Scope:       int(netlink.SCOPE_HOST),
+							Label:       "eth0",
+							PreferedLft: 0,
+							ValidLft:    0,
+						},
 					},
 				},
 			},
@@ -294,9 +238,9 @@ func TestShowLinks(t *testing.T) {
 		},
 		{
 			name: "Single link with IPv4 address",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -306,20 +250,19 @@ func TestShowLinks(t *testing.T) {
 						Group:        0,
 						TxQLen:       1000,
 					},
-				},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.IPv4(192, 168, 1, 1),
-							Mask: net.CIDRMask(24, 32),
+					typeName: "device",
+					addresses: []netlink.Addr{
+						{
+							IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 1),
+								Mask: net.CIDRMask(24, 32),
+							},
+							Broadcast:   net.IPv4(192, 168, 1, 255),
+							Scope:       int(netlink.SCOPE_HOST),
+							Label:       "eth0",
+							PreferedLft: 0,
+							ValidLft:    0,
 						},
-						Broadcast:   net.IPv4(192, 168, 1, 255),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
 					},
 				},
 			},
@@ -328,9 +271,9 @@ func TestShowLinks(t *testing.T) {
 		},
 		{
 			name: "Single link with IPv4 address brief",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -340,20 +283,19 @@ func TestShowLinks(t *testing.T) {
 						Group:        0,
 						TxQLen:       1000,
 					},
-				},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.IPv4(192, 168, 1, 1),
-							Mask: net.CIDRMask(24, 32),
+					typeName: "device",
+					addresses: []netlink.Addr{
+						{
+							IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 1),
+								Mask: net.CIDRMask(24, 32),
+							},
+							Broadcast:   net.IPv4(192, 168, 1, 255),
+							Scope:       int(netlink.SCOPE_HOST),
+							Label:       "eth0",
+							PreferedLft: 0,
+							ValidLft:    0,
 						},
-						Broadcast:   net.IPv4(192, 168, 1, 255),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
 					},
 				},
 			},
@@ -362,9 +304,9 @@ func TestShowLinks(t *testing.T) {
 		},
 		{
 			name: "Single link brief",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -374,52 +316,17 @@ func TestShowLinks(t *testing.T) {
 						Group:        0,
 						TxQLen:       1000,
 					},
+					typeName: "device",
 				},
 			},
 			opts:     flags{Brief: true},
 			expected: "eth0                      up         00:1a:2b:3c:4d:5e   <UP>\n",
 		},
 		{
-			name: "Filter other type",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
-						Name:         "eth0",
-						Flags:        net.FlagUp,
-						OperState:    netlink.OperUp,
-						HardwareAddr: net.HardwareAddr{0x00, 0x1a, 0x2b, 0x3c, 0x4d, 0x5e},
-						Index:        1,
-						MTU:          1500,
-						Group:        0,
-						TxQLen:       1000,
-					},
-				},
-				&netlink.GenericLink{},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.IPv4(192, 168, 1, 1),
-							Mask: net.CIDRMask(24, 32),
-						},
-						Broadcast:   net.IPv4(192, 168, 1, 255),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
-					},
-				},
-			},
-			opts:     flags{Brief: true},
-			filter:   []string{"device"},
-			expected: "eth0                 up         192.168.1.1\n",
-		},
-		{
 			name: "Stats",
-			links: []netlink.Link{
-				&netlink.Device{
-					LinkAttrs: netlink.LinkAttrs{
+			links: []linkData{
+				{
+					attrs: &netlink.LinkAttrs{
 						Name:         "eth0",
 						Flags:        net.FlagUp,
 						OperState:    netlink.OperUp,
@@ -439,20 +346,19 @@ func TestShowLinks(t *testing.T) {
 							TxDropped: 2,
 						},
 					},
-				},
-			},
-			addresses: [][]netlink.Addr{
-				{
-					{
-						IPNet: &net.IPNet{
-							IP:   net.IPv4(192, 168, 1, 1),
-							Mask: net.CIDRMask(24, 32),
+					typeName: "device",
+					addresses: []netlink.Addr{
+						{
+							IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 1),
+								Mask: net.CIDRMask(24, 32),
+							},
+							Broadcast:   net.IPv4(192, 168, 1, 255),
+							Scope:       int(netlink.SCOPE_HOST),
+							Label:       "eth0",
+							PreferedLft: 0,
+							ValidLft:    0,
 						},
-						Broadcast:   net.IPv4(192, 168, 1, 255),
-						Scope:       int(netlink.SCOPE_HOST),
-						Label:       "eth0",
-						PreferedLft: 0,
-						ValidLft:    0,
 					},
 				},
 			},
@@ -466,6 +372,7 @@ func TestShowLinks(t *testing.T) {
 `,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var out bytes.Buffer
@@ -474,7 +381,7 @@ func TestShowLinks(t *testing.T) {
 				Opts: tt.opts,
 			}
 
-			err := cmd.showLinks(tt.addresses, tt.links, tt.filter...)
+			err := cmd.printLinks(false, tt.links)
 			if err != nil {
 				t.Fatalf("showLinks() error = %v", err)
 			}
