11c5539f8f4385df577e575fbfc11ccaed93dbf4
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2025-03-11 07:32:56 +0200

    pkg/ls: reduce code duplication
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- pkg/ls/fileinfo_unix.go
+++ pkg/ls/fileinfo_unix.go
@@ -10,8 +10,6 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"os/user"
-	"regexp"
 	"strconv"
 	"strings"
 	"syscall"
@@ -20,9 +18,6 @@ import (
 	humanize "github.com/dustin/go-humanize"
 	"golang.org/x/sys/unix"
 )
-
-// Matches characters which would interfere with ls's formatting.
-var unprintableRe = regexp.MustCompile("[[:cntrl:]\n]")
 
 // FileInfo holds file metadata.
 //
@@ -70,73 +65,6 @@ func FromOSFileInfo(path string, fi os.FileInfo) FileInfo {
 		MTime:         fi.ModTime(),
 		SymlinkTarget: link,
 	}
-}
-
-// PrintableName returns a printable file name.
-func (fi FileInfo) PrintableName() string {
-	return unprintableRe.ReplaceAllLiteralString(fi.Name, "?")
-}
-
-// Without this cache, `ls -l` is orders of magnitude slower.
-var (
-	uidCache = map[uint32]string{}
-	gidCache = map[uint32]string{}
-)
-
-// Convert uid to username, or return uid on error.
-func lookupUserName(id uint32) string {
-	if s, ok := uidCache[id]; ok {
-		return s
-	}
-	s := fmt.Sprint(id)
-	if u, err := user.LookupId(s); err == nil {
-		s = u.Username
-	}
-	uidCache[id] = s
-	return s
-}
-
-// Convert gid to group name, or return gid on error.
-func lookupGroupName(id uint32) string {
-	if s, ok := gidCache[id]; ok {
-		return s
-	}
-	s := fmt.Sprint(id)
-	if g, err := user.LookupGroupId(s); err == nil {
-		s = g.Name
-	}
-	gidCache[id] = s
-	return s
-}
-
-// Stringer provides a consistent way to format FileInfo.
-type Stringer interface {
-	// FileString formats a FileInfo.
-	FileString(fi FileInfo) string
-}
-
-// NameStringer is a Stringer implementation that just prints the name.
-type NameStringer struct{}
-
-// FileString implements Stringer.FileString and just returns fi's name.
-func (ns NameStringer) FileString(fi FileInfo) string {
-	return fi.PrintableName()
-}
-
-// QuotedStringer is a Stringer that returns the file name surrounded by qutoes
-// with escaped control characters.
-type QuotedStringer struct{}
-
-// FileString returns the name surrounded by quotes with escaped control characters.
-func (qs QuotedStringer) FileString(fi FileInfo) string {
-	return fmt.Sprintf("%#v", fi.Name)
-}
-
-// LongStringer is a Stringer that returns the file info formatted in `ls -l`
-// long format.
-type LongStringer struct {
-	Human bool
-	Name  Stringer
 }
 
 // FileString implements Stringer.FileString.
--- pkg/ls/ls.go
+++ pkg/ls/ls.go
@@ -4,3 +4,46 @@
 
 // Package ls implements formatting tools to list files like the Linux ls tool.
 package ls
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// Matches characters which would interfere with ls's formatting.
+var unprintableRe = regexp.MustCompile("[[:cntrl:]\n]")
+
+// PrintableName returns a printable file name.
+func (fi FileInfo) PrintableName() string {
+	return unprintableRe.ReplaceAllLiteralString(fi.Name, "?")
+}
+
+// Stringer provides a consistent way to format FileInfo.
+type Stringer interface {
+	// FileString formats a FileInfo.
+	FileString(fi FileInfo) string
+}
+
+// NameStringer is a Stringer implementation that just prints the name.
+type NameStringer struct{}
+
+// FileString implements Stringer.FileString and just returns fi's name.
+func (ns NameStringer) FileString(fi FileInfo) string {
+	return fi.PrintableName()
+}
+
+// QuotedStringer is a Stringer that returns the file name surrounded by qutoes
+// with escaped control characters.
+type QuotedStringer struct{}
+
+// FileString returns the name surrounded by quotes with escaped control characters.
+func (qs QuotedStringer) FileString(fi FileInfo) string {
+	return fmt.Sprintf("%#v", fi.Name)
+}
+
+// LongStringer is a Stringer that returns the file info formatted in `ls -l`
+// long format.
+type LongStringer struct {
+	Human bool
+	Name  Stringer
+}
