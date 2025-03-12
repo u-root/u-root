a00fb8474db185d80b61b551ba7922c33e3c6b0e
Laszlo Ersek <laszlo.ersek@9elements.com>
2025-03-12 18:12:28 +0100

    netstat_linux: update whitespace in help text
    The earlier patch
    
      netstat_linux: correct "--interface" typo in help text
    
    shrank the left-hand side column of the option documentation; adjust the
    right-hand side column now.
    
    "git show -b" produces no output for this patch.
    
    Signed-off-by: Laszlo Ersek <laszlo.ersek@9elements.com>

--- cmds/core/netstat/netstat_linux.go
+++ cmds/core/netstat/netstat_linux.go
@@ -23,30 +23,30 @@ var (
        netstat [-vWnNcaeol] [<Socket> ...]
        netstat { [-vWeenNac] -I[<Iface>] | [-veenNac] -i | [-cnNe] -M | -s [-6tuw] } [delay]
 
-        -r, --route              display routing table
+        -r, --route             display routing table
         -I, --interface=<Iface> display interface table for <Iface>
-        -i, --interfaces         display interface table
-        -g, --groups             display multicast group memberships
-        -s, --statistics         display networking statistics (like SNMP)
-        -M, --masquerade         display masqueraded connections
+        -i, --interfaces        display interface table
+        -g, --groups            display multicast group memberships
+        -s, --statistics        display networking statistics (like SNMP)
+        -M, --masquerade        display masqueraded connections
 
-        -v, --verbose            be verbose
-        -W, --wide               don't truncate IP addresses
-        -n, --numeric            don't resolve names
-        --numeric-hosts          don't resolve host names
-        --numeric-ports          don't resolve port names
-        --numeric-users          don't resolve user names
-        -N, --symbolic           resolve hardware names
-        -e, --extend             display other/more information
-        -p, --programs           display PID/Program name for sockets
-        -o, --timers             display timers
-        -c, --continuous         continuous listing
+        -v, --verbose           be verbose
+        -W, --wide              don't truncate IP addresses
+        -n, --numeric           don't resolve names
+        --numeric-hosts         don't resolve host names
+        --numeric-ports         don't resolve port names
+        --numeric-users         don't resolve user names
+        -N, --symbolic          resolve hardware names
+        -e, --extend            display other/more information
+        -p, --programs          display PID/Program name for sockets
+        -o, --timers            display timers
+        -c, --continuous        continuous listing
 
-        -l, --listening          display listening server sockets
-        -a, --all                display all sockets (default: connected)
-        -F, --fib                display Forwarding Information Base (default)
-        -C, --cache              display routing cache instead of FIB
-        -Z, --context            display SELinux security context for sockets
+        -l, --listening         display listening server sockets
+        -a, --all               display all sockets (default: connected)
+        -F, --fib               display Forwarding Information Base (default)
+        -C, --cache             display routing cache instead of FIB
+        -Z, --context           display SELinux security context for sockets
 
   <Socket>={-t|--tcp} {-u|--udp} {-U|--udplite} {-S|--sctp} {-w|--raw}
            {-x|--unix} --ax25 --ipx --netrom
