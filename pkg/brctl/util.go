12a4004ed56f7e2ae2dffc9153b499b073195d7d
Jens Drenhaus <jens.drenhaus@9elements.com>
2025-03-27 00:38:24 +0100

    brctl: fix showstp
    This extends the implementation of the brctl sub command. It now
    also prints the missing information on the respective ports of a
    bridge.
    
    Further some minor refactoring of the related codeis done, like
    name adaptions or removing unused code.
    
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- pkg/brctl/util.go
+++ pkg/brctl/util.go
@@ -23,31 +23,253 @@ import (
 
 var errno0 = syscall.Errno(0)
 
-// BridgeInfo contains information about a bridge
+// Info contains details about a bridge device and its ports.
 // This information is not exhaustive, only the most important fields are included
-// Feel free to add more fields if needed.
-type BridgeInfo struct {
-	Name                     string
-	DesignatedRoot           string
-	BridgeID                 string
-	RootPathCost             int
-	MaxAge                   unix.Timeval
-	HelloTime                unix.Timeval
-	ForwardDelay             unix.Timeval
-	BridgeMaxAge             unix.Timeval
-	BridgeHelloTime          unix.Timeval
-	BridgeForwardDelay       unix.Timeval
-	RootPort                 uint16
-	StpEnabled               bool
-	TrillEnabled             bool
-	TopologyChange           bool
-	TopologyChangeDetected   bool
-	AgingTime                unix.Timeval
-	HelloTimerValue          unix.Timeval
-	TCNTimerValue            unix.Timeval
-	TopologyChangeTimerValue unix.Timeval
-	GCTimerValue             unix.Timeval
-	Interfaces               []string
+type Info struct {
+	Name                   string
+	RootID                 string
+	BridgeID               string
+	RootPathCost           int
+	MaxAge                 unix.Timeval
+	HelloTime              unix.Timeval
+	ForwardDelay           unix.Timeval
+	BridgeMaxAge           unix.Timeval
+	BridgeHelloTime        unix.Timeval
+	BridgeForwardDelay     unix.Timeval
+	RootPort               uint16
+	StpEnabled             bool
+	TopologyChange         bool
+	TopologyChangeDetected bool
+	AgingTime              unix.Timeval
+	HelloTimer             unix.Timeval
+	TCNTimer               unix.Timeval
+	TopologyChangeTimer    unix.Timeval
+	GCTimer                unix.Timeval
+	Interfaces             []PortInfo
+}
+
+// NewInfo returns a new Info struct populated with the details of the bridge
+// device with the given name and its ports. Information is read from sysfs.
+func NewInfo(name string) (Info, error) {
+	info := Info{
+		Name: name,
+	}
+	var err error
+
+	basePath := filepath.Join(BRCTL_SYS_NET, name, BRCTL_BRIDGE_DIR)
+
+	info.RootID, err = readString(basePath, BRCTL_ROOT_ID)
+	if err != nil {
+		return Info{}, fmt.Errorf("root id: %w", err)
+	}
+
+	info.BridgeID, err = readString(basePath, BRCTL_BRIDGE_ID)
+	if err != nil {
+		return Info{}, fmt.Errorf("bridge id: %w", err)
+	}
+
+	info.RootPathCost, err = readInt(basePath, BRCTL_ROOT_PATH_COST)
+	if err != nil {
+		return Info{}, fmt.Errorf("root path cost: %w", err)
+	}
+
+	info.MaxAge, err = readTimeVal(basePath, BRCTL_MAX_AGE)
+	if err != nil {
+		return Info{}, fmt.Errorf("max age: %w", err)
+	}
+
+	info.BridgeMaxAge, err = readTimeVal(basePath, BRCTL_MAX_AGE)
+	if err != nil {
+		return Info{}, fmt.Errorf("bridge max age: %w", err)
+	}
+
+	info.HelloTime, err = readTimeVal(basePath, BRCTL_HELLO_TIME)
+	if err != nil {
+		return Info{}, fmt.Errorf("hello time: %w", err)
+	}
+
+	info.BridgeHelloTime, err = readTimeVal(basePath, BRCTL_HELLO_TIME)
+	if err != nil {
+		return Info{}, fmt.Errorf("bridge hello time: %w", err)
+	}
+
+	info.ForwardDelay, err = readTimeVal(basePath, BRCTL_FORWARD_DELAY)
+	if err != nil {
+		return Info{}, fmt.Errorf("forward delay: %w", err)
+	}
+
+	info.BridgeForwardDelay, err = readTimeVal(basePath, BRCTL_FORWARD_DELAY)
+	if err != nil {
+		return Info{}, fmt.Errorf("bridge forward delay: %w", err)
+	}
+
+	info.AgingTime, err = readTimeVal(basePath, BRCTL_AGEING_TIME)
+	if err != nil {
+		return Info{}, fmt.Errorf("aging time: %w", err)
+	}
+
+	info.HelloTimer, err = readTimeVal(basePath, BRCTL_HELLO_TIMER)
+	if err != nil {
+		return Info{}, fmt.Errorf("hello timer: %w", err)
+	}
+
+	info.TCNTimer, err = readTimeVal(basePath, BRCTL_TCN_TIMER)
+	if err != nil {
+		return Info{}, fmt.Errorf("tcn timer: %w", err)
+	}
+
+	info.TopologyChangeTimer, err = readTimeVal(basePath, BRCTL_TOPOLOGY_CHANGE_TIMER)
+	if err != nil {
+		return Info{}, fmt.Errorf("topology change timer: %w", err)
+	}
+
+	info.GCTimer, err = readTimeVal(basePath, BRCTL_GC_TIMER)
+	if err != nil {
+		return Info{}, fmt.Errorf("gc timer: %w", err)
+	}
+
+	rootPort, err := readInt(basePath, BRCTL_ROOT_PORT)
+	if err != nil {
+		return Info{}, fmt.Errorf("root port: %w", err)
+	}
+
+	if rootPort > 0xFFFF {
+		return Info{}, fmt.Errorf("root port %w", strconv.ErrRange)
+	}
+	info.RootPort = uint16(rootPort)
+
+	info.StpEnabled, err = readBool(basePath, BRCTL_STP_STATE)
+	if err != nil {
+		return Info{}, fmt.Errorf("stp state: %w", err)
+	}
+
+	info.TopologyChange, err = readBool(basePath, BRCTL_TOPOLOGY_CHANGE)
+	if err != nil {
+		return Info{}, fmt.Errorf("topology change: %w", err)
+	}
+
+	info.TopologyChangeDetected, err = readBool(basePath, BRCTL_TOPOLOGY_CHANGE_DETECTED)
+	if err != nil {
+		return Info{}, fmt.Errorf("topology change detected: %w", err)
+	}
+
+	// get respective ports from sysfs
+	interfaceDir, err := os.ReadDir(filepath.Join(BRCTL_SYS_NET, name, BRCTL_BRIDGE_INTERFACE_DIR))
+	if err != nil {
+		return Info{}, fmt.Errorf("listing bridge interfaces: %w", err)
+	}
+
+	info.Interfaces = make([]PortInfo, 0, len(interfaceDir))
+	for i := range interfaceDir {
+		bridgeName := name
+		portName := interfaceDir[i].Name()
+		portInfo, err := NewPortInfo(bridgeName, portName)
+		if err != nil {
+			return Info{}, fmt.Errorf("getting port info for %q: %w", portName, err)
+		}
+		info.Interfaces = append(info.Interfaces, portInfo)
+	}
+
+	return info, nil
+
+}
+
+type PortInfo struct {
+	Name              string
+	PortID            string
+	PortNumber        int
+	State             int
+	PathCost          int
+	DesignatedRoot    string
+	DesignatedCost    int
+	DesignatedBridge  string
+	DesignatedPort    string
+	MessageAgeTimer   unix.Timeval
+	ForwardDelayTimer unix.Timeval
+	HoldTimer         unix.Timeval
+	HairpinMode       bool
+}
+
+// NewPortInfo returns a new PortInfo struct populated with the details of the
+// port with the given name on the bridge device with the given name. Information
+// is read from sysfs.
+func NewPortInfo(bridge, port string) (PortInfo, error) {
+	info := PortInfo{
+		Name: port,
+	}
+
+	basePath := filepath.Join(BRCTL_SYS_NET, bridge, BRCTL_BRIDGE_INTERFACE_DIR, port)
+
+	var err error
+
+	info.PortID, err = readString(basePath, BRCTL_PORT_ID)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("port id: %w", err)
+	}
+
+	portNumberStr, err := readString(basePath, BRCTL_PORT_NO)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("port number: %w", err)
+	}
+
+	portNumberStr = strings.TrimPrefix(portNumberStr, "0x")
+	portNumber, err := strconv.ParseUint(portNumberStr, 16, 16)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("port number: %w", err)
+	}
+	info.PortNumber = int(portNumber)
+
+	info.State, err = readInt(basePath, BRCTL_PORT_STATE)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("port state: %w", err)
+	}
+
+	info.PathCost, err = readInt(basePath, BRCTL_PATH_COST)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("path cost: %w", err)
+	}
+
+	info.DesignatedRoot, err = readString(basePath, BRCTL_DESIGNATED_ROOT)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("designated root: %w", err)
+	}
+
+	info.DesignatedCost, err = readInt(basePath, BRCTL_DESIGNATED_COST)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("designated cost: %w", err)
+	}
+
+	info.DesignatedBridge, err = readString(basePath, BRCTL_DESIGNATED_BRIDGE)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("designated bridge: %w", err)
+	}
+
+	info.DesignatedPort, err = readString(basePath, BRCTL_DESIGNATED_PORT)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("designated port: %w", err)
+	}
+
+	info.MessageAgeTimer, err = readTimeVal(basePath, BRCTL_MSG_AGE_TIMER)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("message age timer: %w", err)
+	}
+
+	info.ForwardDelayTimer, err = readTimeVal(basePath, BRCTL_FORWARD_DELAY_TIMER)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("forward delay timer: %w", err)
+	}
+
+	info.HoldTimer, err = readTimeVal(basePath, BRCTL_HOLD_TIMER)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("hold timer: %w", err)
+	}
+
+	info.HairpinMode, err = readBool(basePath, BRCTL_HAIRPIN)
+	if err != nil {
+		return PortInfo{}, fmt.Errorf("hairpin mode: %w", err)
+	}
+
+	return info, nil
 }
 
 func sysconfhz() (int, error) {
@@ -154,7 +376,7 @@ func stringToJiffies(in string) (int, error) {
 	return int(tv.Seconds() * float64(hz)), nil
 }
 
-func readID(p string, obj string) (string, error) {
+func readString(p string, obj string) (string, error) {
 	ret, err := os.ReadFile(filepath.Join(p, obj))
 	if err != nil {
 		return "", fmt.Errorf("os.ReadFile: %w", err)
