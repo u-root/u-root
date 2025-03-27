0920a1f4ecf24b8676141a324b821731e8cfc722
Laszlo Ersek <laszlo.ersek@9elements.com>
2025-03-27 21:17:03 +0100

    integration/generic-tests/netstat_test: work around gosh redirection bug
    Gosh -- at least the version we currently use in u-root -- does not
    properly bequeath file descriptors to a command that is backgrounded in a
    subshell. Consider the following script:
    
      rm -f wc.out
      sleep 3 | { wc -l >wc.out & } &
      sleep 2
      cat wc.out
      echo END
    
    When executed by bash, this script (correctly) prints
    
      END
    
    When executed by gosh, the script (incorrectly) prints
    
      wc: 'standard input': Bad file descriptor
      wc: -: Bad file descriptor
      0
      END
    
    The problem is that gosh does not allow "wc" to inherit, as its stdin, the
    pipe that connects its parent shell with "sleep 3". Therefore, "wc"
    immediately encounters a read error and writes its output "0" to "wc.out".
    The correct behavior is that, right after the "sleep 2", the file "wc.out"
    exists, but is empty; "wc" is still blocked on the pipe.
    
    This gosh bug affects our netstat integration test. In that test, we start
    several netcat processes (in connect mode) backgrounded in a subshell
    (like "wc" is, above). Reading stdin in each such netcat process is
    supposed to block on a pipe. That's not what happens though; the netcat
    processes actually see EOF (or EBADF) on stdin immediately.
    
    Thus far, this has been masked, because netcat in connect mode hasn't
    forwarded EOF from stdin to the socket. The upcoming patch
    
      cmds/core/netcat: forward EOF in connect mode
    
    in this series fixes that though, and exposes the gosh bug. Said netcat
    clients start forwarding EOF to the corresponding netcat servers at once,
    which causes those servers to close the connections, which in turn causes
    the netcat clients to exit. As a result, the "netstat" output just doesn't
    show the established connections that we expect in the integration test
    (and the various packet counters are off as well, due to the uncalled-for
    TCP FIN packets).
    
    Work around the gosh bug by spelling out the redirections for each
    backgrounded netcat command individually.
    
    Signed-off-by: Laszlo Ersek <laszlo.ersek@9elements.com>

--- integration/generic-tests/netstat_test.go
+++ integration/generic-tests/netstat_test.go
@@ -156,25 +156,22 @@ func TestNetstat(t *testing.T) {
 			echo ::1 localhost6
 		} >>/etc/hosts
 
-		{
-			netcat --keep-open --listen -4                 127.0.0.1 5005 &
-			netcat --keep-open --listen -4         --udp   127.0.0.1 5005 &
-			netcat --keep-open --listen -6                 ::1       5005 &
-			netcat --keep-open --listen -6         --udp   ::1       5005 &
-			netcat --keep-open --listen --unixsock         stream.sock    &
-			netcat --keep-open --listen --unixsock --udp   datagram.sock  &
-		} </dev/null >/dev/null
+		netcat --keep-open --listen -4                 127.0.0.1 5005 </dev/null >/dev/null &
+		netcat --keep-open --listen -4         --udp   127.0.0.1 5005 </dev/null >/dev/null &
+		netcat --keep-open --listen -6                 ::1       5005 </dev/null >/dev/null &
+		netcat --keep-open --listen -6         --udp   ::1       5005 </dev/null >/dev/null &
+		netcat --keep-open --listen --unixsock         stream.sock    </dev/null >/dev/null &
+		netcat --keep-open --listen --unixsock --udp   datagram.sock  </dev/null >/dev/null &
 		sleep 2
 
-		sleep 3600 \
-		| {
-			netcat -4                 127.0.0.1 5005 &
-			netcat -4         --udp   127.0.0.1 5005 &
-			netcat -6                 ::1       5005 &
-			netcat -6         --udp   ::1       5005 &
-			netcat --unixsock         stream.sock    &
-			netcat --unixsock --udp   datagram.sock  &
-		} >/dev/null &
+		mkfifo fifo
+		sleep 3600 >fifo &
+		netcat -4                 127.0.0.1 5005 <fifo >/dev/null &
+		netcat -4         --udp   127.0.0.1 5005 <fifo >/dev/null &
+		netcat -6                 ::1       5005 <fifo >/dev/null &
+		netcat -6         --udp   ::1       5005 <fifo >/dev/null &
+		netcat --unixsock         stream.sock    <fifo >/dev/null &
+		netcat --unixsock --udp   datagram.sock  <fifo >/dev/null &
 		sleep 2
 	`)
 	for _, test := range tests {
@@ -186,6 +183,7 @@ func TestNetstat(t *testing.T) {
 			uimage.WithBusyboxCommands(
 				"github.com/u-root/u-root/cmds/core/echo",
 				"github.com/u-root/u-root/cmds/core/ip",
+				"github.com/u-root/u-root/cmds/core/mkfifo",
 				"github.com/u-root/u-root/cmds/core/netcat",
 				"github.com/u-root/u-root/cmds/core/ping",
 				"github.com/u-root/u-root/cmds/core/sleep",
