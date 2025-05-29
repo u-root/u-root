e28f1ad2c4748a3d78e09bdaece4f50a914434fb
leongross <leon.gross@9elements.com>
2025-05-29 12:47:39 +0200

    cmds/exp: traceroute: add udpv6 fallback option for local networks
    Signed-off-by: leongross <leon.gross@9elements.com>

--- pkg/traceroute/util.go
+++ pkg/traceroute/util.go
@@ -7,6 +7,7 @@ package traceroute
 import (
 	"fmt"
 	"net"
+	"os"
 	"strings"
 )
 
@@ -45,15 +46,63 @@ func DestAddr(dest, proto string) (net.IP, error) {
 	return ipAddr.IP, nil
 }
 
+func isVirtual(iface net.Interface) bool {
+	dev := "/sys/devices/virtual/net/" + iface.Name
+	if _, err := os.Stat(dev); err == nil {
+		return true
+	}
+	return false
+}
+
 func SrcAddr(proto string) (*net.IP, error) {
 	var sAddr net.Addr
+	var found bool
+
 	if strings.Contains(proto, "6") {
+		// this assumes connection to a well-known DNS server
 		conn, err := net.Dial("udp6", "[2001:4860:4860::8844]:53")
 		if err != nil {
-			return nil, err
+			ifaces, err := net.Interfaces()
+			if err != nil {
+				return nil, fmt.Errorf("failed to get interfaces: %v", err)
+			}
+
+			for _, i := range ifaces {
+				if found {
+					break
+				}
+
+				addrs, err := i.Addrs()
+				if err != nil {
+					return nil, err
+				}
+				for _, addr := range addrs {
+					var ip net.IP
+					if addr.(*net.IPNet).IP.To4() != nil || isVirtual(i) {
+						continue // skip IPv4 addresses
+					}
+
+					switch v := addr.(type) {
+					case *net.IPNet:
+						ip = v.IP
+						found = true
+					case *net.IPAddr:
+						ip = v.IP
+						found = true
+					}
+					if found {
+						sAddr = &net.UDPAddr{IP: ip, Port: 0}
+						break
+					}
+				}
+			}
+			if !found {
+				return nil, fmt.Errorf("no suitable IPv6 address found for traceroute")
+			}
+		} else {
+			sAddr = conn.LocalAddr().(*net.UDPAddr)
+			conn.Close()
 		}
-		sAddr = conn.LocalAddr().(*net.UDPAddr)
-		conn.Close()
 	} else {
 		conn, err := net.Dial("udp", "8.8.8.8:53")
 		if err != nil {
