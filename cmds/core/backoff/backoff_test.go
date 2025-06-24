7af71d1432c82837baa82eb9293eaffac48678d8
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2025-06-24 12:42:46 +0300

    cmds/core/backoff: fix 30s default timeout
    Use time.Duration instead of string, fix documentation, remove parsing
    test that are no longer needed, rename files according to other u-root
    commands, make verbose false by default to match documentation.
    
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/backoff/backoff_test.go
+++ cmds/core/backoff/backoff_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 the u-root Authors. All rights reserved
+// Copyright 2021-2025 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -7,10 +7,10 @@ package main
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/u-root/u-root/pkg/testutil"
 )
@@ -18,38 +18,31 @@ import (
 func TestRunIt(t *testing.T) {
 	for _, tt := range []struct {
 		name    string
-		timeout string
+		timeout time.Duration
 		cmd     string
 		args    []string
 		wantErr error
 	}{
 		{
 			name:    "_date",
-			timeout: "3s",
+			timeout: 3 * time.Second,
 			cmd:     "date",
 		},
 		{
 			name:    "noCmd",
-			timeout: "3s",
+			timeout: 3 * time.Second,
 			cmd:     "",
-			wantErr: ErrNoCmd,
+			wantErr: errNoCmd,
 		},
 		{
 			name:    "echo",
-			timeout: "3s",
+			timeout: 3 * time.Second,
 			cmd:     "echo",
 			args:    []string{"hi"},
-		},
-		{
-			name:    "echo_missing_unit",
-			timeout: "3",
-			cmd:     "echo",
-			args:    []string{"hi"},
-			wantErr: fmt.Errorf("time: missing unit in duration \"3\""),
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := runit(tt.timeout, tt.cmd, tt.args...); !errors.Is(err, tt.wantErr) {
+			if err := run(tt.timeout, tt.cmd, tt.args...); !errors.Is(err, tt.wantErr) {
 				if err != nil {
 					if !strings.Contains(err.Error(), tt.wantErr.Error()) {
 						t.Errorf("runit(%s, %s, %s)= %q, want %q", tt.timeout, tt.cmd, tt.args, err, tt.wantErr)
@@ -70,9 +63,8 @@ func TestOK(t *testing.T) {
 	}{
 		{args: []string{}, stdout: "", exitok: false},
 		{args: []string{"date"}, stdout: ".*", exitok: true},
-		{args: []string{"-t", "wh", "date"}, stdout: ".*", stderr: ".*invalid.*duration.*wh", exitok: false},
 		{args: []string{"echo", "hi"}, stdout: ".*hi", exitok: true},
-		{args: []string{"-t", "3s", "false"}, exitok: false},
+		{args: []string{"-t", "1s", "false"}, exitok: false},
 	}
 
 	for _, v := range tests {
