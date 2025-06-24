3fda59b41e80dd273c85ca625975ae1acf17f4e2
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2025-06-24 12:42:46 +0300

    cmds/core/backoff: fix 30s default timeout
    Use time.Duration instead of string, fix documentation, remove parsing
    test that are no longer needed, rename files according to other u-root
    commands, make verbose false by default to match documentation.
    
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/backoff/backoff.go
+++ cmds/core/backoff/backoff.go
@@ -1,4 +1,4 @@
-// Copyright 2012-2021 the u-root Authors. All rights reserved
+// Copyright 2012-2025 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -11,13 +11,13 @@
 //	backoff will run the command until it succeeds or a timeout has passed.
 //	The default timeout is 30s.
 //	If -v is set, it will show what it is running, each time it is tried.
-//	If no args are given, it will just return.
+//	If no args are given, it will print command help.
 //
 // Example:
 //	$ backoff echo hi
 //	hi
 //	$
-//	$ backoff -v -t 2s false
+//	$ backoff -v -t=2s false
 //	  2022/03/31 14:29:37 Run ["false"]
 //	  2022/03/31 14:29:37 Set timeout to 2s
 //	  2022/03/31 14:29:37 "false" []:exit status 1
@@ -31,15 +31,39 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
 )
 
 var (
-	timeout = flag.String("t", "", "Timeout for command")
-	verbose = flag.Bool("v", true, "Log each attempt to run the command")
-	v       = func(string, ...interface{}) {}
+	timeout = flag.Duration("t", 30*time.Second, "Timeout for command")
+	verbose = flag.Bool("v", false, "Log each attempt to run the command")
+	v       = func(string, ...any) {}
+
+	errNoCmd = fmt.Errorf("no command passed")
 )
+
+func run(timeout time.Duration, c string, a ...string) error {
+	if c == "" {
+		return errNoCmd
+	}
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = timeout
+	f := func() error {
+		cmd := exec.Command(c, a...)
+		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+		err := cmd.Run()
+		v("%q %q:%v", c, a, err)
+		return err
+	}
+
+	return backoff.Retry(f, b)
+}
 
 func main() {
 	flag.Parse()
@@ -52,7 +76,7 @@ func main() {
 		os.Exit(1)
 	}
 	v("Run %q", a)
-	if err := runit(*timeout, a[0], a[1:]...); err != nil {
+	if err := run(*timeout, a[0], a[1:]...); err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 }
