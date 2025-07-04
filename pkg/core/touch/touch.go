ea0e839294459d447dfc1717fcb86a195785620d
Andrey Nering <andreynering@users.noreply.github.com>
2025-07-04 15:33:11 -0300

    feat(touch): refactor `touch` core util to use the new api design
    Signed-off-by: Andrey Nering <andreynering@users.noreply.github.com>

---
+++ pkg/core/touch/touch.go
@@ -0,0 +1,149 @@
+// Copyright 2023 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package touch implements the touch core utility.
+package touch
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/u-root/u-root/pkg/core"
+)
+
+// Command implements the touch core utility.
+type Command struct {
+	core.Base
+}
+
+// New creates a new touch command.
+func New() core.Command {
+	c := &Command{}
+	c.Init()
+	return c
+}
+
+type flags struct {
+	access       bool
+	modification bool
+	create       bool
+	dateTime     string
+}
+
+type params struct {
+	time         time.Time
+	access       bool
+	modification bool
+	create       bool
+}
+
+// resolvePath resolves a path relative to the working directory.
+func (c *Command) resolvePath(path string) string {
+	if filepath.IsAbs(path) || c.WorkingDir == "" {
+		return path
+	}
+	return filepath.Join(c.WorkingDir, path)
+}
+
+// parseParams parses the command parameters and returns a params struct.
+func (c *Command) parseParams(dateTime string, access, modification, create bool) (params, error) {
+	t := time.Now()
+	if dateTime != "" {
+		var err error
+		t, err = time.Parse(time.RFC3339, dateTime)
+		if err != nil {
+			return params{}, err
+		}
+	}
+	return params{
+		access:       access || (!access && !modification),
+		modification: modification || (!access && !modification),
+		create:       create,
+		time:         t,
+	}, nil
+}
+
+// touchFiles processes the files according to the parameters.
+func (c *Command) touchFiles(p params, args []string) error {
+	var errs error
+	for _, arg := range args {
+		resolvedArg := c.resolvePath(arg)
+		_, existsErr := os.Stat(resolvedArg)
+		notExist := os.IsNotExist(existsErr)
+		if notExist {
+			if p.create {
+				continue
+			}
+
+			f, err := os.Create(resolvedArg)
+			if err != nil {
+				errs = errors.Join(errs, err)
+				continue
+			}
+			f.Close()
+		}
+
+		accessTime := time.Time{}
+		if p.access || notExist {
+			accessTime = p.time
+		}
+		modificationTime := time.Time{}
+		if p.modification || notExist {
+			modificationTime = p.time
+		}
+
+		err := os.Chtimes(resolvedArg, accessTime, modificationTime)
+		if err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+
+	return errs
+}
+
+// Run executes the touch command.
+func (c *Command) Run(ctx context.Context, args ...string) (int, error) {
+	var f flags
+
+	fs := flag.NewFlagSet("touch", flag.ContinueOnError)
+	fs.SetOutput(c.Stderr)
+
+	fs.BoolVar(&f.access, "a", false, "change only the access time")
+	fs.BoolVar(&f.modification, "m", false, "change only the modification time")
+	fs.BoolVar(&f.create, "c", false, "do not create any file if it does not exist")
+	fs.StringVar(&f.dateTime, "d", "", "use specified time instead of current time RFC3339")
+
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "Usage: touch [-amc] [-d datetime] file...\n\n")
+		fmt.Fprintf(fs.Output(), "touch changes file access and modification times.\n")
+		fmt.Fprintf(fs.Output(), "If a file does not exist, it will be created unless -c is specified.\n\n")
+		fmt.Fprintf(fs.Output(), "Options:\n")
+		fs.PrintDefaults()
+	}
+
+	if err := fs.Parse(args[1:]); err != nil {
+		return 1, err
+	}
+
+	if len(fs.Args()) == 0 {
+		fs.Usage()
+		return 1, fmt.Errorf("no files specified")
+	}
+
+	p, err := c.parseParams(f.dateTime, f.access, f.modification, f.create)
+	if err != nil {
+		return 1, err
+	}
+
+	if err := c.touchFiles(p, fs.Args()); err != nil {
+		return 1, err
+	}
+
+	return 0, nil
+}
