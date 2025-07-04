c9dcf1bd3de92fa92b68a088b5a3b68e55722b04
Andrey Nering <andreynering@users.noreply.github.com>
2025-07-04 15:22:05 -0300

    feat(cat): refactor `cat` core util to use the new api design

---
+++ pkg/core/cat/cat.go
@@ -0,0 +1,109 @@
+// Copyright 2012-2017 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package cat implements the cat core utility.
+package cat
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/u-root/u-root/pkg/core"
+)
+
+// Command implements the cat core utility.
+type Command struct {
+	core.Base
+}
+
+// New creates a new cat command.
+func New() core.Command {
+	c := &Command{}
+	c.Init()
+	return c
+}
+
+type flags struct {
+	u bool // ignored flag for compatibility
+}
+
+var errCopy = fmt.Errorf("error concatenating stdin to stdout")
+
+// resolvePath resolves a path relative to the working directory.
+func (c *Command) resolvePath(path string) string {
+	if filepath.IsAbs(path) || c.WorkingDir == "" {
+		return path
+	}
+	return filepath.Join(c.WorkingDir, path)
+}
+
+// cat copies data from reader to writer.
+func (c *Command) cat(reader io.Reader, writer io.Writer) error {
+	if _, err := io.Copy(writer, reader); err != nil {
+		return errCopy
+	}
+	return nil
+}
+
+// runCat processes the files and concatenates them to stdout.
+func (c *Command) runCat(args []string) error {
+	if len(args) == 0 {
+		return c.cat(c.Stdin, c.Stdout)
+	}
+
+	for _, file := range args {
+		if file == "-" {
+			err := c.cat(c.Stdin, c.Stdout)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		resolvedFile := c.resolvePath(file)
+		f, err := os.Open(resolvedFile)
+		if err != nil {
+			return err
+		}
+
+		if err := c.cat(f, c.Stdout); err != nil {
+			f.Close()
+			return fmt.Errorf("failed to concatenate file %s to given writer", f.Name())
+		}
+		f.Close()
+	}
+	return nil
+}
+
+// Run executes the cat command.
+func (c *Command) Run(ctx context.Context, args ...string) (int, error) {
+	var f flags
+
+	fs := flag.NewFlagSet("cat", flag.ContinueOnError)
+	fs.SetOutput(c.Stderr)
+
+	fs.BoolVar(&f.u, "u", false, "ignored")
+
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "Usage: cat [-u] [FILES]...\n\n")
+		fmt.Fprintf(fs.Output(), "cat concatenates files and prints them to stdout.\n")
+		fmt.Fprintf(fs.Output(), "If no files are specified, read from stdin.\n\n")
+		fmt.Fprintf(fs.Output(), "Options:\n")
+		fs.PrintDefaults()
+	}
+
+	if err := fs.Parse(args[1:]); err != nil {
+		return 1, err
+	}
+
+	if err := c.runCat(fs.Args()); err != nil {
+		return 1, err
+	}
+
+	return 0, nil
+}
