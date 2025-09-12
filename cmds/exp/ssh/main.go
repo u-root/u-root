bd62a4a9098765afedda126218e14e0ffaa83ee9
Ronald G. Minnich <rminnich@gmail.com>
2025-09-12 11:37:03 -0700

    Disable tinygo for cmds/exp/ssh until tinygo is 1.24.x
    Seems this will not build right now for the released tinygo.
    
    Until your tinygo is at 1.24, do not try to build it.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- cmds/exp/ssh/main.go
+++ cmds/exp/ssh/main.go
@@ -1,7 +1,7 @@
 // Copyright 2022 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-//go:build !tinygo || tinygo.enable
+//go:build !tinygo
 
 // SSH client.
 //
