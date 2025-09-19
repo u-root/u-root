e950299da3272b736200c43e5a56d05b9d1996f1
Ronald G Minnich <rminnich@gmail.com>
2025-09-19 12:18:10 -0700

    Move the ip space to 192.168.241..4 to avoid any conflict
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- integration/generic-tests-cpu/ip_linux_test.go
+++ integration/generic-tests-cpu/ip_linux_test.go
@@ -8,7 +8,6 @@ package integration
 
 import (
 	"context"
-	"errors"
 	"strings"
 	"testing"
 	"time"
@@ -48,7 +47,6 @@ func TestIP(t *testing.T) {
 		t.Fatalf("Testing kernel=linux arch=amd64: got %v, want nil", err)
 	}
 
-	// Cancel before wg.Wait(), so goroutine can exit.
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -82,44 +80,43 @@ func TestIP(t *testing.T) {
 			{cmd: "cat /sys/class/net/eth1/operstate", includes: []string{"down"}},
 			{cmd: "ip link set eth1 up", delay: 3 * time.Second},
 			{cmd: "cat /sys/class/net/eth1/operstate", includes: []string{"up"}},
-			{cmd: []string{"ip", "addr", "add", "192.168.1.1/24", "dev", "eth1"}},
-			{cmd: []string{"cat", "/proc/net/fib_trie"}, includes: []string{"192.168.1.1"}},
-			{cmd: "ip route add 192.168.2.0/24 via 192.168.1.1 dev eth1"},
-			{cmd: "cat /proc/net/route", includes: []string{"0002A8C0"}},
-			{cmd: "cat /proc/net/route", includes: []string{"0101A8C0"}},
-			{cmd: "ip route del 192.168.2.0/24"},
-			{cmd: "cat /proc/net/route", excludes: []string{"0002A8C0", "0101A8C0"}},
-			{cmd: "ip tunnel add my_test_tunnel mode sit remote 192.168.2.1 local 192.168.1.1 ttl 64"},
+			{cmd: []string{"ip", "addr", "add", "192.168.241.1/24", "dev", "eth1"}},
+			{cmd: []string{"cat", "/proc/net/fib_trie"}, includes: []string{"192.168.241.1"}},
+			{cmd: "ip route add 192.168.242.0/24 via 192.168.241.1 dev eth1"},
+			{cmd: "cat /proc/net/route", includes: []string{"00F2A8C0", "01F1A8C0"}},
+			{cmd: "ip route del 192.168.242.0/24"},
+			{cmd: "cat /proc/net/route", excludes: []string{"00F2A8C0", "01F1A8C0"}},
+			{cmd: "ip tunnel add my_test_tunnel mode sit remote 192.168.242.1 local 192.168.241.1 ttl 64"},
 			{cmd: "cat /proc/net/dev", includes: []string{"my_test_tunnel"}},
 			{cmd: "ip tunnel del my_test_tunnel"},
 			{cmd: "cat /proc/net/dev", excludes: []string{"my_test_tunnel"}},
-			{cmd: "ip tunnel add my_test_tunnel mode sit remote 192.168.2.1 local 192.168.1.1 ttl 64"},
-			{cmd: "ip tunnel show my_test_tunnel", includes: []string{"my_test_tunnel", "remote 192.168.2.1", "local 192.168.1.1", "ttl 64"}},
+			{cmd: "ip tunnel add my_test_tunnel mode sit remote 192.168.242.1 local 192.168.241.1 ttl 64"},
+			{cmd: "ip tunnel show my_test_tunnel", includes: []string{"my_test_tunnel", "remote 192.168.242.1", "local 192.168.241.1", "ttl 64"}},
 			{cmd: "ip tunnel del my_test_tunnel"},
 			{cmd: "cat /proc/net/dev", excludes: []string{"my_test_tunnel"}},
 		},
 		{
 			// Various tunnel tests.
 			// Add a GRE tunnel with key and tos options
-			{cmd: "ip tunnel add gre_tunnel mode gre remote 192.168.2.2 local 192.168.1.1 ttl 128 key 1234 tos 10", failok: true},
+			{cmd: "ip tunnel add gre_tunnel mode gre remote 192.168.242.2 local 192.168.241.1 ttl 128 key 1234 tos 10", failok: false},
 			{cmd: "cat /proc/net/dev", includes: []string{"gre_tunnel"}},
 			// Verify GRE tunnel parameters
-			{cmd: "ip tunnel show gre_tunnel", includes: []string{"gre_tunnel:", "remote 192.168.2.2", "local 192.168.1.1", "ttl 128", "key 1234", "tos 0xa"}},
+			{cmd: "ip tunnel show gre_tunnel", includes: []string{"gre_tunnel:", "remote 192.168.242.2", "local 192.168.241.1", "ttl 128", "key 1234", "tos 0xa"}},
 			{cmd: "ip link set gre_tunnel up"},
 			{cmd: "ip addr add 10.0.0.1/24 dev gre_tunnel"},
-			{cmd: []string{"cat", "/proc/net/fib_trie"}, excludes: []string{"10.0.0.1"}},
+			{cmd: []string{"cat", "/proc/net/fib_trie"}, includes: []string{"10.0.0.1"}},
 			{cmd: "ip link set gre_tunnel down"},
 			{cmd: "ip tunnel del gre_tunnel"},
 			{cmd: "cat /proc/net/dev", excludes: []string{"gre_tunnel"}},
 		},
 		{
-			{cmd: "ip tunnel add vti_tunnel mode vti remote 192.168.2.3 local 192.168.1.1 key 5678", failok: true},
+			{cmd: "ip tunnel add vti_tunnel mode vti remote 192.168.242.3 local 192.168.241.1 key 5678", failok: true},
 
 			// Verify VTI tunnel exists in /proc/net/dev
 			{cmd: "cat /proc/net/dev", includes: []string{"vti_tunnel"}},
 
 			//Verify VTI tunnel parameters
-			{cmd: "ip tunnel show vti_tunnel", includes: []string{"vti_tunnel:", "remote 192.168.2.3", "local 192.168.1.1", "key 5678", "tos 0xa"}},
+			{cmd: "ip tunnel show vti_tunnel", includes: []string{"vti_tunnel:", "remote 192.168.242.3", "local 192.168.241.1", "key 5678"}},
 			{cmd: "ip link set vti_tunnel up"},
 			{cmd: "ip addr add 172.16.0.1/30 dev vti_tunnel"},
 			{cmd: []string{"cat", "/proc/net/fib_trie"}, excludes: []string{"172.16.0.1"}},
@@ -128,13 +125,13 @@ func TestIP(t *testing.T) {
 			{cmd: "cat /proc/net/dev", excludes: []string{"vti_tunnel"}},
 		},
 		{
-			{cmd: "ip tunnel add ipip_tunnel mode ipip remote 192.168.3.1 local 192.168.1.1 ttl 64", failok: true},
+			{cmd: "ip tunnel add ipip_tunnel mode ipip remote 192.168.243.1 local 192.168.241.1 ttl 64", failok: true},
 
 			// Verify IPIP tunnel exists in /proc/net/dev
 			{cmd: "cat /proc/net/dev", includes: []string{"ipip_tunnel"}},
 
 			//Verify IPIP tunnel parameters
-			{cmd: "ip tunnel show ipip_tunnel", includes: []string{"ipip_tunnel:", "remote 192.168.3.1", "local 192.168.1.1", "ttl 64", "tos 0xa"}},
+			{cmd: "ip tunnel show ipip_tunnel", includes: []string{"ipip_tunnel:", "remote 192.168.243.1", "local 192.168.241.1", "ttl 64"}},
 			{cmd: "ip link set ipip_tunnel up"},
 			{cmd: "ip addr add 172.17.0.1/30 dev ipip_tunnel"},
 			{cmd: []string{"cat", "/proc/net/fib_trie"}, excludes: []string{"172.17.0.1"}},
@@ -144,41 +141,41 @@ func TestIP(t *testing.T) {
 		},
 		{
 			// ARP tests
-			{cmd: "ip neigh add 192.168.1.2 lladdr 00:11:22:33:44:55 dev eth1"},
-			{cmd: "cat /proc/net/arp", includes: []string{"192.168.1.2"}},
+			{cmd: "ip neigh add 192.168.241.2 lladdr 00:11:22:33:44:55 dev eth1"},
+			{cmd: "cat /proc/net/arp", includes: []string{"192.168.241.2"}},
 
 			// Verify the neighbor entry
-			{cmd: "ip neigh show dev eth1", includes: []string{"192.168.1.2", "192.168.1.2 dev eth1 lladdr 00:11:22:33:44:55 PERMANENT"}},
-			//{cmd: "test "$neigh_entry" = "192.168.1.2 dev eth1 lladdr 00:11:22:33:44:55 PERMANENT"", includes: []string{},},
+			{cmd: "ip neigh show dev eth1", includes: []string{"192.168.241.2", "192.168.241.2 dev eth1 lladdr 00:11:22:33:44:55 PERMANENT"}},
+			//{cmd: "test "$neigh_entry" = "192.168.241.2 dev eth1 lladdr 00:11:22:33:44:55 PERMANENT"", includes: []string{},},
 
 			// Replace the entry with another hwaddress, nud state and router flag
-			{cmd: "ip neigh replace 192.168.1.2 lladdr 11:22:33:44:55:66 dev eth1 nud stale router", includes: []string{}},
+			{cmd: "ip neigh replace 192.168.241.2 lladdr 11:22:33:44:55:66 dev eth1 nud stale router", includes: []string{}},
 
 			// Verify the modified flags
-			{cmd: "ip neigh show dev eth1", includes: []string{"192.168.1.2", "192.168.1.2 dev eth1 lladdr 11:22:33:44:55:66 router STALE"}},
+			{cmd: "ip neigh show dev eth1", includes: []string{"192.168.241.2", "192.168.241.2 dev eth1 lladdr 11:22:33:44:55:66 router STALE"}},
 
 			// Delete the neighbor
-			{cmd: "ip neigh del 192.168.1.2 dev eth1", includes: []string{}},
-			{cmd: "cat /proc/net/arp", excludes: []string{"192.168.1.2"}},
+			{cmd: "ip neigh del 192.168.241.2 dev eth1", includes: []string{}},
+			{cmd: "cat /proc/net/arp", excludes: []string{"192.168.241.2"}},
 
 			// Test IP Neighbor flush capability
 			// Add 3 neighbors
-			{cmd: "ip neigh add 192.168.1.5 lladdr aa:bb:cc:dd:ee:ff nud stale dev eth1"},
-			{cmd: "ip neigh add 192.168.1.6 lladdr aa:bb:cc:11:22:33 nud stale dev eth1"},
-			{cmd: "ip neigh add 192.168.1.7 lladdr aa:bb:cc:44:55:66 dev eth1"},
+			{cmd: "ip neigh add 192.168.241.5 lladdr aa:bb:cc:dd:ee:ff nud stale dev eth1"},
+			{cmd: "ip neigh add 192.168.241.6 lladdr aa:bb:cc:11:22:33 nud stale dev eth1"},
+			{cmd: "ip neigh add 192.168.241.7 lladdr aa:bb:cc:44:55:66 dev eth1"},
 
 			// Verify all entries exist
-			{cmd: "cat /proc/net/arp", includes: []string{"192.168.1.5", "192.168.1.6", "192.168.1.7"}},
+			{cmd: "cat /proc/net/arp", includes: []string{"192.168.241.5", "192.168.241.6", "192.168.241.7"}},
 
 			// Flush the 2 stale neighbors from the table for eth1
 			{cmd: "ip neigh flush dev eth1", includes: []string{}},
 
 			// Verify the 2 stale entries are gone, the permanent one remains
-			{cmd: "cat /proc/net/arp", includes: []string{"192.168.1.7"}, excludes: []string{"192.168.1.5", "192.168.1.6"}},
+			{cmd: "cat /proc/net/arp", includes: []string{"192.168.241.7"}, excludes: []string{"192.168.241.5", "192.168.241.6"}},
 
 			// Delete the IP address from eth1
-			{cmd: "ip addr del 192.168.1.1/24 dev eth1"},
-			{cmd: []string{"cat", "/proc/net/fib_trie"}, excludes: []string{"192.168.1.1"}},
+			{cmd: "ip addr del 192.168.241.1/24 dev eth1"},
+			{cmd: []string{"cat", "/proc/net/fib_trie"}, excludes: []string{"192.168.241.1"}},
 			// Bring the eth1 interface down
 			{cmd: "ip link set eth1 down", delay: 2 * time.Second},
 			{cmd: "cat /sys/class/net/eth1/operstate", includes: []string{"down"}},
