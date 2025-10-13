b9f02fd8ce7306972053609b0bb9bf48b187618d
Andrey Nering <andreynering@users.noreply.github.com>
2025-10-13 16:05:17 -0300

    refactor(gzip/gzcat): use program name instead of having a separate func
    Signed-off-by: Andrey Nering <andreynering@users.noreply.github.com>

--- pkg/core/gzip/gzip.go
+++ pkg/core/gzip/gzip.go
@@ -21,20 +21,13 @@ import (
 // Gzip implements the gzip command.
 type Gzip struct {
 	core.Base
-	cmd     string
+	prog    string
 	cmdLine *flag.FlagSet
 }
 
 // New returns a new Gzip command.
-func New() core.Command {
-	g := &Gzip{cmd: "gzip"}
-	g.Init()
-	return g
-}
-
-// NewGzcat returns a new Gzip command.
-func NewGzcat() core.Command {
-	g := &Gzip{cmd: "gzcat"}
+func New(prog string) core.Command {
+	g := &Gzip{prog: prog}
 	g.Init()
 	return g
 }
@@ -47,14 +40,14 @@ func (g *Gzip) Run(args ...string) error {
 // RunContext executes the gzip command with the given arguments and context.
 func (g *Gzip) RunContext(ctx context.Context, args ...string) error {
 	// Create a new flag set for each run to avoid flag redefinition errors
-	g.cmdLine = flag.NewFlagSet(g.cmd, flag.ContinueOnError)
+	g.cmdLine = flag.NewFlagSet(g.prog, flag.ContinueOnError)
 	g.cmdLine.SetOutput(g.Stderr)
 	g.cmdLine.Usage = g.usage
 
 	var opts pkggzip.Options
-	if err := opts.ParseArgs(g.cmd, args, g.cmdLine); err != nil {
+	if err := opts.ParseArgs(g.prog, args, g.cmdLine); err != nil {
 		if errors.Is(err, pkggzip.ErrStdoutNoForce) {
-			return fmt.Errorf("%s: %w", g.cmd, err)
+			return fmt.Errorf("%s: %w", g.prog, err)
 		}
 		if errors.Is(err, pkggzip.ErrHelp) {
 			g.cmdLine.Usage()
@@ -69,7 +62,7 @@ func (g *Gzip) RunContext(ctx context.Context, args ...string) error {
 }
 
 func (g *Gzip) usage() {
-	fmt.Fprintf(g.Stderr, "Usage of %s:\n", g.cmd)
+	fmt.Fprintf(g.Stderr, "Usage of %s:\n", g.prog)
 	g.cmdLine.PrintDefaults()
 }
 
