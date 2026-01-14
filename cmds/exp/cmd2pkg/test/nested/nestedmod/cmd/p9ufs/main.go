a87411af86ea5455e3192da7d9a8b8ae0a282bc1
Ronald G Minnich <rminnich@gmail.com>
2026-01-14 10:43:14 -0800

    cmd2pkg: add licenses to tests
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- cmds/exp/cmd2pkg/test/nested/nestedmod/cmd/p9ufs/main.go
+++ cmds/exp/cmd2pkg/test/nested/nestedmod/cmd/p9ufs/main.go
@@ -1,3 +1,7 @@
+// Copyright 2015-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Copyright 2018 The gVisor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
