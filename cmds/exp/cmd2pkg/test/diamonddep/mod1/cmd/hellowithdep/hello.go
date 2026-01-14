45db38e32449f516c7fb15e3cbfbe2e1fccc8deb
Ronald G Minnich <rminnich@gmail.com>
2026-01-14 10:43:14 -0800

    cmd2pkg: add licenses to tests
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- cmds/exp/cmd2pkg/test/diamonddep/mod1/cmd/hellowithdep/hello.go
+++ cmds/exp/cmd2pkg/test/diamonddep/mod1/cmd/hellowithdep/hello.go
@@ -1,3 +1,7 @@
+// Copyright 2015-2019 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // hellowithdep has an internal and external dependency, as well as an external dependency that depends on internal code.
 package main
 
