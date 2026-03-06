727542480b2110c03b7c196f9ab70c91222c1fa5
Ronald G. Minnich <rminnich@gmail.com>
2026-03-05 22:15:37 -0800

    Revert "chore: run `modernize`"
    This reverts commit cc6a3d6e5d8df22b8bf4ef4521661e23d51763a4.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- pkg/core/rm/rm_test.go
+++ pkg/core/rm/rm_test.go
@@ -132,7 +132,7 @@ func TestRm(t *testing.T) {
 			// Update args to use absolute paths for files
 			args := make([]string, len(tt.args))
 			copy(args, tt.args)
-			for i := range args {
+			for i := 0; i < len(args); i++ {
 				if !strings.HasPrefix(args[i], "-") {
 					args[i] = filepath.Join(d, args[i])
 				}
