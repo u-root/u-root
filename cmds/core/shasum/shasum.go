e0f254a86b086e73583b48154dab3e7fc868f2ba
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2026-03-29 15:26:56 +0300

    cmds/core/shasum: make it standalone
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/shasum/shasum.go
+++ cmds/core/shasum/shasum.go
@@ -19,16 +19,80 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"hash"
+	"io"
 	"log"
 	"os"
-
-	"github.com/u-root/u-root/pkg/core/shasum"
 )
 
+var algorithm int
+
+var usage = "Usage:\nshasum -a <algorithm> <File Name>"
+
+// shaGenerator generates SHA hash of given data. The
+// value of algorithm is expected to be 1 for SHA1
+// 256 for SHA256
+// and 512 for SHA512
+func shaGenerator(r io.Reader, algo int) ([]byte, error) {
+	var h hash.Hash
+	switch algo {
+	case 1:
+		h = sha1.New()
+	case 256:
+		h = sha256.New()
+	case 512:
+		h = sha512.New()
+	default:
+		return nil, fmt.Errorf("invalid algorithm, only 1, 256 or 512 are valid:%w", os.ErrInvalid)
+	}
+	if _, err := io.Copy(h, r); err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+
+func shasum(w io.Writer, r io.Reader, args ...string) error {
+	var hashbytes []byte
+	var err error
+	if len(args) == 0 {
+		buf := bufio.NewReader(r)
+		if hashbytes, err = shaGenerator(buf, algorithm); err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%x -\n", hashbytes)
+		return nil
+	}
+	for _, arg := range args {
+		file, err := os.Open(arg)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		if hashbytes, err = shaGenerator(file, algorithm); err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%x %s\n", hashbytes, arg)
+	}
+	return nil
+}
+
 func main() {
-	cmd := shasum.New()
-	err := cmd.Run(os.Args[1:]...)
-	if err != nil {
-		log.Fatal("shasum: ", err)
+	flag.IntVar(&algorithm, "algorithm", 1, "SHA algorithm, valid args are 1, 256 and 512")
+	flag.IntVar(&algorithm, "a", 1, "SHA algorithm, valid args are 1, 256 and 512")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s\n", usage)
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	if err := shasum(os.Stdout, os.Stdin, flag.Args()...); err != nil {
+		log.Fatal(err)
 	}
 }
