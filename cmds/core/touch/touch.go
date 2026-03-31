486be11854a2eae9259bfe27425875972d860504
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2026-03-31 22:11:42 +0300

    cmds/core/touch: make it standalone
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- cmds/core/touch/touch.go
+++ cmds/core/touch/touch.go
@@ -21,20 +21,98 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"log"
 	"os"
-
-	"github.com/u-root/u-root/pkg/core/touch"
+	"time"
 )
 
-func init() {
-	log.SetFlags(0)
+type params struct {
+	time         time.Time
+	access       bool
+	modification bool
+	create       bool
+}
+
+type cmd struct {
+	params
+	args []string
+}
+
+func command(p params, args ...string) *cmd {
+	return &cmd{
+		args:   args,
+		params: p,
+	}
+}
+
+func parseParams(dateTime string, access, modification, create bool) (params, error) {
+	flag.Parse()
+	t := time.Now()
+	if dateTime != "" {
+		var err error
+		t, err = time.Parse(time.RFC3339, dateTime)
+		if err != nil {
+			return params{}, err
+		}
+	}
+	return params{
+		access:       access || (!access && !modification),
+		modification: modification || (!access && !modification),
+		create:       create,
+		time:         t,
+	}, nil
+}
+
+func (c *cmd) run() error {
+	var errs error
+	for _, arg := range c.args {
+		_, existsErr := os.Stat(arg)
+		notExist := os.IsNotExist(existsErr)
+		if notExist {
+			if c.create {
+				continue
+			}
+
+			f, err := os.Create(arg)
+			if err != nil {
+				errs = errors.Join(errs, err)
+				continue
+			}
+			f.Close()
+		}
+
+		accessTime := time.Time{}
+		if c.access || notExist {
+			accessTime = c.time
+		}
+		modificationTime := time.Time{}
+		if c.modification || notExist {
+			modificationTime = c.time
+		}
+
+		err := os.Chtimes(arg, accessTime, modificationTime)
+		if err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+
+	return errs
 }
 
 func main() {
-	cmd := touch.New()
-	err := cmd.Run(os.Args[1:]...)
-	if err != nil {
-		log.Fatal("touch: ", err)
+	access := flag.Bool("a", false, "change only the access time")
+	modification := flag.Bool("m", false, "change only the modification time")
+	create := flag.Bool("c", false, "do not create any file if it does not exist")
+	dateTime := flag.String("d", "", "use specified time instead of current time RFC3339")
+	flag.Parse()
+	p, err := parseParams(*dateTime, *access, *modification, *create)
+	if err != nil || len(flag.Args()) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if err := command(p, flag.Args()...).run(); err != nil {
+		log.Fatalf("touch: %v", err)
 	}
 }
--- cmds/core/touch/touch_test.go
+++ cmds/core/touch/touch_test.go
@@ -1,63 +1,129 @@
 // Copyright 2023 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
 package main
 
 import (
-	"bytes"
 	"errors"
 	"os"
-	"path/filepath"
-	"strings"
 	"testing"
-
-	"github.com/u-root/u-root/pkg/core/touch"
+	"time"
 )
 
 func TestParseParamsDate(t *testing.T) {
-	cmd := touch.New()
-	var stdout, stderr bytes.Buffer
-	cmd.SetIO(bytes.NewReader(nil), &stdout, &stderr)
-
-	// Test valid date
-	err := cmd.Run("-d", "2021-01-01T00:00:00Z", "/tmp/test_touch_date")
+	date := "2021-01-01T00:00:00Z"
+	expected, err := time.Parse(time.RFC3339, date)
 	if err != nil {
 		t.Fatalf("expected no error, got %v", err)
 	}
 
-	// Clean up
-	os.Remove("/tmp/test_touch_date")
+	p, err := parseParams(date, false, false, false)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
 
-	// Test invalid date
-	cmd2 := touch.New()
-	var stdout2, stderr2 bytes.Buffer
-	var stdin2 bytes.Buffer
-	cmd2.SetIO(&stdin2, &stdout2, &stderr2)
+	if !expected.Equal(p.time) {
+		t.Errorf("expected %v, got %v", expected, p.time)
+	}
 
-	err = cmd2.Run("-d", "invalid", "/tmp/test_touch_invalid")
+	date = "invalid"
+	_, err = parseParams(date, false, false, false)
 	if err == nil {
-		t.Error("expected error for invalid date, got nil")
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestParseParams(t *testing.T) {
+	var tests = []struct {
+		expected     params
+		access       bool
+		modification bool
+		create       bool
+	}{
+		{
+			access:       false,
+			modification: false,
+			create:       false,
+			expected: params{
+				access:       true,
+				modification: true,
+				create:       false,
+			},
+		},
+		{
+			access:       true,
+			modification: false,
+			create:       false,
+			expected: params{
+				access:       true,
+				modification: false,
+				create:       false,
+			},
+		},
+		{
+			access:       false,
+			modification: true,
+			create:       true,
+			expected: params{
+				access:       false,
+				modification: true,
+				create:       true,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		p, err := parseParams("", test.access, test.modification, test.create)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if p.access != test.expected.access {
+			t.Errorf("expected %v, got %v", test.expected.access, p.access)
+		}
+		if p.modification != test.expected.modification {
+			t.Errorf("expected %v, got %v", test.expected.modification, p.modification)
+		}
+		if p.create != test.expected.create {
+			t.Errorf("expected %v, got %v", test.expected.create, p.create)
+		}
 	}
 }
 
 var tests = []struct {
 	err  error
+	p    params
 	name string
 	args []string
 }{
 	{
 		name: "create is true, no new files created",
-		args: []string{"-c", "a1", "a2"},
+		args: []string{"a1", "a2"},
+		p: params{
+			access:       true,
+			modification: true,
+			create:       true,
+			time:         time.Now(),
+		},
 	},
 	{
 		name: "create is false, files should be created",
 		args: []string{"a1", "a2"},
+		p: params{
+			access:       true,
+			modification: true,
+			create:       false,
+			time:         time.Now(),
+		},
 	},
 	{
 		name: "no such file or directory",
 		args: []string{"no/such/file/or/direcotry"},
-		err:  os.ErrNotExist,
+		p: params{
+			create: false,
+			time:   time.Now(),
+		},
+		err: os.ErrNotExist,
 	},
 }
 
@@ -65,50 +131,40 @@ func TestTouchEmptyDir(t *testing.T) {
 	for _, test := range tests {
 		temp := t.TempDir()
 		var args []string
-		args = append(args, test.args[0]) // "touch"
-		for i := 1; i < len(test.args); i++ {
-			arg := test.args[i]
-			if !strings.HasPrefix(arg, "-") {
-				args = append(args, filepath.Join(temp, arg))
-			} else {
-				args = append(args, arg)
-			}
+		for _, arg := range test.args {
+			args = append(args, temp+arg)
 		}
-
-		cmd := touch.New()
-		var stdout, stderr bytes.Buffer
-		cmd.SetIO(bytes.NewReader(nil), &stdout, &stderr)
-
-		err := cmd.Run(args...)
+		err := command(test.p, args...).run()
+		if !errors.Is(err, test.err) {
+			t.Fatalf("command() expected %v, got %v", test.err, err)
+		}
 		if test.err != nil {
-			if !errors.Is(err, test.err) {
-				t.Fatalf("Run() expected %v, got %v", test.err, err)
-			}
 			continue
 		}
 
-		if err != nil {
-			t.Fatalf("Run() expected no error, got %v", err)
-		}
+		for _, arg := range args {
+			_, err := os.Stat(arg)
+			if test.p.create {
+				if !os.IsNotExist(err) {
+					t.Errorf("expected %s to not exist", arg)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("expected %s to exist, got %v", arg, err)
+				}
 
-		// Check if files were created (only for non-error cases)
-		for i := 1; i < len(test.args); i++ {
-			arg := test.args[i]
-			if !strings.HasPrefix(arg, "-") {
-				fullPath := filepath.Join(temp, arg)
-				_, err := os.Stat(fullPath)
-				if strings.Contains(test.name, "create is true") {
-					// With -c flag, files should not be created if they don't exist
-					if !os.IsNotExist(err) {
-						t.Errorf("expected %s to not exist", fullPath)
-					}
-				} else {
-					// Without -c flag, files should be created
-					if err != nil {
-						t.Errorf("expected %s to exist, got %v", fullPath, err)
+				stat, err := os.Stat(arg)
+				if err != nil {
+					t.Fatalf("expected no error, got %v", err)
+				}
+
+				if test.p.modification {
+					if stat.ModTime().Unix() != test.p.time.Unix() {
+						t.Errorf("expected %s to have mod time %v, got %v", arg, test.p.time, stat.ModTime())
 					}
 				}
 			}
+
 		}
 	}
 }
